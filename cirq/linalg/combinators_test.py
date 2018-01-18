ce55e65f38887989a84c18c89b92a3cfd65f4b4f
Craig Gidney <Craig.Gidney@gmail.com>
2018-01-18 13:36:20 -0800

    Use np.ndarray instead of np.matrix in cirq/linalg/combinators (#24)

--- cirq/linalg/combinators_test.py
+++ cirq/linalg/combinators_test.py
@@ -29,19 +29,19 @@ def test_kron_multiplies_sizes():
 
 
 def test_kron_spreads_values():
-    u = np.mat([[2, 3], [5, 7]])
+    u = np.array([[2, 3], [5, 7]])
 
     assert np.allclose(
         combinators.kron(np.eye(2), u),
-        np.mat([[2, 3, 0, 0], [5, 7, 0, 0], [0, 0, 2, 3], [0, 0, 5, 7]]))
+        np.array([[2, 3, 0, 0], [5, 7, 0, 0], [0, 0, 2, 3], [0, 0, 5, 7]]))
 
     assert np.allclose(
         combinators.kron(u, np.eye(2)),
-        np.mat([[2, 0, 3, 0], [0, 2, 0, 3], [5, 0, 7, 0], [0, 5, 0, 7]]))
+        np.array([[2, 0, 3, 0], [0, 2, 0, 3], [5, 0, 7, 0], [0, 5, 0, 7]]))
 
     assert np.allclose(
         combinators.kron(u, u),
-        np.mat([[4, 6, 6, 9], [10, 14, 15, 21], [10, 15, 14, 21],
+        np.array([[4, 6, 6, 9], [10, 14, 15, 21], [10, 15, 14, 21],
                 [25, 35, 35, 49]]))
 
 
@@ -51,46 +51,46 @@ def test_acts_like_kron_multiplies_sizes():
         combinators.kron_with_controls(np.eye(2), np.eye(3), np.eye(4)),
         np.eye(24))
 
-    u = np.mat([[2, 3], [5, 7]])
+    u = np.array([[2, 3], [5, 7]])
     assert np.allclose(
         combinators.kron_with_controls(u, u),
-        np.mat([[4, 6, 6, 9], [10, 14, 15, 21], [10, 15, 14, 21],
+        np.array([[4, 6, 6, 9], [10, 14, 15, 21], [10, 15, 14, 21],
                 [25, 35, 35, 49]]))
 
 
 def test_supports_controls():
-    u = np.mat([[2, 3], [5, 7]])
+    u = np.array([[2, 3], [5, 7]])
     assert np.allclose(
         combinators.kron_with_controls(combinators.CONTROL_TAG),
-        np.mat([[1, 0], [0, 1]]))
+        np.array([[1, 0], [0, 1]]))
     assert np.allclose(
         combinators.kron_with_controls(combinators.CONTROL_TAG, u),
-        np.mat([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 2, 3], [0, 0, 5, 7]]))
+        np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 2, 3], [0, 0, 5, 7]]))
     assert np.allclose(
         combinators.kron_with_controls(u, combinators.CONTROL_TAG),
-        np.mat([[1, 0, 0, 0], [0, 2, 0, 3], [0, 0, 1, 0], [0, 5, 0, 7]]))
+        np.array([[1, 0, 0, 0], [0, 2, 0, 3], [0, 0, 1, 0], [0, 5, 0, 7]]))
 
 
 def test_block_diag():
     assert np.allclose(
         combinators.block_diag(),
-        np.mat(np.zeros((0, 0))))
+        np.zeros((0, 0)))
 
     assert np.allclose(
         combinators.block_diag(
-            np.mat([[1, 2],
+            np.array([[1, 2],
                     [3, 4]])),
-        np.mat([[1, 2],
+        np.array([[1, 2],
                 [3, 4]]))
 
     assert np.allclose(
         combinators.block_diag(
-            np.mat([[1, 2],
+            np.array([[1, 2],
                     [3, 4]]),
-            np.mat([[4, 5, 6],
+            np.array([[4, 5, 6],
                     [7, 8, 9],
                     [10, 11, 12]])),
-        np.mat([[1, 2, 0, 0, 0],
+        np.array([[1, 2, 0, 0, 0],
                 [3, 4, 0, 0, 0],
                 [0, 0, 4, 5, 6],
                 [0, 0, 7, 8, 9],
@@ -100,25 +100,25 @@ def test_block_diag():
 def test_block_diag_dtype():
     assert combinators.block_diag().dtype == np.complex128
 
-    assert (combinators.block_diag(np.mat([[1]], dtype=np.int8)).dtype ==
+    assert (combinators.block_diag(np.array([[1]], dtype=np.int8)).dtype ==
             np.int8)
 
     assert combinators.block_diag(
-            np.mat([[1]], dtype=np.float32),
-            np.mat([[2]], dtype=np.float32)).dtype == np.float32
+            np.array([[1]], dtype=np.float32),
+            np.array([[2]], dtype=np.float32)).dtype == np.float32
 
     assert combinators.block_diag(
-            np.mat([[1]], dtype=np.float64),
-            np.mat([[2]], dtype=np.float64)).dtype == np.float64
+            np.array([[1]], dtype=np.float64),
+            np.array([[2]], dtype=np.float64)).dtype == np.float64
 
     assert combinators.block_diag(
-            np.mat([[1]], dtype=np.float32),
-            np.mat([[2]], dtype=np.float64)).dtype == np.float64
+            np.array([[1]], dtype=np.float32),
+            np.array([[2]], dtype=np.float64)).dtype == np.float64
 
     assert combinators.block_diag(
-            np.mat([[1]], dtype=np.float32),
-            np.mat([[2]], dtype=np.complex64)).dtype == np.complex64
+            np.array([[1]], dtype=np.float32),
+            np.array([[2]], dtype=np.complex64)).dtype == np.complex64
 
     assert combinators.block_diag(
-            np.mat([[1]], dtype=np.int),
-            np.mat([[2]], dtype=np.complex128)).dtype == np.complex128
+            np.array([[1]], dtype=np.int),
+            np.array([[2]], dtype=np.complex128)).dtype == np.complex128
