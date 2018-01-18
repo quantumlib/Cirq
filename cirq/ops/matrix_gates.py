7d6f3ffc6f3681256297b3b8d933f1eae10dabd0
Craig Gidney <Craig.Gidney@gmail.com>
2018-01-18 13:56:36 -0800

    Move and refactor Single/TwoQubitMatrixGate (#18)

---
+++ cirq/ops/matrix_gates.py
@@ -0,0 +1,179 @@
+# Copyright 2017 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Quantum gates defined by a matrix."""
+
+from typing import Optional
+
+import numpy as np
+
+from cirq import linalg
+from cirq.ops import gate_features
+
+
+def _phase_matrix(turns: float) -> np.ndarray:
+    return np.diag([1, np.exp(2j * np.pi * turns)])
+
+
+class SingleQubitMatrixGate(gate_features.KnownMatrixGate,
+                            gate_features.PhaseableGate,
+                            gate_features.ExtrapolatableGate,
+                            gate_features.BoundedEffectGate):
+    """A 1-qubit gate defined by its matrix.
+
+    More general than specialized classes like ZGate, but more expensive and
+    more float-error sensitive to work with (due to using eigendecompositions).
+    """
+
+    def __init__(self, matrix: Optional[np.ndarray]):
+        """
+        Initializes the 2-qubit matrix gate.
+
+        Args:
+            matrix: The matrix that defines the gate. Child classes can instead
+                instead implement the matrix method and pass in None.
+        """
+        if matrix is not None and (matrix.shape != (2, 2) or
+                                   not linalg.is_unitary(matrix)):
+            raise ValueError('Not a 2x2 unitary matrix: {}'.format(matrix))
+        self._matrix = matrix
+
+    def validate_args(self, qubits):
+        if len(qubits) != 1:
+            raise ValueError(
+                'Single-qubit gate applied to multiple qubits: {}({})'.format(
+                    self, qubits))
+
+    def extrapolate_effect(self, factor: float):
+        new_mat = linalg.map_eigenvalues(self.matrix(),
+                                         lambda e: complex(e)**factor)
+        return SingleQubitMatrixGate(new_mat)
+
+    def trace_distance_bound(self):
+        vals = np.linalg.eigvals(self.matrix())
+        rotation_angle = abs(np.angle(vals[0] / vals[1]))
+        return rotation_angle * 1.2
+
+    def phase_by(self, phase_turns: float, qubit_index: int):
+        z = _phase_matrix(phase_turns)
+        phased_matrix = z.dot(self.matrix()).dot(np.conj(z.T))
+        return SingleQubitMatrixGate(phased_matrix)
+
+    def matrix(self) -> np.ndarray:
+        if self._matrix is None:
+            raise NotImplementedError(
+                'Children of {} must either provide a '
+                'matrix to the init method or else implement the matrix '
+                'method.'.format(type(self)))
+        return self._matrix
+
+    def __hash__(self):
+        vals = tuple(v for _, v in np.ndenumerate(self.matrix()))
+        return hash((SingleQubitMatrixGate, vals))
+
+    def approx_eq(self, other, ignore_global_phase=True):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        cmp = (linalg.allclose_up_to_global_phase if ignore_global_phase
+               else np.allclose)
+        return cmp(self.matrix(), other.matrix())
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return np.alltrue(self.matrix() == other.matrix())
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __repr__(self):
+        return 'SingleQubitMatrixGate({})'.format(repr(self.matrix()))
+
+    def __str__(self):
+        return str(self.matrix().round(3))
+
+
+class TwoQubitMatrixGate(gate_features.KnownMatrixGate,
+                         gate_features.PhaseableGate,
+                         gate_features.ExtrapolatableGate):
+    """A 2-qubit gate defined only by its matrix.
+
+    More general than specialized classes like CZGate, but more expensive and
+    more float-error sensitive to work with (due to using eigendecompositions).
+    """
+
+    def __init__(self, matrix: Optional[np.ndarray]):
+        """
+        Initializes the 2-qubit matrix gate.
+
+        Args:
+            matrix: The matrix that defines the gate. Child classes can instead
+                instead implement the matrix method and pass in None.
+        """
+
+        if matrix is not None and (matrix.shape != (4, 4) or
+                                   not linalg.is_unitary(matrix)):
+            raise ValueError('Not a 4x4 unitary matrix: {}'.format(matrix))
+        self._matrix = matrix
+
+    def validate_args(self, qubits):
+        if len(qubits) != 2:
+            raise ValueError(
+                'Two-qubit gate not applied to two qubits: {}({})'.format(
+                    self, qubits))
+
+    def extrapolate_effect(self, factor: float):
+        new_mat = linalg.map_eigenvalues(self.matrix(),
+                                         lambda e: complex(e)**factor)
+        return TwoQubitMatrixGate(new_mat)
+
+    def phase_by(self, phase_turns: float, qubit_index: int):
+        i = np.eye(2)
+        z = _phase_matrix(phase_turns)
+        z2 = np.kron(z, i) if qubit_index else np.kron(i, z)
+        phased_matrix = z2.dot(self.matrix()).dot(np.conj(z2.T))
+        return TwoQubitMatrixGate(phased_matrix)
+
+    def approx_eq(self, other, ignore_global_phase=True):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        cmp = (linalg.allclose_up_to_global_phase if ignore_global_phase
+               else np.allclose)
+        return cmp(self.matrix(), other.matrix())
+
+    def matrix(self):
+        if self._matrix is None:
+            raise NotImplementedError(
+                'Children of {} must either provide a '
+                'matrix to the init method or else implement the matrix '
+                'method.'.format(type(self)))
+        return self._matrix
+
+    def __hash__(self):
+        vals = tuple(v for _, v in np.ndenumerate(self.matrix()))
+        return hash((SingleQubitMatrixGate, vals))
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return np.alltrue(self.matrix() == other.matrix())
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __repr__(self):
+        return 'TwoQubitMatrixGate({})'.format(repr(self.matrix()))
+
+    def __str__(self):
+        return str(self.matrix().round(3))
---
+++ cirq/ops/matrix_gates_test.py
@@ -0,0 +1,139 @@
+# Copyright 2017 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+
+from cirq import linalg
+from cirq.ops.matrix_gates import SingleQubitMatrixGate, TwoQubitMatrixGate
+from cirq.testing import EqualsTester
+
+H = np.array([[1, 1], [1, -1]]) * np.sqrt(0.5)
+HH = linalg.kron(H, H)
+QFT2 = np.array([[1, 1, 1, 1],
+                 [1, 1j, -1, -1j],
+                 [1, -1, 1, -1],
+                 [1, -1j, -1, 1j]]) * 0.5
+
+
+def test_single_qubit_init():
+    m = np.array([[1, 1j], [1j, 1]]) * np.sqrt(0.5)
+    x2 = SingleQubitMatrixGate(m)
+    assert np.alltrue(x2.matrix() == m)
+
+
+def test_single_qubit_eq():
+    eq = EqualsTester()
+    eq.make_equality_pair(lambda: SingleQubitMatrixGate(np.eye(2)))
+    eq.make_equality_pair(
+        lambda: SingleQubitMatrixGate(np.array([[0, 1], [1, 0]])))
+    x2 = np.array([[1, 1j], [1j, 1]]) * np.sqrt(0.5)
+    eq.make_equality_pair(lambda: SingleQubitMatrixGate(x2))
+
+
+def test_single_qubit_phase_by():
+    x = SingleQubitMatrixGate(np.array([[0, 1], [1, 0]]))
+    y = SingleQubitMatrixGate(np.array([[0, -1j], [1j, 0]]))
+    z = SingleQubitMatrixGate(np.array([[1, 0], [0, -1]]))
+    assert x.phase_by(0.25, 0).approx_eq(y)
+    assert y.phase_by(-0.25, 0).approx_eq(x)
+    assert z.phase_by(0.25, 0).approx_eq(z)
+
+
+def test_single_qubit_trace_distance_bound():
+    x = SingleQubitMatrixGate(np.array([[0, 1], [1, 0]]))
+    x2 = SingleQubitMatrixGate(
+        np.array([[1, 1j], [1j, 1]]) * np.sqrt(0.5))
+    assert x.trace_distance_bound() >= 1
+    assert x2.trace_distance_bound() >= 0.5
+
+
+def test_single_qubit_approx_eq():
+    x = SingleQubitMatrixGate(np.array([[0, 1], [1, 0]]))
+    i = SingleQubitMatrixGate(np.array([[1, 0], [0, 1]]))
+    i_ish = SingleQubitMatrixGate(
+        np.array([[1, 0.000000000000001], [0, 1]]))
+    assert i.approx_eq(i_ish)
+    assert i.approx_eq(i)
+    assert not i.approx_eq(x)
+    assert i.approx_eq('') is NotImplemented
+
+
+def test_single_qubit_extrapolate():
+    i = SingleQubitMatrixGate(np.eye(2))
+    x = SingleQubitMatrixGate(np.array([[0, 1], [1, 0]]))
+    x2 = SingleQubitMatrixGate(
+        np.array([[1, 1j], [1j, 1]]) * (1 - 1j) / 2)
+    x2i = SingleQubitMatrixGate(np.conj(x2.matrix().T))
+
+    assert x.extrapolate_effect(0).approx_eq(i)
+    assert x2.extrapolate_effect(0).approx_eq(i)
+    assert x2.extrapolate_effect(2).approx_eq(x)
+    assert x2.extrapolate_effect(-1).approx_eq(x2i)
+    assert x2.extrapolate_effect(3).approx_eq(x2i)
+    assert x.extrapolate_effect(-1).approx_eq(x)
+
+    z2 = SingleQubitMatrixGate(np.array([[1, 0], [0, 1j]]))
+    z4 = SingleQubitMatrixGate(
+        np.array([[1, 0], [0, (1 + 1j) * np.sqrt(0.5)]]))
+    assert z2.extrapolate_effect(0.5).approx_eq(z4)
+
+
+def test_two_qubit_init():
+    x2 = TwoQubitMatrixGate(QFT2)
+    assert np.alltrue(x2.matrix() == QFT2)
+
+
+def test_two_qubit_eq():
+    eq = EqualsTester()
+    eq.make_equality_pair(lambda: TwoQubitMatrixGate(np.eye(4)))
+    eq.make_equality_pair(lambda: TwoQubitMatrixGate(QFT2))
+    eq.make_equality_pair(lambda: TwoQubitMatrixGate(HH))
+
+
+def test_two_qubit_phase_by():
+    x = np.array([[0, 1], [1, 0]])
+    y = np.array([[0, -1j], [1j, 0]])
+    z = np.array([[1, 0], [0, -1]])
+
+    xx = TwoQubitMatrixGate(np.kron(x, x))
+    yx = TwoQubitMatrixGate(np.kron(x, y))
+    xy = TwoQubitMatrixGate(np.kron(y, x))
+    yy = TwoQubitMatrixGate(np.kron(y, y))
+    assert xx.phase_by(0.25, 0).approx_eq(yx)
+    assert xx.phase_by(0.25, 1).approx_eq(xy)
+    assert xy.phase_by(0.25, 0).approx_eq(yy)
+    assert xy.phase_by(-0.25, 1).approx_eq(xx)
+
+    zz = TwoQubitMatrixGate(np.kron(z, z))
+    assert zz.phase_by(0.25, 0).approx_eq(zz)
+    assert zz.phase_by(0.25, 1).approx_eq(zz)
+
+
+def test_two_qubit_approx_eq():
+    f = TwoQubitMatrixGate(QFT2)
+    perturb = np.zeros(shape=QFT2.shape, dtype=np.float64)
+    perturb[1, 2] = 0.00000001
+    assert f.approx_eq(TwoQubitMatrixGate(QFT2))
+    assert f.approx_eq(TwoQubitMatrixGate(QFT2 + perturb))
+    assert not f.approx_eq(TwoQubitMatrixGate(HH))
+
+
+def test_two_qubit_extrapolate():
+    cz2 = TwoQubitMatrixGate(np.diag([1, 1, 1, 1j]))
+    cz4 = TwoQubitMatrixGate(np.diag([1, 1, 1, (1 + 1j) * np.sqrt(0.5)]))
+    i = TwoQubitMatrixGate(np.eye(4))
+
+    assert cz2.extrapolate_effect(0).approx_eq(i)
+    assert cz4.extrapolate_effect(0).approx_eq(i)
+    assert cz2.extrapolate_effect(0.5).approx_eq(cz4)
