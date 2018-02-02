326ccd90f20ac77a206056e1deaff9e324eec026
Dave Bacon <dabacon@gmail.com>
2018-02-02 15:16:34 -0800

    Adds first pass at xmon simulator. (#45)
    Adds first pass at xmon simulator.
    
    This version runs on Circuits, and does not support parameterized sweeps or repetitions.

--- cirq/sim/google/benchmarker.py
+++ cirq/sim/google/benchmarker.py
@@ -72,7 +72,7 @@ def simulate(num_qubits, num_gates):
             current_moment[index0] = new_moment + 1
             current_moment[index1] = new_moment + 1
 
-    with xmon_stepper.XmonStepper(
+    with xmon_stepper.Stepper(
         num_qubits=num_qubits, num_prefix_qubits=2) as s:
         for moment in moments:
             phase_map = {}
