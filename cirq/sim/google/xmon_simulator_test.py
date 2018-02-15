42faf4bc9e8d2d8acec696c1162923a5e9aecfa8
Dave Bacon <dabacon@gmail.com>
2018-02-15 14:25:04 -0800

    Adds a study class for simulations.

--- cirq/sim/google/xmon_simulator_test.py
+++ cirq/sim/google/xmon_simulator_test.py
@@ -20,12 +20,13 @@ import math
 import numpy as np
 import pytest
 
-from cirq import circuits
+from cirq.circuits import Circuit
 from cirq.google import (
     ExpWGate, ExpZGate, Exp11Gate, XmonMeasurementGate, XmonQubit,
 )
 from cirq.google import (ParameterizedValue)
 from cirq.google.resolver import ParamResolver
+from cirq.ops.common_gates import CNOT, X
 from cirq.sim.google import xmon_simulator
 
 Q1 = XmonQubit(0, 0)
@@ -36,7 +37,7 @@ def basic_circuit():
     sqrt_x = ExpWGate(half_turns=0.5, axis_half_turns=0.0)
     z = ExpZGate()
     cz = Exp11Gate()
-    circuit = circuits.Circuit()
+    circuit = Circuit()
     circuit.append(
         [sqrt_x(Q1), sqrt_x(Q2),
          cz(Q1, Q2),
@@ -50,7 +51,7 @@ def large_circuit():
     qubits = [XmonQubit(i, 0) for i in range(10)]
     sqrt_x = ExpWGate(half_turns=0.5, axis_half_turns=0.0)
     cz = Exp11Gate()
-    circuit = circuits.Circuit()
+    circuit = Circuit()
     for _ in range(11):
         circuit.append(
             [sqrt_x(qubit) for qubit in qubits if np.random.random() < 0.5])
@@ -77,10 +78,15 @@ def test_xmon_options():
     assert options.min_qubits_before_shard == 0
 
 
+def assert_empty_context(context):
+    assert xmon_simulator.TrialContext(param_dict={}) == context
+
+
 def test_run_no_results():
     simulator = xmon_simulator.Simulator()
-    result = simulator.run(basic_circuit())
+    context, result = simulator.run(basic_circuit())
     assert len(result.measurements) == 0
+    assert_empty_context(context)
 
 
 def test_run():
@@ -91,39 +97,43 @@ def test_run():
          XmonMeasurementGate(key='b')(Q2),])
 
     simulator = xmon_simulator.Simulator()
-    result = simulator.run(circuit)
+    context, result = simulator.run(circuit)
     assert result.measurements == {'a': [False], 'b': [False]}
+    assert_empty_context(context)
 
 
 def test_run_state():
     simulator = xmon_simulator.Simulator()
-    result = simulator.run(basic_circuit(), qubits=[Q1, Q2])
-    np.testing.assert_almost_equal(result.state(),
+    context, result = simulator.run(basic_circuit(), qubits=[Q1, Q2])
+    np.testing.assert_almost_equal(result.final_state,
                                    np.array([-0.5j, 0.5, -0.5, 0.5j]))
+    assert_empty_context(context)
 
 
 def test_run_state_different_order_of_qubits():
     simulator = xmon_simulator.Simulator()
-    result = simulator.run(basic_circuit(), qubits=[Q2, Q1])
-    np.testing.assert_almost_equal(result.state(),
+    context, result = simulator.run(basic_circuit(), qubits=[Q2, Q1])
+    np.testing.assert_almost_equal(result.final_state,
                                    np.array([-0.5j, -0.5, 0.5, 0.5j]))
+    assert_empty_context(context)
 
 
 def test_run_sharded():
     circuit = large_circuit()
 
     simulator = xmon_simulator.Simulator()
-    result = simulator.run(circuit)
+    context, result = simulator.run(circuit)
     assert result.measurements == {
         'meas': [False, False, False, True, False, False, True, False, False,
                  True]}
+    assert_empty_context(context)
 
 
 def test_run_no_sharding():
     circuit = large_circuit()
 
     simulator = xmon_simulator.Simulator()
-    result = simulator.run(circuit, xmon_simulator.Options(num_shards=1))
+    context, result = simulator.run(circuit, xmon_simulator.Options(num_shards=1))
     assert result.measurements == {
         'meas': [False, False, False, True, False, False, True, False, False,
                  True]}
@@ -138,22 +148,9 @@ def test_run_no_sharing_few_qubits():
 
     simulator = xmon_simulator.Simulator()
     options = xmon_simulator.Options(min_qubits_before_shard=0)
-    result = simulator.run(circuit, options=options)
+    context, result = simulator.run(circuit, options=options)
     assert result.measurements == {'a': [False], 'b': [False]}
-
-
-def test_run_set_state_computational_basis():
-    simulator = xmon_simulator.Simulator()
-    result = simulator.run(basic_circuit(), qubits=[Q1, Q2])
-    result.set_state(0)
-    np.testing.assert_almost_equal(result.state(), np.array([1, 0, 0, 0]))
-
-
-def test_run_set_state_nd_array_fail():
-    simulator = xmon_simulator.Simulator()
-    result = simulator.run(basic_circuit(), qubits=[Q1, Q2])
-    with pytest.raises(ValueError):
-        result.set_state(np.array([0.5, 0.5, 0.5, -0.5], dtype=np.float32))
+    assert_empty_context(context)
 
 
 def test_moment_steps_no_results():
@@ -207,12 +204,12 @@ def test_moment_steps_set_state():
 
 def compute_gate(circuit, resolver, num_qubits=1):
     simulator = xmon_simulator.Simulator()
-    result = []
+    gate = []
     for initial_state in range(1 << num_qubits):
-        state = simulator.run(circuit, initial_state=initial_state,
-                              param_resolver=resolver).state()
-        result.append(state)
-    return np.array(result).transpose()
+        _, result = simulator.run(circuit, initial_state=initial_state,
+                                       param_resolver=resolver)
+        gate.append(result.final_state)
+    return np.array(gate).transpose()
 
 
 @pytest.mark.parametrize('offset', (0.0, 0.2))
@@ -220,7 +217,7 @@ def test_param_resolver_exp_w_half_turns(offset):
     exp_w = ExpWGate(
         half_turns=ParameterizedValue('a', offset),
         axis_half_turns=0.0)
-    circuit = circuits.Circuit()
+    circuit = Circuit()
     circuit.append(exp_w(Q1))
     resolver = ParamResolver({'a': 0.5 - offset})
     result = compute_gate(circuit, resolver)
@@ -234,11 +231,10 @@ def test_param_resolver_exp_w_half_turns(offset):
 def test_param_resolver_exp_w_axis_half_turns(offset):
     exp_w = ExpWGate(
         half_turns=1.0, axis_half_turns=ParameterizedValue('a', offset))
-    circuit = circuits.Circuit()
+    circuit = Circuit()
     circuit.append(exp_w(Q1))
     resolver = ParamResolver({'a': 0.5 - offset})
     result = compute_gate(circuit, resolver)
-    amp = 1.0 / math.sqrt(2)
     np.testing.assert_almost_equal(result,
                                    np.array([[0, 1],
                                              [-1, 0]]))
@@ -249,7 +245,7 @@ def test_param_resolver_exp_w_multiple_params(offset):
     exp_w = ExpWGate(
         half_turns=ParameterizedValue('a', offset),
         axis_half_turns=ParameterizedValue('b', offset))
-    circuit = circuits.Circuit()
+    circuit = Circuit()
     circuit.append(exp_w(Q1))
     resolver = ParamResolver({'a': 0.5 - offset, 'b': 0.5 - offset})
     result = compute_gate(circuit, resolver)
@@ -262,7 +258,7 @@ def test_param_resolver_exp_w_multiple_params(offset):
 @pytest.mark.parametrize('offset', (0.0, 0.2))
 def test_param_resolver_exp_z_half_turns(offset):
     exp_z = ExpZGate(half_turns=ParameterizedValue('a', offset))
-    circuit = circuits.Circuit()
+    circuit = Circuit()
     circuit.append(exp_z(Q1))
     resolver = ParamResolver({'a': 0.5 - offset})
     result = compute_gate(circuit, resolver)
@@ -275,7 +271,7 @@ def test_param_resolver_exp_z_half_turns(offset):
 @pytest.mark.parametrize('offset', (0.0, 0.2))
 def test_param_resolver_exp_11_half_turns(offset):
     exp_11 = Exp11Gate(half_turns=ParameterizedValue('a', offset))
-    circuit = circuits.Circuit()
+    circuit = Circuit()
     circuit.append(exp_11(Q1, Q2))
     resolver = ParamResolver({'a': 0.5 - offset})
     result = compute_gate(circuit, resolver, num_qubits=2)
@@ -290,10 +286,21 @@ def test_param_resolver_param_dict(offset):
     exp_w = ExpWGate(
         half_turns=ParameterizedValue('a', offset),
         axis_half_turns=0.0)
-    circuit = circuits.Circuit()
+    circuit = Circuit()
     circuit.append(exp_w(Q1))
     resolver = ParamResolver({'a': 0.5})
 
     simulator = xmon_simulator.Simulator()
-    result = simulator.run(circuit, param_resolver=resolver)
-    assert result.param_dict == {'a': 0.5}
+    context, result = simulator.run(circuit, param_resolver=resolver)
+    assert context.param_dict == {'a': 0.5}
+
+
+def test_composite_gates():
+    circuit = Circuit()
+    circuit.append([X(Q1), CNOT(Q1, Q2)])
+    m = XmonMeasurementGate('a')
+    circuit.append([m(Q1), m(Q2)])
+
+    simulator = xmon_simulator.Simulator()
+    context, result = simulator.run(circuit)
+    assert result.measurements['a'] == [True, True]
---
+++ cirq/study/study.py
@@ -0,0 +1,169 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Defines studies and related classes.
+
+Studies can be used to run repeated trials against different program
+paramters and number of repetitions.
+
+Example use:
+    sim = Simulator()
+    circuit = my_circuit()
+
+    study = ExecutorStudy(executor=sim, program=circuit, repetitions=10)
+    all_trials = study.run_study()
+    # all_trials is a list of 10 (context, result) where context contains
+    # the reptition id, and result contains the program's results.
+"""
+
+import abc
+
+from typing import Dict, Iterable, Tuple, Union
+
+from cirq.circuits import Circuit
+from cirq.google import ParamResolver  # TODO(dabacon): Move to study.
+from cirq.schedules import Schedule
+
+
+class StudyInterface(metaclass=abc.ABCMeta):
+    """An interface for running repeated trials over different parameters."""
+
+    @abc.abstractmethod
+    def run_study(self) -> Iterable[Tuple['TrialContext', 'TrailResult']]:
+        """Runs the study.
+
+        Returns:
+            An iterable of tuples. Each element represents a single run of the
+            executor, a trial. The tuple is of the form (context, result)
+            where context is a TrialContext, describing the settings for this
+            trial, and result is a TrailResult, describing the results of the
+            trial.
+        """
+
+
+class ExecutorStudy(StudyInterface):
+    """A study is a collection of repeated trials run by an executor.
+
+    Here an Executor runs a single quantum program within a context of
+    each individual trials.
+    """
+
+    def __init__(
+            self,
+            executor: 'Executor',
+            program: Union[Circuit, Schedule],
+            param_resolvers: Iterable[ParamResolver] = None,
+            repetitions: int = 0,
+            **executor_kwags: Dict):
+        self.executor = executor
+        self.program = program
+        self.param_resolvers = param_resolvers or [ParamResolver({})]
+        self.repetitions = repetitions
+        self.executor_kwags = executor_kwags
+
+    def run_study(self) -> Iterable[Tuple['TrialContext', 'TrailResult']]:
+        """Runs the study for all parameters and repetitions.
+
+        Returns:
+            An iterable of tuples. Each element represents a single run of the
+            executor, a trial. The tuple is of the form (context, result)
+            where context is a TrialContext, describing the settings for this
+            trial, and result is a TrailResult, describing the results of the
+            trial.
+        """
+        trial_results = []
+        for param_resolver in self.param_resolvers:
+            for rep_id in range(self.repetitions):
+                (context, result) = self.executor.run(
+                    program=self.program,
+                    param_resolver=param_resolver,
+                    **self.executor_kwags)
+                context.repetition_id = rep_id
+                trial_results.append((context, result))
+        return trial_results
+
+
+class Executor(metaclass=abc.ABCMeta):
+    """Encapsulates running a Circuit or Scheduler for fixed parameters.
+
+    Executors are used to run a single quantum program for a fixed
+    context of parameter values.
+    """
+
+    @abc.abstractmethod
+    def run(
+            self,
+            program: Union[Circuit, Schedule],
+            param_resolver: ParamResolver
+    ) -> Tuple['TrialContext', 'TrialResult']:
+        """Run the program using the parameters described in the ParamResolver.
+
+        Args:
+            program: Either the Circuit or Schedule to run. Some executors
+                only support one of these types.
+            param_resolver: Resolves parameters in the program.
+
+        Returns:
+            (context, result): A tuple of TrailContext and TrialResult objects
+                describing the context that generated the result, and the
+                result, respectively.
+        """
+
+        # TODO(dabacon): Async run.
+
+
+class TrialContextMeta(type):
+    """Metaclass that asserts param_dict and repetition_id attributes exist."""
+
+    def __call__(cls, *args, **kwargs):
+        obj = type.__call__(cls, *args, **kwargs)
+        if not hasattr(obj, 'param_dict'):
+            raise NotImplementedError(
+                'TrialContext subclasses must have a param_dict attribute.')
+        if not hasattr(obj, 'repetition_id'):
+            raise NotImplementedError(
+                'TrialContext subclasses must have a repetition_id attribute.')
+        return obj
+
+
+class TrialContext(metaclass=TrialContextMeta):
+    """The context of for a single execution (trial).
+
+    Attributes:
+        param_dict: A dictionary produce by a ParamResolver mapping parameter
+            keys to actual parameter values that produced this result.
+        repetition_id: An integer labeling the repetition (repetition for a
+            fixed param_dict).
+    """
+
+
+class TrialResultMeta(type):
+    """Metaclass that asserts measurements attributes exist."""
+
+    def __call__(cls, *args, **kwargs):
+        obj = type.__call__(cls, *args, **kwargs)
+        if not hasattr(obj, 'measurements'):
+            raise NotImplementedError(
+                'TrialResult subclasses must have a measurements attribute.')
+        return obj
+
+
+class TrialResult(metaclass=TrialResultMeta):
+    """The results of a single execution (trial).
+
+    Attributes:
+        measurements: A dictionary from measurement gate key to measurement
+            results. If a key is reused, the measurement values are returned
+            in the order they appear in the Circuit being simulated.
+    """
