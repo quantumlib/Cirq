36130a971b838f90e57bb57a0b1cd4c255904a3e
Craig Gidney <craiggidney@google.com>
2018-02-22 13:45:28 -0800

    fix

--- cirq/devices/device.py
+++ cirq/devices/device.py
@@ -16,8 +16,8 @@ import abc
 
 from cirq.time import Duration
 
+
 # Note: circuit/schedule types specified by name to avoid circular references.
-assert cirq  # Fix unused warning (actually used in type strings).
 
 
 class Device(metaclass=abc.ABCMeta):
