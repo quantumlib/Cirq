d6dff3ae703db1ef6f60531c5e658d1b3bc8dbfd
Doug Strain <dstrain@google.com>
2018-03-01 18:47:48 -0800

    Add Jobs directory and depolarizing channel (#166)
    Adds a jobs directory for a Job object combining a Circuit and a ParameterSweep. Provides an initial implementation of a depolarizing channel simulator using this framework.

---
+++ cirq/contrib/jobs/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Package for handling a full quantum job.
+
+Types and methods related to transforming circuits in preparation of sending
+them to Quantum Engine.  Contains classes to help with adding parameter
+sweeps and error simulation.
+"""
+
+from cirq.contrib.jobs.job import (
+    Job,
+)
+from cirq.contrib.jobs.depolarizer_channel import (
+    DepolarizerChannel,
+)
---
+++ cirq/contrib/jobs/depolarizer_channel_test.py
@@ -0,0 +1,92 @@
+# Copyright 2017 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cirq import circuits
+from cirq import ops
+from cirq.api.google.v1.params_pb2 import ParameterSweep
+from cirq.contrib.jobs import DepolarizerChannel
+from cirq.contrib.jobs import Job
+from cirq.google import xmon_gates
+from cirq.study.parameterized_value import ParameterizedValue
+
+
+def test_depolarizer_no_errors():
+    q1 = ops.QubitId()
+    q2 = ops.QubitId()
+    cnot = Job(circuits.Circuit([
+        circuits.Moment([ops.CNOT(q1, q2)]),
+        ]))
+    noerrors = DepolarizerChannel(probability=0.0)
+
+    assert noerrors.transform_job(cnot) == cnot
+
+
+def test_depolarizer_all_errors():
+    q1 = ops.QubitId()
+    q2 = ops.QubitId()
+    cnot = Job(circuits.Circuit([
+        circuits.Moment([ops.CNOT(q1, q2)]),
+        ]))
+    allerrors = DepolarizerChannel(probability=1.0)
+    p0 = ParameterizedValue(DepolarizerChannel._parameter_name + '0')
+    p1 = ParameterizedValue(DepolarizerChannel._parameter_name + '1')
+
+    sweep = ParameterSweep()
+    sweep.repetitions = 1
+    sweep.sweep.factors.add()
+    sweep.sweep.factors[0].sweeps.add()
+    sweep.sweep.factors[0].sweeps.add()
+    sweep.sweep.factors[0].sweeps[0].parameter_name = p0.key
+    sweep.sweep.factors[0].sweeps[0].sweep_points.points.append(1.0)
+    sweep.sweep.factors[0].sweeps[1].parameter_name = p1.key
+    sweep.sweep.factors[0].sweeps[1].sweep_points.points.append(1.0)
+
+    cnot_then_z = Job(circuits.Circuit([
+        circuits.Moment([ops.CNOT(q1, q2)]),
+        circuits.Moment([xmon_gates.ExpZGate(half_turns=p0).on(q1),
+                         xmon_gates.ExpZGate(half_turns=p1).on(q2)])]),
+                      sweep)
+
+    assert allerrors.transform_job(cnot) == cnot_then_z
+
+
+def test_depolarizer_multiple_repetitions():
+    q1 = ops.QubitId()
+    q2 = ops.QubitId()
+    cnot = Job(circuits.Circuit([
+        circuits.Moment([ops.CNOT(q1, q2)]),
+        ]))
+    allerrors3 = DepolarizerChannel(probability=1.0, repetitions=3)
+    p0 = ParameterizedValue(DepolarizerChannel._parameter_name + '0')
+    p1 = ParameterizedValue(DepolarizerChannel._parameter_name + '1')
+
+    sweep = ParameterSweep()
+    sweep.repetitions = 1
+    sweep.sweep.factors.add()
+    sweep.sweep.factors[0].sweeps.add()
+    sweep.sweep.factors[0].sweeps.add()
+    sweep.sweep.factors[0].sweeps[0].parameter_name = p0.key
+    sweep.sweep.factors[0].sweeps[0].sweep_points.points.append(1.0)
+    sweep.sweep.factors[0].sweeps[1].parameter_name = p1.key
+    sweep.sweep.factors[0].sweeps[1].sweep_points.points.append(1.0)
+    sweep.sweep.factors[0].sweeps[0].sweep_points.points.append(1.0)
+    sweep.sweep.factors[0].sweeps[0].sweep_points.points.append(1.0)
+    sweep.sweep.factors[0].sweeps[1].sweep_points.points.append(1.0)
+    sweep.sweep.factors[0].sweeps[1].sweep_points.points.append(1.0)
+    cnot_then_z3 = Job(circuits.Circuit([
+        circuits.Moment([ops.CNOT(q1, q2)]),
+        circuits.Moment([xmon_gates.ExpZGate(half_turns=p0).on(q1),
+                         xmon_gates.ExpZGate(half_turns=p1).on(q2)])]),
+                       sweep)
+    assert allerrors3.transform_job(cnot) == cnot_then_z3
