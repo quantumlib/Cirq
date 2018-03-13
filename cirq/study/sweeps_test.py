7e260e35a6b9462c0fc37baf4d2198e824f98466
Matthew Neeley <mneeley@gmail.com>
2018-03-12 20:10:45 -0700

    Make Sweep implement Iterable[ParamResolver] directly (#174)
    Review: @Strilanc

--- cirq/study/sweeps_test.py
+++ cirq/study/sweeps_test.py
@@ -1,5 +1,6 @@
 import pytest
 
+from cirq.study import ParameterizedValue
 from cirq.study.sweeps import Linspace, Points, Unit
 from cirq.testing import EqualsTester
 
@@ -17,7 +18,7 @@ def test_zip_duplicate_keys():
 def test_linspace():
     sweep = Linspace('a', 0.34, 9.16, 7)
     assert len(sweep) == 7
-    params = list(sweep)
+    params = list(sweep.param_tuples())
     assert len(params) == 7
     assert params[0] == (('a', 0.34),)
     assert params[-1] == (('a', 9.16),)
@@ -26,7 +27,7 @@ def test_linspace():
 def test_linspace_one_point():
     sweep = Linspace('a', 0.34, 9.16, 1)
     assert len(sweep) == 1
-    params = list(sweep)
+    params = list(sweep.param_tuples())
     assert len(params) == 1
     assert params[0] == (('a', 0.34),)
 
@@ -36,6 +37,25 @@ def test_points():
     assert len(sweep) == 4
     params = list(sweep)
     assert len(params) == 4
+
+
+def test_zip():
+    sweep = Points('a', [1, 2, 3]) + Points('b', [4, 5, 6, 7])
+    assert len(sweep) == 3
+    assert _values(sweep, 'a') == [1, 2, 3]
+    assert _values(sweep, 'b') == [4, 5, 6]
+
+
+def test_product():
+    sweep = Points('a', [1, 2, 3]) * Points('b', [4, 5, 6, 7])
+    assert len(sweep) == 12
+    assert _values(sweep, 'a') == [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
+    assert _values(sweep, 'b') == [4, 5, 6, 7, 4, 5, 6, 7, 4, 5, 6, 7]
+
+
+def _values(sweep, key):
+    p = ParameterizedValue(key)
+    return [resolver.value_of(p) for resolver in sweep]
 
 
 def test_equality():
