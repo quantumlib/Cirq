502fe63ffa7d2ca2f2236f51b3330e7d005b23b7
Matthew Neeley <maffoo@google.com>
2018-03-28 14:58:49 -0700

    Add unpack_results func to process results with numpy

--- cirq/google/programs_test.py
+++ cirq/google/programs_test.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from cirq.examples import generate_supremacy_circuit
 from cirq.google import Foxtail, programs
 from cirq.schedules import moment_by_moment_schedule
@@ -12,3 +14,66 @@ def test_protobuf_roundtrip():
     s2 = programs.schedule_from_proto(device, protos)
 
     assert s2 == s1
+
+
+def make_bytes(s: str) -> bytes:
+    """Helper function to convert a string of digits into packed bytes.
+
+    Ignores any characters other than 0 and 1, in particular whitespace. The
+    bits are packed in little-endian order within each byte.
+    """
+    buf = []
+    byte = 0
+    idx = 0
+    for c in s:
+        if c == '0':
+            pass
+        elif c == '1':
+            byte |= 1 << idx
+        else:
+            continue
+        idx += 1
+        if idx == 8:
+            buf.append(byte)
+            byte = 0
+            idx = 0
+    if idx:
+        buf.append(byte)
+    return bytes(buf)
+
+
+def test_unpack_results():
+    data = make_bytes("""
+        000 00
+        001 01
+        010 10
+        011 11
+        100 00
+        101 01
+        110 10
+    """)
+    assert len(data) == 5  # 35 data bits + 5 padding bits
+    results = programs.unpack_results(data, 7, [('a', 3), ('b', 2)])
+    assert 'a' in results
+    assert results['a'].shape == (7, 3)
+    np.testing.assert_array_equal(
+        results['a'],
+        [[0, 0, 0],
+         [0, 0, 1],
+         [0, 1, 0],
+         [0, 1, 1],
+         [1, 0, 0],
+         [1, 0, 1],
+         [1, 1, 0],])
+
+    assert 'b' in results
+    assert results['b'].shape == (7, 2)
+    np.testing.assert_array_equal(
+        results['b'],
+        [[0, 0],
+         [0, 1],
+         [1, 0],
+         [1, 1],
+         [0, 0],
+         [0, 1],
+         [1, 0],])
