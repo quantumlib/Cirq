eb23ad67c1e1b665d09df27333c65518038e6026
Craig Gidney <craiggidney@google.com>
2018-04-18 17:35:44 -0700

    lint3

--- continuous-integration/python_ci_utils/check_typecheck.py
+++ continuous-integration/python_ci_utils/check_typecheck.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
-
 import os.path
 
 from python_ci_utils import env_tools, check
