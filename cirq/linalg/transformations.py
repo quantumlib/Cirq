f1d9ae573c7d85e426f70e26624b29e44dbf838e
Craig Gidney <craiggidney@google.com>
2018-04-23 16:31:20 -0700

    Implement reflection_matrix_pow

---
+++ cirq/linalg/transformations.py
@@ -0,0 +1,46 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utility methods for transforming matrices."""
+
+import numpy as np
+
+
+def reflection_matrix_pow(reflection_matrix: np.ndarray, exponent: float):
+    """Raises a matrix with two opposing eigenvalues to a power.
+
+    Args:
+        reflection_matrix: The matrix to raise to a power.
+        exponent: The power to raise the matrix to.
+
+    Returns:
+        The given matrix raised to the given power.
+    """
+
+    # The eigenvalues are x and -x for some complex unit x. Determine x.
+    squared_phase = np.dot(reflection_matrix[:, 0],
+                           reflection_matrix[0, :])
+    phase = complex(np.sqrt(squared_phase))
+
+    # Extract +x and -x eigencomponents of the matrix.
+    i = np.eye(reflection_matrix.shape[0]) * phase
+    pos_part = (i + reflection_matrix) * 0.5
+    neg_part = (i - reflection_matrix) * 0.5
+
+    # Raise the matrix to a power by raising its eigencomponents to that power.
+    pos_factor = phase**(exponent - 1)
+    neg_factor = pos_factor * (-1)**exponent
+    pos_part_raised = pos_factor * pos_part
+    neg_part_raised = neg_part * neg_factor
+    return pos_part_raised + neg_part_raised
