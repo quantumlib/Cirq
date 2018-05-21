d9dfbc4557711fad022937bd615f739e783df34f
Craig Gidney <craiggidney@google.com>
2018-05-21 16:47:05 -0700

    workaround docs/conf.py type error

--- docs/conf.py
+++ docs/conf.py
@@ -133,21 +133,17 @@ htmlhelp_basename = 'Cirqdoc'
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
+    'papersize': 'letterpaper',
 
     # The font size ('10pt', '11pt' or '12pt').
-    #
     # 'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
-    #
     # 'preamble': '',
 
     # Latex figure (float) alignment
-    #
     # 'figure_align': 'htbp',
-}  # type: ignore
+}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
