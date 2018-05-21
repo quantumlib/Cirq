2acc0735075acb6f520b5cf547ff11408de708b3
Craig Gidney <craiggidney@google.com>
2018-05-21 16:38:48 -0700

    ignore

--- docs/conf.py
+++ docs/conf.py
@@ -147,7 +147,7 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
-}
+}  # type: ignore
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
