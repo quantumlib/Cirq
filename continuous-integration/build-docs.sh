829477465da7b0290db3f3c3091986b4999d1329
Tim Swast <swast@google.com>
2018-05-23 11:31:16 -0700

    DOC: move reference/ to generated/ (#420)
    This move makes it clearer that the API reference Sphinx sources should
    not be hand-edited. They are generated from the docstrings in the source
    Python files.
    
    Also, add a check for the generated/ directory in the build-docs.sh.
    Just as with _build, we want to ensure that generated/ is created by
    Sphinx so that the API reference stays fresh.

--- continuous-integration/build-docs.sh
+++ continuous-integration/build-docs.sh
@@ -24,6 +24,12 @@ if [ -d "$repo_root/docs/_build" ] ; then
   exit 1
 fi
 
+if [ -d "$repo_root/docs/generated" ] ; then
+  echo "'$repo_root/docs/generated' directory still exists." >&2
+  echo "Remove the generated directory before continuing." >&2
+  exit 1
+fi
+
 cd "$repo_root/docs"
 pip install -r requirements.txt
 make html
