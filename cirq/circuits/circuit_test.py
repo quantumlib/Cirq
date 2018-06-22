ad9e854113282a68b8118134314735c67c9394af
Craig Gidney <Craig.Gidney@gmail.com>
2018-06-21 18:22:20 -0700

    Implement measure and measure_each (#483)
    - cirq.measure(a) is a more convenient way of writing cirq.MeasurementGate(key='a').on(a)
    - Can use multiple targets, override the key, and specify a mask: cirq.measure(a, b, key='t', invert_mask=(True, False))
    - cirq.measure_each(a, b, key_func=str) is an alternative when one wants the measurements to be distinct.
    - Separately, prevented gates from being applied to zero qubits
    
    Fixes #396

--- cirq/circuits/circuit_test.py
+++ cirq/circuits/circuit_test.py
@@ -605,8 +605,8 @@ def test_to_text_diagram_teleportation_to_diagram():
         Moment([ops.CNOT(msg, ali)]),
         Moment([ops.H(msg)]),
         Moment(
-            [ops.MeasurementGate()(msg),
-             ops.MeasurementGate()(ali)]),
+            [ops.measure(msg),
+             ops.measure(ali)]),
         Moment([ops.CNOT(ali, bob)]),
         Moment([ops.CNOT(msg, tmp)]),
         Moment([ops.CZ(bob, tmp)]),
@@ -716,7 +716,7 @@ def test_to_text_diagram_multi_qubit_gate():
     q1 = ops.NamedQubit('(0, 0)')
     q2 = ops.NamedQubit('(0, 1)')
     q3 = ops.NamedQubit('(0, 2)')
-    c = Circuit([Moment([ops.MeasurementGate('msg').on(q1, q2, q3)])])
+    c = Circuit.from_ops(cirq.measure(q1, q2, q3, key='msg'))
     assert c.to_text_diagram().strip() == """
 (0, 0): ───M───
            │
@@ -977,25 +977,25 @@ def test_circuit_to_unitary_matrix():
         atol=1e-8)
 
     # Measurement gate has no corresponding matrix.
-    c = Circuit.from_ops(ops.MeasurementGate()(a))
+    c = Circuit.from_ops(ops.measure(a))
     with pytest.raises(TypeError):
         _ = c.to_unitary_matrix(ignore_terminal_measurements=False)
 
     # Ignoring terminal measurements.
-    c = Circuit.from_ops(ops.MeasurementGate()(a))
+    c = Circuit.from_ops(ops.measure(a))
     cirq.testing.assert_allclose_up_to_global_phase(
         c.to_unitary_matrix(),
         np.eye(2))
 
     # Non-terminal measurements are not ignored.
-    c = Circuit.from_ops(ops.MeasurementGate()(a), ops.X(a))
+    c = Circuit.from_ops(ops.measure(a), ops.X(a))
     with pytest.raises(TypeError):
         _ = c.to_unitary_matrix()
 
     # Non-terminal measurements are not ignored (multiple qubits).
     c = Circuit.from_ops(
-            ops.MeasurementGate()(a),
-            ops.MeasurementGate()(b),
+            ops.measure(a),
+            ops.measure(b),
             ops.CNOT(a, b))
     with pytest.raises(TypeError):
         _ = c.to_unitary_matrix()
@@ -1050,9 +1050,9 @@ def test_composite_gate_to_unitary_matrix():
             ops.X(a),
             CNOT_composite()(a, b),
             ops.X(a),
-            ops.MeasurementGate()(a),
+            ops.measure(a),
             ops.X(b),
-            ops.MeasurementGate()(b))
+            ops.measure(b))
     mat = c.to_unitary_matrix()
     mat_expected = ops.CNOT.matrix()
 
