61ad680ba2456a5d72577b1c483b825ba5424001
Kevin J. Sung <kevinsung@users.noreply.github.com>
2018-06-27 15:45:24 -0700

    add chosen_angle_to_half_turns (#529)

--- cirq/__init__.py
+++ cirq/__init__.py
@@ -153,6 +153,7 @@ from cirq.study.visualize import (
 from cirq.value import (
     canonicalize_half_turns,
     chosen_angle_to_canonical_half_turns,
+    chosen_angle_to_half_turns,
     Duration,
     Symbol,
     Timestamp,
