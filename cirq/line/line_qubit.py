b216c323a876e01b6aa175e5d5d6db3c07d3fb80
Bryan A. O'Gorman <bryan.a.ogorman@gmail.com>
2018-06-28 15:57:14 -0700

    added __lt__ method to LineQubit

--- cirq/line/line_qubit.py
+++ cirq/line/line_qubit.py
@@ -48,6 +48,9 @@ class LineQubit(ops.QubitId):
     def __ne__(self, other):
         return not self == other
 
+    def __lt__(self, other):
+        return self.x < other.x
+
     def __hash__(self):
         return hash((LineQubit, self.x))
 
