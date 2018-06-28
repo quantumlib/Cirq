afce6a1b687163dc65c4074cdbd6f3b005770938
Craig Gidney <craiggidney@google.com>
2018-06-28 13:50:33 -0700

    Doc

--- cirq/ops/raw_types.py
+++ cirq/ops/raw_types.py
@@ -95,7 +95,7 @@ class Gate:
 
 
 class InterchangeableQubitsGate(metaclass=abc.ABCMeta):
-    """Indicates operations should be equal under any qubit permutation."""
+    """Indicates operations should be equal under some qubit permutations."""
 
     def qubit_index_to_equivalence_group_key(self, index: int) -> int:
         """Returns a key that differs between non-interchangeable qubits."""
