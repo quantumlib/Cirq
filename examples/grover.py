1cda47ec1f95ad268a918e00a7be398359defe32
Anna Nachesa <anachesa@gmail.com>
2018-07-04 03:42:09 +0200

    Generalized for a random 2-bit string

--- examples/grover.py
+++ examples/grover.py
@@ -9,6 +9,7 @@ TODO(ashalynd): DO NOT SUBMIT without a detailed description of grover.
 """
 
 import cirq
+import random
 
 def set_io_qubits(qubit_count):
   input_qubits = [cirq.google.XmonQubit(i, 0) for i in range(qubit_count)]
@@ -26,9 +27,9 @@ def make_grover_circuit(input_qubits, output_qubit, x_bits):
   ])
 
   # Make oracle.
-  # assume that x_bits == (1, 1)
-  # todo: generalize (e.g. negate 0-bits to get to (1,1) case)
   # for (1, 1) it's just a Toffoli gate
+  # otherwise negate the zero-bits.
+  c.append([cirq.Z(q) for (q, bit) in zip(input_qubits, x_bits) if not bit])
   c.append([cirq.TOFFOLI(input_qubits[0], input_qubits[1], output_qubit)])
 
   # Construct Grover operator.
@@ -56,7 +57,8 @@ def main():
   (input_qubits, output_qubit) = set_io_qubits(qubit_count)
 
   #Choose the x' and make an oracle which can recognize it.
-  x_bits = [1, 1] # todo: generalize x_bits and oracle
+  x_bits = [random.randint(0, 1) for _ in range(qubit_count)]
+  print('Secret bit sequence: {}'.format(x_bits))
 
   # Embed the oracle into a quantum circuit implementing Grover's algorithm.
   circuit = make_grover_circuit(input_qubits, output_qubit, x_bits)
@@ -72,7 +74,8 @@ def main():
 
   # Check if we actually found the secret value.
   most_common_bitstring = frequencies.most_common(1)[0][0]
-  print("Most common bitstring: {}".format(most_common_bitstring))
+  print('Most common bitstring: {}'.format(most_common_bitstring))
+  print('Found a match: {}'.format(most_common_bitstring == bitstring(x_bits)))
 
 if __name__ == '__main__':
   main()
