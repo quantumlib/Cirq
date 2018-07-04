81929302ddadbb31f1869705a7fb5d3b45303277
Anna Nachesa <anachesa@gmail.com>
2018-07-04 03:21:16 +0200

    Prototype of Grover algorithm (see https://arxiv.org/pdf/1804.03719.pdf p.8, dont laugh too hard)
    Committer: Anna Nachesa <anachesa@gmail.com>

---
+++ examples/grover.py
@@ -0,0 +1,78 @@
+"""Demonstrates Grover algorithm.
+
+The Grover algorithm takes a black-box oracle implementing a function 
+{f(x) = 1 if x==x', f(x) = 0 if x!= x'} and finds x' within a randomly
+ordered sequence of N items using O(sqrt(N)) operations and O(N log(N)) gates,
+with the probability p >= 2/3.
+
+TODO(ashalynd): DO NOT SUBMIT without a detailed description of grover.
+"""
+
+import cirq
+
+def set_io_qubits(qubit_count):
+  input_qubits = [cirq.google.XmonQubit(i, 0) for i in range(qubit_count)]
+  output_qubit = cirq.google.XmonQubit(qubit_count, 0)
+  return (input_qubits, output_qubit)
+
+def make_grover_circuit(input_qubits, output_qubit, x_bits):
+  c = cirq.Circuit()
+
+  # Initialize qubits.
+  c.append([
+      cirq.X(output_qubit),
+      cirq.H(output_qubit),
+      cirq.H.on_each(input_qubits),
+  ])
+
+  # Make oracle.
+  # assume that x_bits == (1, 1)
+  # todo: generalize (e.g. negate 0-bits to get to (1,1) case)
+  # for (1, 1) it's just a Toffoli gate
+  c.append([cirq.TOFFOLI(input_qubits[0], input_qubits[1], output_qubit)])
+
+  # Construct Grover operator.
+  c.append([cirq.H.on_each(input_qubits)])
+  c.append([cirq.X.on_each(input_qubits)])
+  c.append([cirq.H.on(input_qubits[1])])
+  c.append([cirq.CNOT(*input_qubits)])
+  c.append([cirq.H.on(input_qubits[1])])
+  c.append([cirq.X.on_each(input_qubits)])
+  c.append([cirq.H.on_each(input_qubits)])
+
+  # Measure the result.
+  c.append([cirq.measure(*input_qubits, key='result')])
+
+  return c
+
+def bitstring(bits):
+    return ''.join(str(int(b)) for b in bits)
+
+def main():
+  qubit_count = 2
+  circuit_sample_count = 10
+
+  #Set up input and output qubits.
+  (input_qubits, output_qubit) = set_io_qubits(qubit_count)
+
+  #Choose the x' and make an oracle which can recognize it.
+  x_bits = [1, 1] # todo: generalize x_bits and oracle
+
+  # Embed the oracle into a quantum circuit implementing Grover's algorithm.
+  circuit = make_grover_circuit(input_qubits, output_qubit, x_bits)
+  print('Circuit:')
+  print(circuit)
+
+  # Sample from the circuit a couple times.
+  simulator = cirq.google.XmonSimulator()
+  result = simulator.run(circuit, repetitions=circuit_sample_count)
+
+  frequencies = result.histogram(key='result', fold_func=bitstring)
+  print('Sampled results:\n{}'.format(frequencies))
+
+  # Check if we actually found the secret value.
+  most_common_bitstring = frequencies.most_common(1)[0][0]
+  print("Most common bitstring: {}".format(most_common_bitstring))
+
+if __name__ == '__main__':
+  main()
