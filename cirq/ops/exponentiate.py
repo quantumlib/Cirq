11fd50530212edecf887cd0d635d6e6cf575b6f7
Fabio Sanches <fabiohisanches@gmail.com>
2018-07-06 14:44:09 -0700

    Exponentiate and Expectation value features reviewed from pull request feedback

--- cirq/ops/exponentiate.py
+++ cirq/ops/exponentiate.py
@@ -1,14 +1,31 @@
-# QC Ware Corp.
-# June 2018
+# Copyright 2018 The Cirq Developers
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" Exponentiation tool for Pauli Operators""" 
 
 # Imports
-import cirq
+from typing import Optional, Dict, Tuple, List, Union
+
 import numpy as np
+from cirq import Circuit
+from cirq.ops import RotXGate, RotYGate, RotZGate, CNOT
+from cirq.google import XmonQubit
 
-
-def exponentiate_qubit_operator(time, operator, qubits, trotter_steps=0):
+def exponentiate_qubit_operator(time: Union[int,float], 
+                                operator: Dict[Tuple[int,str], float],
+                                qubits: List[XmonQubit], 
+                                trotter_steps: int = 0):
     """
     Computes the exponential of an operator O: U = exp(i*time*O)
     If operator does not commute trotterization must be implemented manually
@@ -18,11 +35,12 @@ def exponentiate_qubit_operator(time, operator, qubits, trotter_steps=0):
         time: integer or float representing the coefficient in the exponent
 
         operator: operator is of the form dictionary or projectq qubitoperator
-                  If provided as a dictionary, the format should be keys corresponding to
-                  qubit operators organized as tuples where the first entry 
-                  is the qubit acted on and the second a string X, Y, Z for the pauli
+                  If provided as a dictionary, the format should be keys 
+                  corresponding to qubit operators organized as tuples where
+                  the first entry is the qubit acted on and the second a 
+                  string X, Y, Z for the pauli
                   example:
-                  {((0,'Z'), (2,'X')): 3.7, (3,'Y'):2 }
+                  {((0,'Z'), (2,'X')): 3.7, ((3,'Y'),):2 }
 
         qubits: qubits should be array of cirq.google.XmonQubit(x, y) type
 
@@ -33,27 +51,40 @@ def exponentiate_qubit_operator(time, operator, qubits, trotter_steps=0):
                        100*(maximum qubit operator coefficient)
 
     Returns:
-        cirq.circuit() with gates representing the unitary obtained by exponentiating operator argument
+        cirq.circuit() with gates representing the unitary 
+        obtained by exponentiating operator argument
     """
     # Check if input is in correct format
     if type(operator) == dict:
         terms_dict = operator.items()
     else:
         raise ValueError('Operator should be a'
-                         'a Dict representing the operator and its coefficient')
+                         'Dict representing the operator and its coefficient')
 
     # define rotations:
-    to_x_basis = cirq.RotYGate(half_turns=.5)
-    to_y_basis = cirq.RotXGate(half_turns=-.5)
-    from_x_basis = cirq.RotYGate(half_turns=-.5)
-    from_y_basis = cirq.RotXGate(half_turns=.5)
+    basis_rotation = {
+        'X' : RotYGate(half_turns=.5),
+        'Y' : RotXGate(half_turns=-.5),
+        'Z' : None,
+        () : None
+        }
+
+    # rotations back to original basis
+    undo_rotation = {
+        'X' : RotYGate(half_turns=-.5),
+        'Y' : RotXGate(half_turns=.5)
+        }
+
+
+#        from_x_basis = cirq.RotYGate(half_turns=-.5)
+#        from_y_basis = cirq.RotXGate(half_turns=.5)
 
     # setup trotter steps
     if trotter_steps == 0:
-        trotter_steps = int(100 * max(terms_dict.terms.values()))
+        trotter_steps = abs(int(100 * max(dict(terms_dict).values())))
 
     # create circuit
-    circuit = cirq.Circuit()
+    circuit = Circuit()
 
     # trotter loops
     for n in range(trotter_steps):
@@ -67,29 +98,48 @@ def exponentiate_qubit_operator(time, operator, qubits, trotter_steps=0):
             cnot_gates = []
             prev_index = None
             highest_target_index = None
+            
+            # skip identity
+            if term == ():
+                continue
+            
+            # if format for single paulis is wrong, correct it:
+            if not isinstance(term[0],tuple):
+                term = (term,)
 
-            for q_op in term:
-                # qubit = q_op[0]
-                if q_op[1] == 'X':
-                    basis_change.append(to_x_basis.on(qubits[q_op[0]]))
-                    reverse_basis.append(from_x_basis.on(qubits[q_op[0]]))
+            for qubit_number, op in term:
+                rotation = basis_rotation[op]
+                if rotation is not None:
+                    change = rotation.on(qubits[qubit_number])
+                    undo_change = undo_rotation[op].on(qubits[qubit_number])
+                    basis_change.append(change)
+                    reverse_basis.append(undo_change)
 
-                elif q_op[1] == 'Y':
-                    basis_change.append(to_y_basis.on(qubits[q_op[0]]))
-                    reverse_basis.append(from_y_basis.on(qubits[q_op[0]]))
 
-                elif q_op[1] == ():
-                    continue
+#                # qubit = q_op[0]
+#                if q_op[1] == 'X':
+#                    basis_change.append(to_x_basis.on(qubits[q_op[0]]))
+#                    reverse_basis.append(from_x_basis.on(qubits[q_op[0]]))
+#
+#                elif q_op[1] == 'Y':
+#                    basis_change.append(to_y_basis.on(qubits[q_op[0]]))
+#                    reverse_basis.append(from_y_basis.on(qubits[q_op[0]]))
+#
+#                elif q_op[1] == ():
+#                    continue
 
                 if prev_index is not None:
-                    cnot_gates.append(cirq.CNOT.on(qubits[prev_index], qubits[q_op[0]]))
+                    cnot_gates.append(CNOT.on(qubits[prev_index],
+                                                   qubits[qubit_number]))
 
-                prev_index = q_op[0]
-                highest_target_index = q_op[0]
+                prev_index = qubit_number
+                highest_target_index = qubit_number
 
             moment.append(basis_change)
             moment.append(cnot_gates)
-            moment.append(cirq.RotZGate(half_turns=2.0 * (coef / np.pi) * time / trotter_steps).on(qubits[highest_target_index]))
+            moment.append(RotZGate(half_turns=2.0 * (coef / np.pi) * 
+                                        time / trotter_steps).on(
+                                        qubits[highest_target_index]))
             moment.append(list(reversed(cnot_gates)))
             moment.append(reverse_basis)
 
@@ -97,4 +147,4 @@ def exponentiate_qubit_operator(time, operator, qubits, trotter_steps=0):
 
     # TODO: Implement higher order trotter steps
 
-    return(circuit)
+    return circuit
--- cirq/ops/exponentiate_test.py
+++ cirq/ops/exponentiate_test.py
@@ -1,7 +1,21 @@
+# Copyright 2018 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # imports
 import cirq
 import numpy as np
-from exponentiate import exponentiate_qubit_operator
+from cirq.ops.exponentiate import exponentiate_qubit_operator
 
 
 def test_exponentiate_X():
@@ -12,12 +26,14 @@ def test_exponentiate_X():
     sim = cirq.google.XmonSimulator()
 
     op = {((0, 'X'),): np.pi / 4}
-    circuit2 = exponentiate_qubit_operator(operator=op, qubits=qubit, time=1, trotter_steps=1)
+    circuit2 = exponentiate_qubit_operator(operator=op, qubits=qubit,
+                                           time=1, trotter_steps=1)
 
     results1 = sim.simulate(circuit1)
     results2 = sim.simulate(circuit2)
 
-    assert np.array_equal(np.round(results1.final_state, 4), np.round(results2.final_state, 4))
+    np.testing.assert_allclose(np.round(results1.final_state, 4),
+                                      np.round(results2.final_state, 4))
 
 
 def test_exponentiate_Y():
@@ -28,12 +44,14 @@ def test_exponentiate_Y():
     sim = cirq.google.XmonSimulator()
 
     op = {((0, 'Y'),): np.pi / 4}
-    circuit2 = exponentiate_qubit_operator(operator=op, qubits=qubit, time=1, trotter_steps=1)
+    circuit2 = exponentiate_qubit_operator(operator=op, qubits=qubit,
+                                           time=1, trotter_steps=1)
 
     results1 = sim.simulate(circuit1)
     results2 = sim.simulate(circuit2)
 
-    assert np.array_equal(np.round(results1.final_state, 4), np.round(results2.final_state, 4))
+    np.testing.assert_allclose(np.round(results1.final_state, 4),
+                                      np.round(results2.final_state, 4))
 
 
 def test_exponentiate_Z():
@@ -44,9 +62,42 @@ def test_exponentiate_Z():
     sim = cirq.google.XmonSimulator()
 
     op = {((0, 'Z'),): -1 * np.pi / 4}
-    circuit2 = exponentiate_qubit_operator(operator=op, qubits=qubit, time=1, trotter_steps=1)
+    circuit2 = exponentiate_qubit_operator(operator=op, qubits=qubit,
+                                           time=1, trotter_steps=1)
 
     results1 = sim.simulate(circuit1)
     results2 = sim.simulate(circuit2)
 
-    assert np.array_equal(np.round(results1.final_state, 4), np.round(results2.final_state, 4))
+    np.testing.assert_allclose(np.round(results1.final_state, 4), 
+                          np.round(results2.final_state, 4))
+
+def test_exponentiate_XZ():
+
+    qubits = [cirq.google.XmonQubit(0, 0), cirq.google.XmonQubit(0,1)]
+    op = {((0, 'X'),(1, 'Z')):  -1*np.pi/4}
+    circuit = exponentiate_qubit_operator(operator=op,time = 1,
+                                          qubits=qubits,trotter_steps=1)
+
+    sim = cirq.google.XmonSimulator()
+    results = sim.simulate(circuit)
+    res = np.round(results.final_state,6)
+    
+    ratio  = np.round(res[0]/res[2],4)
+
+    assert ratio == 1j
+
+
+def test_exponentiate_xz_zx():
+
+    qubits = [cirq.google.XmonQubit(0, 0), cirq.google.XmonQubit(0,1)]
+    op = {((0, 'X'),(1, 'Z')): +1 * np.pi/2, ((0, 'Z'),(1,'X')): -(1/2)*np.pi/2}
+    circuit = exponentiate_qubit_operator(operator=op,time = 1,
+                                          qubits=qubits,trotter_steps=0)
+
+    sim = cirq.google.XmonSimulator()
+    results = sim.simulate(circuit)
+    res = np.round(results.final_state,4)
+
+    ratio = np.round(res[2]/res[3],4)
+
+    assert ratio == -1j
