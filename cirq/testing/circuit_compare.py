9de3e03405127530a55f3e7f697bf28e43b7b447
Craig Gidney <craiggidney@google.com>
2018-07-17 01:50:18 -0700

    lint

--- cirq/testing/circuit_compare.py
+++ cirq/testing/circuit_compare.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Tuple, List, Iterable
+from typing import Tuple
 
 import numpy as np
 
