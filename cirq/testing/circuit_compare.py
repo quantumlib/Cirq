173730de51c85ae4f96b2bb2bff6f98fab96bd50
Craig Gidney <craiggidney@google.com>
2018-07-17 13:44:20 -0700

    comment

--- cirq/testing/circuit_compare.py
+++ cirq/testing/circuit_compare.py
@@ -20,10 +20,21 @@ from cirq import circuits, ops, linalg
 
 
 def _cancel_qubit_phase(m1: np.ndarray, m2: np.ndarray, k: int) -> None:
+    """Makes the two matrices more similar via phasing of qubit #k.
+
+    This method mutates the given matrices.
+
+    Args:
+        m1: A unitary matrix.
+        m2: Another unitary matrix.
+        k: The index of the qubit we are allowed to phase.
+    """
     n = m1.shape[0]
     b = 1 << k
 
+    # Independently phase the qubit-is-OFF and qubit-is-ON input spaces.
     for t in [False, True]:
+        # Find a location with a large value to use as a phase reference.
         best_loc = max([(i, j)
                         for i in range(n)
                         for j in range(n)
@@ -31,6 +42,8 @@ def _cancel_qubit_phase(m1: np.ndarray, m2: np.ndarray, k: int) -> None:
                        key=lambda e: max(abs(m1[e]), abs(m2[e])))
         best_val_1 = m1[best_loc]
         best_val_2 = m2[best_loc]
+
+        # Determine how to cancel out the reference phase.
         if best_val_1 != 0:
             counter_phase_1 = np.conj(best_val_1) / abs(best_val_1)
         else:
@@ -39,6 +52,8 @@ def _cancel_qubit_phase(m1: np.ndarray, m2: np.ndarray, k: int) -> None:
             counter_phase_2 = np.conj(best_val_2) / abs(best_val_2)
         else:
             counter_phase_2 = 1
+
+        # Cancel using the reference phase.
         for i in range(n):
             if t == bool(i & b):
                 m1[i, :] *= counter_phase_1
@@ -53,19 +68,19 @@ def _canonicalize_up_to_terminal_measurement_phase(
     assert circuit1.are_all_measurements_terminal()
     assert circuit2.are_all_measurements_terminal()
 
-    terminal_1 = {q
+    measured_1 = {q
                   for op in circuit1.all_operations()
                   if ops.MeasurementGate.is_measurement(op)
                   for q in op.qubits}
-    terminal_2 = {q
+    measured_2 = {q
                   for op in circuit2.all_operations()
                   if ops.MeasurementGate.is_measurement(op)
                   for q in op.qubits}
-    assert terminal_1 == terminal_2
+    assert measured_1 == measured_2
 
     matrix1 = circuit1.to_unitary_matrix(qubits_that_should_be_present=qubits)
     matrix2 = circuit2.to_unitary_matrix(qubits_that_should_be_present=qubits)
-    for q in terminal_1:
+    for q in measured_1:
         _cancel_qubit_phase(matrix1, matrix2, order.index(q))
     return matrix1, matrix2
 
@@ -74,14 +89,15 @@ def assert_circuits_with_terminal_measurements_are_equivalent(
         actual: circuits.Circuit,
         expected: circuits.Circuit,
         atol: float) -> None:
-    """ Determines if two circuits are equivalent.
+    """Determines if two circuits have equivalent effects.
 
     The circuits can contain measurements, but the measurements must be at the
-    end of the circuit. Circuits are equivalent if they are observationally
-    indistinguishable (assuming unmeasured qubits count as outputs).
+    end of the circuit. Circuits are equivalent if, for all possible inputs,
+    their outputs (both classical via measurements and quantum via
+    not-measurements) are observationally indistinguishable up to a tolerance.
 
-    For example, inserting a phase operation on an unmeasured qubit changes the
-    function of a circuit but inserting a phase operation just before a
+    For example, applying an extra Z gate to an unmeasured qubit changes the
+    effect of a circuit. But inserting a Z gate operation just before a
     measurement does not.
 
     Args:
