c37d4cc5d004b8fd49533510fa8e179b58fa12e3
Casey Duckering <cduckering@gmail.com>
2018-07-27 21:52:20 -0700

    Add MergeSingleQubitGates and ConvertToCzAndSingleGates optimizers

---
+++ cirq/circuits/convert_to_cz_and_single_gates.py
@@ -0,0 +1,97 @@
+# Copyright 2018 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cirq import ops, decompositions, extension
+from cirq.circuits.circuit import Circuit
+from cirq.circuits.optimization_pass import (
+    PointOptimizationSummary,
+    PointOptimizer,
+)
+
+
+class ConvertToCzAndSingleGates(PointOptimizer):
+    """Attempts to convert strange multi-qubit gates into CZ and single qubit
+    gates.
+
+    First, checks if the given extensions are able to cast the operation into a
+        KnownMatrix. If so, and the gate is a 1-qubit or 2-qubit gate, then
+        performs circuit synthesis of the operation.
+
+    Second, checks if the given extensions are able to cast the operation into a
+        CompositeOperation. If so, recurses on the decomposition.
+
+    Third, if ignore_failures is set, gives up and returns the gate unchanged.
+        Otherwise raises a TypeError.
+    """
+
+    def __init__(self,
+                 extensions: extension.Extensions = None,
+                 ignore_failures: bool = False) -> None:
+        """
+        Args:
+            extensions: The extensions instance to use when trying to
+                cast gates to known types.
+            ignore_failures: If set, gates that fail to convert are forwarded
+                unchanged. If not set, conversion failures raise a TypeError.
+        """
+        self.extensions = extensions or extension.Extensions()
+        self.ignore_failures = ignore_failures
+
+    def _convert_one(self, op: ops.Operation) -> ops.OP_TREE:
+        # Already supported?
+        if (isinstance(op, ops.GateOperation)
+            and isinstance(op.gate, ops.Rot11Gate)):
+            return op
+
+        # Known matrix?
+        mat = self.extensions.try_cast(ops.KnownMatrix, op)
+        if mat is not None and len(op.qubits) == 1:
+            return mat
+        if mat is not None and len(op.qubits) == 2:
+            return decompositions.two_qubit_matrix_to_gates(
+                op.qubits[0],
+                op.qubits[1],
+                mat.matrix(),
+                allow_partial_czs=False)
+
+        # Provides a decomposition?
+        composite_op = self.extensions.try_cast(ops.CompositeOperation, op)
+        if composite_op is not None:
+            return composite_op.default_decompose()
+
+        # Just let it be?
+        if self.ignore_failures:
+            return op
+
+        raise TypeError("Don't know how to work with {!r}. "
+                        "It isn't a 1-qubit KnownMatrix, "
+                        "a 2-qubit KnownMatrix, "
+                        "or a CompositeOperation.".format(op))
+
+    def convert(self, op: ops.Operation) -> ops.OP_TREE:
+        converted = self._convert_one(op)
+        if converted is op:
+            return converted
+        return (self.convert(e) for e in ops.flatten_op_tree(converted))
+
+    def optimization_at(self, circuit: Circuit, index: int, op: ops.Operation
+                        ) -> PointOptimizationSummary:
+        converted = self.convert(op)
+        if converted is op:
+            return None
+
+        return PointOptimizationSummary(
+            clear_span=1,
+            new_operations=converted,
+            clear_qubits=op.qubits)
---
+++ cirq/circuits/merge_single_qubit_gates_test.py
@@ -0,0 +1,128 @@
+# Copyright 2018 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+
+import cirq
+
+
+def assert_optimizes(before, after, optimizer=None):
+    if optimizer is None:
+        optimizer = cirq.MergeSingleQubitGates()
+    optimizer.optimize_circuit(before)
+
+    # Ignore differences that would be caught by follow-up optimizations.
+    followup_optimizations = [
+        cirq.DropNegligible(),
+        cirq.DropEmptyMoments()
+    ]
+    for post in followup_optimizations:
+        post.optimize_circuit(before)
+        post.optimize_circuit(after)
+
+    if before != after:
+        # coverage: ignore
+        print("before:", before)
+        print("after:", after)
+    assert before == after
+
+
+def test_leaves_singleton():
+    m = cirq.MergeSingleQubitGates(0.000001)
+    q = cirq.QubitId()
+    c = cirq.Circuit([cirq.Moment([cirq.X(q)])])
+
+    m.optimization_at(c, 0, c.operation_at(q, 0))
+
+    assert c == cirq.Circuit([cirq.Moment([cirq.X(q)])])
+
+
+def test_combines_sequence():
+    m = cirq.MergeSingleQubitGates(0.000001)
+    q = cirq.QubitId()
+    c = cirq.Circuit([
+        cirq.Moment([cirq.X(q)**0.5]),
+        cirq.Moment([cirq.Z(q)**0.5]),
+        cirq.Moment([cirq.X(q)**-0.5]),
+    ])
+
+    assert (m.optimization_at(c, 0, c.operation_at(q, 0)) ==
+            cirq.PointOptimizationSummary(clear_span=3,
+                                          clear_qubits=[q],
+                                          new_operations=cirq.Y(q)**0.5))
+
+
+def test_removes_identity_sequence():
+    q = cirq.QubitId()
+    assert_optimizes(
+        before=cirq.Circuit([
+            cirq.Moment([cirq.Z(q)]),
+            cirq.Moment([cirq.H(q)]),
+            cirq.Moment([cirq.X(q)]),
+            cirq.Moment([cirq.H(q)]),
+        ]),
+        after=cirq.Circuit())
+
+
+def test_stopped_at_2qubit():
+    m = cirq.MergeSingleQubitGates(0.000001)
+    q = cirq.QubitId()
+    q2 = cirq.QubitId()
+    c = cirq.Circuit([
+        cirq.Moment([cirq.Z(q)]),
+        cirq.Moment([cirq.H(q)]),
+        cirq.Moment([cirq.X(q)]),
+        cirq.Moment([cirq.H(q)]),
+        cirq.Moment([cirq.CZ(q, q2)]),
+        cirq.Moment([cirq.H(q)]),
+    ])
+
+    assert (m.optimization_at(c, 0, c.operation_at(q, 0)) ==
+            cirq.PointOptimizationSummary(clear_span=4,
+                                          clear_qubits=[q],
+                                          new_operations=[]))
+
+
+def test_ignores_2qubit_target():
+    m = cirq.MergeSingleQubitGates(0.000001)
+    q = cirq.QubitId()
+    q2 = cirq.QubitId()
+    c = cirq.Circuit([
+        cirq.Moment([cirq.CZ(q, q2)]),
+    ])
+
+    m.optimization_at(c, 0, c.operation_at(q, 0))
+
+    assert c == cirq.Circuit([cirq.Moment([cirq.CZ(q, q2)])])
+
+
+def test_extension():
+    class DummyGate(cirq.Gate):
+        pass
+
+    ext = cirq.Extensions()
+    ext.add_cast(cirq.KnownMatrix,
+                 DummyGate,
+                 lambda _: cirq.SingleQubitMatrixGate(
+                     np.array([[0, 1], [1, 0]])))
+    optimizer = cirq.MergeSingleQubitGates(extensions=ext)
+
+    q = cirq.QubitId()
+    c = cirq.Circuit([
+        cirq.Moment([DummyGate().on(q)]),
+    ])
+    assert_optimizes(
+        before=c,
+        after=cirq.Circuit([cirq.Moment([cirq.X(q)])]),
+        optimizer=optimizer)
