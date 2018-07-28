ae199d8128a404241492d7237f23a5b2b305005f
Casey Duckering <cduckering@gmail.com>
2018-07-28 02:23:21 -0700

    Add test

--- cirq/ops/pauli_string_test.py
+++ cirq/ops/pauli_string_test.py
@@ -405,6 +405,14 @@ def test_pass_operations_over_double(shift, t_or_f1, t_or_f2, neg):
     _assert_pass_over([op0], ps_before, ps_after)
 
 
+def test_pass_operations_over_cz():
+    q0, q1 = _make_qubits(2)
+    op0 = cirq.CZ(q0, q1)
+    ps_before = cirq.PauliString({q0: cirq.Pauli.Z, q1: cirq.Pauli.Y})
+    ps_after = cirq.PauliString({q1: cirq.Pauli.Y})
+    _assert_pass_over([op0], ps_before, ps_after)
+
+
 def test_pass_unsupported_operations_over():
     q0, = _make_qubits(1)
     pauli_string = cirq.PauliString({q0: cirq.Pauli.X})
