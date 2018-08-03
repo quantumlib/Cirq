40d0d6394db310e8b41d055da21e207e6ff519be
Casey Duckering <cduckering@gmail.com>
2018-08-03 15:35:40 -0700

    Address comments

--- cirq/contrib/paulistring/clifford_optimize.py
+++ cirq/contrib/paulistring/clifford_optimize.py
@@ -34,38 +34,41 @@ def clifford_optimized_circuit(circuit: circuits.Circuit,
                          string_op: PauliStringPhasor,
                          stop_at_cz: bool,
                          ) -> Tuple[int, PauliStringPhasor]:
+        STOP = 0
+        CONTINUE = 1
+        SKIP = 2
         def continue_condition(op: ops.Operation,
                                current_string: PauliStringPhasor,
                                is_first: bool) -> int:
             if (isinstance(op, ops.GateOperation)
                 and isinstance(op.gate, ops.CliffordGate)):
-                return len(current_string.pauli_string) != 1
+                return (CONTINUE if len(current_string.pauli_string) != 1
+                                 else STOP)
             if (isinstance(op, ops.GateOperation)
                 and isinstance(op.gate, ops.Rot11Gate)):
-                return not stop_at_cz
+                return STOP if stop_at_cz else CONTINUE
             if (isinstance(op, PauliStringPhasor)
                 and len(op.qubits) == 1
                 and (op.pauli_string[op.qubits[0]]
                      == current_string.pauli_string[op.qubits[0]])):
-                return 2
-            return False
+                return SKIP
+            return STOP
 
         modified_op = string_op
         furthest_op = string_op
         furthest_i = start_i + 1
-        i = start_i
         for i in range(start_i+1, len(all_ops)):
             op = all_ops[i]
             if not set(op.qubits) & set(modified_op.qubits):
                 # No qubits in common
                 continue
             cont_cond = continue_condition(op, modified_op, i == start_i+1)
-            if not cont_cond:
+            if cont_cond == STOP:
                 if len(modified_op.pauli_string) == 1:
                     furthest_op = modified_op
                     furthest_i = i
                 break
-            if cont_cond == 1:
+            if cont_cond == CONTINUE:
                 modified_op = modified_op.pass_operations_over(
                                     [op], after_to_before=True)
             if len(modified_op.pauli_string) == 1:
@@ -81,7 +84,7 @@ def clifford_optimized_circuit(circuit: circuits.Circuit,
                 cast(ops.CliffordGate, cliff_op.gate).decompose_rotation()):
             trans = remaining_cliff_gate.transform(pauli)
             pauli = trans.to
-            quarter_turns *= (1, -1)[trans.flip]
+            quarter_turns *= -1 if trans.flip else 1
             string_op = PauliStringPhasor(
                 ops.PauliString.from_single(cliff_op.qubits[0], pauli),
                 half_turns=quarter_turns / 2)
@@ -140,6 +143,30 @@ def clifford_optimized_circuit(circuit: circuits.Circuit,
             all_ops[start_i] = remaining_cliff_gate(orig_qubit)
             return False
 
+    def try_merge_cz(cz_op: ops.GateOperation, start_i: int) -> int:
+        """Returns the number of operations removed at or before start_i."""
+        for i in reversed(range(start_i)):
+            op = all_ops[i]
+            if not set(cz_op.qubits) & set(op.qubits):
+                # Don't share qubits
+                # Keep looking
+                continue
+            elif not (isinstance(op, ops.GateOperation)
+                      and isinstance(op.gate, ops.Rot11Gate)
+                      and op.gate.half_turns == 1):
+                # Not a CZ gate
+                return 0
+            elif cz_op == op:
+                # Cancel two CZ gates
+                all_ops.pop(start_i)
+                all_ops.pop(i)
+                return 2
+            else:
+                # Two CZ gates that share one qubit
+                # Pass through and keep looking
+                continue
+        return 0
+
     i = 0
     while i < len(all_ops):
         op = all_ops[i]
@@ -150,26 +177,8 @@ def clifford_optimized_circuit(circuit: circuits.Circuit,
         elif (isinstance(op, ops.GateOperation)
               and isinstance(op.gate, ops.Rot11Gate)
               and op.gate.half_turns == 1):
-            for j in reversed(range(i)):
-                op2 = all_ops[j]
-                if not set(op.qubits) & set(op2.qubits):
-                    # Don't share qubits
-                    continue
-                elif not (isinstance(op2, ops.GateOperation)
-                          and isinstance(op2.gate, ops.Rot11Gate)
-                          and op.gate.half_turns == 1):
-                    # Not a CZ gate
-                    break
-                elif op == op2:
-                    # Cancel two CZ gates
-                    all_ops.pop(i)
-                    all_ops.pop(j)
-                    i -= 2
-                    break
-                else:
-                    # Two CZ gates that share one qubit
-                    # Pass through and keep looking
-                    continue
+            num_rm = try_merge_cz(op, i)
+            i -= num_rm
         i += 1
 
     return circuits.Circuit.from_ops(
