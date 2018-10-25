ee9a8344e2359e80052af4cba540b07d1aed3761
Craig Gidney <Craig.Gidney@gmail.com>
2018-10-25 13:04:40 -0700

    Rename cirq.google.ExpWGate's attributes to more closely match cirq.PhasedXGate (#1058)
    Part of https://github.com/quantumlib/Cirq/issues/952

--- cirq/google/eject_full_w_test.py
+++ cirq/google/eject_full_w_test.py
@@ -63,34 +63,34 @@ def test_absorbs_z():
     # Full Z.
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.125).on(q)],
+            [cg.ExpWGate(phase_exponent=0.125).on(q)],
             [cirq.Z(q)],
         ),
         expected=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.625).on(q)],
+            [cg.ExpWGate(phase_exponent=0.625).on(q)],
             [],
         ))
 
     # Partial Z.
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.125).on(q)],
+            [cg.ExpWGate(phase_exponent=0.125).on(q)],
             [cirq.S(q)],
         ),
         expected=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.375).on(q)],
+            [cg.ExpWGate(phase_exponent=0.375).on(q)],
             [],
         ))
 
     # Multiple Zs.
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.125).on(q)],
+            [cg.ExpWGate(phase_exponent=0.125).on(q)],
             [cirq.S(q)],
             [cirq.T(q)**-1],
         ),
         expected=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
             [],
             [],
         ))
@@ -103,23 +103,23 @@ def test_crosses_czs():
     # Full CZ.
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(a)],
+            [cg.ExpWGate(phase_exponent=0.25).on(a)],
             [cirq.CZ(a, b)],
         ),
         expected=quick_circuit(
             [cirq.Z(b)],
             [cirq.CZ(a, b)],
-            [cg.ExpWGate(axis_half_turns=0.25).on(a)],
+            [cg.ExpWGate(phase_exponent=0.25).on(a)],
         ))
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.125).on(a)],
+            [cg.ExpWGate(phase_exponent=0.125).on(a)],
             [cirq.CZ(b, a)],
         ),
         expected=quick_circuit(
             [cirq.Z(b)],
             [cirq.CZ(a, b)],
-            [cg.ExpWGate(axis_half_turns=0.125).on(a)],
+            [cg.ExpWGate(phase_exponent=0.125).on(a)],
         ))
 
     # Partial CZ.
@@ -137,16 +137,16 @@ def test_crosses_czs():
     # Double cross.
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.125).on(a)],
-            [cg.ExpWGate(axis_half_turns=0.375).on(b)],
+            [cg.ExpWGate(phase_exponent=0.125).on(a)],
+            [cg.ExpWGate(phase_exponent=0.375).on(b)],
             [cirq.CZ(a, b)**0.25],
         ),
         expected=quick_circuit(
             [],
             [],
             [cirq.CZ(a, b)**0.25],
-            [cg.ExpWGate(axis_half_turns=0.5).on(b),
-             cg.ExpWGate(axis_half_turns=0.25).on(a)],
+            [cg.ExpWGate(phase_exponent=0.5).on(b),
+             cg.ExpWGate(phase_exponent=0.25).on(a)],
         ))
 
 
@@ -157,7 +157,7 @@ def test_toggles_measurements():
     # Single.
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(a)],
+            [cg.ExpWGate(phase_exponent=0.25).on(a)],
             [cirq.measure(a, b)],
         ),
         expected=quick_circuit(
@@ -166,7 +166,7 @@ def test_toggles_measurements():
         ))
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(b)],
+            [cg.ExpWGate(phase_exponent=0.25).on(b)],
             [cirq.measure(a, b)],
         ),
         expected=quick_circuit(
@@ -177,8 +177,8 @@ def test_toggles_measurements():
     # Multiple.
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(a)],
-            [cg.ExpWGate(axis_half_turns=0.25).on(b)],
+            [cg.ExpWGate(phase_exponent=0.25).on(a)],
+            [cg.ExpWGate(phase_exponent=0.25).on(b)],
             [cirq.measure(a, b)],
         ),
         expected=quick_circuit(
@@ -190,7 +190,7 @@ def test_toggles_measurements():
     # Xmon.
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(a)],
+            [cg.ExpWGate(phase_exponent=0.25).on(a)],
             [cirq.measure(a, b, key='t')],
         ),
         expected=quick_circuit(
@@ -204,8 +204,8 @@ def test_cancels_other_full_w():
 
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
         ),
         expected=quick_circuit(
             [],
@@ -214,8 +214,8 @@ def test_cancels_other_full_w():
 
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
-            [cg.ExpWGate(axis_half_turns=0.125).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
+            [cg.ExpWGate(phase_exponent=0.125).on(q)],
         ),
         expected=quick_circuit(
             [],
@@ -225,7 +225,7 @@ def test_cancels_other_full_w():
     assert_optimizes(
         before=quick_circuit(
             [cg.ExpWGate().on(q)],
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
         ),
         expected=quick_circuit(
             [],
@@ -234,7 +234,7 @@ def test_cancels_other_full_w():
 
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
             [cg.ExpWGate().on(q)],
         ),
         expected=quick_circuit(
@@ -249,44 +249,44 @@ def test_phases_partial_ws():
     assert_optimizes(
         before=quick_circuit(
             [cg.ExpWGate().on(q)],
-            [cg.ExpWGate(axis_half_turns=0.25, half_turns=0.5).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25, exponent=0.5).on(q)],
         ),
         expected=quick_circuit(
             [],
-            [cg.ExpWGate(axis_half_turns=-0.25, half_turns=0.5).on(q)],
+            [cg.ExpWGate(phase_exponent=-0.25, exponent=0.5).on(q)],
             [cg.ExpWGate().on(q)],
         ))
 
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
-            [cg.ExpWGate(half_turns=0.5).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
+            [cg.ExpWGate(exponent=0.5).on(q)],
         ),
         expected=quick_circuit(
             [],
-            [cg.ExpWGate(axis_half_turns=0.5, half_turns=0.5).on(q)],
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
+            [cg.ExpWGate(phase_exponent=0.5, exponent=0.5).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
         ))
 
     assert_optimizes(
         before=quick_circuit(
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
-            [cg.ExpWGate(axis_half_turns=0.5, half_turns=0.75).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
+            [cg.ExpWGate(phase_exponent=0.5, exponent=0.75).on(q)],
         ),
         expected=quick_circuit(
             [],
-            [cg.ExpWGate(half_turns=0.75).on(q)],
-            [cg.ExpWGate(axis_half_turns=0.25).on(q)],
+            [cg.ExpWGate(exponent=0.75).on(q)],
+            [cg.ExpWGate(phase_exponent=0.25).on(q)],
         ))
 
     assert_optimizes(
         before=quick_circuit(
             [cg.ExpWGate().on(q)],
-            [cg.ExpWGate(half_turns=-0.25, axis_half_turns=0.5).on(q)]
+            [cg.ExpWGate(exponent=-0.25, phase_exponent=0.5).on(q)]
         ),
         expected=quick_circuit(
             [],
-            [cg.ExpWGate(half_turns=-0.25, axis_half_turns=-0.5).on(q)],
+            [cg.ExpWGate(exponent=-0.25, phase_exponent=-0.5).on(q)],
             [cg.ExpWGate().on(q)],
         ))
 
--- cirq/google/xmon_gates_test.py
+++ cirq/google/xmon_gates_test.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import numpy as np
-
 import cirq
 import cirq.google as cg
 
@@ -31,59 +29,56 @@ def test_is_supported():
 def test_w_eq():
     eq = cirq.testing.EqualsTester()
     eq.add_equality_group(cg.ExpWGate(),
-                          cg.ExpWGate(half_turns=1, axis_half_turns=0),
-                          cg.ExpWGate(degs=180, axis_degs=0),
-                          cg.ExpWGate(rads=np.pi, axis_rads=0))
+                          cg.ExpWGate(exponent=1, phase_exponent=0))
     eq.make_equality_group(
-        lambda: cg.ExpWGate(half_turns=cirq.Symbol('a')))
-    eq.make_equality_group(lambda: cg.ExpWGate(half_turns=0))
+        lambda: cg.ExpWGate(exponent=cirq.Symbol('a')))
+    eq.make_equality_group(lambda: cg.ExpWGate(exponent=0))
     eq.make_equality_group(
-        lambda: cg.ExpWGate(half_turns=0, axis_half_turns=cirq.Symbol('a')))
+        lambda: cg.ExpWGate(exponent=0, phase_exponent=cirq.Symbol('a')))
     eq.add_equality_group(
-        cg.ExpWGate(half_turns=0, axis_half_turns=0.5),
-        cg.ExpWGate(half_turns=0, axis_rads=np.pi / 2))
+        cg.ExpWGate(exponent=0, phase_exponent=0.5))
     eq.make_equality_group(
         lambda: cg.ExpWGate(
-            half_turns=cirq.Symbol('ab'),
-            axis_half_turns=cirq.Symbol('xy')))
+            exponent=cirq.Symbol('ab'),
+            phase_exponent=cirq.Symbol('xy')))
 
     # Flipping the axis and negating the angle gives the same rotation.
     eq.add_equality_group(
-        cg.ExpWGate(half_turns=0.25, axis_half_turns=1.5),
-        cg.ExpWGate(half_turns=1.75, axis_half_turns=0.5))
+        cg.ExpWGate(exponent=0.25, phase_exponent=1.5),
+        cg.ExpWGate(exponent=1.75, phase_exponent=0.5))
     # ...but not when there are parameters.
     eq.add_equality_group(cg.ExpWGate(
-        half_turns=cirq.Symbol('a'),
-        axis_half_turns=1.5))
+        exponent=cirq.Symbol('a'),
+        phase_exponent=1.5))
     eq.add_equality_group(cg.ExpWGate(
-        half_turns=cirq.Symbol('a'),
-        axis_half_turns=0.5))
+        exponent=cirq.Symbol('a'),
+        phase_exponent=0.5))
     eq.add_equality_group(cg.ExpWGate(
-        half_turns=0.25,
-        axis_half_turns=cirq.Symbol('a')))
+        exponent=0.25,
+        phase_exponent=cirq.Symbol('a')))
     eq.add_equality_group(cg.ExpWGate(
-        half_turns=1.75,
-        axis_half_turns=cirq.Symbol('a')))
+        exponent=1.75,
+        phase_exponent=cirq.Symbol('a')))
 
     # Adding or subtracting whole turns/phases gives the same rotation.
     eq.add_equality_group(
-        cg.ExpWGate(half_turns=-2.25, axis_half_turns=1.25),
-        cg.ExpWGate(half_turns=7.75, axis_half_turns=11.25))
+        cg.ExpWGate(exponent=-2.25, phase_exponent=1.25),
+        cg.ExpWGate(exponent=7.75, phase_exponent=11.25))
 
 
 def test_w_str():
     assert str(cg.ExpWGate()) == 'X'
-    assert str(cg.ExpWGate(axis_half_turns=0.99999, half_turns=0.5)) == 'X^-0.5'
-    assert str(cg.ExpWGate(axis_half_turns=0.5, half_turns=0.25)) == 'Y^0.25'
-    assert str(cg.ExpWGate(axis_half_turns=0.25,
-                           half_turns=0.5)) == 'W(0.25)^0.5'
+    assert str(cg.ExpWGate(phase_exponent=0.99999, exponent=0.5)) == 'X^-0.5'
+    assert str(cg.ExpWGate(phase_exponent=0.5, exponent=0.25)) == 'Y^0.25'
+    assert str(cg.ExpWGate(phase_exponent=0.25,
+                           exponent=0.5)) == 'W(0.25)^0.5'
 
 
 def test_w_decomposition():
     q = cirq.NamedQubit('q')
     cirq.testing.assert_allclose_up_to_global_phase(
         cirq.Circuit.from_ops(
-            cg.ExpWGate(half_turns=0.25, axis_half_turns=0.3).on(q)
+            cg.ExpWGate(exponent=0.25, phase_exponent=0.3).on(q)
         ).to_unitary_matrix(),
         cirq.Circuit.from_ops(
             cirq.Z(q) ** -0.3,
@@ -94,39 +89,39 @@ def test_w_decomposition():
 
 
 def test_w_inverse():
-    assert cirq.inverse(cg.ExpWGate(half_turns=cirq.Symbol('a')), None) is None
+    assert cirq.inverse(cg.ExpWGate(exponent=cirq.Symbol('a')), None) is None
     assert cirq.inverse(cg.ExpWGate()) == cg.ExpWGate()
 
 
 def test_w_parameterize():
-    parameterized_gate = cg.ExpWGate(half_turns=cirq.Symbol('a'),
-                                     axis_half_turns=cirq.Symbol('b'))
+    parameterized_gate = cg.ExpWGate(exponent=cirq.Symbol('a'),
+                                     phase_exponent=cirq.Symbol('b'))
     assert cirq.is_parameterized(parameterized_gate)
     assert not cirq.has_unitary(parameterized_gate)
     assert cirq.unitary(parameterized_gate, None) is None
     resolver = cirq.ParamResolver({'a': 0.1, 'b': 0.2})
     resolved_gate = cirq.resolve_parameters(parameterized_gate, resolver)
-    assert resolved_gate == cg.ExpWGate(half_turns=0.1, axis_half_turns=0.2)
+    assert resolved_gate == cg.ExpWGate(exponent=0.1, phase_exponent=0.2)
 
 
 def test_w_repr():
-    gate = cg.ExpWGate(half_turns=0.1, axis_half_turns=0.2)
+    gate = cg.ExpWGate(exponent=0.1, phase_exponent=0.2)
     assert repr(gate
-                ) == 'cirq.google.ExpWGate(half_turns=0.1, axis_half_turns=0.2)'
+                ) == 'cirq.google.ExpWGate(exponent=0.1, phase_exponent=0.2)'
 
 
 def test_trace_bound():
-    assert cirq.trace_distance_bound(cg.ExpWGate(half_turns=.001)) < 0.01
+    assert cirq.trace_distance_bound(cg.ExpWGate(exponent=.001)) < 0.01
     assert cirq.trace_distance_bound(cg.ExpWGate(
-        half_turns=cirq.Symbol('a'))) >= 1
+        exponent=cirq.Symbol('a'))) >= 1
 
 
 def test_cirq_symbol_diagrams():
     q00 = cirq.GridQubit(0, 0)
     q01 = cirq.GridQubit(0, 1)
     c = cirq.Circuit.from_ops(
-        cg.ExpWGate(axis_half_turns=cirq.Symbol('a'),
-                    half_turns=cirq.Symbol('b')).on(q00),
+        cg.ExpWGate(phase_exponent=cirq.Symbol('a'),
+                    exponent=cirq.Symbol('b')).on(q00),
         cirq.Z(q01)**cirq.Symbol('c'),
         cirq.CZ(q00, q01)**cirq.Symbol('d'),
     )
@@ -155,10 +150,10 @@ def test_z_diagram_chars():
 def test_w_diagram_chars():
     q = cirq.GridQubit(0, 1)
     c = cirq.Circuit.from_ops(
-        cg.ExpWGate(axis_half_turns=0).on(q),
-        cg.ExpWGate(axis_half_turns=0.25).on(q),
-        cg.ExpWGate(axis_half_turns=0.5).on(q),
-        cg.ExpWGate(axis_half_turns=cirq.Symbol('a')).on(q),
+        cg.ExpWGate(phase_exponent=0).on(q),
+        cg.ExpWGate(phase_exponent=0.25).on(q),
+        cg.ExpWGate(phase_exponent=0.5).on(q),
+        cg.ExpWGate(phase_exponent=cirq.Symbol('a')).on(q),
     )
     cirq.testing.assert_has_diagram(c, """
 (0, 1): ───X───W(0.25)───Y───W(a)───
