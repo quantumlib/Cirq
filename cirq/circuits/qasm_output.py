d2a965b36a62ce6e78664e1e7fdbc5ccffaeadcd
Craig Gidney <craiggidney@google.com>
2018-10-25 13:11:40 -0700

    fix

--- cirq/circuits/qasm_output.py
+++ cirq/circuits/qasm_output.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""An optimization pass that combines adjacent single-qubit rotations."""
+"""Utility classes for representing QASM."""
 
 from typing import Set  # pylint: disable=unused-import
 from typing import (
