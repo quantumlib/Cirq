ccb67e048aef0136eef096e1b37cf9a0cc6a9eef
Dave Bacon <dabacon@gmail.com>
2018-10-31 10:40:01 -0700

    Add back generated proto files

--- dev_tools/python2.7-generate.sh
+++ dev_tools/python2.7-generate.sh
@@ -21,6 +21,10 @@
 # output directory defaults to "python2.7-output" in the current working
 # directory.
 
+# This script also requires that the protocol buffer compiler "protoc" command
+# is installed. This can be done using apt-get:
+#     sudo apt-get install protobuf-compiler
+
 # Takes three optional arguments:
 #     1. Output directory. Defaults to 'python2.7-output'. Fails if the
 #         directory already exists.
@@ -73,6 +77,11 @@ find "${out_dir}" | grep "\.py\.bak$" | xargs rm -f
 
 # Move examples without conversion (they must be cross-compatible).
 cp -r "${in_dir}/examples" "${out_dir}/examples"
+
+# Build protobufs.
+proto_dir="${out_dir}/${project_name}/api/google/v1"
+find ${proto_dir} | grep '_pb2\.py' | xargs rm -f
+protoc -I="${out_dir}" --python_out="${out_dir}" ${proto_dir}/*.proto
 
 # Include requirements files.
 cp "${in_dir}/dev_tools/python2.7-requirements.txt" "${out_dir}/requirements.txt"
