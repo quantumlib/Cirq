2f4259d769ff6579a03826b911e8229e0c95e81a
Craig Gidney <craiggidney@google.com>
2018-11-08 15:49:42 -0800

    Doc

--- cirq/value/value_equality.py
+++ cirq/value/value_equality.py
@@ -20,9 +20,32 @@ class _SupportsValueEquality(Protocol):
     """An object decorated with the value equality decorator."""
 
     def _value_equality_values_(self) -> Any:
+        """Returns a value or values that define the identity of this object.
+
+        For example, a Point2D would be defined by the tuple (x, y) and so it
+        would return `(x, y)` from this method.
+
+        The decorated class is responsible for implementing this method.
+
+        Returns:
+            Values used when determining if the receiving object is equal to
+            another object.
+        """
         pass
 
     def _value_equality_values_cls_(self) -> Any:
+        """Automatically implemented by the `cirq.value_equality` decorator.
+
+        This method encodes the logic used to determine whether or not objects
+        that have the same equivalence values but different types are considered
+        to be equal. By default, this returns the decorated type. But there is
+        an option (`distinct_child_types`) to make it return `type(self)`
+        instead.
+
+        Returns:
+            Type used when determining if the receiving object is equal to
+            another object.
+        """
         pass
 
 
