4016bef0c174b8e6dac5f8c9f5e39990f7913451
Craig Gidney <craiggidney@google.com>
2018-11-07 21:22:13 -0800

    Add publish-dev-package and verify-published-pypi-package scripts - Move packaging scripts into dev_tools/packaging - Refactor publish-to-test-pypi into publish-dev-package
    Example usage and output:
    
    ```bash
    $ ./dev_tools/packaging/publish-dev-package.sh 0.4.0.dev42
    ```
    
    ```
    Producing package with version 0.4.0.dev4220181107211638 to upload to TEST pypi repository
    Producing python 3 package files...
    Generating python 2.7 source...
    Producing python 2.7 package files...
    cirq-0.4.0.dev4220181107211638-py2-none-any.whl  cirq-0.4.0.dev4220181107211638-py3-none-any.whl
    Uploading distributions to https://test.pypi.org/legacy/
    Uploading cirq-0.4.0.dev4220181107211638-py3-none-any.whl
    100%|████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 526k/526k [00:02<00:00, 244kB/s]
    Uploading cirq-0.4.0.dev4220181107211638-py2-none-any.whl
    100%|████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 499k/499k [00:01<00:00, 275kB/s]
    Uploaded package with version 0.4.0.dev4220181107211638 to TEST pypi repository
    ```
    
    ```bash
    $ ./dev_tools/packaging/verify-published-package.sh 0.4.0.dev4220181107211638 --test
    ```
    
    ```
    python2
    Working in a fresh virtualenv at /tmp/verify-published-package.3b7tvOh6VRcx8lLp/python2
    Pre-installing dependencies since they don't all exist in TEST pypi
    Installing cirq==0.4.0.dev4220181107211638 from TEST pypi
    Checking that code executes
    (0, 0)───(0, 1)───(0, 2)───(0, 3)───(0, 4)───(0, 5)───(0, 6)───(0, 7)───(0, 8)───(0, 9)───(0, 10)
    │        │        │        │        │        │        │        │        │        │        │
    │        │        │        │        │        │        │        │        │        │        │
    (1, 0)───(1, 1)───(1, 2)───(1, 3)───(1, 4)───(1, 5)───(1, 6)───(1, 7)───(1, 8)───(1, 9)───(1, 10)
    0: ───@───
          │
    1: ───@───
    Installing pytest requirements
    Running tests
    ...[...]...
    5217 passed, 3 warnings in 44.93 seconds
    
    python3
    Working in a fresh virtualenv at /tmp/verify-published-package.3b7tvOh6VRcx8lLp/python3
    Pre-installing dependencies since they don't all exist in TEST pypi
    Installing cirq==0.4.0.dev4220181107211638 from TEST pypi
    Checking that code executes
    (0, 0)───(0, 1)───(0, 2)───(0, 3)───(0, 4)───(0, 5)───(0, 6)───(0, 7)───(0, 8)───(0, 9)───(0, 10)
    │        │        │        │        │        │        │        │        │        │        │
    │        │        │        │        │        │        │        │        │        │        │
    (1, 0)───(1, 1)───(1, 2)───(1, 3)───(1, 4)───(1, 5)───(1, 6)───(1, 7)───(1, 8)───(1, 9)───(1, 10)
    0: ───@───
          │
    1: ───@───
    Installing pytest requirements
    Running tests
    ...[...]...
    5232 passed, 455 warnings in 40.47 seconds
    
    VERIFIED
    ```

---
+++ dev_tools/packaging/verify-published-package.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+################################################################################
+# Downloads and tests cirq wheels from the pypi package repository. Uses the
+# prod pypi repository unless the --test switch is added.
+#
+# CAUTION: when targeting the test pypi repository, this script assumes that the
+# local version of cirq has the same dependencies as the remote one (because the
+# dependencies must be installed from the non-test pypi repository). If the
+# dependencies disagree, the tests can spuriously fail.
+#
+# Usage:
+#     dev_tools/packaging/verify-published-package.sh PACKAGE_VERSION [--test|--prod]
+################################################################################
+
+set -e
+trap "{ echo -e '\e[31mFAILED\e[0m'; }" ERR
+
+
+CIRQ_VERSION=$1
+PROD_SWITCH=$2
+
+if [ -z "${CIRQ_VERSION}" ]; then
+    echo -e "\e[31mFirst argument must be the package version to test.\e[0m"
+    exit 1
+fi
+
+if [ "${PROD_SWITCH}" = "--test" ]; then
+    PYPI_REPOSITORY_FLAG="--index-url=https://test.pypi.org/simple/"
+    PYPI_REPO_NAME="TEST"
+elif [ -z "${PROD_SWITCH}" ] || [ "${PROD_SWITCH}" = "--prod" ]; then
+    PYPI_REPOSITORY_FLAG=''
+    PYPI_REPO_NAME="PROD"
+else
+    echo -e "\e[31mSecond argument must be empty, '--prod' or '--test'.\e[0m"
+    exit 1
+fi
+
+# Find the repo root.
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+# Temporary workspace.
+tmp_dir=$(mktemp -d "/tmp/verify-published-package.XXXXXXXXXXXXXXXX")
+cd "${tmp_dir}"
+trap "{ rm -rf ${tmp_dir}; }" EXIT
+
+# Test both the python 2 and python 3 versions.
+for PYTHON_VERSION in python2 python3; do
+    # Prepare.
+    if [ "${PYTHON_VERSION}" = "python2" ]; then
+        RUNTIME_DEPS_FILE="${REPO_ROOT}/dev_tools/python2.7-requirements.txt"
+        DEV_DEPS_FILE="${REPO_ROOT}/dev_tools/conf/pip-list-python2.7-test-tools.txt"
+    else
+        RUNTIME_DEPS_FILE="${REPO_ROOT}/requirements.txt"
+        DEV_DEPS_FILE="${REPO_ROOT}/dev_tools/conf/pip-list-dev-tools.txt"
+    fi
+    echo -e "\n\e[32m${PYTHON_VERSION}\e[0m"
+    echo "Working in a fresh virtualenv at ${tmp_dir}/${PYTHON_VERSION}"
+    virtualenv --quiet "--python=/usr/bin/${PYTHON_VERSION}" "${tmp_dir}/${PYTHON_VERSION}"
+
+    # Install package.
+    if [ "${PYPI_REPO_NAME}" == "TEST" ]; then
+        echo "Pre-installing dependencies since they don't all exist in TEST pypi"
+        "${tmp_dir}/${PYTHON_VERSION}/bin/pip" install --quiet -r "${RUNTIME_DEPS_FILE}"
+    fi
+    echo Installing cirq=="${CIRQ_VERSION} from ${PYPI_REPO_NAME} pypi"
+    "${tmp_dir}/${PYTHON_VERSION}/bin/pip" install --quiet ${PYPI_REPOSITORY_FLAG} "cirq==${CIRQ_VERSION}"
+
+    # Check that code runs without dev deps.
+    echo Checking that code executes
+    "${tmp_dir}/${PYTHON_VERSION}/bin/python" -c "import cirq; print(cirq.google.Foxtail)"
+    "${tmp_dir}/${PYTHON_VERSION}/bin/python" -c "import cirq; print(cirq.Circuit.from_ops(cirq.CZ(*cirq.LineQubit.range(2))))"
+
+    # Run tests.
+    echo Installing pytest requirements
+    if [ "${PYTHON_VERSION}" = "python2" ]; then
+        "${tmp_dir}/${PYTHON_VERSION}/bin/pip" install --quiet pytest mock
+    else
+        "${tmp_dir}/${PYTHON_VERSION}/bin/pip" install --quiet pytest
+    fi
+    PY_VER=$(ls "${tmp_dir}/${PYTHON_VERSION}/lib")
+    echo Running tests
+    "${tmp_dir}/${PYTHON_VERSION}/bin/pytest" --quiet --disable-pytest-warnings "${tmp_dir}/${PYTHON_VERSION}/lib/${PY_VER}/site-packages/cirq"
+done
+
+echo
+echo -e '\e[32mVERIFIED\e[0m'
