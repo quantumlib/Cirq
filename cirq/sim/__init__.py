5296baf89d277818185de26db03a6d2eb1c9ac17
Dave Bacon <dabacon@gmail.com>
2018-11-11 12:03:05 -0800

    Use numpy simulator for Simulator class

--- cirq/sim/__init__.py
+++ cirq/sim/__init__.py
@@ -21,8 +21,11 @@ from cirq.sim.simulator import (
     SimulatesIntermediateWaveFunction,
     SimulatesFinalWaveFunction,
 )
-
-from cirq.sim.wavefunction import (
+from cirq.sim.sparse_simulator import (
+    Simulator,
+    SimulatorStep,
+)
+from cirq.sim.wave_function import (
     dirac_notation,
     measure_state_vector,
     sample_state_vector,
