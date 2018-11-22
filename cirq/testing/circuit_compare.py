65ae070cd7e1e37cd694da1ab1552c5ebd4a37a7
Kevin J. Sung <kevinjefferysung@gmail.com>
2018-11-21 20:57:24 -0800

    remove unnecessary qubit inference

--- cirq/testing/circuit_compare.py
+++ cirq/testing/circuit_compare.py
@@ -366,8 +366,6 @@ def _infer_qubit_count(val: Any) -> int:
         return 3
     if isinstance(val, ops.ControlledGate):
         return 1 + _infer_qubit_count(val.sub_gate)
-    if protocols.has_unitary(val):
-        return protocols.unitary(val).shape[0].bit_length() - 1
 
     raise NotImplementedError(
         'Failed to infer qubit count of <{!r}>. Specify it.'.format(
