b68d6b7eace63f999b8765b1bebff2084b2133de
Wojciech Mruczkiewicz <mrwojtek@google.com>
2018-12-04 16:17:33 +0100

    Add more test cases for aprox_eq.

--- cirq/protocols/approximate_equality_test.py
+++ cirq/protocols/approximate_equality_test.py
@@ -24,6 +24,15 @@ def test_approx_eq_primitives():
     assert not cirq.approx_eq(complex(1, 1), complex(1.1, 1.2), atol=0.1)
 
 
+def test_approx_eq_special_numerics():
+    assert not cirq.approx_eq(float('nan'), 0, atol=0.0)
+    assert not cirq.approx_eq(float('nan'), float('nan'), atol=0.0)
+    assert not cirq.approx_eq(float('inf'), float('-inf'), atol=0.0)
+    assert not cirq.approx_eq(float('inf'), 5, atol=0.0)
+    assert not cirq.approx_eq(float('inf'), 0, atol=0.0)
+    assert cirq.approx_eq(float('inf'), float('inf'), atol=0.0)
+
+
 def test_approx_eq_tuple():
     assert cirq.approx_eq((1, 1), (1, 1), atol=0.0)
     assert not cirq.approx_eq((1, 1), (1, 1, 1), atol=0.0)
@@ -32,8 +41,23 @@ def test_approx_eq_tuple():
     assert not cirq.approx_eq((1.1, 1.2, 1.3), (1, 1, 1), atol=0.2)
 
 
+def test_approx_eq_list():
+    assert cirq.approx_eq([], [], atol=0.0)
+    assert not cirq.approx_eq([], [[]], atol=0.0)
+    assert cirq.approx_eq([1, 1], [1, 1], atol=0.0)
+    assert not cirq.approx_eq([1, 1], [1, 1, 1], atol=0.0)
+    assert not cirq.approx_eq([1, 1], [1,], atol=0.0)
+    assert cirq.approx_eq([1.1, 1.2, 1.3], [1, 1, 1], atol=0.4)
+    assert not cirq.approx_eq([1.1, 1.2, 1.3], [1, 1, 1], atol=0.2)
+
+
 def test_approx_eq_iterables():
+    def gen_1_1():
+        yield 1
+        yield 1
     assert cirq.approx_eq((1, 1), [1, 1], atol=0.0)
+    assert cirq.approx_eq((1, 1), gen_1_1(), atol=0.0)
+    assert cirq.approx_eq(gen_1_1(), [1, 1], atol=0.0)
 
 
 class A:
@@ -92,3 +116,5 @@ def test_approx_eq_types_mismatch():
     assert not cirq.approx_eq(A(0), C(0), atol=0.0)
     assert not cirq.approx_eq(complex(0, 0), 0, atol=0.0)
     assert not cirq.approx_eq(0, complex(0, 0), atol=0.0)
+    assert not cirq.approx_eq(0, [0], atol=1.0)
+    assert not cirq.approx_eq([0], 0, atol=0.0)
