cd3feb747246d5a0a3918d499c6fbfa5d0701955
Wojciech Mruczkiewicz <mrwojtek@google.com>
2018-12-06 17:36:55 +0100

    Import approx_eq directly.

--- cirq/value/value_equality.py
+++ cirq/value/value_equality.py
@@ -18,7 +18,7 @@ from typing import Union, Callable, overload, Any
 
 from typing_extensions import Protocol
 
-from cirq import protocols
+from cirq.protocols import approx_eq
 
 
 class _SupportsValueEquality(Protocol):
@@ -104,7 +104,7 @@ def _value_equality_approx_eq(self: _SupportsValueEquality,
         return False
 
     # Delegate to cirq.approx_eq for approximate equality comparison.
-    return protocols.approx_eq(
+    return approx_eq(
         self._value_equality_approximate_values_(),
         other._value_equality_approximate_values_(),
         atol=atol
