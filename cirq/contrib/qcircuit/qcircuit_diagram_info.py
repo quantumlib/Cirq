e016e23f57a73df2d7c29439cf10c0f02a04c238
Bryan A. O'Gorman <bryan.a.ogorman@gmail.com>
2018-12-06 16:14:27 -0800

    removed _HardcodedQCircuitSymbolsGate

--- cirq/contrib/qcircuit/qcircuit_diagram_info.py
+++ cirq/contrib/qcircuit/qcircuit_diagram_info.py
@@ -12,29 +12,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import cast
+from typing import cast, Optional, Tuple
 
 from cirq import ops, protocols
 
 from cirq.contrib.qcircuit.qcircuit_diagrammable import (
-    known_qcircuit_operation_symbols,
+    QCircuitDiagrammable,
     _TextToQCircuitDiagrammable,
     _FallbackQCircuitGate,
 )
 
 
+def hardcoded_qcircuit_diagram_info(
+        op: ops.Operation) -> Optional[protocols.CircuitDiagramInfo]:
+    if not isinstance(op, ops.GateOperation):
+        return None
+    symbols = (
+        (r'\targ',) if op.gate == ops.X else
+        (r'\control', r'\control') if op.gate == ops.CZ else
+        (r'\control', r'\targ') if op.gate == ops.CNOT else
+        (r'\meter',) if ops.MeasurementGate.is_measurement(op.gate) else
+        ())
+    return (protocols.CircuitDiagramInfo(cast(Tuple[str, ...], symbols))
+            if symbols else None)
+
+
 def get_qcircuit_diagram_info(op: ops.Operation,
                               args: protocols.CircuitDiagramInfoArgs
                               ) -> protocols.CircuitDiagramInfo:
-    diagrammable = known_qcircuit_operation_symbols(op)
-    if diagrammable is None:
+    info = hardcoded_qcircuit_diagram_info(op)
+    if info is None:
         info = protocols.circuit_diagram_info(op, default=None)
         if info is not None:
             diagrammable = _TextToQCircuitDiagrammable(
-                cast(protocols.SupportsCircuitDiagramInfo, op))
+                cast(protocols.SupportsCircuitDiagramInfo, op)
+                ) # type: QCircuitDiagrammable
         elif isinstance(op, ops.GateOperation):
             diagrammable = _FallbackQCircuitGate(op.gate)
         else:
             diagrammable = _FallbackQCircuitGate(op)
-    return diagrammable.qcircuit_diagram_info(args)
+        info = diagrammable.qcircuit_diagram_info(args)
+    return info
 
--- cirq/contrib/qcircuit/qcircuit_diagrammable.py
+++ cirq/contrib/qcircuit/qcircuit_diagrammable.py
@@ -41,15 +41,6 @@ def _escape_text_for_latex(text):
     return '\\text{' + escaped + '}'
 
 
-class _HardcodedQCircuitSymbolsGate(QCircuitDiagrammable):
-    def __init__(self, *symbols: str) -> None:
-        self.symbols = symbols
-
-    def qcircuit_diagram_info(self, args: protocols.CircuitDiagramInfoArgs
-                              ) -> protocols.CircuitDiagramInfo:
-        return protocols.CircuitDiagramInfo(self.symbols)
-
-
 def _get_multigate_parameters(gate: Any,
                               args: protocols.CircuitDiagramInfoArgs
                               ) -> Optional[Tuple[int, int]]:
@@ -109,23 +100,3 @@ class _FallbackQCircuitGate(QCircuitDiagrammable):
         symbols = [name] + ['#{}'.format(i + 1) for i in range(1, qubit_count)]
         escaped_symbols = tuple(_escape_text_for_latex(s) for s in symbols)
         return protocols.CircuitDiagramInfo(escaped_symbols)
-
-
-def known_qcircuit_operation_symbols(op: ops.Operation
-                                     ) -> Optional[QCircuitDiagrammable]:
-    if isinstance(op, ops.GateOperation):
-        return _known_gate_symbols(op.gate)
-    return None
-
-
-def _known_gate_symbols(
-        gate: ops.Gate) -> Optional[QCircuitDiagrammable]:
-    if gate == ops.X:
-        return _HardcodedQCircuitSymbolsGate(r'\targ')
-    if gate == ops.CZ:
-        return _HardcodedQCircuitSymbolsGate(r'\control', r'\control')
-    if gate == ops.CNOT:
-        return _HardcodedQCircuitSymbolsGate(r'\control', r'\targ')
-    if ops.MeasurementGate.is_measurement(gate):
-        return _HardcodedQCircuitSymbolsGate(r'\meter')
-    return None
