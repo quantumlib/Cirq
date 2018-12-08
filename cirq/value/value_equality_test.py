ff645bf483daebc022e4f261ec202d10e6df69b0
Wojciech Mruczkiewicz <mrwojtek@google.com>
2018-12-08 10:46:24 +0100

    Debugging python2 tests.

--- cirq/value/value_equality_test.py
+++ cirq/value/value_equality_test.py
@@ -147,6 +147,15 @@ def test_value_equality_forgot_method():
             pass
 
 
+@cirq.value_equality
+class BasicCE:
+    def __init__(self, x):
+        self.x = x
+
+    def _value_equality_values_(self):
+        return self.x
+
+
 @cirq.value_equality(approximate=True)
 class ApproxE:
     def __init__(self, xy):
