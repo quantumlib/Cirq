8706a2791c9a83d5b59e1025c8cc121a5081327e
vtomole <vtomole@iastate.edu>
2018-12-12 19:57:19 -0600

    ignore types

--- cirq/optimizers/merge_single_qubit_gates_test.py
+++ cirq/optimizers/merge_single_qubit_gates_test.py
@@ -33,8 +33,8 @@ def assert_optimizes(
         cirq.DropEmptyMoments()
     ]
     for post in followup_optimizations:
-        post.optimize_circuit(before)
-        post.optimize_circuit(expected)
+        post.optimize_circuit(before) # type: ignore
+        post.optimize_circuit(expected) # type: ignore
 
     try:
         assert before == expected
