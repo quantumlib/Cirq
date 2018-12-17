7a6d7f1fae1ff75d4e74d966ca6e7240fe438b3b
Kevin J. Sung <kevinjefferysung@gmail.com>
2018-12-17 14:32:11 -0800

    fix test

--- cirq/sim/sparse_simulator_test.py
+++ cirq/sim/sparse_simulator_test.py
@@ -178,8 +178,9 @@ def test_simulate_random_unitary(dtype):
             result = simulator.simulate(random_circuit, qubit_order=[q0, q1],
                                         initial_state=x)
             circuit_unitary.append(result.final_state)
-        np.testing.assert_almost_equal(np.transpose(circuit_unitary),
-                                       random_circuit.to_unitary_matrix())
+        np.testing.assert_almost_equal(
+            np.transpose(circuit_unitary),
+            random_circuit.to_unitary_matrix(qubit_order=[q0, q1]))
 
 
 @pytest.mark.parametrize('dtype', [np.complex64, np.complex128])
