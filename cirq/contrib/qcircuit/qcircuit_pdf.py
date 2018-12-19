eaf264bd066ac8d26929f012f5229f2cf3bb2afd
Bryan A. O'Gorman <bryan.a.ogorman@gmail.com>
2018-12-18 17:51:17 -0800

    refactored qcircuit_pdf

--- cirq/contrib/qcircuit/qcircuit_pdf.py
+++ cirq/contrib/qcircuit/qcircuit_pdf.py
@@ -16,20 +16,60 @@
 
 import errno
 import os
+from typing import Any, Dict, Iterable, Optional
 
-from pylatex import Document, NoEscape, Package
+import pylatex
 
 from cirq import circuits
 from cirq.contrib.qcircuit.qcircuit_diagram import (
         circuit_to_latex_using_qcircuit)
 
 
-def circuit_to_pdf_using_qcircuit_via_tex(circuit: circuits.Circuit,
-                                          filepath: str,
-                                          pdf_kwargs=None,
-                                          qcircuit_kwargs=None,
-                                          clean_ext=('dvi', 'ps'),
-                                          documentclass='article'):
+def tex_to_pdf(
+        tex: str,
+        filepath: str,
+        pdf_kwargs: Dict[str, Any] = None,
+        clean_ext: Iterable[str] = ('dvi', 'ps'),
+        documentclass: str = 'article',
+        packages: Iterable[str] = ('amsmath', 'qcircuit')
+        ) -> None:
+    """Compiles latex.
+
+    Args:
+        tex: The tex to compile.
+        filepath: Where to output the pdf.
+        pdf_kwargs: The arguments to pass to generate_pdf.
+        clean_ext: The file extensions to clean up after compilation. By
+            default, latexmk is used with the '-pdfps' flag, which produces
+            intermediary dvi and ps files.
+        documentclass: The documentclass of the latex file.
+
+        """
+    pdf_kwargs = {'compiler': 'latexmk', 'compiler_args': ['-pdfps'],
+                  **({} if pdf_kwargs is None else pdf_kwargs)}
+
+    doc = pylatex.Document(
+            documentclass=documentclass, document_options='dvips')
+    for package in packages:
+        doc.packages.append(pylatex.Package(package))
+    doc.append(pylatex.NoEscape(tex))
+    doc.generate_pdf(filepath, **pdf_kwargs)
+    for ext in clean_ext:
+        try:
+            os.remove(filepath + '.' + ext)
+        except (OSError, IOError) as e:
+            if e.errno != errno.ENOENT:
+                raise
+
+
+def circuit_to_pdf_using_qcircuit_via_tex(
+        circuit: circuits.Circuit,
+        filepath: str,
+        pdf_kwargs: Optional[Dict[str, Any]] = None,
+        qcircuit_kwargs: Optional[Dict[str, Any]] = None,
+        clean_ext: Iterable[str] = ('dvi', 'ps'),
+        documentclass: str = 'article'
+        ) -> None:
     """Compiles the QCircuit-based latex diagram of the given circuit.
 
     Args:
@@ -43,18 +83,7 @@ def circuit_to_pdf_using_qcircuit_via_tex(circuit: circuits.Circuit,
             intermediary dvi and ps files.
         documentclass: The documentclass of the latex file.
     """
-    pdf_kwargs = {'compiler': 'latexmk', 'compiler_args': ['-pdfps'],
-                  **({} if pdf_kwargs is None else pdf_kwargs)}
     qcircuit_kwargs = {} if qcircuit_kwargs is None else qcircuit_kwargs
     tex = circuit_to_latex_using_qcircuit(circuit, **qcircuit_kwargs)
-    doc = Document(documentclass=documentclass, document_options='dvips')
-    doc.packages.append(Package('amsmath'))
-    doc.packages.append(Package('qcircuit'))
-    doc.append(NoEscape(tex))
-    doc.generate_pdf(filepath, **pdf_kwargs)
-    for ext in clean_ext:
-        try:
-            os.remove(filepath + '.' + ext)
-        except (OSError, IOError) as e:
-            if e.errno != errno.ENOENT:
-                raise
+    tex_to_pdf(tex, filepath, pdf_kwargs=pdf_kwargs, clean_ext=clean_ext,
+               documentclass=documentclass)
