e2b287ff064fa9b9dd21f4ab391e5b599b5bee12
Kevin J. Sung <kevinjefferysung@gmail.com>
2018-12-21 19:14:43 -0500

    set __hash__ to None

--- cirq/study/compute_displays_result.py
+++ cirq/study/compute_displays_result.py
@@ -27,6 +27,8 @@ class ComputeDisplaysResult:
         display_values: A dictionary from display key to display value.
     """
 
+    __hash__ = None
+
     def __init__(self,
                  params: resolver.ParamResolver,
                  display_values: Dict) -> None:
