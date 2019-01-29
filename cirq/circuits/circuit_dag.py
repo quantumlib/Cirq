5faf8c8bbdf59bb665afc2c8bed1a6819539d3f1
Bryan A. O'Gorman <bryan.a.ogorman@gmail.com>
2019-01-28 18:15:57 -0800

    added docstring to CircuitDag.is_topologically_sorted

--- cirq/circuits/circuit_dag.py
+++ cirq/circuits/circuit_dag.py
@@ -184,7 +184,30 @@ class CircuitDag(networkx.DiGraph):
                     strategy=circuit.InsertStrategy.EARLIEST,
                     device=self.device)
 
-    def is_topologically_sorted(self, operations: ops.OP_TREE):
+    def is_topologically_sorted(self, operations: ops.OP_TREE) -> bool:
+        """Whether a given order of operations is consistent with the dag.
+
+        For example, suppose the (transitive reduction of the) circuit DAG is 
+
+             ╭─> Op2 ─╮
+        Op1 ─┤        ├─> Op4
+             ╰─> Op3 ─╯
+
+        Then [Op1, Op2, Op3, Op4] and [Op1, Op3, Op2, Op4] (and any operations
+        tree that flattens to one of them) are topologically sorted according
+        to the DAG, and any other ordering of the four operations is not.
+
+        Evaluates to False when the set of operations is different from those
+        in the nodes of the DAG, regardless of the ordering.
+    
+        Args:
+            operations: The ordered operations.
+
+        Returns: 
+            Whether or not the operations given are topologically sorted
+            according to the DAG.
+        """
+
         remaining_dag = self.copy()
         frontier = [node for node in remaining_dag.nodes()
                     if not remaining_dag.pred[node]]
