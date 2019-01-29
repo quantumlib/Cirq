88b7ff171a65a22e583a682252850895d59cc9ec
Bryan A. O'Gorman <bryanaogorman@gmail.com>
2019-01-28 16:23:20 -0800

    Pairs of pairs acquaintance strategy (#1306)
    Implements [UpCCGSD](https://arxiv.org/abs/1810.02327) ansatz.

--- cirq/contrib/acquaintance/strategies/__init__.py
+++ cirq/contrib/acquaintance/strategies/__init__.py
@@ -16,3 +16,6 @@
 
 from cirq.contrib.acquaintance.strategies.complete import (
     complete_acquaintance_strategy)
+
+from cirq.contrib.acquaintance.strategies.quartic_paired import (
+    quartic_paired_acquaintance_strategy)
