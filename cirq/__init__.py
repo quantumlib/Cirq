7f5b9547af4bfd85b082f4615a5d927ffe6b1d16
Andreas Bengtsson <andreas.bengtsson@gmail.com>
2019-02-02 01:50:06 +0100

    Extract wavefunction convenience methods into abstract StateVector class (#1327)
    Fixes #1231 
    
    I moved bloch_vector along with density_matrix and dirac_notation to a new class called State. 
    Following #1247, these new methods also require that QubitIds are used instead of indices. 
    
    On a similar note, SimulationTrialResult still uses indices for its state methods, should this be changed to QubitId as well?
    
    Let me know what you think.

--- cirq/__init__.py
+++ cirq/__init__.py
@@ -202,6 +202,7 @@ from cirq.sim import (
     StepResult,
     SimulatesFinalWaveFunction,
     SimulatesIntermediateWaveFunction,
+    StateVector,
     bloch_vector_from_state_vector,
     density_matrix_from_state_vector,
     dirac_notation,
