ba9fa7765f4fc61d51fa81480277a1855aa5b313
Dave Bacon <dabacon@gmail.com>
2019-02-05 11:38:44 -0800

    actually run probablity tests

--- cirq/value/probability_test.py
+++ cirq/value/probability_test.py
@@ -18,11 +18,11 @@ import cirq
 
 
 @pytest.mark.parametrize('p', [0.0, 0.1, 0.6, 1.0])
-def validate_probability_valid(p):
+def test_validate_probability_valid(p):
     assert p == cirq.validate_probability(p, 'p')
 
 
 @pytest.mark.parametrize('p', [-0.1, 1.1])
-def validate_probability_invalid(p):
+def test_validate_probability_invalid(p):
     with pytest.raises(ValueError, match='p'):
         cirq.validate_probability(p, 'p')
