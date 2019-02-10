56ee8b195d79e00d1cbfe9442b34db8c9668276f
Victory Omole <vtomole@iastate.edu>
2019-02-10 15:03:35 -0600

    Add pytest-benchmark init

--- examples/examples_test.py
+++ examples/examples_test.py
@@ -9,8 +9,8 @@ import examples.bcs_mean_field
 import examples.phase_estimator
 
 
-def test_example_runs_bernstein_vazirani():
-    examples.bernstein_vazirani.main()
+def test_example_runs_bernstein_vazirani(benchmark):
+    benchmark(examples.bernstein_vazirani.main)
 
     # Check empty oracle case. Cover both biases.
     a = cirq.NamedQubit('a')
@@ -20,29 +20,29 @@ def test_example_runs_bernstein_vazirani():
         [], a, [], True)) == [cirq.X(a)]
 
 
-def test_example_runs_hello_line():
-    examples.place_on_bristlecone.main()
+def test_example_runs_hello_line(benchmark):
+    benchmark(examples.place_on_bristlecone.main)
 
 
-def test_example_runs_hello_qubit():
-    examples.hello_qubit.main()
+def test_example_runs_hello_qubit(benchmark):
+    benchmark(examples.hello_qubit.main)
 
 
-def test_example_runs_bell_inequality():
-    examples.bell_inequality.main()
+def test_example_runs_bell_inequality(benchmark):
+    benchmark(examples.bell_inequality.main)
 
 
-def test_example_runs_quantum_fourier_transform():
-    examples.quantum_fourier_transform.main()
+def test_example_runs_quantum_fourier_transform(benchmark):
+    benchmark(examples.quantum_fourier_transform.main)
 
 
-def test_example_runs_bcs_mean_field():
-    examples.bcs_mean_field.main()
+def test_example_runs_bcs_mean_field(benchmark):
+    benchmark(examples.bcs_mean_field.main)
 
 
-def test_example_runs_grover():
-    examples.grover.main()
+def test_example_runs_grover(benchmark):
+    benchmark(examples.grover.main)
 
 
-def test_example_runs_phase_estimator():
-    examples.phase_estimator.main()
+def test_example_runs_phase_estimator(benchmark):
+    benchmark(examples.phase_estimator.main)
