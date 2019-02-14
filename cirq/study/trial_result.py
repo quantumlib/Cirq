b733ab09cde96996bb0558ac24e54a1e1f1514a0
Craig Gidney <craiggidney@google.com>
2019-02-14 15:53:23 -0800

    compat fix

--- cirq/study/trial_result.py
+++ cirq/study/trial_result.py
@@ -83,7 +83,9 @@ def _keyed_repeated_bitstrings(vals: Dict[str, np.ndarray]
 
 
 def _key_to_str(key: TMeasurementKey) -> str:
-    if isinstance(key, (str, ops.QubitId)):
+    if isinstance(key, str):
+        return key
+    if isinstance(key, ops.QubitId):
         return str(key)
     return ','.join(str(q) for q in key)
 
