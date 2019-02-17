8d6bf59696366f2fa08cfc1f4445e96254708c7b
Andreas Bengtsson <andreas.bengtsson@gmail.com>
2019-02-17 19:52:15 +0100

    Fix

--- cirq/sim/sparse_simulator_test.py
+++ cirq/sim/sparse_simulator_test.py
@@ -529,7 +529,6 @@ class MultiHTestGate(cirq.TwoQubitGate):
 def test_simulates_composite():
     c = cirq.Circuit.from_ops(MultiHTestGate().on(*cirq.LineQubit.range(2)))
     expected = np.array([0.5] * 4)
-    print(c.all_qubits())
     np.testing.assert_allclose(c.apply_unitary_effect_to_state(),
                                expected)
     np.testing.assert_allclose(cirq.Simulator().simulate(c).state_vector(),
