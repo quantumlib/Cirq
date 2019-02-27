84ac639ae42b18784f7917e2f05fc7a707162a65
Craig Gidney <craiggidney@google.com>
2019-02-26 23:23:28 -0800

    Auto-wrap dictionaries into ParamResolver and skip empty resolutions - cirq.resolve_parameters, cirq.Simulator, etc can now be given a raw dictionary instead of one wrapped into cirq.ParamResolver - cirq.resolve_parameters now bounces its input when given a vacuous parameter resolver

--- cirq/protocols/resolve_parameters.py
+++ cirq/protocols/resolve_parameters.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, TypeVar, TYPE_CHECKING
+from typing import Any, TypeVar, TYPE_CHECKING, Union, Dict
 from typing_extensions import Protocol
 
 if TYPE_CHECKING:
@@ -56,7 +56,9 @@ def is_parameterized(val: Any) -> bool:
         return False
 
 
-def resolve_parameters(val: Any, param_resolver: 'cirq.ParamResolver') -> Any:
+def resolve_parameters(val: Any,
+                       param_resolver: Union[Dict[str, float],
+                                             'cirq.ParamResolver']) -> Any:
     """Resolves symbol parameters in the effect using the param resolver.
 
     This function will use the `_resolve_parameters_` magic method
@@ -73,6 +75,15 @@ def resolve_parameters(val: Any, param_resolver: 'cirq.ParamResolver') -> Any:
         If `val` has no `_resolve_parameters_` method or if it returns
         NotImplemented, `val` itself is returned.
     """
+    # HACK: break cycle.
+    from cirq import ParamResolver
+
+    if not param_resolver:
+        return val
+
+    # Ensure its a dictionary wrapped in a ParamResolver.
+    param_resolver = ParamResolver(param_resolver)
+
     getter = getattr(val, '_resolve_parameters_', None)
     result = NotImplemented if getter is None else getter(param_resolver)
 
--- cirq/study/resolver.py
+++ cirq/study/resolver.py
@@ -14,10 +14,12 @@
 
 """Resolves ParameterValues to assigned values."""
 
-from typing import Dict, Union
+from typing import Dict, Union, Optional
 import sys
 
 import sympy
+
+from cirq import value
 
 
 class ParamResolver(object):
@@ -33,9 +35,21 @@ class ParamResolver(object):
             assigned value.
     """
 
-    def __init__(self, param_dict: Dict[str, float]) -> None:
-        self.param_dict = param_dict
-        self._param_hash = hash(frozenset(param_dict.items()))
+    def __new__(cls, param_dict: Union[None,
+                                       Dict[str, float],
+                                       'ParamResolver'] = None):
+        if isinstance(param_dict, ParamResolver):
+            return param_dict
+        return super().__new__(cls)
+
+    def __init__(self, param_dict: Union[None,
+                                         Dict[str, float],
+                                         'ParamResolver'] = None) -> None:
+        if hasattr(self, '_param_hash'):
+            return  # Already initialized. Got wrapped as part of the __new__.
+
+        self.param_dict = {} if param_dict is None else param_dict
+        self._param_hash = hash(frozenset(self.param_dict.items()))
 
     def value_of(
             self,
@@ -66,6 +80,9 @@ class ParamResolver(object):
             return v if v.free_symbols else float(v)
         return value
 
+    def __bool__(self):
+        return bool(self.param_dict)
+
     def __getitem__(self, key):
         return self.value_of(key)
 
@@ -76,6 +93,9 @@ class ParamResolver(object):
         if not isinstance(other, ParamResolver):
             return NotImplemented
         return self.param_dict == other.param_dict
+
+    def __ne__(self, other):
+        return not self == other
 
     def __repr__(self):
         return 'cirq.ParamResolver({})'.format(repr(self.param_dict))
