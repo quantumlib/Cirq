42c2db5744da96ac60084d632158f777e14a7e9b
Kevin J. Sung <kevinjefferysung@gmail.com>
2019-02-28 14:22:45 -0500

    types and doc

--- cirq/contrib/model_gradient_descent/model_gradient_descent.py
+++ cirq/contrib/model_gradient_descent/model_gradient_descent.py
@@ -12,13 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Callable, List, Tuple
+
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 
 
-def _get_least_squares_model_gradient(xs, ys, xopt, linear_model):
+def _get_least_squares_model_gradient(
+        xs: List[np.ndarray],
+        ys: List[np.ndarray],
+        xopt: np.ndarray,
+        linear_model: LinearRegression):
     model = Pipeline([('poly', PolynomialFeatures(degree=2)),
                       ('linear_model', linear_model),
                       ]
@@ -31,7 +37,7 @@ def _get_least_squares_model_gradient(xs, ys, xopt, linear_model):
     return linear_coeffs
 
 
-def _random_point_in_ball(n, radius):
+def _random_point_in_ball(n: int, radius: float):
     point_on_sphere = np.random.randn(n)
     point_on_sphere /= np.linalg.norm(point_on_sphere)
     length = np.random.uniform()
@@ -40,15 +46,42 @@ def _random_point_in_ball(n, radius):
 
 
 def model_gradient_descent(
-        f,
-        x0,
-        sample_radius=1e-1,
-        n_sample_points=100,
-        rate=1e-1,
-        tol=1e-8,
-        known_values=None,
-        max_evaluations=None,
-        verbose=False):
+        f: Callable[[np.ndarray], float],
+        x0: np.ndarray,
+        sample_radius: float=1e-1,
+        n_sample_points: int=100,
+        rate: float=1e-1,
+        tol: float=1e-8,
+        known_values: Tuple[List[np.ndarray], List[np.ndarray]]=None,
+        max_evaluations: int=None,
+        verbose: bool=False):
+    """Model gradient descent algorithm for black-box optimization.
+
+    The idea of this algorithm is to perform gradient descent, but estimate
+    the gradient using a surrogate model instead of, say, by
+    finite-differencing. The surrogate model is a least-squared quadratic
+    fit to points sampled from the vicinity of the current iterate.
+    This algorithm works well when you have an initial guess which is in the
+    convex neighborhood of a local optimum and you want to converge to that
+    local optimum. It's meant to be used when the function is stochastic.
+
+    Args:
+        f: The function to minimize.
+        x0: An initial guess.
+        sample_radius: The radius around the current iterate to sample
+            points from to build the quadratic model.
+        n_sample_points: The number of points to sample in each iteration.
+        rate: The learning rate for the gradient descent.
+        tol: The algorithm terminates when the difference between the current
+            iterate and the next suggested iterate is smaller than this value.
+        known_values: Any prior known values of the objective function.
+            This is given as a tuple where the first element is a list
+            of points and the second element is a list of the function values
+            at those points.
+        max_evaluations: The maximum number of function evaluations to allow
+            before termination.
+        verbose: Whether to print some information as the optimization proceeds.
+    """
 
     if known_values is not None:
         known_xs, known_ys = known_values
