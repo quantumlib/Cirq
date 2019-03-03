612efc353799680e197494132c21f8ad97849f2c
David Cox <davemcox@gmail.com>
2019-03-03 09:05:38 -0500

    Updated to address Travis CI checks
    Cleaned up some of the introductory commentary to address an issue with non-ASCII characters and moved the in-line commentary.

--- examples/superdense_coding.py
+++ examples/superdense_coding.py
@@ -11,15 +11,15 @@ https://en.m.wikipedia.org/wiki/Superdense_coding
 
 === EXAMPLE OUTPUT ===
 Circuit:
-0: ───────────────M───────@───────────────────
-                          │
-1: ───X───────────M───@───┼───────────────────
-                      │   │
-2: ───────H───@───────X───@───×───────────────
-              │               │
-3: ───────────┼───────────────×───@───H───M───
-              │                   │
-4: ───────────X───────────────────X───────M───
+0: ---------------M-------@-------------------
+                          |
+1: ---X-----------M---@---|-------------------
+                      |   |
+2: -------H---@-------X---@---x---------------
+              |               |
+3: -----------|---------------x---@---H---M---
+              |                   |
+4: -----------X-------------------X-------M---
 
 Results:
 0=0
@@ -36,15 +36,24 @@ def make_superdense_circuit():
     circuit = cirq.Circuit()
     (q0, q1, q2, q3, q4) = cirq.LineQubit.range(5)
 
-    circuit.append([cirq.X(q1)])                             # Sets q1 to 1 (and leaves q0 at 0)
-    circuit.append([cirq.H(q2), cirq.CNOT(q2, q4)])          # Creates Bell State to be shared on q2 and q4
-    circuit.append([cirq.measure(q0), cirq.measure(q1)])     # Measures q0 and q1, both of which will be sent to the receiver
-    circuit.append([cirq.CNOT(q1, q2)])                      # Step 1 of encoding (controlled NOT gate on q1 / q2)
-    circuit.append([cirq.CZ(q0, q2)])                        # Step 2 of encoding (controlled Z gate on q0 / q2)
-    circuit.append([cirq.SWAP(q2, q3)])                      # Sends encoded information to receiver
-    circuit.append([cirq.CNOT(q3, q4)])                      # Step 1 of decoding (controlled NOT gate on q3 and q4)
-    circuit.append([cirq.H(q3)])                             # Step 2 of decoding (Hadamard gate on q3)
-    circuit.append([cirq.measure(q3), cirq.measure(q4)])     # Measurement by receiver to decode bits
+    # Sets q1 to 1 (and leaves q0 at 0)
+    circuit.append([cirq.X(q1)])
+    # Creates Bell State to be shared on q2 and q4
+    circuit.append([cirq.H(q2), cirq.CNOT(q2, q4)])
+    # Measures q0 and q1, both of which will be sent to the receiver
+    circuit.append([cirq.measure(q0), cirq.measure(q1)])
+    # Step 1 of encoding (controlled NOT gate on q1 / q2)
+    circuit.append([cirq.CNOT(q1, q2)])
+    # Step 2 of encoding (controlled Z gate on q0 / q2)
+    circuit.append([cirq.CZ(q0, q2)])
+    # Sends encoded information to receiver
+    circuit.append([cirq.SWAP(q2, q3)])
+    # Step 1 of decoding (controlled NOT gate on q3 and q4)
+    circuit.append([cirq.CNOT(q3, q4)])
+    # Step 2 of decoding (Hadamard gate on q3)
+    circuit.append([cirq.H(q3)])
+    # Measurement by receiver to decode bits
+    circuit.append([cirq.measure(q3), cirq.measure(q4)])
 
     return circuit
 
