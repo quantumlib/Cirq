c778d0ab9abe2ec5da98bff44a75d9cdafcabf85
David Cox <davemcox@gmail.com>
2019-03-06 18:43:35 -0500

    Changed randomness on input bits and output format
    The two input bits are now randomized by using a Hadamard Measure gate. The results were also revised to make it more clear what the input and output bits are. The description text was also adjusted accordingly.

--- examples/superdense_coding.py
+++ examples/superdense_coding.py
@@ -5,56 +5,50 @@ pre-sharing an entangled state between the sender and the receiver. This
 entangled state allows the receiver of the one qubit of information to
 decode the two classical bits that were originally encoded by the sender.
 
-In the following example output, a sender sets qubit 0 (i.e., q0) to 0 and
-qubit 1 (i.e., q1) to 1. By sharing one qubit of information (i.e., q2),
-the receiver is able to decode the original 0 and 1 in qubits 3 and 4,
-respectively, when qubits 3 and 4 are measured. This is only possible given
-that an entangled state is pre-shared between the sender and receiver.
+In the following example output, qubit 0 and qubit 1 are randomly set to
+either 0 or 1 by using Hadamard and Measure gates. By sending one qubit of
+information between qubit 2 and qubit 3, the receiver is able to decode the
+originally encoded information when qubits 3 and 4 are measured. This is
+only possible given that an entangled state is pre-shared between the
+sender and receiver.
 
-In the actual program below, qubit 0 and qubit 1 are randomly set to
-either 1 or 0.
+The two input bits and the two output bits returned by this circuit should
+be identical.
 
 === REFERENCE ===
 https://en.m.wikipedia.org/wiki/Superdense_coding
 
 === EXAMPLE OUTPUT ===
 Circuit:
-0: ---------------M-------@-------------------
-                          |
-1: ---X-----------M---@---|-------------------
-                      |   |
-2: -------H---@-------X---@---x---------------
-              |               |
-3: -----------|---------------x---@---H---M---
-              |                   |
-4: -----------X-------------------X-------M---
+0: ---H---M('input')---------------@-----------------------------
+          |                        |
+1: ---H---M--------------------@---|-----------------------------
+                               |   |
+2: --------------------H---@---X---@---x-------------------------
+                           |           |
+3: ------------------------|-----------x---@---H---M('output')---
+                           |               |       |
+4: ------------------------X---------------X-------M-------------
 
 Results:
-0=0
-1=1
-3=0
-4=1
+input=0, 1
+output=0, 1
 
 """
 
-import random
 import cirq
 
 
 def make_superdense_circuit():
     circuit = cirq.Circuit()
-    (q0, q1, q2, q3, q4) = cirq.LineQubit.range(5)
+    q0, q1, q2, q3, q4 = cirq.LineQubit.range(5)
 
     # Randomly sets q0 and q1 to either 0 or 1
-    if random.randint(0, 1):
-        circuit.append([cirq.X(q0)]) # coverage: ignore
-    if random.randint(0, 1):
-        circuit.append([cirq.X(q1)]) # coverage: ignore
+    circuit.append([cirq.H(q0), cirq.H(q1)])
+    circuit.append(cirq.measure(q0, q1, key="input"))
 
     # Creates Bell State to be shared on q2 and q4
     circuit.append([cirq.H(q2), cirq.CNOT(q2, q4)])
-    # Measures q0 and q1, both of which will be sent to the receiver
-    circuit.append([cirq.measure(q0), cirq.measure(q1)])
     # Step 1 of encoding (controlled NOT gate on q1 / q2)
     circuit.append([cirq.CNOT(q1, q2)])
     # Step 2 of encoding (controlled Z gate on q0 / q2)
@@ -66,7 +60,7 @@ def make_superdense_circuit():
     # Step 2 of decoding (Hadamard gate on q3)
     circuit.append([cirq.H(q3)])
     # Measurement by receiver to decode bits
-    circuit.append([cirq.measure(q3), cirq.measure(q4)])
+    circuit.append(cirq.measure(q3, q4, key="output"))
 
     return circuit
 
