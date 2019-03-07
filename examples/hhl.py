Fixed eigenspace projector calculation; added more test cases to help with debugging

examples/hhl.py
@@ -120,7 +120,7 @@ class HamiltonianSimulation(cirq.EigenGate, cirq.SingleQubitGate):
         self.eigen_components = []
         for w, v in zip(ws, vs.T):
             theta = w*t / math.pi
-            P = np.outer(v, v)
+            P = np.outer(v, np.conj(v))
             self.eigen_components.append((theta, P))
 
     def _with_exponent(self, exponent):
@@ -317,7 +317,7 @@ def main():
 
     testcases = [
         {
-            # Eigendecomposition: [(2, |+>), (1, |->)
+            # Eigendecomposition: [(2, |+>), (1, |->)]
             # |b> = |0>
             # |x> = (0.948683, -0.316228)
             'A': np.array([[1.5, 0.5], [0.5, 1.5]]),
@@ -327,7 +327,7 @@ def main():
             'expected': (-0.6, 0.0, 0.8),
         },
         {
-            # Eigendecomposition: [(4, |+>), (1, |->)
+            # Eigendecomposition: [(4, |+>), (1, |->)]
             # |b> = |0>
             # |x> = (0.857493, -0.514496)
             'A': np.array([[2.5, 1.5], [1.5, 2.5]]),
@@ -337,7 +337,7 @@ def main():
             'expected': (-0.882353, 0.0, 0.470588),
         },
         {
-            # Eigendecomposition: [(8, |+>), (1, |->)
+            # Eigendecomposition: [(8, |+>), (1, |->)]
             # |b> = |+>
             # |x> = (0.707107, 0.707107)
             'A': np.array([[4.5, 3.5], [3.5, 4.5]]),
@@ -347,7 +347,7 @@ def main():
             'expected': (1.0, 0.0, 0.0),
         },
         {
-            # Eigendecomposition: [(3, |+>), (1, |->)
+            # Eigendecomposition: [(3, |+>), (1, |->)]
             # |b> = |0>
             # |x> = (2/sqrt(5), -1/sqrt(5))
             'A': np.array([[2, 1], [1, 2]]),
@@ -357,7 +357,7 @@ def main():
             'expected': (-0.8, 0.0, 0.6),
         },
         {
-            # Eigendecomposition: [(4, |+>), (2, |->)
+            # Eigendecomposition: [(4, |+>), (2, |->)]
             # |b> = |0>
             # |x> = (3/sqrt(10), -1/sqrt(10))
             'A': np.array([[3, 1], [1, 3]]),
@@ -367,7 +367,7 @@ def main():
             'expected': (-0.6, 0.0, 0.8),
         },
         {
-            # Eigendecomposition: [(0.5, |+>), (0.25, |->)
+            # Eigendecomposition: [(0.5, |+>), (0.25, |->)]
             # |b> = |0>
             # |x> = (0.948683, -0.316228)
             'A': np.array([[0.375, 0.125], [0.125, 0.375]]),
@@ -376,22 +376,68 @@ def main():
             'input_prep_gates': [],
             'expected': (-0.6, 0.0, 0.8),
         },
-        # TODO verify math for test case below.
-        # {
-        #     # Eigendecomposition:
-        #     #   (4.537, [0.97149-0.0116j, 0.05508-0.23032j])
-        #     #   (0.349, [0.05508-0.23032j, 0.86132+0.44949j])
-        #     # |b> = (0.64510-0.47848j, 0.35490-0.47848j)
-        #     # |x> = (-0.0662741-0.214548j, 0.784392-0.578192j)
-        #     'A': np.array([[4.30217-6.93889e-18j,
-        #                     0.235321+0.934396j],
-        #                    [0.235321-0.934396j,
-        #                     0.583866-6.938893903907228e-18j]]),
-        #     't': 0.358166*math.pi,
-        #     'register_size': 4,
-        #     'input_prep_gates': [cirq.Rx(1.276359), cirq.Rz(1.276359)],
-        #     'expected': (0.144130, -0.413218, -0.899154),
-        # },
+        {
+            # Eigendecomposition: [(2, |0>), (1, |1>)]
+            # |b> = |+>
+            # |x> = [0.4472136, 0.89442719]
+            'A': np.array([[2, 0], [0, 1]]),
+            't': math.pi,
+            'register_size': 1,
+            'input_prep_gates': [cirq.H],
+            'expected': (0.8, 0.0, -0.6),
+        },
+        {
+            # Eigendecomposition: [(2, |+>), (1, |->)]
+            # |b> = [0.5-0.5j, 0.5+0.5j]
+            # |x> = [0.31622777-0.63245553j 0.31622777+0.63245553j]
+            'A': np.array([[1.5, 0.5], [0.5, 1.5]]),
+            't': math.pi,
+            'register_size': 1,
+            'input_prep_gates': [cirq.X**-0.5],
+            'expected': (-0.6, 0.8, 0.0),
+        },
+        {
+            # Eigendecomposition:
+            #   (2, [1/sqrt(2), 1j/sqrt(2)])
+            #   (1, [1/sqrt(2), -1j/sqrt(2)])
+            # |b> = |0>
+            # |x> = [0.9486833, -0.31622777j]
+            'A': np.array([[1.5, 0.5j], [-0.5j, 1.5]]),
+            't': math.pi,
+            'register_size': 1,
+            'input_prep_gates': [],
+            'expected': (0.0, -0.6, 0.8),
+        },
+        {
+            # Eigendecomposition:
+            #   (2, [-0.971555, -0.0578339+0.229643j])
+            #   (1, [-0.236813, 0.237270-0.942137j])
+            # |b> = |0>
+            # |x> = (0.97708125+1.32901563e-08j, -0.05198566+2.06421005e-01j)
+            'A': np.array([[1.94391945-1.43646965e-08j,
+                            0.05618864+2.23110352e-01j],
+                           [0.05618883-2.23110884e-01j,
+                            1.05608055+1.43646965e-08j]]),
+            't': math.pi,
+            'register_size': 1,
+            'input_prep_gates': [],
+            'expected': (-0.101588, 0.403380, 0.909376)
+        },
+        {
+            # Eigendecomposition:
+            #   (4.537, [-0.971555, -0.0578339+0.229643j])
+            #   (0.349, [-0.236813, 0.237270-0.942137j])
+            # |b> = (0.64510-0.47848j, 0.35490-0.47848j)
+            # |x> = (-0.0662724-0.214548j, 0.784392-0.578192j)
+            'A': np.array([[4.30213466-6.01593490e-08j,
+                            0.23531802+9.34386156e-01j],
+                           [0.23531882-9.34388383e-01j,
+                            0.58386534+6.01593489e-08j]]),
+            't': 0.358166*math.pi,
+            'register_size': 4,
+            'input_prep_gates': [cirq.Rx(1.276359), cirq.Rz(1.276359)],
+            'expected': (0.144130, 0.413217, -0.899154),
+        },
     ]
 
     for t in testcases:
@@ -408,6 +454,7 @@ def main():
         print("Actual: ")
         simulate(hhl_circuit(t['A'], t['C'], t['t'], t['register_size'],
                              *t['input_prep_gates']))
+
         print()
 
 