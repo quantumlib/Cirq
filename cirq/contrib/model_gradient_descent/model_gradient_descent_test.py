615a615bdca6bc11506376a8590561947ea122b2
Kevin J. Sung <kevinjefferysung@gmail.com>
2019-03-06 19:28:47 -0500

    cov

--- cirq/contrib/model_gradient_descent/model_gradient_descent_test.py
+++ cirq/contrib/model_gradient_descent/model_gradient_descent_test.py
@@ -34,8 +34,49 @@ def test_model_gradient_descent():
         tol=1e-8,
         known_values=None,
         max_evaluations=None,
-        verbose=False)
+        verbose=True)
 
     assert np.allclose(result.x, np.zeros(len(result.x)), atol=1e-7)
     assert np.allclose(result.fun, 0)
     assert isinstance(result.nfev, int)
+
+
+def test_model_gradient_descent_with_known_values():
+    x0 = np.random.randn(10)
+    sample_radius = 1e-1
+    rate = 1e-1
+    known_xs = [np.ones(10)]
+    known_ys = [10.0]
+    result = model_gradient_descent(
+        sum_of_squares,
+        x0,
+        sample_radius=sample_radius,
+        n_sample_points=10,
+        rate=rate,
+        tol=1e-8,
+        known_values=(known_xs, known_ys),
+        max_evaluations=None,
+        verbose=True)
+
+    assert len(known_xs) == 1
+    assert len(known_ys) == 1
+
+
+def test_model_gradient_descent_limited_evaluations():
+    x0 = np.random.randn(10)
+    sample_radius = 1e-1
+    rate = 1e-1
+    result = model_gradient_descent(
+        sum_of_squares,
+        x0,
+        sample_radius=sample_radius,
+        n_sample_points=10,
+        rate=rate,
+        tol=1e-8,
+        known_values=None,
+        max_evaluations=20,
+        verbose=True)
+
+    assert isinstance(result.x, np.ndarray)
+    assert isinstance(result.fun, float)
+    assert result.nfev == 20
