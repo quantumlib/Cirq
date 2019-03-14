bcb3ae22f2e6ef06bd560820f576aa5d0737c5b8
balopat <balintp@google.com>
2019-03-13 20:05:08 -0700

    linting

--- cirq/sim/sparse_simulator_test.py
+++ cirq/sim/sparse_simulator_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Union, Iterator
+from typing import Union
 
 import pytest
 import numpy as np
