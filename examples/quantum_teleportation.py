835245bdae2113ef44b6e190944265e329a16f96
David Cox <davemcox@gmail.com>
2019-03-20 14:08:56 -0400

    Update quantum_teleportation.py for comments
    Updated the circuit slightly, changed some nomenclature for key variables, changed the manner in which the message is generated and initially read, and revised the descriptive commentary accordingly.

--- examples/quantum_teleportation.py
+++ examples/quantum_teleportation.py
@@ -1,15 +1,17 @@
 """Quantum Teleportation.
 Quantum Teleportation is a process by which a quantum state can be transmitted
 by sending only two classical bits of information. This is accomplished by
-pre-sharing an entangled state between the sender and the receiver. This
-entangled state allows the receiver of the two classical bits of information
-to possess a qubit with the same state as the one held by the sender.
+pre-sharing an entangled state between the sender (Alice) and the receiver
+(Bob). This entangled state allows the receiver (Bob) of the two classical
+bits of information to possess a qubit with the same state as the one held by
+the sender (Alice).
 
-In the following example output, qubit 0 is set to a random state by applying
-X and Y gates. By sending two classical bits of information after qubit 0 and
-qubit 1 are measured, the final state of qubit 2 will be identical to the
-original random state of qubit 0. This is only possible given that an
-entangled state is pre-shared between the sender and receiver.
+In the following example output, qubit 0 (the Message) is set to a random state
+by applying X and Y gates. By sending two classical bits of information after
+qubit 0 (the Message) and qubit 1 (Alice's entangled qubit) are measured, the
+final state of qubit 2 (Bob's entangled qubit) will be identical to the
+original random state of qubit 0 (the Message). This is only possible given
+that an entangled state is pre-shared between Alice and Bob.
 
 === REFERENCE ===
 https://en.wikipedia.org/wiki/Quantum_teleportation
@@ -17,13 +19,13 @@ https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.70.1895
 
 === EXAMPLE OUTPUT ===
 Circuit:
-0: ---X^0.25---Y^0.125-----------@---H---M-------@---
-                                 |               |
-1: ----------------------H---@---X-------M---@---|---
-                             |               |   |
-2: --------------------------X---------------X---@---
+0: -----------X^0.25---Y^0.125---@---H---M-------@---
+                                 |       |       |
+1: ---H---@----------------------X-------M---@---|---
+          |                                  |   |
+2: -------X----------------------------------X---@---
 
-Bloch Sphere of Qubit 0 After Random X and Y Gates:
+Bloch Sphere of Message After Random X and Y Gates:
 x:  0.2706 y:  -0.7071 z:  0.6533
 
 Bloch Sphere of Qubit 2 at Final State:
@@ -36,41 +38,43 @@ import numpy as np
 import cirq
 
 
-def make_quantum_teleportation_circuit():
+def make_quantum_teleportation_circuit(ranX, ranY):
     circuit = cirq.Circuit()
-    q0, q1, q2 = cirq.LineQubit.range(3)
+    msg, alice, bob = cirq.LineQubit.range(3)
 
-    # Creates a random state for q0
-    circuit.append([cirq.X(q0)**random.random(), cirq.Y(q0)**random.random()])
-    # Creates Bell State to be shared on q1 and q2
-    circuit.append([cirq.H(q1), cirq.CNOT(q1, q2)])
-    # Bell measurement of q0 (qubit to be teleported) and q1 (entangled qubit)
-    circuit.append([cirq.CNOT(q0, q1), cirq.H(q0)])
-    circuit.append([cirq.measure(q0), cirq.measure(q1)])
-    # Uses q0 and q1 to perform operation on q2 to recover the state of q0
-    circuit.append([cirq.CNOT(q1, q2), cirq.CZ(q0, q2)])
+    # Creates Bell state to be shared between Alice and Bob
+    circuit.append([cirq.H(alice), cirq.CNOT(alice, bob)])
+    # Creates a random state for the Message
+    circuit.append([cirq.X(msg)**ranX, cirq.Y(msg)**ranY])
+    # Bell measurement of the Message and Alice's entangled qubit
+    circuit.append([cirq.CNOT(msg, alice), cirq.H(msg)])
+    circuit.append(cirq.measure(msg, alice))
+    # Uses the two classical bits from the Bell measurement to recover the
+    # original quantum Message on Bob's entangled qubit
+    circuit.append([cirq.CNOT(alice, bob), cirq.CZ(msg, bob)])
 
     return circuit
 
 
 def main():
     state = []
-    circuit = make_quantum_teleportation_circuit()
+    ranX = random.random()
+    ranY = random.random()
+    circuit = make_quantum_teleportation_circuit(ranX, ranY)
 
     print("Circuit:")
     print(circuit)
 
     sim = cirq.Simulator()
 
-    # Records in a list each state of q0 for the simulation
-    step_results = sim.simulate_moment_steps(circuit)
-    for step in step_results:
-        state.append \
-            (cirq.bloch_vector_from_state_vector(step.state_vector(), 0))
+    # Produces the message using random X and Y gates
+    message = sim.simulate(cirq.Circuit.from_ops(
+        [cirq.X(0)**ranX, cirq.Y(0)**ranY]))
 
-    print("\nBloch Sphere of Qubit 0 After Random X and Y Gates:")
-    # Prints the Bloch Sphere of q0 after the X and Y gates
-    b0X, b0Y, b0Z = state[1]
+    print("\nBloch Sphere of Message After Random X and Y Gates:")
+    # Prints the Bloch Sphere of the Message after the X and Y gates
+    b0X, b0Y, b0Z = cirq.bloch_vector_from_state_vector(
+        message.final_state, 0)
     print("x: ", np.around(b0X, 4),
           "y: ", np.around(b0Y, 4),
           "z: ", np.around(b0Z, 4))
@@ -79,9 +83,9 @@ def main():
     final_results = sim.simulate(circuit)
 
     print("\nBloch Sphere of Qubit 2 at Final State:")
-    # Prints the Bloch Sphere of q2 at the final state
-    b2X, b2Y, b2Z = \
-          cirq.bloch_vector_from_state_vector(final_results.final_state, 2)
+    # Prints the Bloch Sphere of Bob's entangled qubit at the final state
+    b2X, b2Y, b2Z = cirq.bloch_vector_from_state_vector(
+        final_results.final_state, 2)
     print("x: ", np.around(b2X, 4),
           "y: ", np.around(b2Y, 4),
           "z: ", np.around(b2Z, 4))
