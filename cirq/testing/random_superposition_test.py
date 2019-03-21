3db779c35886f87ba6f235b2f6e3ea3032adc00f
Dave Bacon <dabacon@gmail.com>
2019-03-21 16:21:17 -0700

    Add random_superposition to cirq.testing (#1456)
    * Also fix some lint

--- cirq/testing/random_superposition_test.py
+++ cirq/testing/random_superposition_test.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pytest
 
-from cirq.testing.random_superposition import random_superposition
+import cirq.testing
 
 @pytest.mark.parametrize('dim',  range(1, 10))
 def test_random_superposition(dim):
-    state = random_superposition(dim)
+    state = cirq.testing.random_superposition(dim)
 
     assert dim == len(state)
     assert np.isclose(np.linalg.norm(state), 1.0)
