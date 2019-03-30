c999687bcc9dd4add4fcce906541e8febc49a281
balopat <balintp@google.com>
2019-03-30 12:16:56 -0700

    fix experiment

--- cirq/ops/common_gates.py
+++ cirq/ops/common_gates.py
@@ -545,10 +545,6 @@ def measure_each(*qubits: raw_types.Qid,
     return [MeasurementGate(1, key_func(q)).on(q) for q in qubits]
 
 
-def FourQubit():
-    return class
-
-
 @value.value_equality
 class IdentityGate(raw_types.Gate):
     """A Gate that perform no operation on qubits.
