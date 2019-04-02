adf5bc47313457208c5d3d6f415a86c28f4ed510
Muyuan Li <mlihc15@gmail.com>
2019-04-02 16:15:19 -0400

    Add cirq.IonDevice (#1420)
    adding cirq.ion.ion_device.py, cirq.ion.ion_device_test.py: create an ion trap device with functionalities such as device validation, circuit (gate, operation, schedule) validation, decompose operation/circuit into ion trap native operations.
    
    adding cirq.ion.convert_to_ion_gates.py, cirq.ion.convert_to_ion_gates_test.py: convert operations/circuits of known gates or with knows matrix into operations/circuits compiled with ion trap native operations.

--- cirq/__init__.py
+++ cirq/__init__.py
@@ -307,6 +307,9 @@ from cirq.protocols import (
 
 from cirq.ion import (
     MS,
+    two_qubit_matrix_to_ion_operations,
+    IonDevice,
+    ConvertToIonGates,
 )
 # pylint: enable=redefined-builtin
 
