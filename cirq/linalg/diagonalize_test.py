9384c65694b58e699816bcbae408b7497f714a86
Dave Bacon <dabacon@gmail.com>
2019-04-05 09:01:26 -0700

    Avoid warning about log10(0)

--- cirq/linalg/diagonalize_test.py
+++ cirq/linalg/diagonalize_test.py
@@ -68,12 +68,12 @@ def random_bi_diagonalizable_pair(
     b = cirq.dot(u, z, v)
     return a, b
 
+
 def _get_assert_diagonalized_by_str(m, p, d):
     return 'm.round(3) : {}, p.round(3) : {}, ' \
-        'np.log10(np.abs(p.T @ m @ p)).round(2): {}'.format(np.round(m, 3),
-                                                            np.round(p, 3),
-                                                            np.log10(np.abs(d))\
-                                                            .round(2))
+           'np.abs(p.T @ m @ p).round(2): {}'.format(np.round(m, 3),
+                                                     np.round(p, 3),
+                                                     np.abs(d).round(2))
 
 def assert_diagonalized_by(m, p, atol: float = 1e-8):
     d = p.T.dot(m).dot(p)
@@ -81,13 +81,13 @@ def assert_diagonalized_by(m, p, atol: float = 1e-8):
     assert cirq.is_orthogonal(p) and cirq.is_diagonal(d, atol=atol), \
         _get_assert_diagonalized_by_str(m, p, d)
 
+
 def _get_assert_bidiagonalized_by_str(m, p, q, d):
     return 'm.round(3) : {}, p.round(3) : {}, q.round(3): {}, ' \
-        'np.log10(np.abs(p.T @ m @ p)).round(2): {}'.format(np.round(m, 3),
-                                                            np.round(p, 3),
-                                                            np.round(q, 3),
-                                                            np.log10(np.abs(d) \
-                                                                     .round(2)))
+           'np.abs(p.T @ m @ p).round(2): {}'.format(np.round(m, 3),
+                                                     np.round(p, 3),
+                                                     np.round(q, 3),
+                                                     np.abs(d).round(2))
 
 def assert_bidiagonalized_by(m, p, q, rtol: float = 1e-5,
                              atol: float = 1e-8):
