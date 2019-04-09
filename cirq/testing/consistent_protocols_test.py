6ad4572469652165b8e20a354baf71ef675e282b
Smit <smitsanghavi22@gmail.com>
2019-04-08 23:02:32 -0700

    Remove all other actually unused imports. Remaining pylint warnings are actually used

--- cirq/testing/consistent_protocols_test.py
+++ cirq/testing/consistent_protocols_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, Sequence, Union
+from typing import Sequence, Union
 
 import pytest
 
