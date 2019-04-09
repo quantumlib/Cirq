3df64265ebb715ae0c42b422a3d2307366809d6a
Smit <smitsanghavi22@gmail.com>
2019-04-08 22:35:19 -0700

    Fix pylint warnings in cirq/protocols dir

--- cirq/protocols/resolve_parameters.py
+++ cirq/protocols/resolve_parameters.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, TypeVar, TYPE_CHECKING, Union, Dict
+from typing import Any, TypeVar, TYPE_CHECKING
 from typing_extensions import Protocol
 
 import sympy
