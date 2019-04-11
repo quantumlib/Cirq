a08170b89dd7fc1aaa2b336b4c61356a77d4f4d6
Craig Gidney <Craig.Gidney@gmail.com>
2019-04-11 14:36:28 -0700

    Add cirq.sample and cirq.sample_sweep utility methods to mux over simulators (#1518)
    I went with "sample" instead of "run" because this is how the user indicates what kind of result they want. They want samples.

--- cirq/__init__.py
+++ cirq/__init__.py
@@ -211,6 +211,8 @@ from cirq.sim import (
     dirac_notation,
     measure_state_vector,
     measure_density_matrix,
+    sample,
+    sample_sweep,
     sample_density_matrix,
     sample_state_vector,
     SimulatesFinalState,
