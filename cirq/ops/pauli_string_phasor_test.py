afab404b6cbd921984a08879ae09982505c3b390
Craig Gidney <craiggidney@google.com>
2019-04-12 14:06:52 -0700

    format

--- cirq/ops/pauli_string_phasor_test.py
+++ cirq/ops/pauli_string_phasor_test.py
@@ -29,16 +29,14 @@ def test_init():
     with pytest.raises(ValueError, match='eigenvalues'):
         _ = cirq.PauliStringPhasor(1j * cirq.X(a))
 
-    v1 = cirq.PauliStringPhasor(-cirq.X(a),
-                                exponent_neg=0.25,
-                                exponent_pos=-0.5)
+    v1 = cirq.PauliStringPhasor(
+        -cirq.X(a), exponent_neg=0.25, exponent_pos=-0.5)
     assert v1.pauli_string == cirq.X(a)
     assert v1.exponent_neg == -0.5
     assert v1.exponent_pos == 0.25
 
-    v2 = cirq.PauliStringPhasor(cirq.X(a),
-                                exponent_neg=0.75,
-                                exponent_pos=-0.125)
+    v2 = cirq.PauliStringPhasor(
+        cirq.X(a), exponent_neg=0.75, exponent_pos=-0.125)
     assert v2.pauli_string == cirq.X(a)
     assert v2.exponent_neg == 0.75
     assert v2.exponent_pos == -0.125
@@ -477,6 +475,8 @@ def test_str():
         }, -1))**-0.5
     assert str(ps) == '(X(q0)*Y(q1)*Z(q2))**0.5'
 
-    assert str(np.exp(1j * np.pi * cirq.X(q0) * cirq.Y(q1))) == 'exp(iπ*X(q0)*Y(q1))'
-    assert str(np.exp(-0.5j * np.pi * cirq.X(q0) * cirq.Y(q1))) == 'exp(-iπ0.5*X(q0)*Y(q1))'
+    assert str(
+        np.exp(1j * np.pi * cirq.X(q0) * cirq.Y(q1))) == 'exp(iπ*X(q0)*Y(q1))'
+    assert str(np.exp(-0.5j * np.pi * cirq.X(q0) * cirq.Y(
+        q1))) == 'exp(-iπ0.5*X(q0)*Y(q1))'
     assert str(np.exp(1j * np.pi * cirq.PauliString())) == 'exp(iπ*I)'
