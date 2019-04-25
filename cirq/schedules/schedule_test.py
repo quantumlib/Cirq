3ca96f3f1fca44cd0fdf48265f7ae21e2e79530e
Timothy Man <hontin.man@gmail.com>
2019-04-25 07:33:44 +0200

    Changes to pass CI checks.

--- cirq/schedules/schedule_test.py
+++ cirq/schedules/schedule_test.py
@@ -178,6 +178,8 @@ def test_query_overlapping_operations_exclusive():
     assert schedule.query(time=zero + 3.5 * ps, duration=ps) == []
 
 
+# In python 2, timedelta doesn't support multiplication with floats
+@cirq.testing.only_test_in_python3
 def test_query_timedelta():
     q = cirq.NamedQubit('q')
     zero = cirq.Timestamp(picos=0)
