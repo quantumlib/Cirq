4a5ed2ea09a25a0060e6cf7ca686e3d2b6637ee1
Craig Gidney <craiggidney@google.com>
2019-04-30 19:29:29 -0700

    Add consistency test of PauliStringPhasor - Fix bad repr when parameterized

--- cirq/ops/common_gates.py
+++ cirq/ops/common_gates.py
@@ -745,7 +745,7 @@ class CZPowGate(eigen_gate.EigenGate,
             return NotImplemented
 
         c = 1j**(2 * self._exponent)
-        one_one = linalg.slice_for_qubits_equal_to(args.axes, 0b11)
+        one_one = args.subspace_index(0b11)
         args.target_tensor[one_one] *= c
         p = 1j**(2 * self._exponent * self._global_shift)
         if p != 1:
