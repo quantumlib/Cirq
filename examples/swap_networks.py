149a69c85df67518b0ac5bdbb5693f8a821608e8
Bryan A. O'Gorman <bryan.a.ogorman@gmail.com>
2019-05-13 09:55:52 -0700

    added explanation and types

--- examples/swap_networks.py
+++ examples/swap_networks.py
@@ -1,10 +1,28 @@
-"""Demonstrates swap networks."""
+"""Demonstrates swap networks.
+
+Swap networks are used to get around limited connectivity in a hardware device. In this example, we'll give a concrete application by using the acquaintance submodule to implement a single-round QAOA for Maximum Cut on a linearly connected device even with an arbitrary problem graph.
+
+Specifically, we'd like to implement the circuit U_mix(β) * U_phase(𝛾), where 
+
+    U_mix(β) = X**β ⊗ X**β ⊗ ⋯ ⊗ X**β,
+    U_phase(𝛾) = ZZ_{e_m}**𝛾 ⋯ ZZ_{e_2}**𝛾· ZZ_{e_1}**𝛾, and
+    {e_1, e_2, …, e_m} are the edges of the graph whose maximum cut we would like to find.
+
+The details of QAOA are irrelevant; for our purposes here, it is sufficient to say that we want to apply a 2-qubit gate to pairs of qubits, even when the implied graph is not a subgraph of the hardware adjacency graph.
+"""
 
 import itertools
 import random
 
 import cirq
 import cirq.contrib.acquaintance as cca
+
+
+LogicalIndex = TypeVar('LogicalIndex', int, ops.Qid)
+LogicalIndexSequence = Union[Sequence[int], Sequence[ops.Qid]]
+LogicalGates = Dict[Tuple[LogicalIndex, ...], ops.Gate]
+LogicalMappingKey = TypeVar('LogicalMappingKey', bound=ops.Qid)
+LogicalMapping = Dict[LogicalMappingKey, LogicalIndex]
 
 
 def get_random_graph(n_vertices, edge_prob=0.5):
@@ -14,12 +32,16 @@ def get_random_graph(n_vertices, edge_prob=0.5):
     ]
 
 
-def get_max_cut_qaoa_circuit(n_vertices, edges, beta, gamma):
+def get_max_cut_qaoa_circuit(
+        vertices: LogicalIndexSequence,
+        edges: Sequence[LogicalIndexSequence],
+        beta: float,
+        gamma: float):
     """Implement a single round of QAOA for MaxCut using linearly connected
     qubits.
 
     Args:
-        n_vertices: The number of vertices.
+        vertices: The vertices of the graph.
         edges: The list of edges.
         beta: The mixing angle.
         gamma: The phase separation angle.
@@ -27,10 +49,18 @@ def get_max_cut_qaoa_circuit(n_vertices, edges, beta, gamma):
     Returns:
         A circuit implementing a single round of QAOA with the specified
         angles.
+
+
+    Note that here we index the logical gates by tuples of integers, but we
+    could also index them by logical qubits. All that matters is that the keys
+    of the gates dict correspond to the values of the initial_mapping dict.
     """
 
+    assert all(set(edge) <= set(vertices) for edge in edges)
+    assert all(len(edge) == 2 for edge in edges)
+
     # G_{i,j} ∝ exp(i gamma (|01><01| + |10><10|))
-    phase_sep_gates = {edge: cirq.ZZ**gamma for edge in edges}
+    phase_sep_gates = {edge: cirq.ZZ**gamma for edge in edges} # type: LogicalMapping
 
     qubits = cirq.LineQubit.range(n_vertices)
 
@@ -72,9 +102,10 @@ def get_max_cut_qaoa_circuit(n_vertices, edges, beta, gamma):
 
 def main():
     n_vertices = 6
+    vertices = range(n_vertices)
     edges = get_random_graph(n_vertices)
     beta, gamma = 0.1, 0.3
-    circuit = get_max_cut_qaoa_circuit(n_vertices, edges, beta, gamma)
+    circuit = get_max_cut_qaoa_circuit(vertices, edges, beta, gamma)
     print('1-round QAOA circuit:')
     print(circuit)
 
