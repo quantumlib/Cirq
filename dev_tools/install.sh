b54d122347f408a9a577c95bc63ed67690f7cade
vtomole <vtomole@iastate.edu>
2019-05-17 13:31:55 -0500

    ls dev_tools

--- dev_tools/install.sh
+++ dev_tools/install.sh
@@ -11,8 +11,7 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     export PYENV_VERSION=$PYTHON
     export PATH="/Users/travis/.pyenv/shims:${PATH}"
     pyenv virtualenv venv
-    ls
-    ls ../
-    source ../venv/bin/activate
+    ls dev_tools/
+    source dev_tools/venv/bin/activate
     python --version
     fi
