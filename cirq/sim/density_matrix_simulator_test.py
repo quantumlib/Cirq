5f49f39bb05eee222fa382fe5c7f7a050de9f7b3
Craig Gidney <craiggidney@google.com>
2019-05-17 19:17:15 -0700

    Implement PauliString._decompose_ (#1656)
    Fixes https://github.com/quantumlib/Cirq/issues/1631

--- cirq/sim/density_matrix_simulator_test.py
+++ cirq/sim/density_matrix_simulator_test.py
@@ -644,3 +644,13 @@ def test_works_on_pauli_string_phasor():
     np.testing.assert_allclose(result.reshape(4, 4),
                                np.diag([0, 0, 0, 1]),
                                atol=1e-8)
+
+
+def test_works_on_pauli_string():
+    a, b = cirq.LineQubit.range(2)
+    c = cirq.Circuit.from_ops(cirq.X(a) * cirq.X(b))
+    sim = cirq.DensityMatrixSimulator()
+    result = sim.simulate(c).final_simulator_state.density_matrix
+    np.testing.assert_allclose(result.reshape(4, 4),
+                               np.diag([0, 0, 0, 1]),
+                               atol=1e-8)
