f4eaf8cb2aa69c9ab4179f8d77baf31f5c74603d
peterse <peterse583@gmail.com>
2019-06-04 13:03:34 -0500

    more tests

--- cirq/protocols/equal_up_to_global_phase_test.py
+++ cirq/protocols/equal_up_to_global_phase_test.py
@@ -17,12 +17,14 @@ import cirq
 
 def test_equal_up_to_global_phase_primitives():
     assert cirq.equal_up_to_global_phase(1.0 + 1j, 1.0 + 1j, atol=1e-9)
+    assert not cirq.equal_up_to_global_phase(2.0, 1.0 + 1j, atol=1e-9)
     assert cirq.equal_up_to_global_phase(1.0 + 1j, 1.0 - 1j, atol=1e-9)
     assert cirq.equal_up_to_global_phase(np.exp(1j * 3.3),
                                          1.0 + 0.0j,
                                          atol=1e-9)
-    assert cirq.equal_up_to_global_phase(np.exp(1j * 3.3), 1.0, atol=1e-9)
+    assert cirq.equal_up_to_global_phase(np.exp(1j * 3.3), 1.0j, atol=1e-9)
     assert cirq.equal_up_to_global_phase(np.exp(1j * 3.3), 1, atol=1e-9)
+    assert not cirq.equal_up_to_global_phase(np.exp(1j * 3.3), 0, atol=1e-9)
     assert cirq.equal_up_to_global_phase(1j, 1 + 1e-10, atol=1e-9)
     assert not cirq.equal_up_to_global_phase(1j, 1 + 1e-10, atol=1e-11)
     # atol is applied to magnitude of complex vector, not components.
