fe3c3dc904bd37c4990d15b7f7c6e04f595ec1be
Craig Gidney <Craig.Gidney@gmail.com>
2019-06-10 16:33:57 -0700

    Add cirq.axis_angle utility method (#1592)
    Ran into the need for this while trying to decompose gates into fsims.
    
    Also fixed 1 not approximately equalling 1+0j.

--- cirq/protocols/approximate_equality_test.py
+++ cirq/protocols/approximate_equality_test.py
@@ -206,6 +206,8 @@ def test_approx_eq_uses__eq__():
     assert not cirq.approx_eq(C(1), C(2), atol=0.0)
     assert cirq.approx_eq([C(0)], [C(0)], atol=0.0)
     assert not cirq.approx_eq([C(1)], [C(2)], atol=0.0)
+    assert cirq.approx_eq(complex(0, 0), 0, atol=0.0)
+    assert cirq.approx_eq(0, complex(0, 0), atol=0.0)
 
 
 def test_approx_eq_types_mismatch():
