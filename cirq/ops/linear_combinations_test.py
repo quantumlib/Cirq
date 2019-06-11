b495607e0b6c1022cf263c5f47b0cde7ed1e9623
Matthew Harrigan <mpharrigan@google.com>
2019-06-11 15:36:56 -0700

    Single qubit construction

--- cirq/ops/linear_combinations_test.py
+++ cirq/ops/linear_combinations_test.py
@@ -575,6 +575,22 @@ def test_pauli_sum_construction():
     assert psum == psum2
 
 
+def test_pauli_sum_from_single_pauli():
+    q = cirq.LineQubit.range(2)
+    psum1 = cirq.X(q[0]) + cirq.Y(q[1])
+    assert psum1 == cirq.PauliSum.from_pauli_strings([
+        cirq.X(q[0]) * 1, cirq.Y(q[1]) * 1
+    ])
+
+    psum2 = cirq.X(q[0]) * cirq.X(q[1]) + cirq.Y(q[1])
+    assert psum2 == cirq.PauliSum.from_pauli_strings([
+        cirq.X(q[0]) * cirq.X(q[1]), cirq.Y(q[1]) * 1
+    ])
+
+    psum3 = cirq.Y(q[1]) + cirq.X(q[0]) * cirq.X(q[1])
+    assert psum3 == psum2
+
+
 def test_pauli_sub():
     q = cirq.LineQubit.range(2)
     pstr1 = cirq.X(q[0]) * cirq.X(q[1])
