506457d2596a6e7c9d837ca42b82cd5dd40ae170
Matthew Neeley <maffoo@google.com>
2019-06-18 13:31:23 -0700

    Add script to generate protobuf files, including type stubs (#1725)
    This uses the grpcio-tools package, which bundles the protoc compiler. We also add the [mypy-protobuf package](https://github.com/dropbox/mypy-protobuf), which generates mypy stubs for the protobuf code, so that we get type checking of the code to build proto messages. The latter requires also updating the mypy version to handle the generated stubs.

---
+++ dev_tools/build-protos.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+################################################################################
+# Generates python from protobuf definitions, including mypy stubs.
+#
+# Usage:
+#     dev_tools/build-protos.sh
+################################################################################
+
+set -e
+trap "{ echo -e '\e[31mFAILED\e[0m'; }" ERR
+
+# Get the working directory to the repo root.
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $(git rev-parse --show-toplevel)
+
+# Build protos for each protobuf package.
+for package in cirq/api/google/v1 cirq/api/google/v2
+do
+  python -m grpc_tools.protoc -I=. --python_out=. --mypy_out=. ${package}/*.proto
+done
