7cb47fa1ce8eef84c568839a9376562d879c7e19
bees <brady.ben12@gmail.com>
2019-06-23 20:46:13 -0700

    Added cirq import to three_qubit_gates

--- cirq/ops/three_qubit_gates.py
+++ cirq/ops/three_qubit_gates.py
@@ -19,6 +19,7 @@ from typing import Any, List, Optional, Tuple, Union
 import numpy as np
 import sympy
 
+import cirq
 from cirq import linalg, protocols, value
 from cirq._compat import proper_repr
 from cirq.ops import (
