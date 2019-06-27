9c71c64dd7a0b457844294b842e05bb14cb980d7
Craig Gidney <craiggidney@google.com>
2019-06-27 14:17:55 -0700

    doc

--- cirq/_compat.py
+++ cirq/_compat.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Workarounds for sympy issues"""
+"""Workarounds for compatibility issues between versions and libraries."""
+
 from typing import Any, Callable
 import contextlib
 import os
