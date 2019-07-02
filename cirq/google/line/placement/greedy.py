b335080a8fb90411b64acc17b5b74382b1dfbb06
Victory Omole <vtomole@iastate.edu>
2019-07-02 17:43:17 -0500

    Remove Unnecessary 'elif' after 'return' statements (#1769)

--- cirq/google/line/placement/greedy.py
+++ cirq/google/line/placement/greedy.py
@@ -176,8 +176,10 @@ class GreedySequenceSearch:
             return path
 
         other = {p: q, q: p}
-        parents = {p: dict(), q: dict()} \
-            # type: Dict[GridQubit, Dict[GridQubit, GridQubit]]
+        parents = {
+            p: dict(),
+            q: dict()
+        }  # type: Dict[GridQubit, Dict[GridQubit, GridQubit]]
         visited = {p: set(), q: set()}  # type: Dict[GridQubit, Set[GridQubit]]
 
         queue = collections.deque([(p, p), (q, q)])
@@ -194,7 +196,7 @@ class GreedySequenceSearch:
                     if s == q:
                         path.reverse()
                     return path
-                elif n_adj not in used and n_adj not in visited[s]:
+                if n_adj not in used and n_adj not in visited[s]:
                     # Append n_adj to the end of queue of qubit s.
                     queue.append((n_adj, s))
                     visited[s].add(n_adj)
