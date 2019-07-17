958c7a90e0daa5b1d72e6fb283124f6d3c404c72
vtomole <vtomole@iastate.edu>
2019-07-17 16:02:40 -0500

    yapf disable

--- cirq/sim/density_matrix_utils_test.py
+++ cirq/sim/density_matrix_utils_test.py
@@ -33,18 +33,28 @@ def test_von_neumann_entropy():
     assert cirq.von_neumann_entropy(
         np.array([1, 0, 0, 1] * np.array([[1], [0], [0], [1]]))) == 0
     # Maximally mixed state
-    assert cirq.von_neumann_entropy([[0.5, 0], [0, 0.5]]) == 1
+    # yapf: disable
+    assert cirq.von_neumann_entropy(np.array(
+        [[0.5, 0],
+        [0, 0.5]])) == 1
     # 3x3 state
-    assert np.isclose(
-        cirq.von_neumann_entropy(
-            np.array(
-                np.array([[0.5, 0.5j, 1], [-0.5j, 0.5, 0], [0.7, 0.4, 0.6]]))),
-        1.9, 1e01)
+    assert np.isclose(cirq.von_neumann_entropy(
+        np.array(
+            [[0.5, 0.5j, 1],
+            [-0.5j, 0.5, 0],
+            [0.7, 0.4, 0.6]])),
+                      1.37,
+                      atol=1e-01)
     # 4X4 state
-    assert np.isclose(
-        cirq.von_neumann_entropy(
-            np.array([[0.5, 0.5j, 1, 3], [-0.5j, 0.5, 0, 4], [0.7, 0.4, 0.6, 5],
-                      [6, 7, 8, 9]])), 52, 1e01)
+    assert np.isclose(cirq.von_neumann_entropy(
+        np.array(
+            [[0.5, 0.5j, 1, 3],
+            [-0.5j, 0.5, 0, 4],
+            [0.7, 0.4, 0.6, 5],
+            [6, 7, 8, 9]])),
+                      1.12,
+                      atol=1e-01)
+    # yapf: enable
     # 2x2 random unitary, each column as a ket, each ket as a density matrix,
     # linear combination of the two with coefficients 0.1 and 0.9
     res = cirq.testing.random_unitary(2)
@@ -53,9 +63,15 @@ def test_von_neumann_entropy():
     second_column = res[:, 1]
     second_density_matrix = 0.9 * np.outer(second_column,
                                            np.conj(second_column))
-    assert np.isclose(
-        cirq.von_neumann_entropy(first_density_matrix + second_density_matrix),
-        0.4689, 1e04)
+    assert np.isclose(cirq.von_neumann_entropy(first_density_matrix +
+                                               second_density_matrix),
+                      0.4689,
+                      atol=1e-04)
+
+    assert np.isclose(cirq.von_neumann_entropy(
+        np.diag([0, 0, 0.1, 0, 0.2, 0.3, 0.4, 0])),
+                      1.8464,
+                      atol=1e-04)
 
 
 def test_to_valid_density_matrix_from_density_matrix():
