e74c9a7308c8d98061f95e46b9e24fb570889889
Bryan A. O'Gorman <bryan.a.ogorman@gmail.com>
2019-07-16 19:05:05 -0700

    added tests for empty and non-parameterized sequences

--- cirq/protocols/resolve_parameters_test.py
+++ cirq/protocols/resolve_parameters_test.py
@@ -61,14 +61,22 @@ def test_resolve_parameters():
     a, b, c = tuple(sympy.Symbol(l) for l in 'abc')
     x, y, z = 0, 4, 7
     resolver = {a: x, b: y, c: z}
+
     assert cirq.resolve_parameters((a, b, c), resolver) == (x, y, z)
     assert cirq.resolve_parameters([a, b, c], resolver) == [x, y, z]
+    assert cirq.resolve_parameters((x, y, z), resolver) == (x, y, z)
+    assert cirq.resolve_parameters([x, y, z], resolver) == [x, y, z]
+    assert cirq.resolve_parameters((), resolver) == ()
+    assert cirq.resolve_parameters([], resolver) == []
+
     assert not cirq.is_parameterized((x, y))
     assert not cirq.is_parameterized([x, y])
     assert cirq.is_parameterized([a, b])
     assert cirq.is_parameterized([a, x])
     assert cirq.is_parameterized((a, b))
     assert cirq.is_parameterized((a, x))
+    assert not cirq.is_parameterized(())
+    assert not cirq.is_parameterized([])
 
 
 def test_skips_empty_resolution():
