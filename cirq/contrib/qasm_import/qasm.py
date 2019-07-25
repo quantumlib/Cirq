e2395e7b137d98865568740b33cd9737e47a0cff
balopat <balintp@google.com>
2019-07-24 20:06:04 -0700

    remove :obj:

--- cirq/contrib/qasm_import/qasm.py
+++ cirq/contrib/qasm_import/qasm.py
@@ -17,7 +17,7 @@ from cirq.contrib.qasm_import._parser import QasmParser
 
 
 def circuit_from_qasm(qasm: str) -> circuits.Circuit:
-    """Parses an OpenQASM string to :obj:`cirq.Circuit`.
+    """Parses an OpenQASM string to `cirq.Circuit`.
 
     Args:
         qasm: The OpenQASM string
