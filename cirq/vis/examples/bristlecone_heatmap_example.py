240ba360a664afbb5535ec3f09cb419b85a2a3a7
Ping Yeh <pingyeh@google.com>
2019-08-12 17:01:18 -0700

    Add a test for the example script for incremental coverage.

--- cirq/vis/examples/bristlecone_heatmap_example.py
+++ cirq/vis/examples/bristlecone_heatmap_example.py
@@ -4,10 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cirq
 
-value_map = {(qubit.row, qubit.col): np.random.random()
-             for qubit in cirq.google.known_devices.Bristlecone.qubits}
+def main():
+    value_map = {(qubit.row, qubit.col): np.random.random()
+                 for qubit in cirq.google.known_devices.Bristlecone.qubits}
 
-heatmap = cirq.Heatmap(value_map)
-fig, ax = plt.subplots(figsize=(9, 9))
-heatmap.plot(ax)
-plt.show()
+    heatmap = cirq.Heatmap(value_map)
+    fig, ax = plt.subplots(figsize=(9, 9))
+    heatmap.plot(ax)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
