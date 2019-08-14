40be3873cc5f2ceac89495600c24cae78a7f5292
Casey Duckering <cduckering@gmail.com>
2019-08-14 16:13:18 -0700

    Add doc

--- cirq/optimizers/eject_z.py
+++ cirq/optimizers/eject_z.py
@@ -58,6 +58,9 @@ class EjectZ():
             tolerance: Maximum absolute error tolerance. The optimization is
                  permitted to simply drop negligible combinations of Z gates,
                  with a threshold determined by this tolerance.
+            eject_parameterized: If True, the optimization will attempt to eject
+                parametrized Z gates as well.  This may result in other gates
+                parameterized by symbolic expressions.
         """
         self.tolerance = tolerance
         self.eject_parameterized = eject_parameterized
