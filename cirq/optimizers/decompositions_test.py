f47714baef052c12c84b911fea4b5e082d0a0ab8
Casey Duckering <cduckering@gmail.com>
2019-08-19 15:22:29 -0700

    Add expression test

--- cirq/optimizers/decompositions_test.py
+++ cirq/optimizers/decompositions_test.py
@@ -49,7 +49,10 @@ def test_is_negligible_turn():
     assert not cirq.is_negligible_turn(-0.5, 1e-5)
     assert not cirq.is_negligible_turn(0.5, 1e-5)
     assert not cirq.is_negligible_turn(4.5, 1e-5)
+    # Variable sympy expression
     assert not cirq.is_negligible_turn(sympy.Symbol('a'), 1e-5)
+    assert not cirq.is_negligible_turn(sympy.Symbol('a') + 1, 1e-5)
+    assert not cirq.is_negligible_turn(sympy.Symbol('a') * 1e-10, 1e-5)
     # Sympy expression for a constant
     assert cirq.is_negligible_turn(sympy.Symbol('a') * 0 + 3 + 1e-6, 1e-5)
     assert not cirq.is_negligible_turn(sympy.Symbol('a') * 0 + 1.5 - 1e-6, 1e-5)
