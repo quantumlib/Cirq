601b5b0d41fe219d86872fb66ed0852c02da0606
Casey Duckering <cduckering@gmail.com>
2019-08-19 16:08:58 -0700

    Formatting

--- cirq/value/angle_test.py
+++ cirq/value/angle_test.py
@@ -29,8 +29,8 @@ def test_canonicalize_half_turns():
     assert cirq.canonicalize_half_turns(101.5) == -0.5
     # Variable sympy expression
     assert cirq.canonicalize_half_turns(sympy.Symbol('a')) == sympy.Symbol('a')
-    assert (cirq.canonicalize_half_turns(sympy.Symbol('a') + 1) ==
-            sympy.Symbol('a') + 1)
+    assert (cirq.canonicalize_half_turns(sympy.Symbol('a') +
+                                         1) == sympy.Symbol('a') + 1)
     # Constant sympy expression
     assert cirq.canonicalize_half_turns(sympy.Symbol('a') * 0 + 3) == 1
 
