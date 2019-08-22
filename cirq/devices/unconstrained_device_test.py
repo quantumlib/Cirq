b465068911b91edac1d0669808ae884a7fe4001f
Dave Bacon <dabacon@gmail.com>
2019-08-22 13:40:02 -0700

    fix tesT

--- cirq/devices/unconstrained_device_test.py
+++ cirq/devices/unconstrained_device_test.py
@@ -20,5 +20,5 @@ def test_repr():
 
 
 def test_infinitely_fast():
-    assert cirq.UNCONSTRAINED_DEVICE.duration_of(
-        cirq.X(cirq.NamedQubit('a'))) == 0
+    assert cirq.UNCONSTRAINED_DEVICE.duration_of(cirq.X(
+        cirq.NamedQubit('a'))) == cirq.Duration(picos=0)
