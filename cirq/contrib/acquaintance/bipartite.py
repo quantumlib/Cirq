958b73b1cf8e81b0fee95f6f6172bb2b6529a289
Casey Duckering <cduckering@gmail.com>
2019-08-27 22:56:40 -0700

    Fix lint

--- cirq/contrib/acquaintance/bipartite.py
+++ cirq/contrib/acquaintance/bipartite.py
@@ -17,14 +17,12 @@ import itertools
 from typing import TYPE_CHECKING, Dict, Sequence, Tuple, Union
 
 from cirq import ops
-
-if TYPE_CHECKING:
-    from cirq import protocols
-
-
 from cirq.contrib.acquaintance.gates import acquaint
 from cirq.contrib.acquaintance.permutation import (
         PermutationGate, SwapPermutationGate)
+
+if TYPE_CHECKING:
+    from cirq import protocols
 
 
 @enum.unique
