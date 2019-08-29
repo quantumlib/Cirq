63025af140ff702c0b85e62543d6f552a3f0c3ed
Casey Duckering <cduck@users.noreply.github.com>
2019-08-28 17:07:21 -0700

    Fix circular import issues and inter-module dependency ordering (#2037)
    * Add top-level submodule import order, add protocols delayed import
    
    * Add import order pytest and dev tool
    
    * Fix import issues

--- cirq/ops/phased_x_gate.py
+++ cirq/ops/phased_x_gate.py
@@ -46,8 +46,7 @@ class PhasedXPowGate(gate_features.SingleQubitGate):
         self._exponent = exponent
         self._global_shift = global_shift
 
-    def _qasm_(self,
-               args: protocols.QasmArgs,
+    def _qasm_(self, args: 'protocols.QasmArgs',
                qubits: Tuple[raw_types.Qid, ...]) -> Optional[str]:
         if cirq.is_parameterized(self):
             return None
@@ -148,8 +147,8 @@ class PhasedXPowGate(gate_features.SingleQubitGate):
             phase_exponent=self._phase_exponent + phase_turns * 2,
             global_shift=self._global_shift)
 
-    def _circuit_diagram_info_(self, args: protocols.CircuitDiagramInfoArgs
-                               ) -> protocols.CircuitDiagramInfo:
+    def _circuit_diagram_info_(self, args: 'protocols.CircuitDiagramInfoArgs'
+                              ) -> 'protocols.CircuitDiagramInfo':
         """See `cirq.SupportsCircuitDiagramInfo`."""
 
         if (isinstance(self.phase_exponent, sympy.Basic) or
