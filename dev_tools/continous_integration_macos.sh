9aa54550599508e98022326cc71555320e5707a5
Victory Omole <victory.omole@kingland.com>
2019-09-10 23:20:20 -0500

    3.6

--- dev_tools/continous_integration_macos.sh
+++ dev_tools/continous_integration_macos.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 
     # Install some custom requirements on macOS
     # e.g. brew install pyenv-virtualenv
-    PYTHON=3.7.0
+    PYTHON=3.6.0
     brew update
     brew install openssl readline pyenv-virtualenv
     brew outdated pyenv || brew upgrade pyenv
@@ -12,11 +12,10 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     export PYENV_VERSION=$PYTHON
     export PATH="$(pwd)/.pyenv/shims:${PATH}"
     pyenv virtualenv venv
-    source /Users/travis/.pyenv/versions/3.7.0/envs/venv/bin/activate
+    source /Users/travis/.pyenv/versions/3.6.0/envs/venv/bin/activate
     pip install -r requirements.txt
     pip install -r cirq/contrib/contrib-requirements.txt
     pip install -r dev_tools/conf/pip-list-dev-tools.txt
-    check/pytest-and-incremental-coverage master
     #`import matplotlib` throws a  RuntimeError without this
     mkdir -p ~/.matplotlib && echo "backend: TkAgg" >> ~/.matplotlib/matplotlibrc
 else
