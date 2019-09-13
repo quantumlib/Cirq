a2c88c2597a390ed2261fb60427629d61cface25
Kevin J. Sung <kevinjefferysung@gmail.com>
2019-09-13 10:38:50 -0700

    add sampler method to Engine

--- cirq/google/engine/engine.py
+++ cirq/google/engine/engine.py
@@ -41,7 +41,8 @@ from cirq.api.google import v1, v2
 from cirq.google import gate_sets, serializable_gate_set
 from cirq.google.api import v1 as api_v1
 from cirq.google.api import v2 as api_v2
-from cirq.google.engine import calibration, engine_job, engine_program
+from cirq.google.engine import (
+        calibration, engine_job, engine_program, engine_sampler)
 
 gcs_prefix_pattern = re.compile('gs://[a-z0-9._/-]+')
 TYPE_PREFIX = 'type.googleapis.com/'
@@ -692,3 +693,11 @@ class Engine:
         response = self.service.projects().processors().calibrations().get(
             name=calibration_name).execute()
         return calibration.Calibration(response['data']['data'])
+
+    def sampler(self,
+                processor_id: Union[str, List[str]],
+                gate_set: serializable_gate_set.SerializableGateSet
+                ) -> engine_sampler.QuantumEngineSampler:
+        return engine_sampler.QuantumEngineSampler(engine=self,
+                                                   processor_id=processor_id,
+                                                   gate_set=gate_set)
