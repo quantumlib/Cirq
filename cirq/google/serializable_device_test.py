b1e8580e0709ecc9046bf07eee1ef1d9a36a50e5
Doug Strain <dstrain@google.com>
2019-09-16 13:13:30 -0700

    Add missing license and missing covered lines of test

--- cirq/google/serializable_device_test.py
+++ cirq/google/serializable_device_test.py
@@ -1,3 +1,18 @@
+# Copyright 2019 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
 import pytest
 
 import cirq
@@ -16,6 +31,7 @@ def test_foxtail():
     foxtail.validate_operation(cirq.X(valid_qubit1))
     foxtail.validate_operation(cirq.X(valid_qubit2))
     foxtail.validate_operation(cirq.X(valid_qubit3))
+    foxtail.validate_operation(cirq.XPowGate(exponent = 0.1)(valid_qubit1))
     with pytest.raises(ValueError):
         foxtail.validate_operation(cirq.X(invalid_qubit1))
     with pytest.raises(ValueError):
@@ -31,6 +47,19 @@ def test_foxtail():
     # Unsupport op
     with pytest.raises(ValueError):
         foxtail.validate_operation(cirq.H(invalid_qubit1))
+
+
+def test_mismatched_proto_serializer():
+    augmented_proto = copy.deepcopy(cg.known_devices.FOXTAIL_PROTO)
+    # Remove measurement gate
+    del augmented_proto.valid_gate_sets[0].valid_gates[3]
+
+    # Should throw value error that measurement gate is serialized
+    # but not supported by the hardware
+    with pytest.raises(ValueError):
+        _ = cg.SerializableDevice(proto=augmented_proto,
+                                    gate_set=cg.gate_sets.XMON)
+
 
 
 def test_duration_of():
