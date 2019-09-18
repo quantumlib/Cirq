9cb3b8b79823955677771a29f21ec7686f89587c
Craig Gidney <craiggidney@google.com>
2019-09-18 13:40:08 -0700

    Add `cirq.scatter_plot_normalized_kak_interaction_coefficients` (#2117)
    This PR also:
    
    - Marks `plt.show()` lines as not needing to be covered
    - Fixes a deprecation warning w.r.t. `np.asscalar`
    - Allows `plt.show()` to be called in doctested example code
    - Allows `cirq.kak_decompose` to be called on objects with a `_unitary_` method, such as `cirq.Circuit`.
    
    Plotting KAK interaction coefficients visually is something I've found really handy for understanding two qubit operation decomposition. I put together this flexible method for doing so.
    
    Example:
    
    ```python
    import cirq
    import numpy as np
    import matplotlib.pyplot as plt
    
    ax = None
    for y in np.linspace(0, 0.5, 10):
        a, b = cirq.LineQubit.range(2)
        circuits = [
            cirq.Circuit.from_ops(
                cirq.CZ(a, b)**0.5,
                cirq.X(a)**y, cirq.X(b)**x,
                cirq.CZ(a, b)**0.5,
                cirq.X(a)**x, cirq.X(b)**y,
                cirq.CZ(a, b) ** 0.5,
            )
            for x in np.linspace(0, 1, 100)
        ]
    
        ax = cirq.scatter_plot_normalized_kak_interaction_coefficients(
            circuits,
            s=1,
            ax=ax,
            include_frame=ax is None,
            label=f'y={y:0.2f}')
    ax.legend()
    plt.show()
    ```
    
    ![image](https://user-images.githubusercontent.com/79941/65005029-5c40b180-d8b3-11e9-8393-25a5c5bde871.png)
    
    ![image](https://user-images.githubusercontent.com/79941/65005043-64005600-d8b3-11e9-9b05-efd8c7155cde.png)

--- cirq/__init__.py
+++ cirq/__init__.py
@@ -128,6 +128,7 @@ from cirq.linalg import (
     one_hot,
     partial_trace,
     PAULI_BASIS,
+    scatter_plot_normalized_kak_interaction_coefficients,
     pow_pauli_combination,
     reflection_matrix_pow,
     slice_for_qubits_equal_to,
