6363422ab07cc8f7b1a1c72b853e9bd012998d7e
Craig Gidney <craiggidney@google.com>
2019-09-24 16:09:57 -0700

    Replace usage of cirq.Circuit.from_ops(...) with cirq.Circuit(...) in documentation
    Part of #1787
    Comes after #2163

--- examples/qubit_characterizations_example.py
+++ examples/qubit_characterizations_example.py
@@ -27,13 +27,13 @@ def main():
     rb_result_2q.plot()
 
     # State-tomography of q_0 after application of an X/2 rotation.
-    cir_1q = cirq.Circuit.from_ops(cirq.X(q_0) ** 0.5)
+    cir_1q = cirq.Circuit(cirq.X(q_0)**0.5)
     tomography_1q = cirq.experiments.single_qubit_state_tomography(simulator,
                                                                    q_0, cir_1q)
     tomography_1q.plot()
 
     # State-tomography of a Bell state between q_0 and q_1.
-    cir_2q = cirq.Circuit.from_ops(cirq.H(q_0), cirq.CNOT(q_0, q_1))
+    cir_2q = cirq.Circuit(cirq.H(q_0), cirq.CNOT(q_0, q_1))
     tomography_2q = cirq.experiments.two_qubit_state_tomography(simulator,
                                                                 q_0, q_1,
                                                                 cir_2q)
