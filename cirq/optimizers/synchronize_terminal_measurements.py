aab8fc36fd076ae961ca4c368b4b6fd035aaa1cd
Michael Broughton <mbbrough@google.com>
2019-09-25 16:42:14 -0700

    feedback.

--- cirq/optimizers/synchronize_terminal_measurements.py
+++ cirq/optimizers/synchronize_terminal_measurements.py
@@ -17,26 +17,24 @@ from cirq import circuits, ops, protocols
 
 
 class SynchronizeTerminalMeasurements():
-    """Move measurements to the end.
+    """Move measurements to the end of the circuit.
 
-    Move all terminal measurements in a circuit to the final moment if it
-    can accomodate them (without overlapping with other operations). If
-    self._measure_only_moment is true then a new moment will be added to
-    the end of the circuit containing all the measurements that could be
-    brough forward.
+    Move all measurements in a circuit to the final moment if it can accomodate
+    them (without overlapping with other operations). If
+    self._after_other_operations is true then a new moment will be added to the
+    end of the circuit containing all the measurements that should be brought
+    forward.
     """
 
-    def __init__(self, measurements_only_moment: bool = True):
+    def __init__(self, after_other_operations: bool = True):
         """
         Args:
-            measurements_only_moment: Bool indicating whether or not all
-                measurements should be moved to the last existing moment so
-                that the circuit might contain a mixture of measurements and
-                other ops (False), or a new moment containing them should be
-                added to the end with the guarantee to contain only
-                measurements (True).
+            after_other_operations: Set by default. If the circuit's final
+                moment contains non-measurement operations and this is set then
+                a new empty moment is appended to the circuit before pushing
+                measurements to the end.
         """
-        self._measurement_only_moment = measurements_only_moment
+        self._after_other_operations = after_other_operations
 
     def __call__(self, circuit: circuits.Circuit):
         self.optimize_circuit(circuit)
@@ -44,22 +42,24 @@ class SynchronizeTerminalMeasurements():
     def optimize_circuit(self, circuit: circuits.Circuit) -> None:
         deletions = []
         terminal_measures = set()
-        deepest_measurement = -1
-        all_measurements = circuit.findall_operations(protocols.is_measurement)
-        for index, op in all_measurements:
-            if circuit.next_moment_operating_on(op.qubits, index + 1) is None:
-                deepest_measurement = max(deepest_measurement, index)
-                deletions.append((index, op))
+
+        qubits = circuit.all_qubits()
+        for qubit in qubits:
+            moment_index = circuit.prev_moment_operating_on((qubit,))
+            op = circuit.operation_at(qubit, moment_index)
+            if protocols.is_measurement(op):
+                deletions.append((moment_index, op))
                 terminal_measures.add(op)
 
+        if not deletions:
+            return
+
         circuit.batch_remove(deletions)
-        if not self._measurement_only_moment or circuit[
-                deepest_measurement] == ops.Moment([]):
-            # Can safely add to deepest measurement if
-            # self._measurement_only_moment is false or we happen to get an
+        if circuit[-1] and self._after_other_operations:
+            # Can safely add to the end if
+            # self._after_other_operations is false or we happen to get an
             # empty final moment before re-adding all the measurements.
-            for op in terminal_measures:
-                circuit[deepest_measurement] = circuit[
-                    deepest_measurement].with_operation(op)
-        else:
-            circuit.append(ops.Moment(list(terminal_measures)))
+            circuit.append(ops.Moment())
+
+        for op in terminal_measures:
+            circuit[-1] = circuit[-1].with_operation(op)
