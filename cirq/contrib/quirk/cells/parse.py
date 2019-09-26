d3656c3d7bb785df743a2fd62b1d3b1cb2f71574
Craig Gidney <craiggidney@google.com>
2019-09-26 02:30:09 -0700

    Define cirq.contrib.quirk.quirk_url_to_circuit - Support single qubit rotation gates - Support controls - Support qubit permutations - Define the 'Cell' type, which represents an entry in Quirk's grid (including operation modifiers and displays) - Include assert_url_to_circuit_returns testing utility method - Basic formula string and matrix string parsing
    Other operations coming in later PRs
    
    Pulled out of https://github.com/quantumlib/Cirq/pull/2138

---
+++ cirq/contrib/quirk/cells/parse.py
@@ -0,0 +1,94 @@
+# Copyright 2018 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import (
+    Any,
+    Union,
+)
+
+import numpy as np
+import sympy
+from sympy.parsing.sympy_parser import parse_expr
+
+
+def parse_formula(formula: Any) -> Union[float, sympy.Basic]:
+    if not isinstance(formula, str):
+        raise TypeError('formula must be a string: {!r}'.format(formula))
+
+    formula = expand_unicode_fractions(formula)
+    try:
+        result = parse_expr(formula)
+    except Exception as ex:
+        raise ValueError(
+            f'Failed to parse the gate formula {repr(formula)}.\n'
+            'This is likely due to differences in how sympy and Quirk parse.\n'
+            'For example, Quirk allows "2 pi" whereas sympy requires "2*pi"\n'
+            'Parsing of sympy-incompatible formulas is not supported yet.'
+        ) from ex
+    if not result.free_symbols <= {sympy.Symbol('t')}:
+        raise ValueError(
+            f'Formula has variables besides time "t": {repr(formula)}')
+    if not result.free_symbols:
+        result = float(result)
+    return result
+
+
+UNICODE_FRACTIONS = {
+    "½": "(1/2)",
+    "¼": "(1/4)",
+    "¾": "(3/4)",
+    "⅓": "(1/3)",
+    "⅔": "(2/3)",
+    "⅕": "(1/5)",
+    "⅖": "(2/5)",
+    "⅗": "(3/5)",
+    "⅘": "(4/5)",
+    "⅙": "(1/6)",
+    "⅚": "(5/6)",
+    "⅐": "(1/7)",
+    "⅛": "(1/8)",
+    "⅜": "(3/8)",
+    "⅝": "(5/8)",
+    "⅞": "(7/8)",
+    "⅑": "(1/9)",
+    "⅒": "(1/10)",
+}
+
+
+def expand_unicode_fractions(text: str) -> str:
+    for k, v in UNICODE_FRACTIONS.items():
+        text = text.replace('√' + k, f'(sqrt{v})')
+        text = text.replace(k, v)
+    return text
+
+
+def parse_matrix(text: str) -> np.ndarray:
+    if not text.startswith('{{') or not text.endswith('}}'):
+        raise ValueError('No opening/closing braces.\ntext: {!r}'.format(text))
+    text = expand_unicode_fractions(text[2:-2])
+    rows = text.split('},{')
+    return np.array([[_parse_complex(c) for c in row.split(',')] for row in rows
+                    ])
+
+
+def _parse_complex(text: str) -> complex:
+    try:
+        if (text.endswith('i') and len(text) > 1 and not text.endswith('+i') and
+                not text.endswith('-i')):
+            text = text[:-1] + '*i'
+        expr = sympy.parsing.sympy_parser.parse_expr(text)
+        return complex(expr.subs({'i': 1j}))
+    except Exception as ex:
+        raise ValueError(
+            'Failed to parse complex from {!r}'.format(text)) from ex
---
+++ cirq/contrib/quirk/cells/parse_test.py
@@ -0,0 +1,67 @@
+# Copyright 2018 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import pytest
+import sympy
+
+from cirq.contrib.quirk.cells.parse import parse_matrix, parse_formula
+
+
+def test_parse_matrix():
+    s = np.sqrt(0.5)
+    np.testing.assert_allclose(parse_matrix('{{√½,√½},{-√½,√½}}'),
+                               np.array([[s, s], [-s, s]]),
+                               atol=1e-8)
+    np.testing.assert_allclose(parse_matrix('{{√½,√½i},{√½i,√½}}'),
+                               np.array([[s, s * 1j], [s * 1j, s]]),
+                               atol=1e-8)
+    np.testing.assert_allclose(parse_matrix('{{1,-i},{i,1+i}}'),
+                               np.array([[1, -1j], [1j, 1 + 1j]]),
+                               atol=1e-8)
+
+
+def test_parse_matrix_failures():
+    with pytest.raises(ValueError, match='opening/closing braces'):
+        _ = parse_matrix('1')
+    with pytest.raises(ValueError, match='opening/closing braces'):
+        _ = parse_matrix('{{1}')
+    with pytest.raises(ValueError, match='opening/closing braces'):
+        _ = parse_matrix('{1}}')
+    with pytest.raises(ValueError, match='opening/closing braces'):
+        _ = parse_matrix('1}}')
+    with pytest.raises(ValueError, match='Failed to parse complex'):
+        _ = parse_matrix('{{x}}')
+
+
+def test_parse_formula():
+    t = sympy.Symbol('t')
+    assert parse_formula('t*t + ln(t)') == t * t + sympy.ln(t)
+    assert parse_formula('cos(pi*t)') == sympy.cos(sympy.pi * t)
+    np.testing.assert_allclose(parse_formula('cos(pi)'), -1, atol=1e-8)
+    assert type(parse_formula('cos(pi)')) is float
+
+
+def test_parse_formula_failures():
+    with pytest.raises(TypeError, match='formula must be a string'):
+        _ = parse_formula(2)
+
+    with pytest.raises(TypeError, match='formula must be a string'):
+        _ = parse_formula([])
+
+    with pytest.raises(ValueError, match='Failed to parse the gate formula'):
+        _ = parse_formula('5*__**DSA **)SADD')
+
+    with pytest.raises(ValueError, match='variables besides time'):
+        _ = parse_formula('5*x')
