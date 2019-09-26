c793403b0efa753268e47339ee45a05e21647a37
MichaelBroughton <MichaelBroughton@users.noreply.github.com>
2019-09-25 23:11:42 -0700

    Added Optimization to move terminal measurements to final moment. (#2141)
    fixes #2141 .

--- cirq/optimizers/__init__.py
+++ cirq/optimizers/__init__.py
@@ -48,5 +48,8 @@ from cirq.optimizers.decompositions import (
     single_qubit_op_to_framed_phase_form,
 )
 
+from cirq.optimizers.synchronize_terminal_measurements import (
+    SynchronizeTerminalMeasurements,)
+
 from cirq.optimizers.two_qubit_decompositions import (
     two_qubit_matrix_to_operations,)
