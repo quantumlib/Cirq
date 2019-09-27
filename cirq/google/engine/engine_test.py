a43c1782641a9a51983367894f6421f6a932e3b6
Doug Strain <dstrain@google.com>
2019-09-27 08:56:13 -0700

    Support receiving both api.google and google.api protos
    - Make sure that engine class can receive both versions of the
    proto.

--- cirq/google/engine/engine_test.py
+++ cirq/google/engine/engine_test.py
@@ -14,6 +14,7 @@
 
 """Tests for engine."""
 import base64
+import copy
 import re
 from unittest import mock
 import numpy as np
@@ -142,7 +143,7 @@ _CALIBRATION = {
     'timestamp': '2019-07-09T23:39:59Z',
     'data': {
         '@type':
-        'type.googleapis.com/cirq.api.google.v2.MetricsSnapshot',
+        'type.googleapis.com/cirq.google.api.v2.MetricsSnapshot',
         'timestampMs':
         '1562544000021',
         'metrics': [{
@@ -465,6 +466,62 @@ def test_run_sweep_params(build):
 
 
 @mock.patch.object(discovery, 'build')
+def test_run_sweep_params_new_proto(build):
+    service = mock.Mock()
+    build.return_value = service
+    programs = service.projects().programs()
+    jobs = programs.jobs()
+    results_new_proto = copy.deepcopy(_RESULTS)
+    results_new_proto['@type'] = 'type.googleapis.com/cirq.google.api.v1.Result'
+    programs.create().execute.return_value = {
+        'name': 'projects/project-id/programs/test'
+    }
+    jobs.create().execute.return_value = {
+        'name': 'projects/project-id/programs/test/jobs/test',
+        'executionStatus': {
+            'state': 'READY'
+        }
+    }
+    jobs.get().execute.return_value = {
+        'name': 'projects/project-id/programs/test/jobs/test',
+        'executionStatus': {
+            'state': 'SUCCESS'
+        }
+    }
+    jobs.getResult().execute.return_value = {'result': results_new_proto}
+
+    engine = cg.Engine(project_id='project-id')
+    job = engine.run_sweep(
+        program=_SCHEDULE,
+        job_config=cg.JobConfig('project-id', gcs_prefix='gs://bucket/folder'),
+        params=[cirq.ParamResolver({'a': 1}),
+                cirq.ParamResolver({'a': 2})])
+    results = job.results()
+    assert len(results) == 2
+    for i, v in enumerate([1, 2]):
+        assert results[i].repetitions == 1
+        assert results[i].params.param_dict == {'a': v}
+        assert results[i].measurements == {'q': np.array([[0]], dtype='uint8')}
+    build.assert_called_with('quantum',
+                             'v1alpha1',
+                             discoveryServiceUrl=('https://{api}.googleapis.com'
+                                                  '/$discovery/rest?version='
+                                                  '{apiVersion}'),
+                             requestBuilder=mock.ANY)
+    assert programs.create.call_args[1]['parent'] == 'projects/project-id'
+    sweeps = jobs.create.call_args[1]['body']['run_context']['parameter_sweeps']
+    assert len(sweeps) == 2
+    for i, v in enumerate([1, 2]):
+        assert sweeps[i]['repetitions'] == 1
+        assert sweeps[i]['sweep']['factors'][0]['sweeps'][0]['points'][
+            'points'] == [v]
+    assert jobs.create.call_args[1][
+        'parent'] == 'projects/project-id/programs/test'
+    assert jobs.get().execute.call_count == 1
+    assert jobs.getResult().execute.call_count == 1
+
+
+@mock.patch.object(discovery, 'build')
 def test_run_sweep_v1(build):
     service = mock.Mock()
     build.return_value = service
@@ -589,6 +646,63 @@ def test_run_sweep_v2(build):
         }
     }
     jobs.getResult().execute.return_value = {'result': _RESULTS_V2}
+
+    engine = cg.Engine(
+        project_id='project-id',
+        proto_version=cg.engine.engine.ProtoVersion.V2,
+    )
+    job = engine.run_sweep(program=_SCHEDULE,
+                           job_config=cg.JobConfig(
+                               'project-id', gcs_prefix='gs://bucket/folder'),
+                           params=cirq.Points('a', [1, 2]))
+    results = job.results()
+    assert engine.proto_version == cg.engine.engine.ProtoVersion.V2
+    assert len(results) == 2
+    for i, v in enumerate([1, 2]):
+        assert results[i].repetitions == 1
+        assert results[i].params.param_dict == {'a': v}
+        assert results[i].measurements == {'q': np.array([[0]], dtype='uint8')}
+    build.assert_called_with('quantum',
+                             'v1alpha1',
+                             discoveryServiceUrl=('https://{api}.googleapis.com'
+                                                  '/$discovery/rest?version='
+                                                  '{apiVersion}'),
+                             requestBuilder=mock.ANY)
+    assert programs.create.call_args[1]['parent'] == 'projects/project-id'
+    sweeps = jobs.create.call_args[1]['body']['run_context']['parameterSweeps']
+    assert len(sweeps) == 1
+    assert sweeps[0]['repetitions'] == 1
+    assert sweeps[0]['sweep']['singleSweep']['points']['points'] == [1, 2]
+    assert jobs.create.call_args[1][
+        'parent'] == 'projects/project-id/programs/test'
+    assert jobs.get().execute.call_count == 1
+    assert jobs.getResult().execute.call_count == 1
+
+
+@mock.patch.object(discovery, 'build')
+def test_run_sweep_v2_new_proto(build):
+    service = mock.Mock()
+    build.return_value = service
+    programs = service.projects().programs()
+    jobs = programs.jobs()
+    programs.create().execute.return_value = {
+        'name': 'projects/project-id/programs/test'
+    }
+    jobs.create().execute.return_value = {
+        'name': 'projects/project-id/programs/test/jobs/test',
+        'executionStatus': {
+            'state': 'READY'
+        }
+    }
+    jobs.get().execute.return_value = {
+        'name': 'projects/project-id/programs/test/jobs/test',
+        'executionStatus': {
+            'state': 'SUCCESS'
+        }
+    }
+    results_new_proto = copy.deepcopy(_RESULTS_V2)
+    results_new_proto['@type'] = 'type.googleapis.com/cirq.google.api.v2.Result'
+    jobs.getResult().execute.return_value = {'result': results_new_proto}
 
     engine = cg.Engine(
         project_id='project-id',
