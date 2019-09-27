79022f972ef90ec64fcfcf95d53de4dff91be109
Bryan A. O'Gorman <bryan.a.ogorman@gmail.com>
2019-09-26 23:15:02 -0700

    formatting

--- cirq/contrib/routing/swap_network_test.py
+++ cirq/contrib/routing/swap_network_test.py
@@ -64,8 +64,9 @@ def test_swap_network_str():
     phys_qubits = cirq.GridQubit.rect(n_qubits, 1)
     log_qubits = cirq.LineQubit.range(n_qubits)
 
-    gates = {(l, ll): cirq.ZZ
-            for l, ll in itertools.combinations(log_qubits, 2)}
+    gates = {
+        (l, ll): cirq.ZZ for l, ll in itertools.combinations(log_qubits, 2)
+    }
     initial_mapping = {p: l for p, l in zip(phys_qubits, log_qubits)}
     execution_strategy = cca.GreedyExecutionStrategy(gates, initial_mapping)
     routed_circuit = cca.complete_acquaintance_strategy(phys_qubits, 2)
