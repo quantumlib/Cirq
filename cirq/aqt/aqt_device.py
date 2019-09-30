a095c3d5eca6e69f27cdbe6b8bd0272f65272ebb
Dave Bacon <dabacon@gmail.com>
2019-09-30 09:42:05 -0700

    format

--- cirq/aqt/aqt_device.py
+++ cirq/aqt/aqt_device.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Current device parameters for the AQT/UIBK ion trap device
 
 The device is based on a linear calcium ion string with
--- cirq/experiments/fidelity_estimation.py
+++ cirq/experiments/fidelity_estimation.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Estimation of fidelity associated with experimental circuit executions."""
 
 from typing import Sequence
