b09bbfa2eaa38ed47c9954d42a71736433cd3f3d
Tony Bruguier <tonybruguier@google.com>
2019-09-30 21:31:03 -0700

    show matt

--- examples/fidelity.py
+++ examples/fidelity.py
@@ -1,5 +1,4 @@
-"""
-Simulates the fidelity estimation.
+"""Simulates the fidelity estimation.
 
 Direct Fidelity Estimation from Few Pauli Measurements
 https://arxiv.org/abs/1104.4695
@@ -9,7 +8,9 @@ https://arxiv.org/abs/1104.3835
 """
 
 import cirq
+import heapq
 import itertools
+
 
 def build_circuit():
   qubits = [cirq.LineQubit(i) for i in range(3)]
@@ -23,17 +24,51 @@ def build_circuit():
 
   return circuit, qubits
 
+
+def rotated_trace(mat):
+  return abs(sum([mat[i][i] for i in range(len(mat))]))
+
+
 def main():
   circuit, qubits = build_circuit()
 
   n = len(qubits)
+  d = 2**n
 
-  for pauli_ops in itertools.product({cirq.I, cirq.X, cirq.Y, cirq.Z}, repeat=n):
+  highest_rho_s = []
+
+  for i, pauli_ops in enumerate(
+      itertools.product({cirq.I, cirq.X, cirq.Y, cirq.Z}, repeat=n)):
     op_dict = dict(zip(qubits, pauli_ops))
 
     circuit_copy = circuit.copy()
     circuit_copy.append(cirq.PauliString(op_dict))
-    circuit_copy.append(cirq.measure(*qubits, key='measure'))
+
+    rho_i = rotated_trace(circuit_copy.unitary())
+
+    if rho_i > 0:
+      heapq.heappush(highest_rho_s, (rho_i, i, pauli_ops))
+    if len(highest_rho_s) > n:
+      heapq.heappop(highest_rho_s)
+
+  noisy_gate = cirq.amplitude_damp(0.01)
+
+  fidelity = 0.0
+  for rho_tuple in highest_rho_s:
+    rho_i = rho_tuple[0]
+    Pr_i = rho_i * rho_i / d
+    op_dict = dict(zip(qubits, rho_tuple[2]))
+
+    circuit_copy = circuit.copy()
+    for qubit in qubits:
+      circuit_copy.append(noisy_gate(qubit))
+    circuit_copy.append(cirq.PauliString(op_dict))
+
+    sigma_i = rotated_trace(circuit_copy.unitary())
+    fidelity += Pr_i * sigma_i / rho_i
+
+  print(fidelity)
+
 
 if __name__ == '__main__':
   main()
