6c69515f4d5d3e9cf6d72462e4a11106a4e9d7d9
Craig Gidney <craiggidney@google.com>
2019-10-02 22:47:46 -0700

    comments and polish

--- cirq/contrib/quirk/cells/frequency_space_cells.py
+++ cirq/contrib/quirk/cells/frequency_space_cells.py
@@ -1,4 +1,4 @@
-# Copyright 2018 The Cirq Developers
+# Copyright 2019 The Cirq Developers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,25 +25,25 @@ from cirq.contrib.quirk.cells.cell import (
 
 def generate_all_frequency_space_cell_makers() -> Iterator[CellMaker]:
     # Frequency space.
-    yield from reg_family("QFT", lambda n: cirq.QuantumFourierTransformGate(n))
-    yield from reg_family(
+    yield from _family("QFT", lambda n: cirq.QuantumFourierTransformGate(n))
+    yield from _family(
         "QFT†", lambda n: cirq.inverse(cirq.QuantumFourierTransformGate(n)))
-    yield from reg_family(
+    yield from _family(
         "PhaseGradient", lambda n: cirq.PhaseGradientGate(num_qubits=n,
                                                           exponent=0.5))
-    yield from reg_family(
+    yield from _family(
         "PhaseUngradient", lambda n: cirq.PhaseGradientGate(num_qubits=n,
                                                             exponent=-0.5))
-    yield from reg_family(
+    yield from _family(
         "grad^t", lambda n: cirq.PhaseGradientGate(
             num_qubits=n, exponent=2**(n - 1) * sympy.Symbol('t')))
-    yield from reg_family(
+    yield from _family(
         "grad^-t", lambda n: cirq.PhaseGradientGate(
             num_qubits=n, exponent=-2**(n - 1) * sympy.Symbol('t')))
 
 
-def reg_family(identifier_prefix: str,
-               gate_maker: Callable[[int], cirq.Gate]) -> Iterator[CellMaker]:
+def _family(identifier_prefix: str,
+            gate_maker: Callable[[int], cirq.Gate]) -> Iterator[CellMaker]:
     f = lambda args: ExplicitOperationsCell(
         [gate_maker(len(args.qubits)).on(*args.qubits)])
     yield CellMaker(identifier_prefix, 1, f)
