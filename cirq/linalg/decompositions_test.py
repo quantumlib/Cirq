5dbfd60b1b8e74d34859d3d5091acfa85803724f
dkafri <dkafri@google.com>
2019-10-07 17:22:53 -0700

    format

--- cirq/linalg/decompositions_test.py
+++ cirq/linalg/decompositions_test.py
@@ -616,11 +616,12 @@ def test_kak_vector_on_weyl_chamber_face():
     k_vecs = np.zeros((10, 3))
     k_vecs[:, (0, 1)] = theta_swap[:, np.newaxis]
 
-    kwargs = dict(global_phase=1j, single_qubit_operations_before=(X, Y),
+    kwargs = dict(global_phase=1j,
+                  single_qubit_operations_before=(X, Y),
                   single_qubit_operations_after=(Z, 1j * X))
     unitaries = np.array([
-        cirq.unitary(cirq.KakDecomposition(interaction_coefficients=(t, t, 0),
-                                           **kwargs))
+        cirq.unitary(
+            cirq.KakDecomposition(interaction_coefficients=(t, t, 0), **kwargs))
         for t in theta_swap
     ])
 
