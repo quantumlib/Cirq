33afb835f4c6a2038f67f64df59a920e82eebf55
Matthew Neeley <maffoo@google.com>
2019-10-17 13:46:08 -0700

    Allow creating circuit from pre-parsed quirk json data

--- cirq/contrib/quirk/__init__.py
+++ cirq/contrib/quirk/__init__.py
@@ -24,4 +24,5 @@ from cirq.contrib.quirk.export_to_quirk import (
     circuit_to_quirk_url,)
 
 from cirq.contrib.quirk.url_to_circuit import (
+    quirk_json_to_circuit,
     quirk_url_to_circuit,)
