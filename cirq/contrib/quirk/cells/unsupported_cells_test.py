5a5a2628b81f0c21b1e40bf697f82c5bfac8e6f3
Craig Gidney <craiggidney@google.com>
2019-10-19 01:43:31 -0700

    fix test

--- cirq/contrib/quirk/cells/unsupported_cells_test.py
+++ cirq/contrib/quirk/cells/unsupported_cells_test.py
@@ -18,10 +18,10 @@ from cirq.contrib.quirk.url_to_circuit import quirk_url_to_circuit
 
 
 def test_non_physical_operations():
-    with pytest.raises(NotImplementedError, match="Unphysical operation"):
+    with pytest.raises(NotImplementedError, match="unphysical operation"):
         _ = quirk_url_to_circuit('https://algassert.com/quirk#circuit={"cols":['
                                  '["__error__"]]}')
-    with pytest.raises(NotImplementedError, match="Unphysical operation"):
+    with pytest.raises(NotImplementedError, match="unphysical operation"):
         _ = quirk_url_to_circuit('https://algassert.com/quirk#circuit={"cols":['
                                  '["__unstable__UniversalNot"]]}')
 
