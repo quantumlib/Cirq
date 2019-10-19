e6e9f75862ebe17631d3110e9fbae799ea1a83cf
Craig Gidney <craiggidney@google.com>
2019-10-18 19:15:29 -0700

    cover

--- cirq/protocols/mul_protocol_test.py
+++ cirq/protocols/mul_protocol_test.py
@@ -90,3 +90,5 @@ def test_symbol_special_case():
     x = sympy.Symbol('x')
     assert cirq.mul(x, 1.0) is x
     assert cirq.mul(1.0, x) is x
+    assert str(cirq.mul(-1.0, x)) == '-x'
+    assert str(cirq.mul(x, -1.0)) == '-x'
