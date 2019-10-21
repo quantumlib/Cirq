ff208180bc545395ecde5a3fc5fd95fe6532f88f
Matthew Neeley <maffoo@google.com>
2019-10-21 08:36:12 -0700

    Include mypy stubs for protos in cirq packages. (#2383)
    This allows typechecking of protos in code that depends on cirq now that we ship a `py.typed` file.

--- setup.py
+++ setup.py
@@ -71,8 +71,8 @@ setup(name=name,
       packages=cirq_packages,
       package_data={
           'cirq': ['py.typed'],
-          'cirq.api.google.v1': ['*.proto'],
-          'cirq.api.google.v2': ['*.proto'],
-          'cirq.google.api.v1': ['*.proto'],
-          'cirq.google.api.v2': ['*.proto'],
+          'cirq.api.google.v1': ['*.proto', '*.pyi'],
+          'cirq.api.google.v2': ['*.proto', '*.pyi'],
+          'cirq.google.api.v1': ['*.proto', '*.pyi'],
+          'cirq.google.api.v2': ['*.proto', '*.pyi'],
       })
