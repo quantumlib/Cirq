862458f288152ea091a3cc101de3706f49df7554
Abhishek Chakraborty <12121913+deadbeatfour@users.noreply.github.com>
2019-10-23 23:39:34 +0530

    Better formatting in examples_test.py

--- examples/examples_test.py
+++ examples/examples_test.py
@@ -25,16 +25,16 @@ import examples.shor
 import examples.superdense_coding
 import examples.swap_networks
 
-    
+
 def test_example_runs_bernstein_vazirani():
     examples.bernstein_vazirani.main(qubit_count=3)
 
     # Check empty oracle case. Cover both biases.
     a = cirq.NamedQubit('a')
-    assert list(examples.bernstein_vazirani.make_oracle(
-        [], a, [], False)) == []
-    assert list(examples.bernstein_vazirani.make_oracle(
-        [], a, [], True)) == [cirq.X(a)]
+    assert list(examples.bernstein_vazirani.make_oracle([], a, [],
+                                                        False)) == []
+    assert list(examples.bernstein_vazirani.make_oracle([], a, [],
+                                                        True)) == [cirq.X(a)]
 
 
 def test_example_runs_deutsch():
@@ -52,8 +52,10 @@ def test_example_runs_hello_qubit():
 def test_example_runs_bell_inequality():
     examples.bell_inequality.main()
 
+
 def test_example_runs_bb84():
     examples.bb84.main()
+
 
 def test_example_runs_quantum_fourier_transform():
     examples.quantum_fourier_transform.main()
@@ -72,7 +74,7 @@ def test_example_runs_basic_arithmetic():
 
 
 def test_example_runs_phase_estimator():
-    examples.phase_estimator.main(qnums=(2,), repetitions=2)
+    examples.phase_estimator.main(qnums=(2, ), repetitions=2)
 
 
 def test_example_runs_bristlecone_heatmap():
