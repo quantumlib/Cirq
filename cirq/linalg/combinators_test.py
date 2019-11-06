dc96a060b1fe63919f651cf8762dc058692ab9d0
Craig Gidney <craiggidney@google.com>
2019-11-05 17:10:13 -0800

    Define cirq.STATE_VECTOR_LIKE (#2376)
    - Add list-of-qid-values to the supported STATE_VECTOR_LIKE types
    - Add tensor-of-amplitudes to the supported STATE_VECTOR_LIKE types
    - Add non-numpy sequences to supported STATE_VECTOR_LIKE types
    - Infer values-vs-tensor-vs-vector using shape, fail when ambiguous (can only occur for non-standard shapes involving qudits of dimension 1)
    - Make `num_qubits` argument of `to_valid_state_vector` optional (if `qid_shape` specified)

--- cirq/linalg/combinators_test.py
+++ cirq/linalg/combinators_test.py
@@ -40,6 +40,14 @@ def test_dot():
 
 
 def test_kron_multiplies_sizes():
+    assert cirq.kron(np.array([1, 2])).shape == (1, 2)
+    assert cirq.kron(np.array([1, 2]), shape_len=1).shape == (2,)
+    assert cirq.kron(np.array([1, 2]), np.array([3, 4, 5]),
+                     shape_len=1).shape == (6,)
+    assert cirq.kron(shape_len=0).shape == ()
+    assert cirq.kron(shape_len=1).shape == (1,)
+    assert cirq.kron(shape_len=2).shape == (1, 1)
+
     assert np.allclose(cirq.kron(1j, np.array([2, 3])), np.array([2j, 3j]))
     assert np.allclose(cirq.kron(), np.eye(1))
     assert np.allclose(cirq.kron(np.eye(1)), np.eye(1))
