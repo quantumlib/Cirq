f395ad439ed42cf5475e5e85c8068f55d5d7c062
Craig Gidney <craiggidney@google.com>
2019-11-18 23:25:34 -0800

    Implement decompose_two_qubit_interaction_into_four_fsim_gates_via_bdecompose
    - This code is not very performant, but it does the job

--- cirq/linalg/decompositions_test.py
+++ cirq/linalg/decompositions_test.py
@@ -293,6 +293,16 @@ def test_kak_decomposition_eq():
         single_qubit_operations_after=(np.eye(2), cirq.unitary(cirq.Z)),
     ))
 
+    eq.add_equality_group(
+        cirq.KakDecomposition(
+            global_phase=1,
+            single_qubit_operations_before=(np.eye(2), np.eye(2)),
+            interaction_coefficients=(0.3, 0.2, 0.1),
+            single_qubit_operations_after=(np.eye(2), np.eye(2)),
+        ),
+        cirq.KakDecomposition(interaction_coefficients=(0.3, 0.2, 0.1)),
+    )
+
     eq.make_equality_group(lambda: cirq.KakDecomposition(
         global_phase=1,
         single_qubit_operations_before=(cirq.unitary(cirq.X),
