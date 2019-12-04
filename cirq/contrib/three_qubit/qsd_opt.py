5c9ac35639b9da33bc47b2fd615d2ab55ca06d38
balopat <balintp@google.com>
2019-12-03 19:39:20 -0800

    reproduced the 20 CNOT 3 qubit result

--- cirq/contrib/three_qubit/qsd_opt.py
+++ cirq/contrib/three_qubit/qsd_opt.py
@@ -12,12 +12,16 @@ H = H + H.conj().T
 D, U = eigh(H)
 np.set_printoptions(precision=2, suppress=False, linewidth=300,
                     floatmode='maxprec_equal')
+print("Randomly generated U: ")
 print(U)
+
+
+
+n_qubits = 3
+M = 2 ** n_qubits  # size of unitary matrix
 
 P = int(M / 2)  # number of rows in upper left block
 u1, u2, v1h, v2h, theta = cs_decomp(U, P, P)
-
-decimals = 14  # desired precision
 
 z = np.zeros((P, P))
 
@@ -35,22 +39,23 @@ C = np.diag(np.cos(theta))
 S = np.diag(np.sin(theta))
 CS = np.vstack((np.hstack((C, -S)), np.hstack((S, C))))
 
-assert_almost_equal(U, UD @ CS @ VDH, decimals)
+assert_almost_equal(U, UD @ CS @ VDH)
 
 a, b, c = cirq.LineQubit.range(3)
 # Note: we are using / 2 as the thetas are already half angles - and ry takes
 # full angles.
 # we are using CZ's as an optimization as per Appendix A.1 in
 circuit_CS = cirq.Circuit([
-    cirq.ry((theta[0]+theta[1]+theta[2]+theta[3])/2).on(a),
-    cirq.CZ(b,a),
-    cirq.ry((theta[0]+theta[1]-theta[2]-theta[3])/2).on(a),
-    cirq.CZ(c,a),
-    cirq.ry((theta[0]-theta[1]-theta[2]+theta[3])/2).on(a),
-    cirq.CZ(b,a),
-    cirq.ry((theta[0]-theta[1]+theta[2]-theta[3])/2).on(a)])
+    cirq.ry((theta[0] + theta[1] + theta[2] + theta[3]) / 2).on(a),
+    cirq.CZ(b, a),
+    cirq.ry((theta[0] + theta[1] - theta[2] - theta[3]) / 2).on(a),
+    cirq.CZ(c, a),
+    cirq.ry((theta[0] - theta[1] - theta[2] + theta[3]) / 2).on(a),
+    cirq.CZ(b, a),
+    cirq.ry((theta[0] - theta[1] + theta[2] - theta[3]) / 2).on(a)])
 
-#print(circuit_CS)
+
+# print(circuit_CS)
 
 ##
 ## What we need is ..
@@ -62,15 +67,31 @@ circuit_CS = cirq.Circuit([
 ##
 # print (circuit_CS._unitary_())
 # print(CS)
-#assert_almost_equal(circuit_CS._unitary_(), CS, decimals)
+# assert_almost_equal(circuit_CS._unitary_(), CS, decimals)
 
 
 # print(np.cos(theta))
 # print(np.sin(theta))
 # for th in theta:
 #     print(cirq.ry(th*2)._unitary_())
+def special(u):
+    return u / (np.linalg.det(u) ** (1 / 4))
 
-def multiplexor_to_circuit(u1, u2):
+
+def g(u):
+    yy = np.kron(cirq.Y._unitary_(), cirq.Y._unitary_())
+    return u @ yy @ u.T @ yy
+
+
+def extract_right_diag(a,b,U):
+    u = special(U)
+    t = g(u.T).T.diagonal()
+    psi = np.arctan(np.imag(np.sum(t)) / np.real(t[0] + t[3] - t[1] - t[2]))
+    c_d = cirq.Circuit([cirq.CNOT(a, b), cirq.rz(psi)(b), cirq.CNOT(a, b)])
+    return c_d._unitary_()
+
+
+def multiplexor_to_circuit(u1, u2, shiftLeft=True, diagonal=np.eye(4)):
     u1u2 = u1 @ u2.conj().T
     eigvals, V = np.linalg.eig(u1u2)
 
@@ -129,40 +150,51 @@ def multiplexor_to_circuit(u1, u2):
     #     print(circuit_u1u2_mid._unitary_())
     np.testing.assert_almost_equal(mid, circuit_u1u2_mid._unitary_())
 
-    circuit_u1u2_left = cirq.Circuit(
+    # TODO:
+    # extract diagonal if CNOT count is 3
+    # otherwise just do a KAK
+    V = diagonal @ V
+    dV = extract_right_diag(b,c,V)
+    V = V @ dV
+    circuit_u1u2_R = cirq.Circuit(
         cirq.optimizers.two_qubit_matrix_to_operations(b, c, V,
                                                        allow_partial_czs=False))
+    # print(circuit_u1u2_R)
     cirq.testing.assert_allclose_up_to_global_phase(V,
-                                                    circuit_u1u2_left._unitary_(),
+                                                    circuit_u1u2_R._unitary_(),
                                                     atol=1e-8)
-    cirq.linalg.match_global_phase(V, circuit_u1u2_left._unitary_())
 
-    circuit_u1u2_right = cirq.Circuit(
+    W = dV.conj().T @ W
+    dW = extract_right_diag(b,c,W)
+    if shiftLeft:
+        W = W @ dW
+    circuit_u1u2_L = cirq.Circuit(
         cirq.optimizers.two_qubit_matrix_to_operations(b, c, W,
                                                        allow_partial_czs=False))
     cirq.testing.assert_allclose_up_to_global_phase(W,
-                                                    circuit_u1u2_right._unitary_(),
+                                                    circuit_u1u2_L._unitary_(),
                                                     atol=1e-8)
-    cirq.linalg.match_global_phase(W, circuit_u1u2_right._unitary_())
 
-    return cirq.Circuit(
-        [circuit_u1u2_right, circuit_u1u2_mid, circuit_u1u2_left])
+
+    return dW.conj().T, cirq.Circuit(
+        [circuit_u1u2_L,
+         circuit_u1u2_mid,
+         circuit_u1u2_R])
 
 
 # optimization A.1 - merging the CZ(c,a) from the end of CS into UD
-u2 =  u2 @ np.kron(np.eye(2), np.array([[1,0],[0,-1]]))
-UD =  UD @ cirq.Circuit(cirq.CZ(c,a), cirq.IdentityGate(1).on(b))._unitary_()
+u2 = u2 @ np.kron(np.eye(2), np.array([[1, 0], [0, -1]]))
+UD = UD @ cirq.Circuit(cirq.CZ(c, a), cirq.IdentityGate(1).on(b))._unitary_()
 
-c_UD = multiplexor_to_circuit(u1, u2)
-cirq.testing.assert_allclose_up_to_global_phase(UD, c_UD._unitary_(), atol=1e-8)
+dUD, c_UD = multiplexor_to_circuit(u1, u2, shiftLeft=True)
+cirq.testing.assert_allclose_up_to_global_phase(UD, c_UD._unitary_() @ np.kron(np.eye(2), dUD), atol=1e-8)
 
+dVDH, c_VDH = multiplexor_to_circuit(v1h, v2h, shiftLeft=False, diagonal=dUD)
 
-c_VDH = multiplexor_to_circuit(v1h, v2h)
-
-cirq.testing.assert_allclose_up_to_global_phase(VDH, c_VDH._unitary_(),
+cirq.testing.assert_allclose_up_to_global_phase(np.kron(np.eye(2), dUD) @ VDH, c_VDH._unitary_(),
                                                 atol=1e-8)
 
-final_circuit = cirq.Circuit([c_VDH,  circuit_CS, c_UD])
+final_circuit = cirq.Circuit([c_VDH, circuit_CS, c_UD])
 cirq.testing.assert_allclose_up_to_global_phase(U, final_circuit._unitary_(),
                                                 atol=1e-8)
 
@@ -170,5 +202,5 @@ print(final_circuit)
 print(sum(
     [1 for op in final_circuit.all_operations() if op.gate.num_qubits() == 2]),
     len(final_circuit))
-for op in final_circuit.all_operations():
-    if op.gate.num_qubits() == 2:  print(op)
+# for op in final_circuit.all_operations():
+#     if op.gate.num_qubits() == 2:  print(op)
