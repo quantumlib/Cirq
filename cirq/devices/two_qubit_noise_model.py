9a425aac95044e5254242ba2c448b66cbdcb2a43
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2019-12-19 16:48:38 +0000

    ran format-incremental

--- cirq/devices/two_qubit_noise_model.py
+++ cirq/devices/two_qubit_noise_model.py
@@ -25,9 +25,9 @@ class TwoQubitNoiseModel(cirq.NoiseModel):
         self.two_qubit_noise_gate = two_qubit_noise_gate
 
     def noisy_operation(
-            self, operation: cirq.Operation
-    ) -> Tuple[cirq.Operation, Union[List[cirq.GateOperation], cirq.
-                                     GateOperation]]:
+        self, operation: cirq.Operation
+    ) -> Tuple[cirq.Operation, Union[List[cirq.GateOperation],
+                                     cirq.GateOperation]]:
         """
         Checks if the gate in the operation is a one- or two- qubit gate,
         and applies self.single_qubit_noise_gate or self.two_qubit_noise_gate appropriately.
