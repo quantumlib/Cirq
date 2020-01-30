6b750fb6429c292a05ca8a15b3b63d4c1b510264
Dmytro Fedoriaka <fedimser@yandex.ru>
2020-01-30 09:52:37 +0000

    Added test for gate count in general case

--- cirq/optimizers/controlled_gate_decomposition_test.py
+++ cirq/optimizers/controlled_gate_decomposition_test.py
@@ -114,13 +114,28 @@ def _decomposition_size(U, controls_count):
 
 
 def test_decompose_size_special_unitary():
-    matrix = _random_special_unitary()
-    assert _decomposition_size(matrix, 0) == (1, 0, 0)
-    assert _decomposition_size(matrix, 1) == (3, 2, 0)
-    assert _decomposition_size(matrix, 2) == (8, 8, 0)
-    assert _decomposition_size(matrix, 3) == (8, 6, 2)
-    assert _decomposition_size(matrix, 4) == (24, 18, 4)
-    assert _decomposition_size(matrix, 5) == (40, 30, 12)
+    np.random.seed(0)
+    u = _random_special_unitary()
+    assert _decomposition_size(u, 0) == (1, 0, 0)
+    assert _decomposition_size(u, 1) == (3, 2, 0)
+    assert _decomposition_size(u, 2) == (8, 8, 0)
+    assert _decomposition_size(u, 3) == (8, 6, 2)
+    assert _decomposition_size(u, 4) == (24, 18, 4)
+    assert _decomposition_size(u, 5) == (40, 30, 12)
     for i in range(6, 20):
-        assert _decomposition_size(matrix,
-                                   i) == (64 * i - 312, 48 * i - 234, 16)
+        assert _decomposition_size(u, i) == (64 * i - 312, 48 * i - 234, 16)
+
+
+def test_decompose_size_unitary():
+    np.random.seed(0)
+    u = _random_unitary()
+    assert _decomposition_size(u, 0) == (1, 0, 0)
+    assert _decomposition_size(u, 1) == (4, 2, 0)
+    assert _decomposition_size(u, 2) == (12, 8, 0)
+    assert _decomposition_size(u, 3) == (20, 12, 2)
+    assert _decomposition_size(u, 4) == (44, 28, 6)
+    assert _decomposition_size(u, 5) == (84, 56, 18)
+    assert _decomposition_size(u, 6) == (172, 120, 26)
+    assert _decomposition_size(u, 7) == (340, 244, 38)
+    assert _decomposition_size(u, 8) == (524, 380, 46)
+    assert _decomposition_size(u, 9) == (820, 600, 58)
