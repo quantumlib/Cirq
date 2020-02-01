f569ae154a3bddfc43755263dcc18e4a12487d86
Kevin J. Sung <kevinjefferysung@gmail.com>
2020-01-31 16:26:10 -0800

    document type and fix docstring

--- cirq/value/random_state.py
+++ cirq/value/random_state.py
@@ -12,21 +12,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import cast, Any, Optional, Union
+from typing import cast, Any
 
 import numpy as np
 
-RANDOM_STATE_LIKE = Optional[Union[np.random.RandomState, int, Any]]
+from cirq._doc import document
+
+RANDOM_STATE_LIKE = Any
+document(
+    RANDOM_STATE_LIKE,
+    """A pseudorandom number generator or object that can be converted to one.
+
+    If None, turns into the module `np.random`.
+
+    If an integer, turns into a `np.random.RandomState` seeded with that
+    integer.
+
+    If none of the above, it is used unmodified. In this case, it is assumed
+    that the object implements whatever methods are required for the use case
+    at hand. For example, it might be an existing instance of
+    `np.random.RandomState`.
+    """)
 
 
 def parse_random_state(random_state: RANDOM_STATE_LIKE
                       ) -> np.random.RandomState:
-    """Interpret an object as the state of a pseudorandom number generator.
+    """Interpret an object as a pseudorandom number generator.
 
-    If `random_state` is None, returns the module `np.random`.
-    If `random_state` is an integer, returns
-    `np.random.RandomState(random_state)`.
-    Otherwise, returns `random_state` unmodified.
+    Args:
+        random_state: The object to be used as or converted to a pseudorandom
+            number generator.
+
+    Returns:
+        If `random_state` is None, returns the module `np.random`.
+        If `random_state` is an integer, returns
+        `np.random.RandomState(random_state)`.
+        Otherwise, returns `random_state` unmodified.
     """
     if random_state is None:
         return cast(np.random.RandomState, np.random)
