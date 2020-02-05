523a0b51f08307b5f305b65fa90440bbf856ec9a
Adam Zalcman <viathor@google.com>
2020-02-04 17:38:15 -0800

    Fix n-qubit state tomography

--- cirq/experiments/n_qubit_tomography_test.py
+++ cirq/experiments/n_qubit_tomography_test.py
@@ -11,10 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Sequence
 
 import numpy as np
+import pytest
 
 import cirq
+
+Q0, Q1, Q2, Q3 = cirq.LineQubit.range(4)
 
 
 def test_state_tomography_diagonal():
@@ -63,3 +67,64 @@ def test_make_experiment_no_rots():
          cirq.LineQubit(1),
          cirq.LineQubit(2)])
     assert len(exp.rot_sweep) > 0
+
+
+def compute_density_matrix(circuit: cirq.Circuit,
+                           qubits: Sequence[cirq.Qid]) -> np.ndarray:
+    """Computes density matrix prepared by circuit based on its unitary."""
+    u = circuit.unitary(qubit_order=qubits)
+    phi = u[:, 0]
+    rho = np.outer(phi, np.conjugate(phi))
+    return rho
+
+
+@pytest.mark.parametrize('circuit, qubits', (
+    (cirq.Circuit(cirq.X(Q0)**0.25), (Q0,)),
+    (cirq.Circuit(cirq.CNOT(Q0, Q1)**0.25), (Q0, Q1)),
+    (cirq.Circuit(cirq.CNOT(Q0, Q1)**0.25), (Q1, Q0)),
+    (cirq.Circuit(cirq.TOFFOLI(Q0, Q1, Q2)), (Q1, Q0, Q2)),
+    (cirq.Circuit(
+        cirq.H(Q0),
+        cirq.H(Q1),
+        cirq.CNOT(Q0, Q2),
+        cirq.CNOT(Q1, Q3),
+        cirq.X(Q0),
+        cirq.X(Q1),
+        cirq.CNOT(Q1, Q0),
+    ), (Q1, Q0, Q2, Q3)),
+))
+def test_density_matrix_from_state_tomography_is_correct(circuit, qubits):
+    sim = cirq.Simulator()
+    tomography_result = cirq.experiments.state_tomography(sim,
+                                                          qubits,
+                                                          circuit,
+                                                          repetitions=5000)
+    actual_rho = tomography_result.data
+    expected_rho = compute_density_matrix(circuit, qubits)
+    error_rho = actual_rho - expected_rho
+    assert np.linalg.norm(error_rho) < 0.05
+    assert np.max(np.abs(error_rho)) < 0.05
+
+
+@pytest.mark.parametrize('circuit', (
+    cirq.Circuit(cirq.CNOT(Q0, Q1)**0.3),
+    cirq.Circuit(cirq.H(Q0), cirq.CNOT(Q0, Q1)),
+    cirq.Circuit(cirq.X(Q0)**0.25, cirq.ISWAP(Q0, Q1)),
+))
+def test_agrees_with_two_qubit_state_tomography(circuit):
+    qubits = (Q0, Q1)
+    sim = cirq.Simulator()
+    tomography_result = cirq.experiments.state_tomography(sim,
+                                                          qubits,
+                                                          circuit,
+                                                          repetitions=5000)
+    actual_rho = tomography_result.data
+
+    two_qubit_tomography_result = cirq.experiments.two_qubit_state_tomography(
+        sim, qubits[0], qubits[1], circuit, repetitions=5000)
+    expected_rho = two_qubit_tomography_result.data
+
+    error_rho = actual_rho - expected_rho
+
+    assert np.linalg.norm(error_rho) < 0.05
+    assert np.max(np.abs(error_rho)) < 0.05
