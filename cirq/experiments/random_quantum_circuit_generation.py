23ce2b3997e2b3d2f791c1a394f9a91b04d9f3e6
Kevin J. Sung <kevinjefferysung@gmail.com>
2020-02-12 13:52:14 -0800

    format

--- cirq/experiments/random_quantum_circuit_generation.py
+++ cirq/experiments/random_quantum_circuit_generation.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Code for generating random quantum circuits."""
 
 from typing import (Callable, Container, Dict, Iterable, List, Sequence,
