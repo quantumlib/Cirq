24f34c89fc30d8a60c903ebd121452f1aa99a5f1
Doug Strain <dstrain@google.com>
2020-02-12 10:46:41 -0800

    review comments

--- cirq/google/ops/physical_z_tag.py
+++ cirq/google/ops/physical_z_tag.py
@@ -18,16 +18,16 @@ from cirq import protocols
 class PhysicalZTag:
     """Class to add as a tag onto an Operation to denote a Physical Z operation.
 
-  By default, all Z rotations on Google devices are considered to be virtual.
-  When performing the Z operation, the device will update its internal phase
-  tracking mechanisms, essentially commuting it forwards through the circuit
-  until it hits a non-commuting operation (Such as a sqrt(iSwap)).
+    By default, all Z rotations on Google devices are considered to be virtual.
+    When performing the Z operation, the device will update its internal phase
+    tracking mechanisms, essentially commuting it forwards through the circuit
+    until it hits a non-commuting operation (Such as a sqrt(iSwap)).
 
-  By attaching this to a Z rotation operation, this class will denote to the
-  hardware that an actual physical operation should be done instead.  This
-  class can only be applied to instances of `cirq.ZPowGate`.  If applied to
-  other gates (such as PhasedXZGate), this class will have no effect.
-  """
+    When applied to a Z rotation operation, this tag indicates to the hardware
+    that an actual physical operation should be done instead.  This class can
+    only be applied to instances of `cirq.ZPowGate`.  If applied to other gates
+    (such as PhasedXZGate), this class will have no effect.
+    """
 
     def __str__(self):
         return 'PhysicalZTag()'
