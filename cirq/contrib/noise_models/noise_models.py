b0fdeef876dfd12aaa309946fd85edaf555dd9ad
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2020-02-26 14:13:46 -0800

    Keep 'exp' import.

--- cirq/contrib/noise_models/noise_models.py
+++ cirq/contrib/noise_models/noise_models.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from math import exp
 from typing import Dict, Sequence, TYPE_CHECKING
 
 from cirq import devices, value, ops, protocols
