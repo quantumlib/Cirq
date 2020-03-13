fa98d62a26562bf0ba56cc5ee6dd81ebd9a1b6c9
Abhishek Chakraborty <abhishekchak52@gmail.com>
2020-03-14 00:37:39 +0530

    Added eavesdropping example

--- examples/bb84.py
+++ examples/bb84.py
@@ -82,15 +82,17 @@ Expected key:   110111
 Actual key:     110111
 
 """
-import random
 import cirq
+import numpy as np
 
 
 def main(num_qubits=8):
-
-    alice_basis = [random.randint(0, 1) for _ in range(num_qubits)]
-    alice_state = [random.randint(0, 1) for _ in range(num_qubits)]
-    bob_basis = [random.randint(0, 1) for _ in range(num_qubits)]
+    np.random.seed(200)
+    # Setup non-eavesdropped protocol
+    print('Simulating non-eavesdropped protocol')
+    alice_basis = [np.random.randint(0, 2) for _ in range(num_qubits)]
+    alice_state = [np.random.randint(0, 2) for _ in range(num_qubits)]
+    bob_basis = [np.random.randint(0, 2) for _ in range(num_qubits)]
 
     expected_key = bitstring([
         alice_state[i]
@@ -99,11 +101,10 @@ def main(num_qubits=8):
     ])
 
     circuit = make_bb84_circ(num_qubits, alice_basis, bob_basis, alice_state)
-    print(circuit)
 
     # Run simulations.
     repetitions = 1
-    print('Simulating...')
+
     result = cirq.Simulator().run(program=circuit, repetitions=repetitions)
     result_bitstring = bitstring(
         [int(result.measurements[str(i)]) for i in range(num_qubits)])
@@ -116,7 +117,55 @@ def main(num_qubits=8):
     ])
 
     assert expected_key == obtained_key, "Keys don't match"
+    print(circuit)
+    print_results(alice_basis, bob_basis, alice_state, expected_key,
+                  obtained_key)
 
+    # Setup eavesdropped protocol
+    print('Simulating eavesdropped protocol')
+    alice_basis = [np.random.randint(0, 2) for _ in range(num_qubits)]
+    alice_state = [np.random.randint(0, 2) for _ in range(num_qubits)]
+    bob_basis = [np.random.randint(0, 2) for _ in range(num_qubits)]
+    eve_basis = [np.random.randint(0, 2) for _ in range(num_qubits)]
+
+    expected_key = bitstring([
+        alice_state[i]
+        for i in range(num_qubits)
+        if alice_basis[i] == bob_basis[i]
+    ])
+
+    # Eve intercepts the qubits
+
+    alice_eve_circuit = make_bb84_circ(num_qubits, alice_basis, eve_basis,
+                                       alice_state)
+
+    # Run simulations.
+    repetitions = 1
+    result = cirq.Simulator().run(program=alice_eve_circuit,
+                                  repetitions=repetitions)
+    eve_state = [int(result.measurements[str(i)]) for i in range(num_qubits)]
+
+    eve_bob_circuit = make_bb84_circ(num_qubits, eve_basis, bob_basis,
+                                     eve_state)
+
+    # Run simulations.
+    repetitions = 1
+    result = cirq.Simulator().run(program=eve_bob_circuit,
+                                  repetitions=repetitions)
+    result_bitstring = bitstring(
+        [int(result.measurements[str(i)]) for i in range(num_qubits)])
+
+    # Take only qubits where bases match
+    obtained_key = ''.join([
+        result_bitstring[i]
+        for i in range(num_qubits)
+        if alice_basis[i] == bob_basis[i]
+    ])
+
+    assert expected_key != obtained_key, "Keys shoudn't match"
+
+    circuit = alice_eve_circuit + eve_bob_circuit
+    print(circuit)
     print_results(alice_basis, bob_basis, alice_state, expected_key,
                   obtained_key)
 
