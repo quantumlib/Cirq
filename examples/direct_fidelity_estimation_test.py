5299d0296817243184eeef7f5fd4fb717aa19329
Tony Bruguier <tonybruguier@google.com>
2020-03-14 12:36:48 -0700

    PR fix order of imports

--- examples/direct_fidelity_estimation_test.py
+++ examples/direct_fidelity_estimation_test.py
@@ -1,7 +1,7 @@
+import sys
 import numpy as np
 import cirq
 import examples.direct_fidelity_estimation as direct_fidelity_estimation
-import sys
 
 
 def test_direct_fidelity_estimation_no_noise_clifford():
