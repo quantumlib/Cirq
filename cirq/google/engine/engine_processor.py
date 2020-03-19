e151dc7ab6921c0e5d143460aff9e3c060692f98
William Courtney <wcourtney@google.com>
2020-03-19 08:43:49 -0700

    Fix filters to include time slots that do not start within the window. (#2848)

--- cirq/google/engine/engine_processor.py
+++ cirq/google/engine/engine_processor.py
@@ -266,9 +266,9 @@ class EngineProcessor:
         """
         filters = []
         if from_time:
-            filters.append(f'start_time >= {int(from_time.timestamp())}')
+            filters.append(f'start_time < {int(to_time.timestamp())}')
         if to_time:
-            filters.append(f'start_time <= {int(to_time.timestamp())}')
+            filters.append(f'end_time > {int(from_time.timestamp())}')
         filter_str = ' AND '.join(filters)
         return self.context.client.list_reservations(self.project_id,
                                                      self.processor_id,
@@ -288,9 +288,9 @@ class EngineProcessor:
         """
         filters = []
         if from_time:
-            filters.append(f'start_time >= {int(from_time.timestamp())}')
+            filters.append(f'start_time < {int(to_time.timestamp())}')
         if to_time:
-            filters.append(f'start_time <= {int(to_time.timestamp())}')
+            filters.append(f'end_time > {int(from_time.timestamp())}')
         if time_slot_type:
             raise ValueError('Filtering by time_slot_type not yet supported')
         filter_str = ' AND '.join(filters)
