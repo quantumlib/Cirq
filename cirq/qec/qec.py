d85704473015b3f59c8b22d4d26a4f2df4523438
vtomole <vtomole2@gmail.com>
2020-04-10 17:59:51 -0500

    qec init

---
+++ cirq/qec/qec.py
@@ -0,0 +1,21 @@
+import cirq
+
+# Circuit
+#     Measurement
+#     Output
+class Code:
+    def __init__(qubits):
+        self.input = qubits
+
+    def encode():
+        self.encoded_circuit = cirq.Cicuit
+    (cirq.CNOT, (1, 4)),
+    (cirq.CNOT, (1, 7)),
+    (cirq.H, (1)), (cirq.H, (4)), (cirq.H, (7)),
+    (cirq.CNOT, (1, 2)), (cirq.CNOT, (4, 5)), (cirq.CNOT, (7, 8)),
+    (cirq.CNOT, (1, 3)), (cirq.CNOT, (4, 6)), (cirq.CNOT, (7, 9))
+
+
+q0 = cirq.NamedQubit('a')
+mycode = Code(q0) 
+    
