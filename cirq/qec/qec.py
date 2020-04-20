fc8ad30b83d17d86717a51e9477cc823b3352041
crystalzhaizhai <31509747+crystalzhaizhai@users.noreply.github.com>
2020-04-19 22:21:10 -0700

    fixing the circuit error

--- cirq/qec/qec.py
+++ cirq/qec/qec.py
@@ -8,6 +8,24 @@ class Code:
     def __init__(self):
         self.q0, self.q1, self.q2, self.q3, self.q4, self.q5, self.q6, self.q7, self.q8 = cirq.LineQubit.range(9)
 
+
+    def encode(self):
+
+        self.current_circuit = cirq.Circuit(cirq.CNOT(self.q0, self.q3),
+                                            cirq.CNOT(self.q0, self.q6),
+                                            cirq.H(self.q0),
+                                            cirq.H(self.q3),
+                                            cirq.H(self.q6),
+                                            cirq.CNOT(self.q0, self.q1),
+                                            cirq.CNOT(self.q3, self.q4),
+                                            cirq.CNOT(self.q6, self.q7),
+                                            cirq.CNOT(self.q0, self.q2),
+                                            cirq.CNOT(self.q3, self.q5),
+                                            cirq.CNOT(self.q6, self.q8))
+
+        return self.current_circuit
+
+
     def decode(self):
         #cirq.CNOT(self.q0, self.q1),
         #cirq.CNOT(self.q3, self.q4),
@@ -29,32 +47,14 @@ class Code:
                                             cirq.CNOT(self.q0, self.q3),
                                             cirq.CNOT(self.q0, self.q6),
                                             cirq.CCNOT(self.q3, self.q6, self.q0))
-        print("Encoded and decoded ", self.encoded_circuit + self.decoded_circuit)
-        return self.encoded_circuit + self.decoded_circuit
+        self.current_circuit = self.current_circuit + self.decoded_circuit
+        return self.current_circuit
 
-
-
-
-    def encode(self):
-
-        self.encoded_circuit = cirq.Circuit(cirq.CNOT(self.q0, self.q3),
-                                            cirq.CNOT(self.q0, self.q6),
-                                            cirq.H(self.q0),
-                                            cirq.H(self.q3),
-                                            cirq.H(self.q6),
-                                            cirq.CNOT(self.q0, self.q1),
-                                            cirq.CNOT(self.q3, self.q4),
-                                            cirq.CNOT(self.q6, self.q7),
-                                            cirq.CNOT(self.q0, self.q2),
-                                            cirq.CNOT(self.q3, self.q5),
-                                            cirq.CNOT(self.q6, self.q8))
-
-        return self.encoded_circuit
 
     def apply_x_error(self):
-        self.encoded_circuit.append(cirq.X(self.q5))
+        self.current_circuit.append(cirq.Z(self.q5))
         #print(self.encoded_circuit)
-        return self.encoded_circuit
+        return self.current_circuit
 
     def measure(self):
         measure_circuit = cirq.Circuit(cirq.measure(self.q0), cirq.measure(self.q1),
@@ -67,10 +67,8 @@ class Code:
                                     cirq.measure(self.q8)
                                     )
 
-        self.encoded_circuit = self.decoded_circuit + measure_circuit
-        return self.encoded_circuit
-
-
+        self.current_circuit = self.current_circuit + measure_circuit
+        return self.current_circuit
 
 
 mycode = Code()
@@ -79,14 +77,12 @@ my_circuit = mycode.encode()
 
 my_circuit = mycode.apply_x_error()
 
-
 my_circuit = mycode.decode()
 
 my_circuit = mycode.measure()
 
+sim1 = cirq.Simulator()
+result = sim1.run(my_circuit, repetitions=20)
 
-
-
-#print(my_circuit)
-
-    
+print(my_circuit)
+print(result)
