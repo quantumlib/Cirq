8ac16837f34f1a45a6a6687739f5d0dafe8fc512
Dmytro Fedoriaka <fedimser@yandex.ru>
2020-05-25 11:45:21 +0100

    Address dabacon's comments

--- cirq/work/zeros_sampler.py
+++ cirq/work/zeros_sampler.py
@@ -20,14 +20,14 @@ import numpy as np
 from cirq import work, study, protocols
 
 if TYPE_CHECKING:
-    import cirq
-    from cirq.google.serializable_gate_set import SerializableGateSet
+    import cirq.google
 
 
 class ZerosSampler(work.Sampler, metaclass=abc.ABCMeta):
     """A dummy sampler for testing. Immediately returns zeroes."""
 
-    def __init__(self, gate_set: 'SerializableGateSet' = None):
+    def __init__(self,
+                 gate_set: 'cirq.google.serializable_gate_set.SerializableGateSet' = None):
         """
         Args:
             gate_set: `SerializableGateSet`. If set, sampler will validate that
@@ -55,7 +55,7 @@ class ZerosSampler(work.Sampler, metaclass=abc.ABCMeta):
         if self.gate_set is not None:
             for op in program.all_operations():
                 assert self.gate_set.is_supported_operation(op), (
-                    "Unsupported operation: %s" % op)
+                        "Unsupported operation: %s" % op)
 
         measurements = {}  # type: Dict[str, np.ndarray]
         for op in program.all_operations():
