17b024c1234bdbb64216bc7a44138879c9a6dc9b
Craig Gidney <craiggidney@google.com>
2020-05-27 15:15:06 -0700

    Test random behavior

--- cirq/sim/act_on_state_vector_args_test.py
+++ cirq/sim/act_on_state_vector_args_test.py
@@ -219,3 +219,18 @@ def test_probability_comes_up_short_results_in_fallback():
         args.target_tensor,
         np.array([0, 1]),
     )
+
+
+def test_random_channel_has_random_behavior():
+    q = cirq.LineQubit(0)
+    s = cirq.Simulator().sample(
+        cirq.Circuit(
+            cirq.X(q),
+            cirq.amplitude_damp(0.4).on(q),
+            cirq.measure(q, key='out'),
+        ),
+        repetitions=100,
+    )
+    v = s['out'].value_counts()
+    assert v[0] > 1
+    assert v[1] > 1
