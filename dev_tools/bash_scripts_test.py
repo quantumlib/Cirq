e56ac3bbbab0dcdb1cff234a32b57f8f399d5471
Dave Bacon <dabacon@gmail.com>
2020-06-02 10:08:21 -0700

    Testing github actions

--- dev_tools/bash_scripts_test.py
+++ dev_tools/bash_scripts_test.py
@@ -79,6 +79,7 @@ chmod +x ./test-script.sh
 @only_on_posix
 def test_pytest_changed_files_file_selection(tmpdir_factory):
 
+
     result = run(script_file='check/pytest-changed-files',
                  tmpdir_factory=tmpdir_factory,
                  arg='HEAD~1',
