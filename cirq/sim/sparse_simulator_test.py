Add KakDecomposition._decompose_ by fixing factor of 2 error in exp(PauliString) (#3125)
- I compared against results from WolframAlpha to make sure the modified tests are now correct

cirq/sim/sparse_simulator_test.py
@@ -695,7 +695,7 @@ def test_simulate_measurement_inversions():
 
 def test_works_on_pauli_string_phasor():
     a, b = cirq.LineQubit.range(2)
-    c = cirq.Circuit(np.exp(1j * np.pi * cirq.X(a) * cirq.X(b)))
+    c = cirq.Circuit(np.exp(0.5j * np.pi * cirq.X(a) * cirq.X(b)))
     sim = cirq.Simulator()
     result = sim.simulate(c).state_vector()
     np.testing.assert_allclose(result.reshape(4),