b3a16daf93eac3c9dddb172988d9f68f63fb8514
Dave Bacon <dabacon@gmail.com>
2020-07-09 21:56:03 -0700

    Support adding numpy arrays to GridQubits (#3131)

--- cirq/devices/grid_qubit_test.py
+++ cirq/devices/grid_qubit_test.py
@@ -15,6 +15,8 @@
 
 import pytest
 
+import numpy as np
+
 import cirq
 
 
@@ -217,29 +219,23 @@ def test_addition_subtraction():
     assert cirq.GridQubit(1, -2) + cirq.GridQubit(3, 5) == cirq.GridQubit(4, 3)
 
     # GridQids
-    assert cirq.GridQid(1, 2, dimension=3) + (2, 5) == cirq.GridQid(3,
-                                                                    7,
-                                                                    dimension=3)
-    assert cirq.GridQid(1, 2, dimension=3) + (0, 0) == cirq.GridQid(1,
-                                                                    2,
-                                                                    dimension=3)
-    assert cirq.GridQid(1, 2, dimension=3) + (-1, 0) == cirq.GridQid(
-        0, 2, dimension=3)
-    assert cirq.GridQid(1, 2, dimension=3) - (2, 5) == cirq.GridQid(-1,
-                                                                    -3,
-                                                                    dimension=3)
-    assert cirq.GridQid(1, 2, dimension=3) - (0, 0) == cirq.GridQid(1,
-                                                                    2,
-                                                                    dimension=3)
-    assert cirq.GridQid(1, 2, dimension=3) - (-1, 0) == cirq.GridQid(
-        2, 2, dimension=3)
+    assert (cirq.GridQid(1, 2, dimension=3) + (2, 5) == cirq.GridQid(
+        3, 7, dimension=3))
+    assert (cirq.GridQid(1, 2, dimension=3) + (0, 0) == cirq.GridQid(
+        1, 2, dimension=3))
+    assert (cirq.GridQid(1, 2, dimension=3) + (-1, 0) == cirq.GridQid(
+        0, 2, dimension=3))
+    assert (cirq.GridQid(1, 2, dimension=3) - (2, 5) == cirq.GridQid(
+        -1, -3, dimension=3))
+    assert (cirq.GridQid(1, 2, dimension=3) - (0, 0) == cirq.GridQid(
+        1, 2, dimension=3))
+    assert (cirq.GridQid(1, 2, dimension=3) - (-1, 0) == cirq.GridQid(
+        2, 2, dimension=3))
 
-    assert (2, 5) + cirq.GridQid(1, 2, dimension=3) == cirq.GridQid(3,
-                                                                    7,
-                                                                    dimension=3)
-    assert (2, 5) - cirq.GridQid(1, 2, dimension=3) == cirq.GridQid(1,
-                                                                    3,
-                                                                    dimension=3)
+    assert ((2, 5) + cirq.GridQid(1, 2, dimension=3) == cirq.GridQid(
+        3, 7, dimension=3))
+    assert ((2, 5) - cirq.GridQid(1, 2, dimension=3) == cirq.GridQid(
+        1, 3, dimension=3))
 
     assert cirq.GridQid(1, 2, dimension=3) + cirq.GridQid(
         3, 5, dimension=3) == cirq.GridQid(4, 7, dimension=3)
@@ -247,6 +243,35 @@ def test_addition_subtraction():
         2, 1, dimension=3) == cirq.GridQid(1, 4, dimension=3)
     assert cirq.GridQid(1, -2, dimension=3) + cirq.GridQid(
         3, 5, dimension=3) == cirq.GridQid(4, 3, dimension=3)
+
+
+@pytest.mark.parametrize('dtype', (np.int8, np.int16, np.int32, np.int64, int))
+def test_addition_subtraction_numpy_array(dtype):
+    assert cirq.GridQubit(1, 2) + np.array([1, 2],
+                                           dtype=dtype) == cirq.GridQubit(2, 4)
+    assert cirq.GridQubit(1, 2) + np.array([0, 0],
+                                           dtype=dtype) == cirq.GridQubit(1, 2)
+    assert (cirq.GridQubit(1, 2) +
+            np.array([-1, 0], dtype=dtype) == cirq.GridQubit(0, 2))
+    assert cirq.GridQubit(1, 2) - np.array([1, 2],
+                                           dtype=dtype) == cirq.GridQubit(0, 0)
+    assert cirq.GridQubit(1, 2) - np.array([0, 0],
+                                           dtype=dtype) == cirq.GridQubit(1, 2)
+    assert (cirq.GridQid(1, 2, dimension=3) -
+            np.array([-1, 0], dtype=dtype) == cirq.GridQid(2, 2, dimension=3))
+
+    assert cirq.GridQid(1, 2, dimension=3) + np.array(
+        [1, 2], dtype=dtype) == cirq.GridQid(2, 4, dimension=3)
+    assert cirq.GridQid(1, 2, dimension=3) + np.array(
+        [0, 0], dtype=dtype) == cirq.GridQid(1, 2, dimension=3)
+    assert (cirq.GridQid(1, 2, dimension=3) +
+            np.array([-1, 0], dtype=dtype) == cirq.GridQid(0, 2, dimension=3))
+    assert cirq.GridQid(1, 2, dimension=3) - np.array(
+        [1, 2], dtype=dtype) == cirq.GridQid(0, 0, dimension=3)
+    assert cirq.GridQid(1, 2, dimension=3) - np.array(
+        [0, 0], dtype=dtype) == cirq.GridQid(1, 2, dimension=3)
+    assert (cirq.GridQid(1, 2, dimension=3) -
+            np.array([-1, 0], dtype=dtype) == cirq.GridQid(2, 2, dimension=3))
 
 
 def test_unsupported_add():
@@ -261,6 +286,11 @@ def test_unsupported_add():
 
     with pytest.raises(TypeError, match='1'):
         _ = cirq.GridQubit(1, 1) - 1
+
+    with pytest.raises(TypeError, match='[1., 2.]'):
+        _ = cirq.GridQubit(1, 1) + np.array([1.0, 2.0])
+    with pytest.raises(TypeError, match='[1, 2, 3]'):
+        _ = cirq.GridQubit(1, 1) + np.array([1, 2, 3], dtype=int)
 
 
 def test_addition_subtraction_type_error():
