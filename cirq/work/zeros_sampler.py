dc7d59ec58d0dd5a23b7f3a7894479f9ee1154e5
Dave Bacon <dabacon@google.com>
2020-07-16 13:31:13 -0700

    Cleanup

--- cirq/work/zeros_sampler.py
+++ cirq/work/zeros_sampler.py
@@ -13,26 +13,15 @@
 # limitations under the License.
 
 import abc
-from typing import (Dict, List, TYPE_CHECKING)
+from typing import Dict, List
 
 import numpy as np
 
 from cirq import work, study, protocols
 
-if TYPE_CHECKING:
-    import cirq.google
-
 
 class ZerosSampler(work.Sampler, metaclass=abc.ABCMeta):
     """A dummy sampler for testing. Immediately returns zeroes."""
-
-    def __init__(self, gate_set: 'cirq.google.SerializableGateSet' = None):
-        """
-        Args:
-            gate_set: `SerializableGateSet`. If set, sampler will validate that
-                all gates in the circuit are from the given gate set.
-        """
-        self.gate_set = gate_set
 
     def run_sweep(
             self,
@@ -51,19 +40,13 @@ class ZerosSampler(work.Sampler, metaclass=abc.ABCMeta):
             TrialResult list for this run; one for each possible parameter
             resolver.
         """
-        if self.gate_set is not None:
-            for op in program.all_operations():
-                assert self.gate_set.is_supported_operation(op), (
-                    "Unsupported operation: %s" % op)
-
         measurements = {}  # type: Dict[str, np.ndarray]
         for op in program.all_operations():
             key = protocols.measurement_key(op, default=None)
             if key is not None:
                 measurements[key] = np.zeros((repetitions, len(op.qubits)),
-                                             dtype=np.int8)
+                                             dtype=int)
         return [
-            study.TrialResult.from_single_parameter_set(
-                params=param_resolver, measurements=measurements)
+            study.TrialResult(params=param_resolver, measurements=measurements)
             for param_resolver in study.to_resolvers(params)
         ]
