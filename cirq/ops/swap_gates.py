404fc57e6eab60c819367b18fb294461a2d80e38
Peter Karalekas <peter@karalekas.com>
2020-08-12 10:57:27 -0400

    Return Quil XY gate when ISwapPowGate has non-unity exponent

--- cirq/ops/swap_gates.py
+++ cirq/ops/swap_gates.py
@@ -262,7 +262,8 @@ class ISwapPowGate(eigen_gate.EigenGate,
                formatter: 'cirq.QuilFormatter') -> Optional[str]:
         if self._exponent == 1:
             return formatter.format('ISWAP {0} {1}\n', qubits[0], qubits[1])
-        return None  #ISwap with rotation not supported in Quil
+        return formatter.format('XY({0}) {1} {2}\n', self._exponent * np.pi,
+                                qubits[0], qubits[1])
 
 
 def riswap(rads: value.TParamVal) -> ISwapPowGate:
