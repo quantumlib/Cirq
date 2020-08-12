898adc92170e1099035bb1e7c7c374ac0937e15e
Oliver O'Brien <oliverobrien111@gmail.com>
2020-08-12 21:03:03 +0100

    Update no_identifier_qubit_test.py

--- cirq/testing/no_identifier_qubit_test.py
+++ cirq/testing/no_identifier_qubit_test.py
@@ -21,3 +21,8 @@ def test_named_qubit_repr():
 
 def test_comparsion_key():
     assert NoIdentifierQubit() == NoIdentifierQubit()
+
+def test_to_json():
+    assert cirq.testing.NoIdentifierQubit()._json_dict_() == {
+        'cirq_type': 'NoIdentifierQubit',
+    }
