41629c22576f989d382c68ab50e2be22115f4dc3
balopat <balintp@google.com>
2020-08-18 16:38:30 -0700

    cleanups

--- dev_tools/docs/sphinx/stage_rtd.py
+++
@@ -1,8 +0,0 @@
-def main(argv):
-    if len(argv) > 1:
-        raise app.UsageError('Too many command-line arguments.')
-    {"install.md": "install.md"}
-
-
-if __name__ == '__main__':
-    app.run(main)
