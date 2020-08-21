df755d9f0a02facff56a99087a0666cf4c8d4543
smitsanghavi <smitsanghavi@users.noreply.github.com>
2020-08-21 13:38:25 -0700

    Use num_qubits protocol in ActOnCliffordTableauArgs (#3260)
    Using the protocol instead of the getter allows us to work with objects that specify their operands through different means (e.g. _qid_shape_ method) and also not silently pass when an object without a defined number of qubits is trying to act_on

--- cirq/testing/consistent_act_on.py
+++ cirq/testing/consistent_act_on.py
@@ -52,8 +52,8 @@ def assert_act_on_clifford_tableau_effect_matches_unitary(val: Any) -> None:
 
     num_qubits_val = protocols.num_qubits(val)
 
-    if not protocols.has_unitary(val) or \
-            protocols.qid_shape(val) != (2,) * num_qubits_val:
+    if protocols.is_parameterized(val) or not protocols.has_unitary(
+            val) or protocols.qid_shape(val) != (2,) * num_qubits_val:
         return None
 
     qubits = LineQubit.range(protocols.num_qubits(val) * 2)
