072595af8986ba7b5a84808d5e2492aa1937c0af
Balint Pato <balopat@users.noreply.github.com>
2020-08-25 06:50:46 -0700

    Fix copyright notes: Google LLC -> The Cirq Developers (#3264)
    As we have the AUTHORS file we should use "The Cirq Developers" type copyright notice.

--- cirq/google/engine/client/quantum_v1alpha1/__init__.py
+++ cirq/google/engine/client/quantum_v1alpha1/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2020 Google LLC
+# Copyright 2020 The Cirq Developers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
