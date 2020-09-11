26d79ea4d9cf34a6d4485fbe5ee9ce9a33c6301d
Tony Bruguier <tony.bruguier@gmail.com>
2020-09-11 00:40:58 +0000

    Add test for untested line

--- cirq/ops/common_channels_test.py
+++ cirq/ops/common_channels_test.py
@@ -116,6 +116,10 @@ def test_asymmetric_depolarizing_channel_bigly_probability(p_x, p_y, p_z):
 def test_asymmetric_depolarizing_channel_text_diagram():
     a = cirq.asymmetric_depolarize(1 / 9, 2 / 9, 3 / 9)
     assert (cirq.circuit_diagram_info(
+        a, args=no_precision) == cirq.CircuitDiagramInfo(
+            wire_symbols=('A(0.1111111111111111,0.2222222222222222,' +
+                          '0.3333333333333333)',)))
+    assert (cirq.circuit_diagram_info(
         a, args=round_to_6_prec) == cirq.CircuitDiagramInfo(
             wire_symbols=('A(0.111111,0.222222,0.333333)',)))
     assert (cirq.circuit_diagram_info(
