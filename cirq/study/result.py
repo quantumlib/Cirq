59dc6a4d458eb291c41537bdf8281200e68313bd
Tony Bruguier <tony.bruguier@gmail.com>
2020-09-21 12:16:06 +0000

    nit and rename

--- cirq/study/result.py
+++ cirq/study/result.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Defines trial results."""
 
 from typing import (Any, Callable, Dict, Iterable, Optional, Sequence,
@@ -144,11 +143,11 @@ class Result:
 
     # Reason for 'type: ignore': https://github.com/python/mypy/issues/5273
     def multi_measurement_histogram(  # type: ignore
-            self,
-            *,  # Forces keyword args.
-            keys: Iterable[TMeasurementKey],
-            fold_func: Callable[[Tuple], T] = cast(Callable[[Tuple], T],
-                                                   _tuple_of_big_endian_int)
+        self,
+        *,  # Forces keyword args.
+        keys: Iterable[TMeasurementKey],
+        fold_func: Callable[[Tuple], T] = cast(Callable[[Tuple], T],
+                                               _tuple_of_big_endian_int)
     ) -> collections.Counter:
         """Counts the number of times combined measurement results occurred.
 
@@ -206,11 +205,11 @@ class Result:
 
     # Reason for 'type: ignore': https://github.com/python/mypy/issues/5273
     def histogram(  # type: ignore
-            self,
-            *,  # Forces keyword args.
-            key: TMeasurementKey,
-            fold_func: Callable[[Tuple], T] = cast(Callable[[Tuple], T],
-                                                   value.big_endian_bits_to_int)
+        self,
+        *,  # Forces keyword args.
+        key: TMeasurementKey,
+        fold_func: Callable[[Tuple], T] = cast(Callable[[Tuple], T],
+                                               value.big_endian_bits_to_int)
     ) -> collections.Counter:
         """Counts the number of times a measurement result occurred.
 
--- cirq/study/visualize.py
+++ cirq/study/visualize.py
@@ -19,7 +19,7 @@ import numpy as np
 from cirq.study import result
 
 
-def plot_state_histogram(result: trial_result.Result) -> np.ndarray:
+def plot_state_histogram(result: result.Result) -> np.ndarray:
     """Plot the state histogram from a single result with repetitions.
 
     States is a bitstring representation of all the qubit states in a single
