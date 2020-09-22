ddd2261a7aab94f1ad4df7220d39f088859139ab
Tony Bruguier <tony.bruguier@gmail.com>
2020-09-22 06:02:52 +0000

    Remove unused variable

--- cirq/ops/common_channels_test.py
+++ cirq/ops/common_channels_test.py
@@ -546,8 +546,8 @@ def test_multi_asymmetric_depolarizing_channel():
 
 def test_multi_asymmetric_depolarizing_channel_bad_init():
     with pytest.raises(ValueError, match="I does not have 2 entries"):
-        d = cirq.asymmetric_depolarize(num_qubits=2,
-                                       error_probabilities={'I': 1.0})
+        cirq.asymmetric_depolarize(num_qubits=2,
+                                   error_probabilities={'I': 1.0})
 
 
 def test_multi_asymmetric_depolarizing_mixture():
