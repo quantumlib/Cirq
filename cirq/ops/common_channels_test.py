def9f73944a2da59a87083c7b3ef583dd6234bf0
Tony Bruguier <tony.bruguier@gmail.com>
2020-09-24 03:36:31 +0000

    nit

--- cirq/ops/common_channels_test.py
+++ cirq/ops/common_channels_test.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import re
+
 import numpy as np
 import pytest
-import re
 
 import cirq
 
