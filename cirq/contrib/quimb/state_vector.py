289297ad7448be055a66c81127760b97aaddcbda
Bálint Pató <balintp@google.com>
2020-10-01 12:48:04 -0400

    docsstring

--- cirq/contrib/quimb/state_vector.py
+++ cirq/contrib/quimb/state_vector.py
@@ -10,6 +10,10 @@ import cirq
 
 # coverage: ignore
 def _get_quimb_version():
+    """Returns the quimb version and parsed (major,minor) numbers if possible.
+    Returns:
+        a tuple of ((major, minor), version string)
+    """
     version = quimb.__version__
     try:
         return tuple(int(x) for x in version.split('.')), version
