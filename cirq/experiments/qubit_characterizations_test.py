724f60b6d99cf5d77563be4a39e8a75da629e5c0
Adam Zalcman <viathor@google.com>
2020-10-14 15:06:24 -0700

    conj().T

--- cirq/experiments/qubit_characterizations_test.py
+++ cirq/experiments/qubit_characterizations_test.py
@@ -84,7 +84,7 @@ def test_single_qubit_cliffords():
     # Check that each unitary fixes the Pauli group.
     for u in xy_unitaries:
         for p in PAULIS:
-            assert is_pauli(u @ p @ np.conjugate(np.transpose(u))), f'{u}'
+            assert is_pauli(u @ p @ u.conj().T), f'{u}'
 
     # Check that XZ decomposition has at most one X gate per clifford.
     for gates in cliffords.c1_in_xz:
