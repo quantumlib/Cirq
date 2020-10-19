Fix all latex expressions without enclosing $ signs (#3407)
Fix all latex expressions without the enclosing $ signs. Fixes issue #3286

Approach: I searched for all the raw strings in the repository, and went these strings one by one to check for missing latex expressions.

Some comments regarding specific changes to make sure they are correct:

* For file `cirq/linalg/transformations.py`: The changes would look like [this](https://hackmd.io/0UK6Na6mQ1aY2tkcEFSlxg?view).

* For file `examples/bcs_mean_field.py`: The changes would look like [this](https://hackmd.io/ZpqO-TdnQbajFo_TSRBiCA?view). I have changed the command `\dag` to `\dagger` as `\dag` wasn't being rendered properly in HackMd.

* For file `cirq/ops/common_channels.py`: Changed `$ \sum_i p_i Pi \rho Pi $` to  `$\sum_i p_i Pi \rho Pi$`, as the first expression isn't being rendered correctly in HackMd ([link](https://hackmd.io/v8_W6pXjTsqj2KuSXlJK6w?view) to view this).

cirq/linalg/transformations.py
@@ -184,11 +184,12 @@ def targeted_conjugate_about(tensor: np.ndarray,
     conj_indices for second $\cdot$).
 
     More specifically this computes
-        sum tensor_{i_0,...,i_{r-1},j_0,...,j_{r-1}}
-        * target_{k_0,...,k_{r-1},l_0,...,l_{r-1}
-        * tensor_{m_0,...,m_{r-1},n_0,...,n_{r-1}}^*
-    where the sum is over indices where j_s = k_s and s is in `indices`
-    and l_s = m_s and s is in `conj_indices`.
+        $\sum tensor_{i_0,...,i_{r-1},j_0,...,j_{r-1}} *
+        target_{k_0,...,k_{r-1},l_0,...,l_{r-1}} *
+        tensor_{m_0,...,m_{r-1},n_0,...,n_{r-1}}^*$
+
+    where the sum is over indices where $j_s$ = $k_s$ and $s$ is in `indices`
+    and $l_s$ = $m_s$ and s is in `conj_indices`.
 
     Args:
         tensor: The tensor that will be conjugated about the target tensor.