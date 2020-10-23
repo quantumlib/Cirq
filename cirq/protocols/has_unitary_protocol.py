c3c93bea2a0454e8e5eba58828c4b86efad26cd8
Bálint Pató <balintp@google.com>
2020-10-23 00:19:36 -0700

    format

--- cirq/protocols/has_unitary_protocol.py
+++ cirq/protocols/has_unitary_protocol.py
@@ -26,7 +26,7 @@ from cirq._doc import doc_private
 from cirq.protocols import qid_shape_protocol
 from cirq.protocols.apply_unitary_protocol import ApplyUnitaryArgs
 from cirq.protocols.decompose_protocol import (
-    _try_decompose_into_operations_and_qubits, )
+    _try_decompose_into_operations_and_qubits,)
 
 TDefault = TypeVar('TDefault')
 
