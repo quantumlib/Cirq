c04dd6e29d6898bd5b3123ce2fce565ebe411e1b
smitsanghavi <smitsanghavi@users.noreply.github.com>
2020-10-28 09:37:01 -0700

    Add _act_on_ for StabilizerStateChForm to GlobalPhaseOp (#3448)
    #2948 #2423

--- cirq/ops/global_phase_op_test.py
+++ cirq/ops/global_phase_op_test.py
@@ -40,6 +40,14 @@ def test_protocols():
                                atol=1e-8)
 
 
+@pytest.mark.parametrize('phase', [1, 1j, -1])
+def test_act_on(phase):
+    state = cirq.StabilizerStateChForm(0)
+    args = cirq.ActOnStabilizerCHFormArgs(state, [])
+    cirq.act_on(cirq.GlobalPhaseOperation(phase), args, allow_decompose=False)
+    assert state.state_vector() == [[phase]]
+
+
 def test_str():
     assert str(cirq.GlobalPhaseOperation(1j)) == '1j'
 
