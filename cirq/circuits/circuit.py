f850e030ebe3a4e2c57c849d11ab84eb40461e24
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2020-11-06 10:38:22 -0800

    Enforce mypy restriction.

--- cirq/circuits/circuit.py
+++ cirq/circuits/circuit.py
@@ -1291,7 +1291,7 @@ class Circuit(AbstractCircuit):
 
     def _with_sliced_moments(self, moments: Sequence['cirq.Moment']):
         new_circuit = Circuit(device=self.device)
-        new_circuit._moments = moments
+        new_circuit._moments = list(moments)
         return new_circuit
 
     # pylint: disable=function-redefined
