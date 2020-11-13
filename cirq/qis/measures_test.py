a60b9eca18056aecfd7a4c5c43c0b8ae7e252fa9
Kevin J. Sung <kevjsung@umich.edu>
2020-11-12 21:49:10 -0500

    fix flaky test by adding atol

--- cirq/qis/measures_test.py
+++ cirq/qis/measures_test.py
@@ -132,10 +132,11 @@ def test_fidelity_product_states():
         cirq.fidelity(
             cirq.KET_ZERO(a) * cirq.KET_ZERO(b),
             cirq.KET_ZERO(a) * cirq.KET_ZERO(b)), 1.0)
-    np.testing.assert_allclose(
-        cirq.fidelity(
-            cirq.KET_ZERO(a) * cirq.KET_ZERO(b),
-            cirq.KET_ZERO(a) * cirq.KET_ONE(b)), 0.0)
+    np.testing.assert_allclose(cirq.fidelity(
+        cirq.KET_ZERO(a) * cirq.KET_ZERO(b),
+        cirq.KET_ZERO(a) * cirq.KET_ONE(b)),
+                               0.0,
+                               atol=1e-7)
     np.testing.assert_allclose(
         cirq.fidelity(
             cirq.KET_ZERO(a) * cirq.KET_ZERO(b),
@@ -148,10 +149,11 @@ def test_fidelity_product_states():
         cirq.fidelity(
             cirq.KET_MINUS(a) * cirq.KET_PLUS(b),
             cirq.KET_MINUS(a) * cirq.KET_PLUS(b)), 1.0)
-    np.testing.assert_allclose(
-        cirq.fidelity(
-            cirq.KET_MINUS(a) * cirq.KET_PLUS(b),
-            cirq.KET_PLUS(a) * cirq.KET_MINUS(b)), 0.0)
+    np.testing.assert_allclose(cirq.fidelity(
+        cirq.KET_MINUS(a) * cirq.KET_PLUS(b),
+        cirq.KET_PLUS(a) * cirq.KET_MINUS(b)),
+                               0.0,
+                               atol=1e-7)
 
     with pytest.raises(ValueError, match='Mismatched'):
         _ = cirq.fidelity(cirq.KET_MINUS(a),
