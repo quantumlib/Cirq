ff856530b3eeb4ddef741350b6ae49dc576c0717
Dave Bacon <dabacon@gmail.com>
2020-11-22 16:28:13 -0800

    Fix format

--- cirq/ionq/ionq_exceptions_test.py
+++ cirq/ionq/ionq_exceptions_test.py
@@ -13,6 +13,7 @@
 
 import cirq.ionq as ionq
 
+
 def test_ionq_exception():
     ex = ionq.IonQException(message='Hello', status_code=500)
     assert str(ex) == 'Status code: 500, Message: \'Hello\''
