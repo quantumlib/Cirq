206491abfb5c7ac95c80f30ffa6f3ab71151501a
Tony Bruguier <tony.bruguier@gmail.com>
2020-11-24 07:53:34 +0000

    Refactor to be like Pauli

--- cirq/ops/projectors.py
+++ cirq/ops/projectors.py
@@ -1,8 +1,9 @@
-from typing import Any, Dict, Iterable, List, Tuple, TYPE_CHECKING, Union
+from typing import Any, Dict, Iterable, Mapping, Sequence, List, Optional, Tuple, TYPE_CHECKING, TypeVar, Union
 
 import numpy as np
 
-from cirq import value
+from cirq import linalg, value
+from cirq.ops import raw_types
 from cirq.qis import states
 from cirq.qis import STATE_VECTOR_LIKE
 
@@ -10,10 +11,19 @@ if TYPE_CHECKING:
     import cirq
     from cirq import protocols
 
+ProjKey = TypeVar('ProjKey', bound=Union[raw_types.Qid, Tuple[raw_types.Qid]])
+
+
+def qid_shape_from_proj_key(proj_key):
+    if isinstance(proj_key, tuple):
+        return [qubit.dimension for qubit in proj_key]
+    else:
+        return [proj_key.dimension]
+
 
 @value.value_equality
 class Projector():
-    """A non-unitary gate that projects onto one or more qubits.
+    """A projection matrix where you can specify the basis.
 
     The input is a matrix representing the basis of the space we project onto.
     The basis vectors need not be orthogonal, but they must be independent (i.e.
@@ -26,68 +36,74 @@ class Projector():
     """
 
     def __init__(self,
-                 projection_basis: Union[STATE_VECTOR_LIKE],
-                 qid_shape: Tuple[int, ...] = (2,),
+                 projection_bases: Dict[ProjKey, Sequence[STATE_VECTOR_LIKE]],
                  enforce_orthonormal_basis: bool = False):
         """
         Args:
-            projection_basis: a (p, 2**num_qubits) matrix that lists the
-                projection vectors, where p is the dimension of the subspace
-                we're projecting on. If you project onto a single vector, then
-                p = 1 and thus the matrix reduces to a single row.
-            qid_shape: Specifies the dimension of the projection. The default is
-                2 (qubit).
+            projection_bases: a dictionary of Qdit tuples to a
+                (p, 2**num_qubits) matrix that lists the projection vectors,
+                where p is the dimension of the subspace we're projecting on. If
+                you project onto a single vector, then p = 1 and thus the matrix
+                reduces to a single row.
             enforce_orthonormal_basis: Whether to enfore the input basis to be
                 orthogonal.
 
         Raises:
             ValueError: If the basis vector is empty.
         """
-        projection_array = np.vstack([states.to_valid_state_vector(x, qid_shape=qid_shape) for x in projection_basis])
+        self._projection_bases = {}
+        for qubits, projection_basis in projection_bases.items():
+            qid_shape = qid_shape_from_proj_key(qubits)
+            projection_array = np.vstack([states.to_valid_state_vector(x, qid_shape=qid_shape) for x in projection_basis])
 
-        if len(projection_array.shape) != 2:
-            raise ValueError('The input projection_basis must be a 2D array')
+            if enforce_orthonormal_basis:
+                B = projection_array @ projection_array.T.conj()
+                if not np.allclose(B, np.eye(projection_array.shape[0])):
+                    raise ValueError('The basis must be orthonormal')
 
-        if enforce_orthonormal_basis:
-            B = projection_array @ projection_array.T.conj()
-            if not np.allclose(B, np.eye(projection_array.shape[0])):
-                raise ValueError('The basis must be orthonormal')
+            if np.linalg.matrix_rank(projection_array) < projection_array.shape[0]:
+                raise ValueError('Vectors in basis must be linearly independent')
 
-        if np.linalg.matrix_rank(projection_array) < projection_array.shape[0]:
-            raise ValueError('Vectors in basis must be linearly independent')
+            if np.prod(qid_shape) != projection_array.shape[1]:
+                raise ValueError(
+                    "Invalid shape " +
+                    f"{np.array(projection_array).shape} for qid_shape {qid_shape}")
 
-        if np.prod(qid_shape) != projection_array.shape[1]:
-            raise ValueError(
-                "Invalid shape " +
-                f"{np.array(projection_array).shape} for qid_shape {qid_shape}")
-        self._projection_basis = projection_array
-        self._qid_shape = qid_shape
+            self._projection_bases[qubits] = projection_array
 
-    def _projection_basis_(self) -> np.ndarray:
-        return self._projection_basis
+    def _projection_bases_(self) -> np.ndarray:
+        return self._projection_bases
 
-    def _qid_shape_(self) -> Tuple[int, ...]:
-        return self._qid_shape
+    def matrix(self, proj_keys: Optional[Iterable[ProjKey]] = None) -> Iterable[np.ndarray]:
+        proj_keys = self._projection_bases.keys() if proj_keys is None else proj_keys
+        factors = []
+        for proj_key in proj_keys:
+            if proj_key not in self._projection_bases:
+                qid_shape = qid_shape_from_proj_key(proj_key)
+                factors.append(np.eye(np.prod(qid_shape)))
+            else:
+                # Make rows into columns
+                A = self._projection_bases[proj_key].T
+                # Left pseudo-inverse
+                pseudoinverse = np.linalg.pinv(A)
+                # Projector to the range (column space) of A
+                factors.append(A @ pseudoinverse)
+        return linalg.kron(*factors)
 
-    def _channel_(self) -> Iterable[np.ndarray]:
-        # Make rows into columns
-        A = self._projection_basis.T
-        # Left pseudo-inverse
-        pseudoinverse = np.linalg.pinv(A)
-        # Projector to the range (column space) of A
-        return (A @ pseudoinverse,)
 
     def __repr__(self) -> str:
-        return ("cirq.Projector(projection_basis=" +
-                f"{self._projection_basis.tolist()})," +
-                f"qid_shape={self._qid_shape})")
+        return f"cirq.Projector(projection_bases={self._projection_bases})"
 
     def _json_dict_(self) -> Dict[str, Any]:
-        return {
+         return {
             'cirq_type': self.__class__.__name__,
-            'projection_basis': self._projection_basis.tolist(),
-            'qid_shape': self._qid_shape,
-        }
+            # JSON requires mappings to have string keys.
+            'projection_bases': list(self._projection_bases.items())}
+
+    @classmethod
+    def _from_json_dict_(cls, projection_bases, **kwargs):
+        return cls(projection_bases=dict(projection_bases))
 
     def _value_equality_values_(self) -> Any:
-        return self._projection_basis.tobytes(), self._qid_shape
+        sorted_items = sorted(self._projection_bases.items())
+        return tuple([(x[0], x[1].tobytes()) for x in sorted_items])
