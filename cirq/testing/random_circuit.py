5b99b8561de7bd43708330c2d487ea523964d0fe
Balint Pato <balopat@users.noreply.github.com>
2020-11-25 20:15:38 -0800

    [3 qubit decomposition] Extract diagonal (#3472)
    Adds `cirq.two_qubit_matrix_to_diagonal_and_operations` that extracts a diagonal if it can from a 3CNOT unitary to a diagonal and a 2 CNOT unitary. 
    
    Another part of #2873.

--- cirq/testing/random_circuit.py
+++ cirq/testing/random_circuit.py
@@ -15,6 +15,7 @@
 from typing import List, Union, Sequence, Dict, Optional, TYPE_CHECKING
 
 from cirq import ops, value
+from cirq.ops import Qid
 from cirq.circuits import Circuit
 from cirq._doc import document
 
@@ -119,3 +120,42 @@ def random_circuit(qubits: Union[Sequence[ops.Qid], int],
         moments.append(ops.Moment(operations))
 
     return Circuit(moments)
+
+
+def random_two_qubit_circuit_with_czs(
+        num_czs: int = 3,
+        q0: Qid = None,
+        q1: Qid = None,
+        random_state: 'cirq.RANDOM_STATE_OR_SEED_LIKE' = None) -> Circuit:
+    """Creates a random two qubit circuit with the given number of CNOTs.
+
+    The resulting circuit will have `num_cnots` number of CNOTs that will be
+    surrounded by random `PhasedXPowGate` instances on both qubits.
+
+    Args:
+         num_czs: the number of CNOTs to be guaranteed in the circuit
+         q0: the first qubit the circuit should operate on
+         q1: the second qubit the circuit should operate on
+         random_state: an optional random seed
+    Returns:
+         the random two qubit circuit
+    """
+    prng = value.parse_random_state(random_state)
+    q0 = ops.NamedQubit('q0') if q0 is None else q0
+    q1 = ops.NamedQubit('q1') if q1 is None else q1
+
+    def random_one_qubit_gate():
+        return ops.PhasedXPowGate(phase_exponent=prng.random(),
+                                  exponent=prng.random())
+
+    def one_cz():
+        return [
+            ops.CZ.on(q0, q1),
+            random_one_qubit_gate().on(q0),
+            random_one_qubit_gate().on(q1),
+        ]
+
+    return Circuit([
+        random_one_qubit_gate().on(q0),
+        random_one_qubit_gate().on(q1), [one_cz() for _ in range(num_czs)]
+    ])
