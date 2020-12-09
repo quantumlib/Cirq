e757daa2a5e76cddaaa22c1987d6ddd68d325dd7
Wojciech Mruczkiewicz <mrwojtek@google.com>
2020-12-09 18:30:20 +0100

    Update simulator initializer

--- cirq/google/calibration/engine_simulator.py
+++ cirq/google/calibration/engine_simulator.py
@@ -1,20 +1,31 @@
-from typing import Dict, List
+from typing import Callable, Dict, List, Optional
 
 import numpy as np
 
 from cirq.circuits import Circuit
+from cirq.ops import (
+    FSimGate,
+    Gate,
+    PhasedFSimGate
+)
 from cirq.sim import SimulatesSamples
 from cirq.study import ParamResolver
 
 from cirq.google.calibration.phased_fsim import (
     PhasedFSimCalibrationRequest,
-    PhasedFSimCalibrationResult
+    PhasedFSimCalibrationResult,
+    sqrt_iswap_gates_translator
 )
 
 
 class PhasedFSimEngineSimulator(SimulatesSamples):
 
-    def __init__(self, simulator: SimulatesSamples) -> None:
+    def __init__(
+            self,
+            simulator: SimulatesSamples,
+            drift_generator: Callable[[FSimGate], PhasedFSimGate],
+            gates_translator: Callable[[Gate], Optional[FSimGate]] = sqrt_iswap_gates_translator
+    ) -> None:
         self._simulator = simulator
 
     def _run(self, circuit: Circuit, param_resolver: ParamResolver, repetitions: int
