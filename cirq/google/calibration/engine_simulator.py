209adc73125597c347ea959106fa410faa8bc703
Wojciech Mruczkiewicz <mrwojtek@google.com>
2020-12-11 15:59:19 +0100

    Add more methods to engine simulator

--- cirq/google/calibration/engine_simulator.py
+++ cirq/google/calibration/engine_simulator.py
@@ -1,12 +1,14 @@
-from typing import Callable, Dict, List, Optional
+from typing import Callable, Dict, Iterable, List, Optional, Union
 
 import numpy as np
+import random
 
 from cirq.circuits import Circuit
 from cirq.ops import (
     FSimGate,
     Gate,
-    PhasedFSimGate
+    PhasedFSimGate,
+    Qid
 )
 from cirq.sim import SimulatesSamples
 from cirq.study import ParamResolver
@@ -24,7 +26,7 @@ class PhasedFSimEngineSimulator(SimulatesSamples):
     def __init__(
             self,
             simulator: SimulatesSamples,
-            drift_generator: Callable[[FSimGate], PhasedFSimGate],
+            drift_generator: Callable[[Qid, Qid, FSimGate], PhasedFSimGate],
             gates_translator: Callable[[Gate], Optional[FSimGate]] = sqrt_iswap_gates_translator
     ) -> None:
         self._simulator = simulator
@@ -32,11 +34,29 @@ class PhasedFSimEngineSimulator(SimulatesSamples):
         self._gates_translator = gates_translator
 
     @staticmethod
-    def create_with_ideal_sqrt_iswap() -> 'PhasedFSimEngineSimulator':
-        return NotImplemented
+    def create_with_ideal_sqrt_iswap(simulator: SimulatesSamples) -> 'PhasedFSimEngineSimulator':
+
+        def sample_gate(_1: Qid, _2: Qid, gate: FSimGate) -> PhasedFSimGate:
+            assert np.isclose(gate.theta, np.pi / 4) and np.isclose(gate.phi, 0.0), (
+                f'Expected ISWAP ** -0.5 like gate, got {gate}'
+            )
+            return PhasedFSimGate(
+                theta=np.pi / 4,
+                zeta=0.0,
+                chi=0.0,
+                gamma=0.0,
+                phi=0.0
+            )
+
+        return PhasedFSimEngineSimulator(
+            simulator,
+            drift_generator=sample_gate,
+            gates_translator=sqrt_iswap_gates_translator
+        )
 
     @staticmethod
-    def create_with_random_gaussian(
+    def create_with_random_gaussian_sqrt_iswap(
+            simulator: SimulatesSamples,
             mean: PhasedFSimParameters,
             sigma: PhasedFSimParameters = PhasedFSimParameters(
                 theta=0.02,
@@ -45,7 +65,53 @@ class PhasedFSimEngineSimulator(SimulatesSamples):
                 gamma=0.05,
                 phi=0.02
             ),
-            gates_translator: Callable[[Gate], Optional[FSimGate]] = sqrt_iswap_gates_translator
+            rand: Optional[Union[int, random.Random]] = None
+    ) -> 'PhasedFSimEngineSimulator':
+
+        def sample_gate(_1: Qid, _2: Qid, gate: FSimGate) -> PhasedFSimGate:
+            assert np.isclose(gate.theta, np.pi / 4) and np.isclose(gate.phi, 0.0), (
+                f'Expected ISWAP ** -0.5 like gate, got {gate}'
+            )
+            return PhasedFSimGate(
+                theta=rand.gauss(mean.theta, sigma.theta),
+                zeta=rand.gauss(mean.zeta, sigma.zeta),
+                chi=rand.gauss(mean.chi, sigma.chi),
+                gamma=rand.gauss(mean.gamma, sigma.gamma),
+                phi=rand.gauss(mean.phi, sigma.phi)
+            )
+
+        # TODO: Check if all values of mean and sigma are filled-in.
+
+        if rand is not None:
+            if isinstance(rand, int):
+                rand = random.Random(rand)
+            elif not isinstance(rand, random.Random):
+                raise ValueError(
+                    f'Provided rand argument {rand} is neither of type int or random.Random')
+        else:
+            rand = random.Random(rand)
+
+        return PhasedFSimEngineSimulator(
+            simulator,
+            drift_generator=sample_gate,
+            gates_translator=sqrt_iswap_gates_translator
+        )
+
+    @staticmethod
+    def create_from_dictionary_sqrt_iswap(
+            simulator: SimulatesSamples,
+            parameters: Dict[str, Callable[[Qid, Qid], float]],
+            ideal_when_missing_gate: bool = False,
+            ideal_when_missing_parameter: bool = False
+    ) -> 'PhasedFSimEngineSimulator':
+        return NotImplemented
+
+    @staticmethod
+    def create_from_characterizations_sqrt_iswap(
+            simulator: SimulatesSamples,
+            characterizations: Iterable[PhasedFSimCalibrationResult],
+            ideal_when_missing_gate: bool = False,
+            ideal_when_missing_parameter: bool = False
     ) -> 'PhasedFSimEngineSimulator':
         return NotImplemented
 
