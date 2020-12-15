7edb715eee38f9d89c39579f2a52ff4234b9137a
Wojciech Mruczkiewicz <mrwojtek@google.com>
2020-12-15 15:02:40 +0100

    Working on engine simulator implementation.

--- cirq/google/calibration/engine_simulator.py
+++ cirq/google/calibration/engine_simulator.py
@@ -1,19 +1,28 @@
-from typing import Callable, Dict, Iterable, List, Optional, Union
+from typing import Callable, Dict, Iterable, List, Optional, Tuple, Union
 
 import numpy as np
 import random
 
-from cirq.circuits import Circuit
+from cirq.circuits import (
+    Circuit,
+    PointOptimizer,
+    PointOptimizationSummary
+)
 from cirq.ops import (
     FSimGate,
     Gate,
+    MeasurementGate,
+    Operation,
     PhasedFSimGate,
-    Qid
+    Qid,
+    SingleQubitGate,
+    WaitGate
 )
 from cirq.sim import SimulatesSamples
 from cirq.study import ParamResolver
 
 from cirq.google.calibration.phased_fsim import (
+    IncompatibleMomentError,
     PhasedFSimCalibrationRequest,
     PhasedFSimCalibrationResult,
     PhasedFSimParameters,
@@ -32,6 +41,7 @@ class PhasedFSimEngineSimulator(SimulatesSamples):
         self._simulator = simulator
         self._drift_generator = drift_generator
         self._gates_translator = gates_translator
+        self._drifted_gates: Dict[Tuple[Qid, Qid, FSimGate], PhasedFSimGate] = {}
 
     @staticmethod
     def create_with_ideal_sqrt_iswap(simulator: SimulatesSamples) -> 'PhasedFSimEngineSimulator':
@@ -115,11 +125,58 @@ class PhasedFSimEngineSimulator(SimulatesSamples):
     ) -> 'PhasedFSimEngineSimulator':
         return NotImplemented
 
-    def _run(self, circuit: Circuit, param_resolver: ParamResolver, repetitions: int
-             ) -> Dict[str, np.ndarray]:
-        return self._simulator._run(circuit, param_resolver, repetitions)
-
     def get_calibrations(self,
                          requests: List[PhasedFSimCalibrationRequest]
                          ) -> List[PhasedFSimCalibrationResult]:
         return NotImplemented
+
+    def _run(self, circuit: Circuit, param_resolver: ParamResolver, repetitions: int
+             ) -> Dict[str, np.ndarray]:
+        converted = self._convert_to_circuit_with_drift(circuit)
+        return self._simulator._run(converted, param_resolver, repetitions)
+
+    def _convert_to_circuit_with_drift(self, circuit: Circuit) -> Circuit:
+        copied = Circuit(circuit)
+        converter = self._PhasedFSimConverter(self)
+        converter.optimize_circuit(copied)
+        return copied
+
+    def _get_or_create_gate(self, a: Qid, b: Qid, gate: FSimGate) -> PhasedFSimGate:
+        drifted_gate = self._drifted_gates.get((a, b, gate), None)
+        if drifted_gate is None:
+            drifted_gate = self._drift_generator(a, b, gate)
+            self._drifted_gates[(a, b, gate)] = drifted_gate
+            self._drifted_gates[(b, a, gate)] = PhasedFSimGate(
+                theta=drifted_gate.theta,
+                zeta=-drifted_gate.zeta,
+                chi=-drifted_gate.chi,
+                gamma=drifted_gate.gamma,
+                phi=drifted_gate.phi
+            )
+        return drifted_gate
+
+    class _PhasedFSimConverter(PointOptimizer):
+
+        def __init__(self, outer: 'PhasedFSimEngineSimulator') -> None:
+            super().__init__()
+            self._outer = outer
+
+        def optimization_at(
+                self, circuit: Circuit, index: int, op: Operation
+        ) -> Optional[PointOptimizationSummary]:
+
+            if isinstance(op.gate, (MeasurementGate, SingleQubitGate, WaitGate)):
+                new_op = op
+            else:
+                translated_gate = self._outer._gates_translator(op.gate)
+                if translated_gate is None:
+                    raise IncompatibleMomentError(
+                        f'Moment contains non-single qubit operation {op} with gate that is not '
+                        f'equal to cirq.ISWAP ** -0.5'
+                    )
+                a, b = op.qubits
+                new_op = self._outer._get_or_create_gate(a, b, translated_gate)
+
+            return PointOptimizationSummary(
+                clear_span=1, clear_qubits=op.qubits, new_operations=new_op
+            )
