f3e62d4b9a4b7f9120608fa00262fa0460ce48f5
Tony Bruguier <tony.bruguier@gmail.com>
2020-12-27 17:55:05 +0000

    Add (failing) unit tests, revealing a bug somewhere.

--- cirq/sim/mps_simulator_test.py
+++ cirq/sim/mps_simulator_test.py
@@ -3,12 +3,12 @@ import numpy as np
 import cirq
 
 
-def assert_same_output_as_dense(circuit, qubit_order):
+def assert_same_output_as_dense(circuit, qubit_order, initial_state=0):
     mps_simulator = cirq.MPSSimulator()
     ref_simulator = cirq.Simulator()
 
-    actual = mps_simulator.simulate(circuit, qubit_order=qubit_order)
-    expected = ref_simulator.simulate(circuit, qubit_order=qubit_order)
+    actual = mps_simulator.simulate(circuit, qubit_order=qubit_order, initial_state=initial_state)
+    expected = ref_simulator.simulate(circuit, qubit_order=qubit_order, initial_state=initial_state)
     np.testing.assert_almost_equal(actual.final_state.to_numpy(), expected.final_state_vector)
     assert len(actual.measurements) == 0
 
@@ -17,3 +17,19 @@ def test_simulate():
     q0, q1 = cirq.LineQubit.range(2)
     circuit = cirq.Circuit(cirq.H(q0), cirq.H(q1), cirq.CNOT(q0, q1))
     assert_same_output_as_dense(circuit=circuit, qubit_order=[q0, q1])
+
+
+def test_cnot():
+    q0, q1 = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit(cirq.CNOT(q0, q1))
+
+    for initial_state in range(4):
+        assert_same_output_as_dense(circuit=circuit, qubit_order=[q0, q1], initial_state=initial_state)
+
+
+def test_cnot_flipped():
+    q0, q1 = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit(cirq.CNOT(q1, q0))
+
+    for initial_state in range(4):
+        assert_same_output_as_dense(circuit=circuit, qubit_order=[q0, q1], initial_state=initial_state)
