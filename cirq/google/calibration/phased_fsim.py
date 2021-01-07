cc1101bc1a258f5ab0f3b2f07b96bc71ab00807a
Wojciech Mruczkiewicz <mrwojtek@google.com>
2021-01-07 13:31:13 +0100

    Add some todos

--- cirq/google/calibration/phased_fsim.py
+++ cirq/google/calibration/phased_fsim.py
@@ -222,6 +222,7 @@ class FloquetPhasedFSimCalibrationRequest(PhasedFSimCalibrationRequest):
         )
 
 
+# TODO: Add support for ISWAP ** 0.5 as well.
 def sqrt_iswap_gates_translator(gate: Gate) -> Optional[FSimGate]:
     if isinstance(gate, FSimGate):
         if not np.isclose(gate.phi, 0.0):
