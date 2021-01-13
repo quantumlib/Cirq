c57ead1ca877664d7c1220791d4417d215678262
Wojciech Mruczkiewicz <mrwojtek@google.com>
2021-01-13 15:09:55 +0100

    Add tests for encoding and decoding of calibration requests/results.

--- cirq/google/calibration/phased_fsim_test.py
+++ cirq/google/calibration/phased_fsim_test.py
@@ -1,4 +1,12 @@
-from cirq.google.calibration.phased_fsim import PhasedFSimCharacterization
+from cirq.google.calibration.phased_fsim import (
+    FloquetPhasedFSimCalibrationOptions,
+    FloquetPhasedFSimCalibrationRequest,
+    FloquetPhasedFSimCalibrationResult,
+    PhasedFSimCharacterization,
+)
+
+import cirq
+import numpy as np
 
 
 def test_asdict():
@@ -45,4 +53,111 @@ def test_override_by():
     other = PhasedFSimCharacterization(gamma=0.4, phi=0.5, theta=0.6)
     assert characterization.override_by(other) == PhasedFSimCharacterization(
         theta=0.6, zeta=0.2, chi=0.3, gamma=0.4, phi=0.5
+    )
+
+
+def test_floquet_to_calibration_layer():
+    q_00, q_01, q_02, q_03 = [cirq.GridQubit(0, index) for index in range(4)]
+    gate = cirq.FSimGate(theta=np.pi / 4, phi=0.0)
+    request = FloquetPhasedFSimCalibrationRequest(
+        gate=gate,
+        gate_set=cirq.google.SQRT_ISWAP_GATESET,
+        pairs=((q_00, q_01), (q_02, q_03)),
+        options=FloquetPhasedFSimCalibrationOptions(
+            characterize_theta=True,
+            characterize_zeta=True,
+            characterize_chi=False,
+            characterize_gamma=False,
+            characterize_phi=True,
+        ),
+    )
+
+    assert request.to_calibration_layer() == cirq.google.CalibrationLayer(
+        calibration_type='floquet_phased_fsim_characterization',
+        program=cirq.Circuit([gate.on(q_00, q_01), gate.on(q_02, q_03)]),
+        args={
+            'est_theta': True,
+            'est_zeta': True,
+            'est_chi': False,
+            'est_gamma': False,
+            'est_phi': True,
+            'readout_corrections': True,
+        },
+    )
+
+
+def test_floquet_parse_result():
+    q_00, q_01, q_02, q_03 = [cirq.GridQubit(0, index) for index in range(4)]
+    gate = cirq.FSimGate(theta=np.pi / 4, phi=0.0)
+    request = FloquetPhasedFSimCalibrationRequest(
+        gate=gate,
+        gate_set=cirq.google.SQRT_ISWAP_GATESET,
+        pairs=((q_00, q_01), (q_02, q_03)),
+        options=FloquetPhasedFSimCalibrationOptions(
+            characterize_theta=True,
+            characterize_zeta=True,
+            characterize_chi=False,
+            characterize_gamma=False,
+            characterize_phi=True,
+        ),
+    )
+
+    result = cirq.google.CalibrationResult(
+        code=cirq.google.api.v2.calibration_pb2.SUCCESS,
+        error_message=None,
+        token=None,
+        valid_until=None,
+        metrics=cirq.google.Calibration(
+            cirq.google.api.v2.metrics_pb2.MetricsSnapshot(
+                metrics=[
+                    cirq.google.api.v2.metrics_pb2.Metric(
+                        name='angles',
+                        targets=[
+                            '0_qubit_a',
+                            '0_qubit_b',
+                            '0_theta_est',
+                            '0_zeta_est',
+                            '0_phi_est',
+                            '1_qubit_a',
+                            '1_qubit_b',
+                            '1_theta_est',
+                            '1_zeta_est',
+                            '1_phi_est',
+                        ],
+                        values=[
+                            cirq.google.api.v2.metrics_pb2.Value(str_val='0_0'),
+                            cirq.google.api.v2.metrics_pb2.Value(str_val='0_1'),
+                            cirq.google.api.v2.metrics_pb2.Value(double_val=0.1),
+                            cirq.google.api.v2.metrics_pb2.Value(double_val=0.2),
+                            cirq.google.api.v2.metrics_pb2.Value(double_val=0.3),
+                            cirq.google.api.v2.metrics_pb2.Value(str_val='0_2'),
+                            cirq.google.api.v2.metrics_pb2.Value(str_val='0_3'),
+                            cirq.google.api.v2.metrics_pb2.Value(double_val=0.4),
+                            cirq.google.api.v2.metrics_pb2.Value(double_val=0.5),
+                            cirq.google.api.v2.metrics_pb2.Value(double_val=0.6),
+                        ],
+                    )
+                ]
+            )
+        ),
+    )
+
+    assert request.parse_result(result) == FloquetPhasedFSimCalibrationResult(
+        parameters={
+            (q_00, q_01): PhasedFSimCharacterization(
+                theta=0.1, zeta=0.2, chi=None, gamma=None, phi=0.3
+            ),
+            (q_02, q_03): PhasedFSimCharacterization(
+                theta=0.4, zeta=0.5, chi=None, gamma=None, phi=0.6
+            ),
+        },
+        gate=gate,
+        gate_set=cirq.google.SQRT_ISWAP_GATESET,
+        options=FloquetPhasedFSimCalibrationOptions(
+            characterize_theta=True,
+            characterize_zeta=True,
+            characterize_chi=False,
+            characterize_gamma=False,
+            characterize_phi=True,
+        ),
     )
