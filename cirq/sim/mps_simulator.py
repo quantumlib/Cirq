Fix bug in 1D computation (sorry) + add failing unit test for 2D

cirq/sim/mps_simulator.py
@@ -16,9 +16,6 @@
 This is based on this paper:
 https://arxiv.org/abs/2002.07730
 
-TODO(tonybruguier): Currently, only linear circuits are handled, while the paper
-handles more general topologies.
-
 TODO(tonybruguier): Currently, numpy is used for tensor computations. For speed
 switch to QIM for speed.
 """
@@ -284,10 +281,15 @@ class MPSState:
             n = idx[0]
             self.M[n] = np.einsum('ij,mnj->mni', U, self.M[n])
         elif len(idx) == 2:
-            n = idx[0]
-            p = idx[1]
-            if abs(n - p) != 1:
+            if abs(idx[0] - idx[1]) != 1:
                 raise ValueError('Can only handle continguous qubits')
+            elif idx[0] < idx[1]:
+                n = idx[0]
+                p = idx[1]
+            else:
+                n = idx[1]
+                p = idx[0]
+                U = np.swapaxes(np.swapaxes(U, 0, 1), 2, 3)
             T = np.einsum('klij,mni,npj->mkpl', U, self.M[n], self.M[p])
             X, S, Y = np.linalg.svd(T.reshape([T.shape[0] * T.shape[1], T.shape[2] * T.shape[3]]))
             X = X.reshape([T.shape[0], T.shape[1], -1])