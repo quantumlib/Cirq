0f74909427c6731d048aad642f92462aedfb8c5f
Matthew Harrigan <mpharrigan@google.com>
2021-01-29 00:05:51 -0800

    parallel

--- cirq/experiments/fidelity_estimation.py
+++ cirq/experiments/fidelity_estimation.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 """Estimation of fidelity associated with experimental circuit executions."""
 import concurrent
-import itertools
 from abc import abstractmethod
 from concurrent.futures.thread import ThreadPoolExecutor
 from dataclasses import dataclass
@@ -27,7 +26,6 @@ from typing import (
     cast,
     TYPE_CHECKING,
     Dict,
-    Any,
     Set,
     ContextManager,
 )
@@ -40,6 +38,7 @@ import tqdm
 
 from cirq import ops, sim, devices
 from cirq.circuits import Circuit
+from cirq.experiments.random_quantum_circuit_generation import CircuitLibraryCombination
 from cirq.ops import QubitOrder, QubitOrderOrList
 from cirq.sim import final_state_vector
 
@@ -412,16 +411,31 @@ class _Sample2qXEBTask:
     """
 
     cycle_depth: int
-    circuit_i: int
+    layer_i: int
+    combination_i: int
     prepared_circuit: 'cirq.Circuit'
+    combination: List[int]
 
 
 class _SampleInBatches:
-    def __init__(self, sampler: 'cirq.Sampler', repetitions: int):
+    def __init__(
+        self,
+        sampler: 'cirq.Sampler',
+        repetitions: int,
+        combinations_by_layer: List[CircuitLibraryCombination],
+    ):
         """This closure will execute a list of `tasks` with one call to
-        `run_batch` on the provided sampler for a given number of repetitions."""
+        `run_batch` on the provided sampler for a given number of repetitions.
+
+        It also keeps a record of the circuit library combinations in order to
+        back out which qubit pairs correspond to each pair index. We tag
+        our return value with this so it is in the resultant DataFrame, which
+        is very convenient for dealing with the results (but not strictly
+        necessary, as the information could be extracted from (`layer_i`, `pair_i`).
+        """
         self.sampler = sampler
         self.repetitions = repetitions
+        self.combinations_by_layer = combinations_by_layer
 
     def __call__(self, tasks: List[_Sample2qXEBTask]):
         prepared_circuits = [task.prepared_circuit for task in tasks]
@@ -430,16 +444,27 @@ class _SampleInBatches:
         records = []
         for task, nested_result in zip(tasks, results):
             (result,) = nested_result  # remove nesting due to potential sweeps.
-            sampled_inds = result.data.values[:, 0]
-            sampled_probs = np.bincount(sampled_inds, minlength=2 ** 2) / len(sampled_inds)
+            for pair_i, circuit_i in enumerate(task.combination):
+                pair_measurement_key = str(pair_i)
+                q0, q1 = self.combinations_by_layer[task.layer_i].pairs[pair_i]
+                sampled_inds = result.data[pair_measurement_key].values
+                sampled_probs = np.bincount(sampled_inds, minlength=2 ** 2) / len(sampled_inds)
 
-            records += [
-                {
-                    'circuit_i': task.circuit_i,
-                    'cycle_depth': task.cycle_depth,
-                    'sampled_probs': sampled_probs,
-                }
-            ]
+                records += [
+                    {
+                        'circuit_i': circuit_i,
+                        'cycle_depth': task.cycle_depth,
+                        'sampled_probs': sampled_probs,
+                        # Additional metadata to track *how* this circuit
+                        # was zipped and executed.
+                        'layer_i': task.layer_i,
+                        'pair_i': pair_i,
+                        'combination_i': task.combination_i,
+                        'pair_name': f'{q0}-{q1}',
+                        'q0': q0,
+                        'q1': q1,
+                    }
+                ]
         return records
 
 
@@ -453,6 +478,13 @@ def _verify_and_get_two_qubits_from_circuits(circuits: Sequence['cirq.Circuit'])
             "`circuits` should be a sequence of circuits each operating on the same two qubits."
         )
     return all_qubits_list
+
+
+def _verify_two_line_qubits_from_circuits(circuits: Sequence['cirq.Circuit']):
+    if _verify_and_get_two_qubits_from_circuits(circuits) != devices.LineQubit.range(2):
+        raise ValueError(
+            "`circuits` should be a sequence of circuits each operating on LineQubit(0) and LineQubit(1)"
+        )
 
 
 class _NoProgress:
@@ -469,17 +501,42 @@ class _NoProgress:
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
-    def update(self, increment: int):
+    def update(self, n: int = 1):
         pass
+
+
+@dataclass(frozen=True)
+class _ZippedCircuit:
+    """A fully-wide circuit made by zipping together a bunch of two-qubit circuits
+    and its provenance data.
+
+    Args:
+        wide_circuit: The zipped circuit on all pairs
+        pairs: The pairs of qubits operated on in the wide circuit.
+        layer_i: The index of the GridInteractionLayer to which the pairs correspond.
+        combination_i: The row index of the combinations matrix that identifies this
+            particular combination of component narrow circuits.
+        combination: The row of the combinations matrix. Each entry is an index
+            into the (narrow) `circuits` library. Each entry indexes the
+            narrow circuit operating on the corresponding pair in `pairs`.
+    """
+
+    wide_circuit: 'cirq.Circuit'
+    pairs: List[Tuple['cirq.Qid', 'cirq.Qid']]
+    layer_i: int
+    combination_i: int
+    combination: List[int]
 
 
 def sample_2q_xeb_circuits(
     sampler: 'cirq.Sampler',
     circuits: Sequence['cirq.Circuit'],
     cycle_depths: Sequence[int],
+    *,
     repetitions: int = 10_000,
     batch_size: int = 9,
     progress_bar: Optional[Callable[[int], ContextManager]] = tqdm.tqdm,
+    combinations_by_layer: Optional[List[CircuitLibraryCombination]] = None,
 ):
     """Sample two-qubit XEB circuits given a sampler.
 
@@ -498,32 +555,97 @@ def sample_2q_xeb_circuits(
             is given by this number.
         progress_bar: A progress context manager following the `tqdm` API or
             `None` to not report progress.
+        combinations_by_layer: Either `None` or the result of
+            `rqcg.get_random_combinations_for_device`.
+            If this is `None`, the circuits specified by `circuits` will be
+            sampled verbatim, resulting in isolated XEB characterization.
+            Otherwise,  this contains all the
+            random combinations and metadata required to combine the circuits
+            in `circuits` into wide, parallel-XEB-style circuits for execution.
 
     Returns:
-        A pandas dataframe with index given by ['circuit_i', 'cycle_depth'] and
-        column "sampled_probs".
+        A pandas dataframe with index given by ['circuit_i', 'cycle_depth'].
+        Columns always include "sampled_probs". If `combinations_by_layer` is
+        not `None` and you are doing parallel XEB, additional metadata columns
+        will be attached to the returned DataFrame.
     """
+    # Set up progress reporting
     if progress_bar is None:
         progress_bar = _NoProgress
 
-    q0, q1 = _verify_and_get_two_qubits_from_circuits(circuits)
-    tasks = []
-    for cycle_depth in cycle_depths:
-        for circuit_i, circuit in enumerate(circuits):
-            circuit_depth = cycle_depth * 2 + 1
-            assert circuit_depth <= len(circuit)
-            truncated_circuit = circuit[:circuit_depth]
-            prepared_circuit = truncated_circuit + ops.measure(q0, q1)
-            tasks.append(
-                _Sample2qXEBTask(
-                    cycle_depth=cycle_depth, circuit_i=circuit_i, prepared_circuit=prepared_circuit
+    # Shim isolated-XEB as a special case of combination-style parallel XEB.
+    if combinations_by_layer is None:
+        q0, q1 = _verify_and_get_two_qubits_from_circuits(circuits)
+        circuits = [
+            circuit.transform_qubits(
+                lambda q: {q0: devices.LineQubit(0), q1: devices.LineQubit(1)}[q]
+            )
+            for circuit in circuits
+        ]
+        combinations_by_layer = [
+            CircuitLibraryCombination(
+                layer=None,
+                combinations=np.arange(len(circuits))[:, np.newaxis],
+                pairs=[(q0, q1)],
+            )
+        ]
+        one_pair = True
+    else:
+        _verify_two_line_qubits_from_circuits(circuits)
+        one_pair = False
+
+    # Check `combinations_by_layer` is compatible with `circuits`.
+    for layer_combinations in combinations_by_layer:
+        if np.any(layer_combinations.combinations < 0) or np.any(layer_combinations.combinations >= len(circuits)):
+            raise ValueError("`combinations_by_layer` has invalid indices.")
+
+    # Construct fully-wide "zipped" circuits.
+    zipped_circuits: List[_ZippedCircuit] = []
+    for layer_i, layer_combinations in enumerate(combinations_by_layer):
+        for combination_i, combination in enumerate(layer_combinations.combinations):
+            wide_circuit = Circuit.zip(
+                *(
+                    circuits[i].transform_qubits(lambda q: pair[q.x])
+                    for i, pair in zip(combination, layer_combinations.pairs)
+                )
+            )
+            zipped_circuits.append(
+                _ZippedCircuit(
+                    layer_i=layer_i,
+                    combination_i=combination_i,
+                    wide_circuit=wide_circuit,
+                    pairs=layer_combinations.pairs,
+                    combination=combination.tolist(),
                 )
             )
 
+    # Construct truncated-with-measurement circuits to run!
+    tasks = []
+    for cycle_depth in cycle_depths:
+        for zipped_circuit in zipped_circuits:
+            circuit_depth = cycle_depth * 2 + 1
+            assert circuit_depth <= len(zipped_circuit.circuit)
+            # Slicing creates a copy, although this isn't documented
+            prepared_circuit = zipped_circuit.circuit[:circuit_depth]
+            for pair_i, pair in enumerate(zipped_circuit.pairs):
+                prepared_circuit += ops.measure(*pair, key=str(pair_i))
+            tasks.append(
+                _Sample2qXEBTask(
+                    cycle_depth=cycle_depth,
+                    layer_i=zipped_circuit.layer_i,
+                    combination_i=zipped_circuit.combination_i,
+                    prepared_circuit=prepared_circuit,
+                    combination=zipped_circuit.combination,
+                )
+            )
+
+    # Batch and run tasks
     n_tasks = len(tasks)
     batched_tasks = [tasks[i : i + batch_size] for i in range(0, n_tasks, batch_size)]
 
-    run_batch = _SampleInBatches(sampler=sampler, repetitions=repetitions)
+    run_batch = _SampleInBatches(
+        sampler=sampler, repetitions=repetitions, combinations_by_layer=combinations_by_layer
+    )
     with ThreadPoolExecutor(max_workers=2) as pool:
         futures = [pool.submit(run_batch, task_batch) for task_batch in batched_tasks]
 
@@ -533,7 +655,22 @@ def sample_2q_xeb_circuits(
                 records += future.result()
                 progress.update(batch_size)
 
-    return pd.DataFrame(records).set_index(['circuit_i', 'cycle_depth'])
+    # Set up the dataframe.
+    df = pd.DataFrame(records).set_index(['circuit_i', 'cycle_depth'])
+    if one_pair:
+        # TODO: is this helpful or annoying?
+        df = df.drop(['layer_i', 'pair_i', 'combination_i'], axis=1)
+    return df
+
+
+@dataclass(frozen=True)
+class _Simulate2qXEBTask:
+    """Helper container for executing simulation tasks, potentially via multiprocessing."""
+
+    circuit_i: int
+    cycle_depths: Sequence[int]
+    circuit: 'cirq.Circuit'
+    param_resolver: Dict[str, float]
 
 
 @dataclass(frozen=True)
@@ -689,10 +826,29 @@ def benchmark_2q_xeb_fidelities(
     df = df.apply(_summary_stats, axis=1)
 
     def per_cycle_depth(df):
+        """This function is applied per cycle_depth in the following groupby aggregation."""
         fid_lsq = df['numerator'].sum() / df['denominator'].sum()
-        return pd.Series({'fidelity': fid_lsq})
+        ret = {'fidelity': fid_lsq}
 
-    return df.reset_index().groupby('cycle_depth').apply(per_cycle_depth).reset_index()
+        def _try_keep(k):
+            """If all the values for a key `k` are the same in this group, we can keep it."""
+            if k not in df.columns:
+                return
+            vals = df[k].unique()
+            if len(vals) == 1:
+                ret[k] = vals[0]
+
+        _try_keep('q0')
+        _try_keep('q1')
+        _try_keep('pair_name')
+        return pd.Series(ret)
+
+    if 'pair_i' in df.columns:
+        groupby_names = ['layer_i', 'pair_i', 'cycle_depth']
+    else:
+        groupby_names = ['cycle_depth']
+
+    return df.reset_index().groupby(groupby_names).apply(per_cycle_depth).reset_index()
 
 
 @dataclass(frozen=True)
--- cirq/experiments/random_quantum_circuit_generation.py
+++ cirq/experiments/random_quantum_circuit_generation.py
@@ -270,6 +270,74 @@ def generate_library_of_2q_circuits(
     ]
 
 
+def _get_active_pairs(graph: nx.Graph, grid_layer: GridInteractionLayer):
+    """Extract pairs of qubits from a device graph and a GridInteractionLayer."""
+    for edge in graph.edges:
+        if edge in grid_layer:
+            yield edge
+
+
+@dataclasses.dataclass(frozen=True)
+class CircuitLibraryCombination:
+    """For a given layer (which defines a set of pairs of qubits), `combinations`
+    is a 2d array of shape (n_combinations, len(pairs)) where each row
+    represents a combination (with replacement) of two-qubit circuits. The
+    actual values are indices into a list of library circuits.
+    """
+
+    layer: Optional[GridInteractionLayer]
+    combinations: np.array
+    pairs: List[Tuple['cirq.Qid', 'cirq.Qid']]
+
+
+def get_random_combinations_for_device(
+    n_library_circuits: int,
+    n_combinations: int,
+    device_graph: nx.Graph,
+    *,
+    pattern: Sequence[GridInteractionLayer] = HALF_GRID_STAGGERED_PATTERN,
+    random_state: 'cirq.RANDOM_STATE_OR_SEED_LIKE' = None,
+) -> List[CircuitLibraryCombination]:
+    """For a given device, prepare a set of combinations to efficiently sample
+    parallel two-qubit XEB circuits.
+
+    Args:
+        n_library_circuits: The number of circuits in your library. Likely the value
+            passed to `generate_library_of_2q_circuits`.
+        n_combinations: The number of combinations (with replacement) to generate
+            using the library circuits. Since this function returns a
+            `CircuitLibraryCombination`, the combinations will be represented
+            by indexes between 0 and `n_library_circuits-1` instead of the circuits
+            themselves.
+        device_graph: A graph whose nodes are qubits and whose edges represent
+            the possibility of doing a two-qubit gate. This combined with the
+            `pattern` argument determines which two qubit pairs are activated
+            when.
+        pattern: A sequence of `GridInteractionLayer`, each of which has
+            a particular set of qubits that are activated simultaneously. These
+            pairs of qubits are deduced by combining this argument with `device_graph`.
+        random_state: A random-state-like object to seed the random combination generation.
+
+    Returns:
+        A list of `CircuitLibraryCombination`, each corresponding to an interaction
+        layer in `pattern`. Each object has a `combinations` matrix of circuit
+        indices of shape `(n_combinations, len(pairs))` where `len(pairs)` may
+        be different for each entry (i.e. for each layer in `pattern`). This
+        returned list can be provided to `sample_2q_xeb_circuits` to efficiently
+        sample parallel XEB circuits.
+    """
+    rs = value.parse_random_state(random_state)
+
+    combinations_by_layer = []
+    for layer in pattern:
+        pairs = sorted(_get_active_pairs(device_graph, layer))
+        combinations = rs.randint(0, n_library_circuits, size=(n_combinations, len(pairs)))
+        combinations_by_layer.append(
+            CircuitLibraryCombination(layer=layer, combinations=combinations, pairs=pairs)
+        )
+    return combinations_by_layer
+
+
 def random_rotations_between_grid_interaction_layers_circuit(
     qubits: Iterable['cirq.GridQubit'],
     depth: int,
