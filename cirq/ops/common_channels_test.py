300acdc16dbbbfda6a6090d32d1bbe2f49e0a621
Craig Gidney <craiggidney@google.com>
2021-02-10 09:32:50 -0400

    test

--- cirq/ops/common_channels_test.py
+++ cirq/ops/common_channels_test.py
@@ -285,13 +285,13 @@ def test_depolarizing_channel_text_diagram():
 def test_depolarizing_channel_text_diagram_two_qubits():
     d = cirq.depolarize(0.1234567, n_qubits=2)
     assert cirq.circuit_diagram_info(d, args=round_to_6_prec) == cirq.CircuitDiagramInfo(
-        wire_symbols=('D(0.123457)',)
+        wire_symbols=('D(0.123457)', '#2')
     )
     assert cirq.circuit_diagram_info(d, args=round_to_2_prec) == cirq.CircuitDiagramInfo(
-        wire_symbols=('D(0.12)',)
+        wire_symbols=('D(0.12)', '#2')
     )
     assert cirq.circuit_diagram_info(d, args=no_precision) == cirq.CircuitDiagramInfo(
-        wire_symbols=('D(0.1234567)',)
+        wire_symbols=('D(0.1234567)', '#2')
     )
 
 
