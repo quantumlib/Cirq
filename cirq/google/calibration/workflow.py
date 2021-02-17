ffd772a804894b98b72d82b990a1f195bd87c3d2
Wojciech Mruczkiewicz <mrwojtek@google.com>
2021-02-17 10:55:37 +0100

    Add claryfing comment

--- cirq/google/calibration/workflow.py
+++ cirq/google/calibration/workflow.py
@@ -421,6 +421,13 @@ def make_zeta_chi_gamma_compensation_for_operations(
     characterizations to the moment structure of the circuits and thus is less accurate because
     some errors caused by cross-talks are not mitigated.
 
+    The major advantage of this method over make_zeta_chi_gamma_compensation_for_moments is that it
+    can work with arbitrary set characterizations that cover all the interactions of the circuit
+    (up to assumptions of merge_matching_results method). In particular for grid-like devices the
+    number of characterizations is bounded by four, where in the case of
+    make_zeta_chi_gamma_compensation_for_moments the number of characterizations is bounded by
+    number of moments in a circuit.
+
     This function preserves a moment structure of the circuit. All single qubit gates appear on new
     moments in the final circuit.
 
