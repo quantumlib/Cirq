f9a82f18a644ebef4088b998cf91c334e34200b3
Balint Pato <balopat@users.noreply.github.com>
2021-02-18 08:19:17 -0800

    Move google noisemodel to cirq.google (#3824)
    Moves cirq.google references out of cirq.contrib.noise_model.
    
    - experimental Google noise models -> `cirq/google/experimental`
    - other experimental noise models -> `cirq/contrib/noise_models`
    - code required by both -> `cirq/devices`
    
    Related to https://github.com/quantumlib/Cirq/issues/2832 but motivated by https://github.com/quantumlib/Cirq/issues/3737. Also related to the Packaging strategy #3228 - taking contrib apart.

--- cirq/google/experimental/noise_models/__init__.py
+++ cirq/google/experimental/noise_models/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 The Cirq Developers
+# Copyright 2021 The Cirq Developers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cirq.contrib.noise_models.noise_models import (
-    DepolarizingNoiseModel,
-    ReadoutNoiseModel,
-    DampedReadoutNoiseModel,
-    DepolarizingWithReadoutNoiseModel,
-    DepolarizingWithDampedReadoutNoiseModel,
+"""Noise models for Google devices."""
+
+from cirq.google.experimental.noise_models.noise_models import (
+    PerQubitDepolarizingWithDampedReadoutNoiseModel,
+    simple_noise_from_calibration_metrics,
 )
