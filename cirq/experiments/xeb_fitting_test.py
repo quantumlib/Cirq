d2f3e2b7b6ad3d1c4c5382487a4d798ef7ea4b92
Matthew Harrigan <mpharrigan@google.com>
2021-02-23 17:27:41 -0800

    [XEB] Enable characterizing other gates
    We don't provide sensible defaults for
    anything other than sqrt(iswap) but with
    these changes, you can write your own
    XEBCalibrationOptions subclass to characterize
    whatever you want.

--- cirq/experiments/xeb_fitting_test.py
+++ cirq/experiments/xeb_fitting_test.py
@@ -25,7 +25,7 @@ import cirq.experiments.random_quantum_circuit_generation as rqcg
 from cirq.experiments.xeb_fitting import (
     SQRT_ISWAP,
     benchmark_2q_xeb_fidelities,
-    parameterize_phased_fsim_circuit,
+    parameterize_circuit,
     SqrtISwapXEBOptions,
     characterize_phased_fsim_parameters_with_xeb,
     characterize_phased_fsim_parameters_with_xeb_by_pair,
@@ -144,7 +144,7 @@ def test_parameterize_phased_fsim_circuit():
         q0, q1, depth=3, two_qubit_op_factory=lambda a, b, _: SQRT_ISWAP(a, b), seed=52
     )
 
-    p_circuit = parameterize_phased_fsim_circuit(circuit, SqrtISwapXEBOptions())
+    p_circuit = parameterize_circuit(circuit, SqrtISwapXEBOptions())
     cirq.testing.assert_has_diagram(
         p_circuit,
         """\
@@ -205,13 +205,13 @@ def test_characterize_phased_fsim_parameters_with_xeb():
         characterize_zeta=False,
         characterize_phi=False,
     )
-    p_circuits = [parameterize_phased_fsim_circuit(circuit, options) for circuit in circuits]
+    p_circuits = [parameterize_circuit(circuit, options) for circuit in circuits]
     with multiprocessing.Pool() as pool:
         result = characterize_phased_fsim_parameters_with_xeb(
             sampled_df=sampled_df,
             parameterized_circuits=p_circuits,
             cycle_depths=cycle_depths,
-            phased_fsim_options=options,
+            options=options,
             # speed up with looser tolerances:
             fatol=1e-2,
             xatol=1e-2,
@@ -264,13 +264,13 @@ def test_parallel_full_workflow(use_pool):
     options = SqrtISwapXEBOptions(
         characterize_zeta=False, characterize_gamma=False, characterize_chi=False
     )
-    p_circuits = [parameterize_phased_fsim_circuit(circuit, options) for circuit in circuits]
+    p_circuits = [parameterize_circuit(circuit, options) for circuit in circuits]
 
     result = characterize_phased_fsim_parameters_with_xeb_by_pair(
         sampled_df=sampled_df,
         parameterized_circuits=p_circuits,
         cycle_depths=cycle_depths,
-        phased_fsim_options=options,
+        options=options,
         # super loose tolerances
         fatol=5e-2,
         xatol=5e-2,
