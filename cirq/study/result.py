ff671da0996e2ecbdc5de36eb89296c07770d484
Victory Omole <vtomole2@gmail.com>
2021-03-03 18:05:50 -0600

    Update cirq/study/result.py
    Co-authored-by: Balint Pato <balopat@users.noreply.github.com>

--- cirq/study/result.py
+++ cirq/study/result.py
@@ -150,7 +150,7 @@ class Result:
 
     @property
     def repetitions(self) -> int:
-        if not self.measurements or not self.measurements.values():
+        if not self.measurements:
             return 0
         # Get the length quickly from one of the keyed results.
         return len(next(iter(self.measurements.values())))
