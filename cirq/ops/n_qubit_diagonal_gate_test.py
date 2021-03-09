ef9246e776cf1246856f67df444c06f83d2ea889
ybc <bichengying@gmail.com>
2021-03-08 16:37:00 -0800

    Use explicit type for _candidate_angeles

--- cirq/ops/n_qubit_diagonal_gate_test.py
+++ cirq/ops/n_qubit_diagonal_gate_test.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import List
 import itertools
 import numpy as np
 import pytest
@@ -19,7 +20,7 @@ import sympy
 
 import cirq
 
-_candidate_angeles = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
+_candidate_angeles: List[float] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
 
 
 @pytest.mark.parametrize(
