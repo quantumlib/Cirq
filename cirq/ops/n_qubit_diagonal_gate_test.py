bacf0a5061b1af322104c615a729df8a279e6291
YBC <bichengying@gmail.com>
2021-03-10 17:54:42 -0800

    Update cirq/ops/n_qubit_diagonal_gate_test.py
    Co-authored-by: Balint Pato <balopat@users.noreply.github.com>

--- cirq/ops/n_qubit_diagonal_gate_test.py
+++ cirq/ops/n_qubit_diagonal_gate_test.py
@@ -19,7 +19,7 @@ import sympy
 
 import cirq
 
-_candidate_angeles: List[float] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
+_candidate_angles: List[float] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
 
 
 @pytest.mark.parametrize(
