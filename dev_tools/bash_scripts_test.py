c2fc6194fa4b194fd5febc3072436dcee408036f
vtomole <vtomole2@gmail.com>
2021-03-10 18:10:50 -0600

    Update bashscripts test

--- dev_tools/bash_scripts_test.py
+++ dev_tools/bash_scripts_test.py
@@ -607,7 +607,7 @@ def test_incremental_format_branch_selection(tmpdir_factory):
 def test_pylint_changed_files_file_selection(tmpdir_factory):
 
     result = run(
-        script_file='check/pylint-changed-files',
+        script_file='check/lint-changed-files',
         tmpdir_factory=tmpdir_factory,
         arg='HEAD~1',
         setup='touch file.py\ngit add -A\ngit commit -m test --quiet --no-gpg-sign\n',
@@ -625,7 +625,7 @@ def test_pylint_changed_files_file_selection(tmpdir_factory):
     intercepted_prefix = 'INTERCEPTED pylint --rcfile=dev_tools/conf/.pylintrc '
 
     result = run(
-        script_file='check/pylint-changed-files',
+        script_file='check/lint-changed-files',
         tmpdir_factory=tmpdir_factory,
         arg='HEAD~1',
         setup='mkdir cirq\n'
@@ -644,7 +644,7 @@ def test_pylint_changed_files_file_selection(tmpdir_factory):
     )
 
     result = run(
-        script_file='check/pylint-changed-files',
+        script_file='check/lint-changed-files',
         tmpdir_factory=tmpdir_factory,
         arg='HEAD~1',
         setup='mkdir cirq\n'
@@ -663,7 +663,7 @@ def test_pylint_changed_files_file_selection(tmpdir_factory):
     )
 
     result = run(
-        script_file='check/pylint-changed-files',
+        script_file='check/lint-changed-files',
         tmpdir_factory=tmpdir_factory,
         arg='HEAD',
         setup='mkdir cirq\n'
@@ -683,7 +683,7 @@ def test_pylint_changed_files_file_selection(tmpdir_factory):
     )
 
     result = run(
-        script_file='check/pylint-changed-files',
+        script_file='check/lint-changed-files',
         tmpdir_factory=tmpdir_factory,
         arg='HEAD~1',
         setup='mkdir cirq dev_tools examples ignore\n'
