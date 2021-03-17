8b2ed1470bfc3237578e2b4c706a80d132792add
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2021-03-17 12:32:41 -0700

    Rewind gate_sets_test format.

--- cirq/google/gate_sets_test.py
+++ cirq/google/gate_sets_test.py
@@ -202,15 +202,11 @@ def test_serialize_circuit():
         circuit=v2.program_pb2.Circuit(
             scheduling_strategy=v2.program_pb2.Circuit.MOMENT_BY_MOMENT,
             moments=[
+                v2.program_pb2.Moment(operations=[cg.XMON.serialize_op(cirq.CZ(q0, q1))]),
                 v2.program_pb2.Moment(
-                    operations=[cg.XMON.serialize_op(cirq.CZ(q0, q1))],
+                    operations=[cg.XMON.serialize_op(cirq.X(q0)), cg.XMON.serialize_op(cirq.Z(q1))]
                 ),
-                v2.program_pb2.Moment(
-                    operations=[cg.XMON.serialize_op(cirq.X(q0)), cg.XMON.serialize_op(cirq.Z(q1))],
-                ),
-                v2.program_pb2.Moment(
-                    operations=[cg.XMON.serialize_op(cirq.measure(q1, key='m'))],
-                ),
+                v2.program_pb2.Moment(operations=[cg.XMON.serialize_op(cirq.measure(q1, key='m'))]),
             ],
         ),
     )
