d58dcd3b094519aed693783612af07e21634f780
Tanuj Khattar <tanujkhattar@google.com>
2021-03-18 02:00:40 +0100

    Fix lint errors

--- cirq/ops/common_gates_test.py
+++ cirq/ops/common_gates_test.py
@@ -1011,21 +1011,21 @@ q: ───Z───Z───Z───S───S───
 
 
 @pytest.mark.parametrize(
-    'theta,pow',
+    'theta,exp',
     [
         {sympy.Symbol("theta"), 1 / 2},
         {np.pi / 2, 1 / 2},
-        {np.pi / 2, sympy.Symbol("pow")},
-        {sympy.Symbol("theta"), sympy.Symbol("pow")},
+        {np.pi / 2, sympy.Symbol("exp")},
+        {sympy.Symbol("theta"), sympy.Symbol("exp")},
     ],
 )
-def test_rxyz_exponent(theta, pow):
+def test_rxyz_exponent(theta, exp):
     def resolve(gate):
-        return cirq.resolve_parameters(gate, {'theta': np.pi / 4}, {'pow': 1 / 4})
+        return cirq.resolve_parameters(gate, {'theta': np.pi / 4}, {'exp': 1 / 4})
 
-    assert resolve(cirq.Rx(rads=theta) ** pow) == resolve(cirq.Rx(rads=theta * pow))
-    assert resolve(cirq.Ry(rads=theta) ** pow) == resolve(cirq.Ry(rads=theta * pow))
-    assert resolve(cirq.Rz(rads=theta) ** pow) == resolve(cirq.Rz(rads=theta * pow))
+    assert resolve(cirq.Rx(rads=theta) ** exp) == resolve(cirq.Rx(rads=theta * exp))
+    assert resolve(cirq.Ry(rads=theta) ** exp) == resolve(cirq.Ry(rads=theta * exp))
+    assert resolve(cirq.Rz(rads=theta) ** exp) == resolve(cirq.Rz(rads=theta * exp))
 
 
 def test_rxyz_circuit_diagram():
