a063959784eaa3a1074319208a950638897970c2
Tanuj Khattar <tanujkhattar@google.com>
2021-03-20 00:45:10 +0100

    improve coverage

--- cirq/vis/histogram_test.py
+++ cirq/vis/histogram_test.py
@@ -24,10 +24,15 @@ from cirq.vis import integrated_histogram
 @pytest.mark.parametrize('data', [range(10), {f'key_{i}': i for i in range(10)}])
 def test_integrated_histogram(data):
     ax = integrated_histogram(
-        data, title='Test Plot', axis_label='X Axis Label', color='r', label='line label'
+        data,
+        title='Test Plot',
+        axis_label='Y Axis Label',
+        color='r',
+        label='line label',
+        cdf_on_x=True,
     )
     assert ax.get_title() == 'Test Plot'
-    assert ax.get_xlabel() == 'X Axis Label'
+    assert ax.get_ylabel() == 'Y Axis Label'
     assert len(ax.get_lines()) == 2
     for line in ax.get_lines():
         assert line.get_color() == 'r'
