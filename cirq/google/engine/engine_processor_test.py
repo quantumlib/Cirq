4266ff08dba608151c445857e90af81028489637
Bálint Pató <balintp@google.com>
2021-03-22 14:19:40 -0700

    coverage ignore

--- cirq/google/engine/engine_processor_test.py
+++ cirq/google/engine/engine_processor_test.py
@@ -575,6 +575,7 @@ def _allow_deprecated_freezegun(func):
             if orig_exist:
                 # mypy can't resolve that orig_exist ensures that orig_value
                 # of type Optional[str] can't be None
+                # coverage: ignore
                 os.environ[ALLOW_DEPRECATION_IN_TEST] = orig_value  # type: ignore
             else:
                 del os.environ[ALLOW_DEPRECATION_IN_TEST]
