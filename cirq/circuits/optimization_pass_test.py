ce7cd3f92368df5e68876501637c0cbb562568fb
vtomole <vtomole2@gmail.com>
2021-03-23 21:59:00 -0500

    100% coverage

--- cirq/circuits/optimization_pass_test.py
+++ cirq/circuits/optimization_pass_test.py
@@ -158,14 +158,16 @@ def test_point_optimizer_raises_on_gates_changing_qubits():
         def optimization_at(
             self, circuit: 'cirq.Circuit', index: int, op: 'cirq.Operation'
         ) -> Optional['cirq.PointOptimizationSummary']:
+            new_op = op
             if len(op.qubits) == 1 and isinstance(op, cirq.GateOperation):
                 new_op = op.gate(cirq.LineQubit(42))
-                return cirq.PointOptimizationSummary(
-                    clear_span=1, clear_qubits=op.qubits, new_operations=new_op
-                )
-            return None
+
+            return cirq.PointOptimizationSummary(
+                clear_span=1, clear_qubits=op.qubits, new_operations=new_op
+            )
 
     c = cirq.Circuit(cirq.X(cirq.LineQubit(0)), cirq.X(cirq.LineQubit(1)))
+
     with pytest.raises(ValueError, match='new qubits'):
         EverythingIs42().optimize_circuit(c)
 
