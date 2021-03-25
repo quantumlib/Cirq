0edf5665a0acebeb147261bff4f017715f45aeab
Dax Fohl <dax.fohl@gmail.com>
2021-03-25 10:17:48 -0700

    Update quantum teleportation

--- examples/quantum_teleportation.py
+++ examples/quantum_teleportation.py
@@ -37,10 +37,11 @@ import random
 import numpy as np
 import cirq
 
+msg, alice, bob = cirq.LineQubit.range(3)
+
 
 def make_quantum_teleportation_circuit(ranX, ranY):
     circuit = cirq.Circuit()
-    msg, alice, bob = cirq.LineQubit.range(3)
 
     # Creates Bell state to be shared between Alice and Bob.
     circuit.append([cirq.H(alice), cirq.CNOT(alice, bob)])
@@ -48,10 +49,21 @@ def make_quantum_teleportation_circuit(ranX, ranY):
     circuit.append([cirq.X(msg) ** ranX, cirq.Y(msg) ** ranY])
     # Bell measurement of the Message and Alice's entangled qubit.
     circuit.append([cirq.CNOT(msg, alice), cirq.H(msg)])
-    circuit.append(cirq.measure(msg, alice))
+    circuit.append(cirq.measure(alice, key='a'))
+    circuit.append(cirq.measure(msg, key='m'))
+    return circuit
+
+
+def make_final_quantum_teleportation_circuit(meas_alice, meas_msg):
+    circuit = cirq.Circuit()
+    circuit.append(cirq.identity_each(msg, alice, bob))
     # Uses the two classical bits from the Bell measurement to recover the
     # original quantum Message on Bob's entangled qubit.
-    circuit.append([cirq.CNOT(alice, bob), cirq.CZ(msg, bob)])
+    if meas_alice:
+        circuit.append(cirq.X(bob))
+
+    if meas_msg:
+        circuit.append(cirq.Z(bob))
 
     return circuit
 
@@ -73,13 +85,12 @@ def main(seed=None):
 
     sim = cirq.Simulator(seed=seed)
 
+    print("\nBloch Sphere of Message After Random X and Y Gates:")
+    # Prints the Bloch Sphere of the Message after the X and Y gates.
     # Run a simple simulation that applies the random X and Y gates that
     # create our message.
     q0 = cirq.LineQubit(0)
     message = sim.simulate(cirq.Circuit([cirq.X(q0) ** ranX, cirq.Y(q0) ** ranY]))
-
-    print("\nBloch Sphere of Message After Random X and Y Gates:")
-    # Prints the Bloch Sphere of the Message after the X and Y gates.
     expected = cirq.bloch_vector_from_state_vector(message.final_state_vector, 0)
     print(
         "x: ",
@@ -90,12 +101,18 @@ def main(seed=None):
         np.around(expected[2], 4),
     )
 
-    # Records the final state of the simulation.
-    final_results = sim.simulate(circuit)
+    args = sim._create_act_on_args(circuit, cirq.QubitOrder.DEFAULT, 0)
+    result = list(sim._core_iterator(circuit, cirq.QubitOrder.DEFAULT, args))
+    meas_alice = result[3].measurements['a'] == [1]
+    meas_msg = result[4].measurements['m'] == [1]
+    final_circuit = make_final_quantum_teleportation_circuit(meas_alice, meas_msg)
+
+    final_result = list(sim._core_iterator(final_circuit, cirq.QubitOrder.DEFAULT, args))
+    final_r = final_result[len(final_result) - 1]
 
     print("\nBloch Sphere of Qubit 2 at Final State:")
     # Prints the Bloch Sphere of Bob's entangled qubit at the final state.
-    teleported = cirq.bloch_vector_from_state_vector(final_results.final_state_vector, 2)
+    teleported = cirq.bloch_vector_from_state_vector(final_r.state_vector(), 2)
     print(
         "x: ",
         np.around(teleported[0], 4),
