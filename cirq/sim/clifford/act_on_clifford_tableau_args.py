c162e10c79663f97b62ba13c65e266853b48fe90
Dax Fohl <dax.fohl@gmail.com>
2021-03-25 08:29:24 -0700

    Migrate MPS to act_on protocol (#3961)
    This migrates the MPS simulator to use the act_on protocol, completing this migration for all the simulators.
    
    @tonybruguier can you verify this change doesn't affect MPS functionality? All the tests pass, but please make sure this doesn't break anything (including performance).

--- cirq/sim/clifford/act_on_clifford_tableau_args.py
+++ cirq/sim/clifford/act_on_clifford_tableau_args.py
@@ -56,7 +56,7 @@ class ActOnCliffordTableauArgs(ActOnArgs):
                 being recorded into. Edit it easily by calling
                 `ActOnCliffordTableauArgs.record_measurement_result`.
         """
-        super().__init__(axes, prng, log_of_measurement_results)
+        super().__init__(prng, axes, log_of_measurement_results)
         self.tableau = tableau
 
     def _act_on_fallback_(self, action: Any, allow_decompose: bool):
