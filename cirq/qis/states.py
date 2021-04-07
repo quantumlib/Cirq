c4ae6100fbc6b277cae57535901992120828a973
Matthew Harrigan <mpharrigan@google.com>
2021-04-07 11:43:53 -0700

    Fix most numpy errors in cirq/qis and friends

--- cirq/qis/states.py
+++ cirq/qis/states.py
@@ -14,7 +14,7 @@
 """Classes and methods for quantum states."""
 
 import itertools
-from typing import Any, cast, Iterable, Optional, Sequence, TYPE_CHECKING, Tuple, Type, Union
+from typing import Any, cast, Iterable, Optional, Sequence, TYPE_CHECKING, Tuple, Union
 
 import numpy as np
 
@@ -23,6 +23,7 @@ from cirq._doc import document
 
 if TYPE_CHECKING:
     import cirq
+    from numpy.typing import DTypeLike
 
 DEFAULT_COMPLEX_DTYPE = np.complex64
 
@@ -61,7 +62,7 @@ class QuantumState:
         data: np.ndarray,
         qid_shape: Optional[Tuple[int, ...]] = None,
         *,  # Force keyword arguments
-        dtype: Optional[Type[np.number]] = None,
+        dtype: Optional['DTypeLike'] = None,
         validate: bool = True,
         atol: float = 1e-7,
     ) -> None:
@@ -87,7 +88,7 @@ class QuantumState:
             )
         self._data = data
         self._qid_shape = qid_shape
-        self._dim = np.prod(self.qid_shape, dtype=int)
+        self._dim = np.prod(self.qid_shape, dtype=int).item()
         if validate:
             self.validate(dtype=dtype, atol=atol)
 
@@ -102,7 +103,7 @@ class QuantumState:
         return self._qid_shape
 
     @property
-    def dtype(self) -> np.ndarray:
+    def dtype(self) -> np.dtype:
         """The data type of the quantum state."""
         return self._data.dtype
 
@@ -135,7 +136,7 @@ class QuantumState:
         (a two-dimensional array).
         """
         if not self._is_density_matrix():
-            state_vector = self.state_vector()
+            state_vector = cast(np.ndarray, self.state_vector())
             return np.outer(state_vector, np.conj(state_vector))
         return self.data
 
@@ -144,7 +145,7 @@ class QuantumState:
         return self.data.shape == (self._dim, self._dim)
 
     def validate(
-        self, *, dtype: Optional[Type[np.number]] = None, atol=1e-7  # Force keyword arguments
+        self, *, dtype: Optional['DTypeLike'] = None, atol=1e-7  # Force keyword arguments
     ) -> None:
         """Check if this quantum state is valid.
 
@@ -159,7 +160,10 @@ class QuantumState:
         is_state_tensor = self.data.shape == self.qid_shape
         if is_state_vector or is_state_tensor:
             validate_normalized_state_vector(
-                self.state_vector(), qid_shape=self.qid_shape, dtype=dtype, atol=atol
+                cast(np.ndarray, self.state_vector()),
+                qid_shape=self.qid_shape,
+                dtype=dtype,
+                atol=atol,
             )
         elif self._is_density_matrix():
             validate_density_matrix(
@@ -179,7 +183,7 @@ def quantum_state(
     *,  # Force keyword arguments
     copy: bool = False,
     validate: bool = True,
-    dtype: Optional[Type[np.number]] = None,
+    dtype: Optional['DTypeLike'] = None,
     atol: float = 1e-7,
 ) -> QuantumState:
     """Create a QuantumState object from a state-like object.
@@ -239,7 +243,7 @@ def quantum_state(
                 'Please specify the qid shape explicitly using '
                 'the qid_shape argument.'
             )
-        dim = np.prod(qid_shape, dtype=int)
+        dim = np.prod(qid_shape, dtype=int).item()
         if dtype is None:
             dtype = DEFAULT_COMPLEX_DTYPE
         data = one_hot(index=state, shape=(dim,), dtype=dtype)
@@ -279,7 +283,7 @@ def density_matrix(
     *,  # Force keyword arguments
     copy: bool = False,
     validate: bool = True,
-    dtype: Optional[Type[np.number]] = None,
+    dtype: Optional['DTypeLike'] = None,
     atol: float = 1e-7,
 ) -> QuantumState:
     """Create a QuantumState object from a density matrix.
@@ -494,7 +498,7 @@ def to_valid_state_vector(
     num_qubits: Optional[int] = None,
     *,  # Force keyword arguments
     qid_shape: Optional[Sequence[int]] = None,
-    dtype: Optional[Type[np.number]] = None,
+    dtype: Optional['DTypeLike'] = None,
     atol: float = 1e-7,
 ) -> np.ndarray:
     """Verifies the state_rep is valid and converts it to ndarray form.
@@ -555,7 +559,7 @@ def _state_like_to_state_tensor(
     *,
     state_like: 'cirq.STATE_VECTOR_LIKE',
     qid_shape: Tuple[int, ...],
-    dtype: Optional[Type[np.number]],
+    dtype: Optional['DTypeLike'],
     atol: float,
 ) -> np.ndarray:
 
@@ -619,7 +623,7 @@ def _amplitudes_to_validated_state_tensor(
     *,
     state_vector: np.ndarray,
     qid_shape: Tuple[int, ...],
-    dtype: Optional[Type[np.number]],
+    dtype: Optional['DTypeLike'],
     atol: float,
 ) -> np.ndarray:
     if dtype is None:
@@ -630,7 +634,7 @@ def _amplitudes_to_validated_state_tensor(
 
 
 def _qudit_values_to_state_tensor(
-    *, state_vector: np.ndarray, qid_shape: Tuple[int, ...], dtype: Optional[Type[np.number]]
+    *, state_vector: np.ndarray, qid_shape: Tuple[int, ...], dtype: Optional['DTypeLike']
 ) -> np.ndarray:
 
     for i in range(len(qid_shape)):
@@ -661,9 +665,9 @@ def _qudit_values_to_state_tensor(
 
 
 def _computational_basis_state_to_state_tensor(
-    *, state_rep: int, qid_shape: Tuple[int, ...], dtype: Optional[Type[np.number]]
+    *, state_rep: int, qid_shape: Tuple[int, ...], dtype: Optional['DTypeLike']
 ) -> np.ndarray:
-    n = np.prod(qid_shape, dtype=int)
+    n = np.prod(qid_shape, dtype=int).item()
     if not 0 <= state_rep < n:
         raise ValueError(
             f'Computational basis state is out of range.\n'
@@ -682,7 +686,7 @@ def validate_normalized_state_vector(
     state_vector: np.ndarray,
     *,  # Force keyword arguments
     qid_shape: Tuple[int, ...],
-    dtype: Optional[Type[np.number]] = None,
+    dtype: Optional['DTypeLike'] = None,
     atol: float = 1e-7,
 ) -> None:
     """Checks that the given state vector is valid.
@@ -754,7 +758,7 @@ def to_valid_density_matrix(
     num_qubits: Optional[int] = None,
     *,  # Force keyword arguments
     qid_shape: Optional[Tuple[int, ...]] = None,
-    dtype: Optional[Type[np.number]] = None,
+    dtype: Optional['DTypeLike'] = None,
     atol: float = 1e-7,
 ) -> np.ndarray:
     """Verifies the density_matrix_rep is valid and converts it to ndarray form.
@@ -799,7 +803,7 @@ def validate_density_matrix(
     density_matrix: np.ndarray,
     *,  # Force keyword arguments
     qid_shape: Tuple[int, ...],
-    dtype: Optional[Type[np.number]] = None,
+    dtype: Optional['DTypeLike'] = None,
     atol: float = 1e-7,
 ) -> None:
     """Checks that the given density matrix is valid.
@@ -867,7 +871,7 @@ def one_hot(
     index: Union[None, int, Sequence[int]] = None,
     shape: Union[int, Sequence[int]],
     value: Any = 1,
-    dtype: Type[np.number],
+    dtype: 'DTypeLike',
 ) -> np.ndarray:
     """Returns a numpy array with all 0s and a single non-zero entry(default 1).
 
@@ -888,9 +892,7 @@ def one_hot(
     return result
 
 
-def eye_tensor(
-    half_shape: Tuple[int, ...], *, dtype: Type[np.number]  # Force keyword args
-) -> np.array:
+def eye_tensor(half_shape: Tuple[int, ...], *, dtype: 'DTypeLike') -> np.ndarray:
     """Returns an identity matrix reshaped into a tensor.
 
     Args:
