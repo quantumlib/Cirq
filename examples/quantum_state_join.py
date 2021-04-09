5c3364b70b9f11a576c60397ebd4f4df35f12808
Dax Fohl <dax.fohl@gmail.com>
2021-04-09 09:54:49 -0700

    comments

--- examples/quantum_state_join.py
+++ examples/quantum_state_join.py
@@ -1,37 +1,3 @@
-"""Quantum Teleportation.
-Quantum Teleportation is a process by which a quantum state can be transmitted
-by sending only two classical bits of information. This is accomplished by
-pre-sharing an entangled state between the sender (Alice) and the receiver
-(Bob). This entangled state allows the receiver (Bob) of the two classical
-bits of information to possess a qubit with the same state as the one held by
-the sender (Alice).
-
-In the following example output, qubit 0 (the Message) is set to a random state
-by applying X and Y gates. By sending two classical bits of information after
-qubit 0 (the Message) and qubit 1 (Alice's entangled qubit) are measured, the
-final state of qubit 2 (Bob's entangled qubit) will be identical to the
-original random state of qubit 0 (the Message). This is only possible given
-that an entangled state is pre-shared between Alice and Bob.
-
-=== REFERENCE ===
-https://en.wikipedia.org/wiki/Quantum_teleportation
-https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.70.1895
-
-=== EXAMPLE OUTPUT ===
-Circuit:
-0: -----------X^0.25---Y^0.125---@---H---M-------@---
-                                 |       |       |
-1: ---H---@----------------------X-------M---@---|---
-          |                                  |   |
-2: -------X----------------------------------X---@---
-
-Bloch Sphere of Message After Random X and Y Gates:
-x:  0.2706 y:  -0.7071 z:  0.6533
-
-Bloch Sphere of Qubit 2 at Final State:
-x:  0.2706 y:  -0.7071 z:  0.6533
-
-"""
 import time
 import random
 from typing import Tuple, cast
@@ -53,7 +19,7 @@ def _run(
     sim: SimulatesIntermediateState[
         TStepResult, TSimulationTrialResult, TSimulatorState, TActOnArgs
     ]
-):
+) -> Tuple[TActOnArgs, TActOnArgs]:
     # Initialize our qubit state space.
     circuit = cirq.Circuit()
     for i in range(circuit_length):
@@ -110,6 +76,7 @@ def _run(
     for i in range(num_qubits):
         print(sam1[i].mean())
         print(sam[i].mean())
+    return args, args_join
 
 
 def main(seed=None):
@@ -123,10 +90,11 @@ def main(seed=None):
     # Run with density matrix simulator
     print('***Run with sparse simulator***')
     sim = cirq.Simulator(seed=seed)
-    _run(num_qubits=22, circuit_length=10, sim=sim)
+    args, args_join = _run(num_qubits=22, circuit_length=10, sim=sim)
+
     print('***Run with density matrix simulator***')
     sim = cirq.DensityMatrixSimulator(seed=seed)
-    _run(num_qubits=11, circuit_length=10, sim=sim)
+    args, args_join = _run(num_qubits=11, circuit_length=10, sim=sim)
 
 
 if __name__ == '__main__':
