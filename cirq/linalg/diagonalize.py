3652c8d1860e6c6981b71182643c64a479da0063
Matthew Harrigan <mpharrigan@google.com>
2021-04-12 13:54:48 -0700

    more cast removal

--- cirq/linalg/diagonalize.py
+++ cirq/linalg/diagonalize.py
@@ -14,7 +14,7 @@
 
 """Utility methods for diagonalizing matrices."""
 
-from typing import Tuple, Callable, List, cast
+from typing import Tuple, Callable, List
 
 import numpy as np
 
@@ -183,9 +183,9 @@ def bidiagonalize_real_matrix_pair_with_symmetric_products(
             raise ValueError('mat1 must be real.')
         if np.any(np.imag(mat2) != 0):
             raise ValueError('mat2 must be real.')
-        if not predicates.is_hermitian(cast(np.ndarray, mat1.dot(mat2.T)), rtol=rtol, atol=atol):
+        if not predicates.is_hermitian(mat1 @ mat2.T, rtol=rtol, atol=atol):
             raise ValueError('mat1 @ mat2.T must be symmetric.')
-        if not predicates.is_hermitian(cast(np.ndarray, mat1.T.dot(mat2)), rtol=rtol, atol=atol):
+        if not predicates.is_hermitian(mat1.T @ mat2, rtol=rtol, atol=atol):
             raise ValueError('mat1.T @ mat2 must be symmetric.')
 
     # Use SVD to bi-diagonalize the first matrix.
