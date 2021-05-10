ca38fa33578050a4d40c6b031ca13653955bd4aa
Tanuj Khattar <tanujkhattar@google.com>
2021-05-10 06:07:40 +0200

    Modify heatmaps tutorials to include demo of config params (#4026)
    - Updates heatmaps tutorials to depict the new functionality of passing `kwargs` in `heatmap.plot()` function. 
    - Also gives an overview of the different configuration parameters that can be used to modify the heatmaps. 
    
    Solves #4017

--- dev_tools/notebooks/isolated_notebook_test.py
+++ dev_tools/notebooks/isolated_notebook_test.py
@@ -41,6 +41,8 @@ NOTEBOOKS_DEPENDING_ON_UNRELEASED_FEATURES = [
     'docs/tutorials/educators/intro.ipynb',
     # the notebook uses cirq.vis.integrated_histogram.
     'docs/tutorials/google/visualizing_calibration_metrics.ipynb',
+    # the notebook uses updated funcationality in cirq.Heatmap()
+    'docs/tutorials/heatmaps.ipynb',
     # these notebooks now use cirq.contrib.calculate_quantum_volume(...device_qubits...)
     # the device_or_qubits parameter is deprecated
     'examples/advanced/quantum_volume_routing.ipynb',
