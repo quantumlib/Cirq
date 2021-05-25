1d2e593e6dc04005c5e37044c863ce312292a048
Bálint Pató <balintp@google.com>
2021-05-25 16:20:33 -0700

    temp save

--- dev_tools/requirements/compile.py
+++ dev_tools/requirements/compile.py
@@ -1,15 +1,18 @@
 import multiprocessing
 import os
+import sys
 from multiprocessing.spawn import freeze_support
+from typing import Tuple
 
 from dev_tools import shell_tools
 from dev_tools.shell_tools import run_shell
 
 
-def pip_compile(f: str):
+def pip_compile(inout: Tuple[str, str]):
+    f, output = inout
     print(f"{f} started.")
     try:
-        run_shell(f"pip-compile --upgrade --quiet {f}")
+        run_shell(f"pip-compile --upgrade --quiet {f} -o {output}")
         print(
             shell_tools.highlight(
                 f"{f} done.",
@@ -28,14 +31,23 @@ def pip_compile(f: str):
 
 
 if __name__ == '__main__':
+    pyver = f"{sys.version_info.major}.{sys.version_info.minor}"
+    print(f"Python version: {pyver}")
+
+    input_dir = "dev_tools/requirements/src"
+    output_dir = f"dev_tools/requirements/dist/{pyver}"
+    os.makedirs(output_dir, exist_ok=True)
     in_files = [
-        os.path.join("dev_tools/requirements", f)
-        for f in os.listdir("dev_tools/requirements")
+        (os.path.join(input_dir, f), os.path.join(output_dir, f))
+        for f in os.listdir(input_dir)
         if f.endswith('.in')
     ]
+    input_dir = f"{input_dir}/deps"
+    output_dir = f"{output_dir}/deps"
+    os.makedirs(output_dir, exist_ok=True)
     in_files += [
-        os.path.join("dev_tools/requirements/deps", f)
-        for f in os.listdir("dev_tools/requirements/deps")
+        (os.path.join(input_dir, f), os.path.join(output_dir, f))
+        for f in os.listdir(input_dir)
         if f.endswith('.in')
     ]
 
