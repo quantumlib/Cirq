eec0b6fdb36fa5b1ebcc41e2ab31cc023327fc2d
Adam Zalcman <viathor@google.com>
2021-05-26 15:51:53 -0700

    Tests for kraus_to_choi

--- cirq-core/cirq/qis/channels_test.py
+++ cirq-core/cirq/qis/channels_test.py
@@ -42,6 +42,33 @@ def expected_choi(channel: cirq.SupportsChannel) -> np.ndarray:
 
 
 @pytest.mark.parametrize(
+    'kraus_operators, expected_choi',
+    (
+        ([np.eye(2)], np.array([[1, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1]])),
+        (
+            [
+                np.eye(2) / 2,
+                np.array([[0, 1], [1, 0]]) / 2,
+                np.array([[0, -1j], [1j, 0]]) / 2,
+                np.diag([1, -1]) / 2,
+            ],
+            np.eye(4) / 2,
+        ),
+        (
+            [
+                np.array([[1, 0, 0], [0, 0, 1]]) / np.sqrt(2),
+                np.array([[1, 0, 0], [0, 0, -1]]) / np.sqrt(2),
+            ],
+            np.diag([1, 0, 0, 0, 0, 1]),
+        ),
+    ),
+)
+def test_kraus_to_choi(kraus_operators, expected_choi):
+    """Verifies that cirq.kraus_to_choi computes the correct Choi matrix."""
+    assert np.allclose(cirq.kraus_to_choi(kraus_operators), expected_choi)
+
+
+@pytest.mark.parametrize(
     'channel',
     (
         cirq.I,
