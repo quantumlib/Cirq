23b54a936a36fe1f06b2e036e8e72618a8d9eccc
Bálint Pató <balintp@google.com>
2021-05-25 17:23:22 -0700

    fix ci

--- cirq-core/setup.py
+++ cirq-core/setup.py
@@ -44,7 +44,7 @@ if 'CIRQ_PRE_RELEASE_VERSION' in os.environ:
     )
 
 # Read in requirements
-requirements = open('requirements.in').readlines()
+requirements = open('requirements.txt').readlines()
 requirements = [r.strip() for r in requirements]
 contrib_requirements = open('cirq/contrib/contrib-requirements.txt').readlines()
 contrib_requirements = [r.strip() for r in contrib_requirements]
