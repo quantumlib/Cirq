a9a9ded9ac6325b89207d5aa3a590a40b43163b3
Adam Zalcman <viathor@google.com>
2021-05-27 15:51:23 -0700

    Channel matrix

--- cirq-core/cirq/qis/channels_test.py
+++ cirq-core/cirq/qis/channels_test.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tests for channels."""
+from typing import Iterable
+
 import numpy as np
 import pytest
 
@@ -28,17 +30,31 @@ def apply_channel(channel: cirq.SupportsChannel, rho: np.ndarray) -> np.ndarray:
     return out
 
 
+def standard_operator_basis(d_out: int, d_in: int) -> Iterable[np.ndarray]:
+    for i in range(d_out):
+        for j in range(d_in):
+            e_ij = np.zeros((d_out, d_in))
+            e_ij[i, j] = 1
+            yield e_ij
+
+
 def expected_choi(channel: cirq.SupportsChannel) -> np.ndarray:
     ks = cirq.channel(channel)
     d_out, d_in = ks[0].shape
     d = d_in * d_out
     c = np.zeros((d, d), dtype=np.complex128)
-    for i in range(d_in):
-        for j in range(d_in):
-            e_ij = np.zeros((d_in, d_in))
-            e_ij[i, j] = 1
-            c += np.kron(apply_channel(channel, e_ij), e_ij)
+    for e in standard_operator_basis(d_in, d_in):
+        c += np.kron(apply_channel(channel, e), e)
     return c
+
+
+def expected_channel_matrix(channel: cirq.SupportsChannel) -> np.ndarray:
+    ks = cirq.channel(channel)
+    d_out, d_in = ks[0].shape
+    m = np.zeros((d_out * d_out, d_in * d_in), dtype=np.complex128)
+    for k, e_in in enumerate(standard_operator_basis(d_in, d_in)):
+        m[:, k] = np.reshape(apply_channel(channel, e_in), d_out * d_out)
+    return m
 
 
 @pytest.mark.parametrize(
@@ -80,7 +96,7 @@ def test_kraus_to_choi(kraus_operators, expected_choi):
     ),
 )
 def test_operation_to_choi(channel):
-    """Verifies that cirq.choi correctly computes the Choi matrix."""
+    """Verifies that cirq.operation_to_choi correctly computes the Choi matrix."""
     n_qubits = cirq.num_qubits(channel)
     actual = cirq.operation_to_choi(channel)
     expected = expected_choi(channel)
@@ -88,6 +104,63 @@ def test_operation_to_choi(channel):
     assert np.all(actual == expected)
 
 
-def test_choi_on_completely_dephasing_channel():
-    """Checks that cirq.choi returns the right matrix for the completely dephasing channel."""
+def test_choi_for_completely_dephasing_channel():
+    """Checks cirq.operation_to_choi on the completely dephasing channel."""
     assert np.all(cirq.operation_to_choi(cirq.phase_damp(1)) == np.diag([1, 0, 0, 1]))
+
+
+@pytest.mark.parametrize(
+    'kraus_operators, expected_channel_matrix',
+    (
+        ([np.eye(2)], np.eye(4)),
+        (
+            [
+                np.eye(2) / 2,
+                np.array([[0, 1], [1, 0]]) / 2,
+                np.array([[0, -1j], [1j, 0]]) / 2,
+                np.diag([1, -1]) / 2,
+            ],
+            np.array([[1, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1]]) / 2,
+        ),
+        (
+            [
+                np.array([[1, 0, 0], [0, 0, 1]]) / np.sqrt(2),
+                np.array([[1, 0, 0], [0, 0, -1]]) / np.sqrt(2),
+            ],
+            np.array(
+                [
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+                ]
+            ),
+        ),
+    ),
+)
+def test_kraus_to_channel_matrix(kraus_operators, expected_channel_matrix):
+    """Verifies that cirq.kraus_to_channel_matrix computes the correct channel matrix."""
+    assert np.allclose(cirq.kraus_to_channel_matrix(kraus_operators), expected_channel_matrix)
+
+
+@pytest.mark.parametrize(
+    'channel',
+    (
+        cirq.I,
+        cirq.X,
+        cirq.CNOT,
+        cirq.depolarize(0.1),
+        cirq.depolarize(0.1, n_qubits=2),
+        cirq.amplitude_damp(0.2),
+    ),
+)
+def test_operation_to_channel_matrix(channel):
+    """Verifies that cirq.channel_matrix correctly computes the channel matrix."""
+    actual = cirq.operation_to_channel_matrix(channel)
+    expected = expected_channel_matrix(channel)
+    assert np.all(actual == expected)
+
+
+def test_channel_matrix_for_completely_dephasing_channel():
+    """Checks cirq.operation_to_channel_matrix on the completely dephasing channel."""
+    assert np.all(cirq.operation_to_channel_matrix(cirq.phase_damp(1)) == np.diag([1, 0, 0, 1]))
