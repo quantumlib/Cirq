bbc844e5b7e9bc685ac175d9190c10e87ccbaa6d
Seun Omonije <oseun22@gmail.com>
2021-06-08 14:44:50 -0700

    Update cirq-web/cirq_ts/src/bloch_sphere/components/Axes.class.d.ts
    Co-authored-by: Balint Pato <balopat@users.noreply.github.com>

--- cirq-web/cirq_ts/src/bloch_sphere/components/Axes.class.d.ts
+++ cirq-web/cirq_ts/src/bloch_sphere/components/Axes.class.d.ts
@@ -1,7 +1,7 @@
 import { LineBasicMaterial, BufferGeometry, Line } from 'three';
 export declare class Axes {
     /**
-     * Creates the x, y, and z axis for the bloch_sphere.
+     * Creates the x, y, and z axis for the Bloch sphere.
      * @param radius The overall radius of the bloch sphere.
      * @returns An object mapping the name of the axis to its corresponding
      * Line object to be rendered by the three.js scene.
