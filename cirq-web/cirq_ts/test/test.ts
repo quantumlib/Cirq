802f9d2ccf1fe1778c4946e2737155f9f8c1a3c2
Seun Omonije <seunomonije@icloud.com>
2021-06-08 11:58:02 -0700

    testing framework setup with simple test run

--- cirq-web/cirq_ts/test/test.ts
+++ cirq-web/cirq_ts/test/test.ts
@@ -1,9 +1,9 @@
 import {Vector3} from 'three';
-const assert = require('assert');
+import {expect} from 'chai';
 
-describe('The THREE object', function() {
+describe('The THREE object', () => {
     it('should be able to construct a Vector3 with default of x=0', function() {
       const vec3 = new Vector3();
-      assert.equal(0, vec3.x);
+      expect(vec3.x).to.equal(0);
     })
 })
