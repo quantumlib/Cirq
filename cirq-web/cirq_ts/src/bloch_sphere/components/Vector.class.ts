d489643a951bf4b2399c8c5a9b031bf79533fac3
Seun Omonije <oseun22@gmail.com>
2021-06-08 14:51:40 -0700

    Update cirq-web/cirq_ts/src/bloch_sphere/components/Vector.class.ts
    Co-authored-by: Balint Pato <balopat@users.noreply.github.com>

--- cirq-web/cirq_ts/src/bloch_sphere/components/Vector.class.ts
+++ cirq-web/cirq_ts/src/bloch_sphere/components/Vector.class.ts
@@ -13,7 +13,7 @@ import {
 export class Vector {
   /**
    * Adds a state vector to the bloch sphere.
-   * @param vectorData information representing the location of the vector
+   * @param vectorData information representing the location of the vector.
    * @returns an ArrowHelper object to be rendered by the scene.
    */
   public static createVector(vectorData?: string): ArrowHelper {
