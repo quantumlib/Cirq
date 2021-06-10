a076caa7df39eb916de413481a342c48123af0f6
ybc <bichengying@gmail.com>
2021-06-09 22:25:04 -0700

    fix the lint

--- cirq-core/cirq/optimizers/clifford_decomposition_test.py
+++ cirq-core/cirq/optimizers/clifford_decomposition_test.py
@@ -23,7 +23,7 @@ def test_misaligned_qubits():
     qubits = cirq.LineQubit.range(1)
     tableau = cirq.CliffordTableau(num_qubits=2)
     with pytest.raises(ValueError):
-        ops = cirq.decompose_clifford_tableau_to_operations(qubits, tableau)
+        cirq.decompose_clifford_tableau_to_operations(qubits, tableau)
 
 
 def test_clifford_decompose_one_qubit():
