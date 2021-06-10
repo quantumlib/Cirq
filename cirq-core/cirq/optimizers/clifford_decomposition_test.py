4ede0af15b3e336ba17b3a093b2012d2385a5062
ybc <bichengying@gmail.com>
2021-06-09 22:20:28 -0700

    Add misaligned_qubits test

--- cirq-core/cirq/optimizers/clifford_decomposition_test.py
+++ cirq-core/cirq/optimizers/clifford_decomposition_test.py
@@ -12,10 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pytest
 import numpy as np
 
 import cirq
 from cirq.testing import assert_allclose_up_to_global_phase
+
+
+def test_misaligned_qubits():
+    qubits = cirq.LineQubit.range(1)
+    tableau = cirq.CliffordTableau(num_qubits=2)
+    with pytest.raises(ValueError):
+        ops = cirq.decompose_clifford_tableau_to_operations(qubits, tableau)
 
 
 def test_clifford_decompose_one_qubit():
