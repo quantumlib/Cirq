6c63fb6a30fc6a0b3f9604558a1b8ba55c12cc9a
Shivanth <fordearlinux@gmail.com>
2021-06-24 00:01:17 +0800

    register marker

--- dev_tools/conftest.py
+++ dev_tools/conftest.py
@@ -17,6 +17,7 @@ import pytest
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark tests as slow")
+    config.addinivalue_line("markers", "docker: mark tests for docker images")
 
 
 def pytest_collection_modifyitems(config, items):
