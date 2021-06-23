faf3254008e0d8ba3e0fd5d27183f87121af5bfe
Shivanth <fordearlinux@gmail.com>
2021-06-24 01:38:37 +0800

    remove tags

--- dev_tools/conftest.py
+++ dev_tools/conftest.py
@@ -17,7 +17,6 @@ import pytest
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark tests as slow")
-    config.addinivalue_line("markers", "docker: mark tests for docker images")
 
 
 def pytest_collection_modifyitems(config, items):
@@ -27,9 +26,6 @@ def pytest_collection_modifyitems(config, items):
         return  # let pytest handle this
 
     skip_slow_marker = pytest.mark.skip(reason='slow marker not selected')
-    skip_docker_marker = pytest.mark.skip(reason='skipping docker tests')
     for item in items:
         if 'slow' in item.keywords:
             item.add_marker(skip_slow_marker)
-        if 'docker' in item.keywords:
-            item.add_marker(skip_docker_marker)
