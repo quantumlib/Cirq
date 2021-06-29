dd2c3772e8c32e93011cf1c396fbe5f84e7189b0
Bálint Pató <balintp@google.com>
2021-06-29 13:46:45 -0400

    simplify ion device

--- cirq-core/cirq/ion/ion_device_test.py
+++ cirq-core/cirq/ion/ion_device_test.py
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 from datetime import timedelta
-import pytest
 
 import numpy as np
+import pytest
 
 import cirq
 import cirq.ion as ci
@@ -162,7 +162,6 @@ def test_can_add_operation_into_moment():
         assert d.can_add_operation_into_moment(cirq.Z(q3), moment)
     circuit = cirq.Circuit([cirq.X(q0)])
     assert d.can_add_operation_into_moment(cirq.XX(q1, q2), circuit[0])
-
 
 
 def test_ion_device_eq():
