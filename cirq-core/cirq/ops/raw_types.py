5ca4ab6e621f5e10190fdaac9e843661b65c367c
Dax Fohl <dax.fohl@gmail.com>
2021-07-09 16:50:43 -0700

    Move on_each to Gate, and deprecate SupportsOnEachGate (#4303)
    Part of #4034. (See https://github.com/quantumlib/Cirq/issues/4034#issuecomment-832913708), moves the on_each functionality to cirq.Gate, and deprecates SupportsOnEachGate.
    
    Note the tests that are deleted here had already been duplicated in raw_types_test.py by https://github.com/quantumlib/Cirq/pull/4281/files.

--- cirq-core/cirq/ops/raw_types.py
+++ cirq-core/cirq/ops/raw_types.py
@@ -23,6 +23,8 @@ from typing import (
     Collection,
     Dict,
     Hashable,
+    Iterable,
+    List,
     Optional,
     Sequence,
     Tuple,
@@ -210,6 +212,52 @@ class Gate(metaclass=value.ABCMetaImplementAnyOneOf):
         from cirq.ops import gate_operation
 
         return gate_operation.GateOperation(self, list(qubits))
+
+    def on_each(self, *targets: Union[Qid, Iterable[Any]]) -> List['cirq.Operation']:
+        """Returns a list of operations applying the gate to all targets.
+        Args:
+            *targets: The qubits to apply this gate to. For single-qubit gates
+            this can be provided as varargs or a combination of nested
+            iterables. For multi-qubit gates this must be provided as an
+            `Iterable[Sequence[Qid]]`, where each sequence has `num_qubits`
+            qubits.
+        Returns:
+            Operations applying this gate to the target qubits.
+        Raises:
+            ValueError if targets are not instances of Qid or Iterable[Qid].
+            ValueError if the gate qubit number is incompatible.
+        """
+        operations: List['cirq.Operation'] = []
+        if self._num_qubits_() > 1:
+            iterator: Iterable = targets
+            if len(targets) == 1:
+                if not isinstance(targets[0], Iterable):
+                    raise TypeError(f'{targets[0]} object is not iterable.')
+                t0 = list(targets[0])
+                iterator = [t0] if t0 and isinstance(t0[0], Qid) else t0
+            for target in iterator:
+                if not isinstance(target, Sequence):
+                    raise ValueError(
+                        f'Inputs to multi-qubit gates must be Sequence[Qid].'
+                        f' Type: {type(target)}'
+                    )
+                if not all(isinstance(x, Qid) for x in target):
+                    raise ValueError(f'All values in sequence should be Qids, but got {target}')
+                if len(target) != self._num_qubits_():
+                    raise ValueError(f'Expected {self._num_qubits_()} qubits, got {target}')
+                operations.append(self.on(*target))
+            return operations
+
+        for target in targets:
+            if isinstance(target, Qid):
+                operations.append(self.on(target))
+            elif isinstance(target, Iterable) and not isinstance(target, str):
+                operations.extend(self.on_each(*target))
+            else:
+                raise ValueError(
+                    f'Gate was called with type different than Qid. Type: {type(target)}'
+                )
+        return operations
 
     def wrap_in_linear_combination(
         self, coefficient: Union[complex, float, int] = 1
