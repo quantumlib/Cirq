d8f698abc7dbc43009664c283b675b58c7f9489a
Seun Omonije <seunomonije@icloud.com>
2021-07-15 09:28:46 -0700

    progress dump

--- cirq-web/cirq_ts/src/circuit/main.ts
+++ cirq-web/cirq_ts/src/circuit/main.ts
@@ -1,9 +1,12 @@
-import {Scene, PerspectiveCamera, WebGLRenderer, AxesHelper, Vector3} from 'three';
+import {Scene, PerspectiveCamera, WebGLRenderer, Raycaster, Vector2, AxesHelper, Vector3} from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import {Circuit} from './circuit';
 
 
-function createAndRenderScene(numQubits: number, sceneId: string): Scene {
+const mouse = new Vector2();
+const raycaster = new Raycaster();
+
+function createAndRenderScene(numQubits: number, sceneId: string): any {
     const WIDTH = 1000;
     const HEIGHT = 700;
     const NUM_QUBITS = numQubits;
@@ -31,7 +34,7 @@ function createAndRenderScene(numQubits: number, sceneId: string): Scene {
     controls.maxDistance = 200;
     controls.maxPolarAngle = Math.PI;
     function animate() {
-        requestAnimationFrame( animate );
+        requestAnimationFrame(animate);
         controls.update();
         renderer.render( scene, camera );
     }
@@ -40,12 +43,36 @@ function createAndRenderScene(numQubits: number, sceneId: string): Scene {
     return scene;
 }
 
+function onMouseMove( event: any ) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY/window.innerHeight) * 2 + 1;
+}
+
+// // Raycasting
+// function addRaycasting() {
+//     raycaster.setFromCamera(mouse, camera);
+//     const intersects = raycaster.intersectObjects(scene.children);
+//     for (let i = 0; i < intersects.length; i++){
+//         console.log(intersects[i]);
+//         //intersects[i].object
+//     }
+//     requestAnimationFrame(addRaycasting);
+//     renderer.render(scene, camera);
+// }
+
+
+// render();
+
+
 export function createGridCircuit(qubits: number[][], numMoments: number, sceneId: string): Circuit {
     const scene = createAndRenderScene(qubits.length, sceneId);
+    addEventListener('mousemove', onMouseMove, false);
+
     const circuit = new Circuit(numMoments);
     for (const qubit of qubits) {
         circuit.addQubit(qubit[0], qubit[1]);
     }
+    console.log(scene);
     scene.add(circuit);
 
     return circuit;
