ba95994ed19bf5412c7f00e8b8f4c02c62a9cee9
Tony Bruguier <tony.bruguier@gmail.com>
2021-07-15 04:13:34 +0000

    nit

--- cirq-core/cirq/ops/boolean_hamiltonian.py
+++ cirq-core/cirq/ops/boolean_hamiltonian.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Represents Boolean functions as a series of CNOT and rotation gates. The Boolean functions are
+"""Represents Boolean functions as a series of CNOT and rotation gates. The Boolean functions are
 passed as Sympy expressions and then turned into an optimized set of gates.
 
 References:
