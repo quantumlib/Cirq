2320ab25283441dc2a1b6001d5f10e7ae7e63fb8
Doug Strain <dstrain@google.com>
2021-07-24 05:52:53 -0700

    Add test circuit operation with symbols.

--- cirq-google/cirq_google/circuit_serializer_test.py
+++ cirq-google/cirq_google/circuit_serializer_test.py
@@ -393,7 +393,7 @@ def test_deserialize_circuit_with_token_strings():
 def test_serialize_deserialize_circuit_with_subcircuit():
     serializer = cg.CircuitSerializer('my_gate_set')
     tag1 = cg.CalibrationTag('abc123')
-    fcircuit = cirq.FrozenCircuit(cirq.X(Q0))
+    fcircuit = cirq.FrozenCircuit(cirq.XPowGate(exponent=2 * sympy.Symbol('t'))(Q0))
     circuit = cirq.Circuit(
         cirq.X(Q1).with_tags(tag1),
         cirq.CircuitOperation(fcircuit).repeat(repetition_ids=['a', 'b']),
@@ -401,13 +401,18 @@ def test_serialize_deserialize_circuit_with_subcircuit():
         cirq.X(Q0),
     )
 
-    op1 = v2.program_pb2.Operation()
-    op1.xpowgate.exponent.float_value = 1.0
-    op1.qubit_constant_index.append(0)
-    op1.token_constant_index = 1
-    op2 = v2.program_pb2.Operation()
-    op2.xpowgate.exponent.float_value = 1.0
-    op2.qubit_constant_index.append(2)
+    op_x = v2.program_pb2.Operation()
+    op_x.xpowgate.exponent.float_value = 1.0
+    op_x.qubit_constant_index.append(2)
+    op_tag = v2.program_pb2.Operation()
+    op_tag.xpowgate.exponent.float_value = 1.0
+    op_tag.qubit_constant_index.append(0)
+    op_tag.token_constant_index = 1
+    op_symbol = v2.program_pb2.Operation()
+    op_symbol.xpowgate.exponent.func.type = 'mul'
+    op_symbol.xpowgate.exponent.func.args.add().arg_value.float_value = 2.0
+    op_symbol.xpowgate.exponent.func.args.add().symbol = 't'
+    op_symbol.qubit_constant_index.append(2)
 
     c_op1 = v2.program_pb2.CircuitOperation()
     c_op1.circuit_constant_index = 3
@@ -428,11 +433,11 @@ def test_serialize_deserialize_circuit_with_subcircuit():
             scheduling_strategy=v2.program_pb2.Circuit.MOMENT_BY_MOMENT,
             moments=[
                 v2.program_pb2.Moment(
-                    operations=[op1],
+                    operations=[op_tag],
                     circuit_operations=[c_op1],
                 ),
                 v2.program_pb2.Moment(
-                    operations=[op2],
+                    operations=[op_x],
                     circuit_operations=[c_op2],
                 ),
             ],
@@ -446,7 +451,7 @@ def test_serialize_deserialize_circuit_with_subcircuit():
                     scheduling_strategy=v2.program_pb2.Circuit.MOMENT_BY_MOMENT,
                     moments=[
                         v2.program_pb2.Moment(
-                            operations=[op2],
+                            operations=[op_symbol],
                         )
                     ],
                 )
