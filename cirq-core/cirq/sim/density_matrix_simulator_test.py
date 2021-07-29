ee1c946ffa74afe89e8e48f1fd5cc8789be62774
Dax Fohl <dax.fohl@gmail.com>
2021-07-28 17:02:16 -0700

    Update density_matrix_simulator_test.py

--- cirq-core/cirq/sim/density_matrix_simulator_test.py
+++ cirq-core/cirq/sim/density_matrix_simulator_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import itertools
-import platform
 import random
 from typing import Type
 from unittest import mock
