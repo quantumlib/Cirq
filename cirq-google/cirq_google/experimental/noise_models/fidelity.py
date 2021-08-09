759f18e9020a5bf302f93054738d97c170c318aa
Ana Sofia Uzsoy <asuzsoy@google.com>
2021-08-09 17:46:58 +0000

    consolidated p00/p11 to one channel, gets sycamore durations

--- cirq-google/cirq_google/experimental/noise_models/fidelity.py
+++ cirq-google/cirq_google/experimental/noise_models/fidelity.py
@@ -18,8 +18,12 @@ class Fidelity():
     """
     Detailed comments about arguments here
     """
-    if not any([t1, decay_constant, xeb_fidelity, pauli_error, p00, p00]):
+    if not any([t1, decay_constant, xeb_fidelity, pauli_error, p00, p11]):
       raise ValueError('At least one metric must be specified')
+
+    for metric in [xeb_fidelity, pauli_error, p00, p11]:
+      if metric is not None and not 0.0 <= metric <= 1.0:
+        raise ValueError('Invalid metric value')
 
     self._t1 = t1
     self._p = decay_constant
@@ -72,7 +76,6 @@ class Fidelity():
       return self._pauli_error
 
 
-_sq_inds = np.arange(4)
 
 class NoiseModelFromFidelity(cirq.NoiseModel):
   def __init__(
@@ -84,41 +87,51 @@ class NoiseModelFromFidelity(cirq.NoiseModel):
     else:
       raise ValueError('A fidelity object must be specified')
 
+  def get_duration(self, gate):
+    if isinstance(gate, cirq.FSimGate):
+      theta,_ = gate._value_equality_values_()
+      if np.abs(theta) == np.pi / 2: ## should be any mutiple of pi/2
+        return 12.0 ## syc
+      return 32.0 ## fsim_pi_4
+    elif isinstance(gate, cirq.ISwapPowGate):
+      return 32.0 #inv_fsim_pi_4
+    elif isinstance(gate, cirq.ZPowGate):
+      return 0.0
+    elif isinstance(gate, cirq.MeasurementGate):
+      return 4000.0
+    return 25.0
+
   def noisy_moment(self, moment: cirq.Moment, system_qubits: Sequence[cirq.Qid]) -> cirq.OP_TREE:
     moments = []
 
-    ## TODO: write better code to get Sycamore duration for each gatee
-    duration = 25.0 # ns
-    if np.any([isinstance(op.gate, cirq.MeasurementGate) for op in moment.operations]):
-      duration = 4000.0
-
     if validate_all_measurements(moment): ## add after measurement gate
-      if self._fidelity._p00 is not None: # apply p00 noise
-        gamma = self._fidelity._p00
-        p = 0.0
-        channel = cirq.GeneralizedAmplitudeDampingChannel(p = p, gamma = gamma)
-        moments.append(cirq.Moment(channel(q) for q in system_qubits))
+      p00 = self._fidelity._p00
+      p11 = self._fidelity._p11
+      if p00 is not None and p11 is not None:
+        p = p11 / (p00 + p11)
+        gamma = p11 / p
+#        print(p, gamma)
+        moments.append(cirq.Moment(cirq.GeneralizedAmplitudeDampingChannel(p = p, gamma = gamma)(q) for q in system_qubits))
+      elif p00 is not None:
+        moments.append(cirq.Moment(cirq.GeneralizedAmplitudeDampingChannel(p = 0.0, gamma = p00)(q) for q in system_qubits))
+      elif p11 is not None:
+        moments.append(cirq.Moment(cirq.GeneralizedAmplitudeDampingChannel(p = 1.0, gamma = p11)(q) for q in system_qubits))
 
-      if self._fidelity._p11 is not None: # apply p11 noise
-        gamma = self._fidelity._p11
-        p = 1.0
-        channel = cirq.GeneralizedAmplitudeDampingChannel(p = p, gamma = gamma)
-        moments.append(cirq.Moment(channel(q) for q in system_qubits))
-      moments.append(moment)
-
-    elif self._fidelity._pauli_error is not None: ## pauli error
-      moments.append(moment)
-
+    moments.append(moment)
+    if self._fidelity._pauli_error is not None: ## pauli error
+      duration = max([self.get_duration(op.gate) for op in moment.operations])
       pauli_error = self._fidelity.pauli_error_from_depolarization(duration)
+
+      _sq_inds = np.arange(4)
       pauli_inds = np.array(list(product(_sq_inds, repeat = 1)))
       num_inds = len(pauli_inds)
       pI = 1 - pauli_error # probability of identity matrix
       p_other = pauli_error / (num_inds - 1) # probability of other pauli gates
       pauli_probs = (np.array([p_other] * (num_inds - 1)))
-      moments.append(cirq.Moment(cirq.AsymmetricDepolarizingChannel(*pauli_probs)(q) for q in system_qubits))
+      moments.append(cirq.Moment(cirq.depolarize(p_other)(q) for q in system_qubits))
 
-    elif self._fidelity._t1 is not None: # t1 decay noise
-      moments.append(moment)
+    if self._fidelity._t1 is not None: # t1 decay noise
+      duration = max([self.get_duration(op.gate) for op in moment.operations])
       moments.append(cirq.Moment(cirq.amplitude_damp(1 - np.exp(-duration / self._fidelity._t1)).on_each(system_qubits)))
     return moments
 
