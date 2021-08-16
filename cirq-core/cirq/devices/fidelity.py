ddc3b817e16dcd85ecd1806f38842c65e3632cd1
Ana Sofia Uzsoy <asuzsoy@google.com>
2021-08-16 20:46:24 +0000

    committing before changing over to noise_properties from fidelity

--- cirq-core/cirq/devices/fidelity.py
+++ cirq-core/cirq/devices/fidelity.py
@@ -1,37 +1,37 @@
-from typing import Sequence
+from typing import Sequence, TYPE_CHECKING
 from itertools import product
-import cirq
+from cirq import ops, protocols, devices
 import numpy as np
-from cirq.devices.noise_model import validate_all_measurements
+
+if TYPE_CHECKING:
+  from typing import Iterable
+  import cirq
 
 
-class Fidelity:
+class NoiseProperties:
     def __init__(
         self,
         *,
-        t1: float = None,
+        t1_ns: float = None,
         decay_constant: float = None,
         xeb_fidelity: float = None,
         pauli_error: float = None,
         p00: float = None,
         p11: float = None,
     ) -> None:
-        """Creates a Fidelity object using the provided metrics
+        """Creates a Fidelity object using the provided metrics.
+
+          Only one of decay_constant, xeb_fidelity, and pauli_error should be specified.
 
         Args:
-          t1: t1 decay constant in ns
+          t1_ns: t1 decay constant in ns
           decay_constant: depolarization decay constant
           xeb_fidelity: 2-qubit XEB Fidelity
           pauli_error: total Pauli error
           p00: probability of qubit initialized as zero being measured as zero
           p11: probability of qubit initialized as one being measured as one
-
-          Only one of decay_constant, xeb_fidelity, and pauli_error should be specified
-
-        Returns:
-          a Fidelity object with the provided metrics that can be used to create a noise model
         """
-        if not any([t1, decay_constant, xeb_fidelity, pauli_error, p00, p11]):
+        if not any([t1_ns, decay_constant, xeb_fidelity, pauli_error, p00, p11]):
             raise ValueError('At least one metric must be specified')
 
         for metric in [xeb_fidelity, pauli_error, p00, p11]:
@@ -48,22 +48,15 @@ class Fidelity:
                 'Only one of xeb fidelity, pauli error, or decay constant should be defined'
             )
 
-        self._t1 = t1
+        self._t1_ns = t1_ns
         self._p = decay_constant
         self._p00 = p00
         self._p11 = p11
-        self._pauli_error = pauli_error
-        self._xeb = xeb_fidelity
 
-        if self._pauli_error is not None:
-            self._p = self.pauli_error_to_decay_constant()
-            self._xeb = self.pauli_error_to_xeb_fidelity()
-        elif self._p is not None:
-            self._pauli_error = self.decay_constant_to_pauli_error()
-            self._xeb = self.decay_constant_to_xeb_fidelity()
-        elif self._xeb is not None:
-            self._p = self.xeb_fidelity_to_decay_constant()
-            self._pauli_error = self.decay_constant_to_pauli_error()
+        if pauli_error is not None:
+            self._p = self.pauli_error_to_decay_constant(pauli_error)
+        elif xeb_fidelity is not None:
+            self._p = self.xeb_fidelity_to_decay_constant(xeb_fidelity)
 
     @property
     def decay_constant(self):
@@ -79,138 +72,181 @@ class Fidelity:
 
     @property
     def pauli_error(self):
-        return self._pauli_error
+        return self.decay_constant_to_pauli_error()
 
     @property
-    def t1(self):
-        return self._t1
+    def t1_ns(self):
+        return self._t1_ns
 
     @property
     def xeb(self):
-        return self._xeb
+        return self.decay_constant_to_xeb_fidelity()
 
-    def decay_constant_to_xeb_fidelity(self, N: int = 4):
+    def decay_constant_to_xeb_fidelity(self, num_qubits: int = 2):
+        """Calculates the XEB noise_properties from the depolarization decay constant.
+
+        Args:
+            num_qubits: number of qubits
+        """
         if self._p is not None:
+            N = 2**num_qubits
             return 1 - ((1 - self._p) * (1 - 1 / N))
         return None
 
-    def decay_constant_to_pauli_error(self, N: int = 2):
+    def decay_constant_to_pauli_error(self, num_qubits: int = 1):
+        """Calculates pauli error from the depolarization decay constant.
+        Args:
+            num_qubits: number of qubits
+        """
         if self._p is not None:
+            N = 2**num_qubits
             return (1 - self._p) * (1 - 1 / N / N)
         return None
 
-    def pauli_error_to_xeb_fidelity(self, N: int = 4):
-        if self._pauli_error is not None:
-            self._p = 1 - (self._pauli_error / (1 - 1 / N))
-            return self.decay_constant_to_xeb_fidelity()
-        return None
+    def pauli_error_to_decay_constant(self, pauli_error: float, num_qubits: int = 1):
+        """Calculates depolarization decay constant from pauli error.
 
-    def pauli_error_to_decay_constant(self, N: int = 2):
-        if self._pauli_error is not None:
-            return 1 - (self._pauli_error / (1 - 1 / N / N))
-        return None
+        Args:
+            pauli_error: The pauli error
+            num_qubits: Number of qubits
+        """
+        N = 2**num_qubits
+        return 1 - (pauli_error / (1 - 1 / N / N))
 
-    def xeb_fidelity_to_decay_constant(self, N: int = 4):
-        if self._xeb is not None:
-            return 1 - (1 - self._xeb) / (1 - 1 / N)
-        return None
+    def xeb_fidelity_to_decay_constant(self, xeb_fidelity: float, num_qubits: int = 2):
+        """Calculates the depolarization decay constant from the XEB noise_properties.
 
-    def pauli_error_from_t1(self, t: float, t1: float):
-        t2 = 2 * t1
-        return (1 - np.exp(-t / t2)) / 2 + (1 - np.exp(-t / t1)) / 4
+        Args:
+            xeb_fidelity: The XEB noise_properties
+            num_qubits: Number of qubits
+        """
+        N = 2**num_qubits
+        return 1 - (1 - xeb_fidelity) / (1 - 1 / N)
+
+    def pauli_error_from_t1(self, t: float, t1_ns: float):
+        """Calculates the pauli error from amplitude damping. Unlike the other methods, this computes a specific case (over time t).
+
+        Args:
+            t: the duration of the gate
+            t1_ns: the t1 decay constant in ns
+        """
+        t2 = 2 * t1_ns
+        return (1 - np.exp(-t / t2)) / 2 + (1 - np.exp(-t / t1_ns)) / 4
 
     def pauli_error_from_depolarization(self, t: float):
-        if self._t1 is not None:
-            return self._pauli_error - self.pauli_error_from_t1(t, self._t1)
-        else:
-            return self._pauli_error
+        """Calculates the amount of pauli error from depolarization. Unlike the other methods, this computes a specific case (over time t).
 
-    def rb_pauli_error(self, N: int = 2):
+        If the pauli error from amplitude damping is more than the total pauli error, just return the pauli error.
+
+        Args:
+            t: the duration of the gate
+        """
+        if self.t1_ns is not None:
+            pauli_error_from_t1 = self.pauli_error_from_t1(t, self._t1_ns)
+            if self.pauli_error >= pauli_error_from_t1:
+                return self.pauli_error - pauli_error_from_t1
+        return self.pauli_error
+
+    def rb_pauli_error(self, num_qubits: int = 1):
+        """Calculates the randomized benchmarking pauli error.
+
+        Args:
+            num_qubits: Number of qubits
+        """
         if self._p is not None:
+            N = 2**num_qubits
             return (1 - self._p) * (1 - 1 / N ** 2)
         return None
 
-    def rb_average_error(self, N: int = 2):
+    def rb_average_error(self, num_qubits: int = 1):
+        """Calculates the randomized benchmarking average error.
+
+        Args:
+            num_qubits: Number of qubits
+        """
         if self._p is not None:
+            N = 2**num_qubits
             return (1 - self._p) * (1 - 1 / N)
         return None
 
 
-class NoiseModelFromFidelity(cirq.NoiseModel):
-    def __init__(self, fidelity: Fidelity) -> None:
-        if fidelity is not None:
-            self._fidelity = fidelity
-        else:
-            raise ValueError('A fidelity object must be specified')
+def get_duration_ns(gate):
+    # Gate durations based on sycamore durations.
+    # TODO: pull the gate durations from cirq_google
+    # or allow users to pass them in
+    if isinstance(gate, ops.FSimGate):
+        theta, _ = gate._value_equality_values_()
+        if np.abs(theta) % (np.pi / 2) == 0:
+            return 12.0
+        return 32.0
+    elif isinstance(gate, ops.ISwapPowGate):
+        return 32.0
+    elif isinstance(gate, ops.ZPowGate):
+        return 0.0
+    elif isinstance(gate, ops.MeasurementGate):
+        return 4000.0
+    elif isinstance(gate, ops.WaitGate):
+        return gate.duration.total_nanos()
+    return 25.0
 
-    def get_duration(self, gate):
-        # Gate durations based on sycamore durations
-        if isinstance(gate, cirq.FSimGate):
-            theta, _ = gate._value_equality_values_()
-            if np.abs(theta) % (np.pi / 2) == 0:
-                return 12.0
-            return 32.0
-        elif isinstance(gate, cirq.ISwapPowGate):
-            return 32.0
-        elif isinstance(gate, cirq.ZPowGate):
-            return 0.0
-        elif isinstance(gate, cirq.MeasurementGate):
-            return 4000.0
-        return 25.0
 
-    def apply_readout_noise(self, p00, p11, moments, system_qubits):
-        if p00 is not None and p11 is not None:
-            p = p11 / (p00 + p11)
-            gamma = p11 / p
-            moments.append(
-                cirq.Moment(
-                    cirq.GeneralizedAmplitudeDampingChannel(p=p, gamma=gamma)(q)
-                    for q in system_qubits
-                )
-            )
-        elif p00 is not None:
-            moments.append(
-                cirq.Moment(
-                    cirq.GeneralizedAmplitudeDampingChannel(p=0.0, gamma=p00)(q)
-                    for q in system_qubits
-                )
-            )
-        elif p11 is not None:
-            moments.append(
-                cirq.Moment(
-                    cirq.GeneralizedAmplitudeDampingChannel(p=1.0, gamma=p11)(q)
-                    for q in system_qubits
-                )
-            )
-
-    def apply_depol_noise(self, pauli_error, moments, system_qubits):
-        _sq_inds = np.arange(4)
-        pauli_inds = np.array(list(product(_sq_inds, repeat=1)))
-        num_inds = len(pauli_inds)
-        p_other = pauli_error / (num_inds - 1)  # probability of X, Y, Z gates
-        moments.append(cirq.Moment(cirq.depolarize(p_other)(q) for q in system_qubits))
-
-    def apply_ampl_damp_noise(self, duration, t1, moments, system_qubits):
-        moments.append(
-            cirq.Moment(cirq.amplitude_damp(1 - np.exp(-duration / t1)).on_each(system_qubits))
+def _apply_readout_noise(p00, p11, moments, measurement_qubits):
+    if p00 is None:
+      p = 1.0
+      gamma = p11
+    elif p11 is None:
+      p = 0.0
+      gamma = p00
+    else:
+      p = p11 / (p00 + p11)
+      gamma = p11 / p
+    moments.append(
+        ops.Moment(
+            ops.GeneralizedAmplitudeDampingChannel(p=p, gamma=gamma)(q)
+            for q in measurement_qubits
         )
+    )
 
-    def noisy_moment(self, moment: cirq.Moment, system_qubits: Sequence[cirq.Qid]) -> cirq.OP_TREE:
+def _apply_depol_noise(pauli_error, moments, system_qubits):
+
+    _sq_inds = np.arange(4)
+    pauli_inds = np.array(list(product(_sq_inds, repeat=1)))
+    num_inds = len(pauli_inds)
+    p_other = pauli_error / (num_inds - 1)  # probability of X, Y, Z gates
+    moments.append(ops.Moment(ops.depolarize(p_other)(q) for q in system_qubits))
+
+def _apply_amplitude_damp_noise(duration, t1, moments, system_qubits):
+    moments.append(
+        ops.Moment(ops.amplitude_damp(1 - np.exp(-duration / t1)).on_each(system_qubits))
+    )
+
+
+
+class NoiseModelFromNoiseProperties(devices.NoiseModel):
+    def __init__(self, noise_properties: NoiseProperties) -> None:
+        if noise_properties is not None:
+            self._noise_properties = noise_properties
+        else:
+            raise ValueError('A NoiseProperties object must be specified')
+
+    def noisy_moment(self, moment: ops.Moment, system_qubits: Sequence['cirq.Qid']) -> 'cirq.OP_TREE':
         moments = []
 
-        if validate_all_measurements(moment):  ## add after measurement gate
-            p00 = self._fidelity._p00
-            p11 = self._fidelity._p11
-            self.apply_readout_noise(p00, p11, moments, system_qubits)
+        if any([protocols.is_measurement(op.gate) for op in moment.operations]):  # Add readout error before measurement gate
+            p00 = self._noise_properties.p00
+            p11 = self._noise_properties.p11
+            measurement_qubits = [list(op.qubits)[0] for op in moment.operations if protocols.is_measurement(op.gate)]
+            if p00 is not None or p11 is not None:
+                _apply_readout_noise(p00, p11, moments, measurement_qubits)
             moments.append(moment)
         else:
             moments.append(moment)
-            if self._fidelity._pauli_error is not None:  ## pauli error
-                duration = max([self.get_duration(op.gate) for op in moment.operations])
-                pauli_error = self._fidelity.pauli_error_from_depolarization(duration)
-                self.apply_depol_noise(pauli_error, moments, system_qubits)
+        if self._noise_properties.pauli_error is not None:  # Add depolarization error#
+            duration = max([get_duration_ns(op.gate) for op in moment.operations])
+            pauli_error = self._noise_properties.pauli_error_from_depolarization(duration)
+            _apply_depol_noise(pauli_error, moments, system_qubits)
 
-            if self._fidelity._t1 is not None:  # t1 decay noise
-                duration = max([self.get_duration(op.gate) for op in moment.operations])
-                self.apply_ampl_damp_noise(duration, self._fidelity._t1, moments, system_qubits)
+        if self._noise_properties.t1_ns is not None:  # Add amplitude damping noise
+            duration = max([get_duration_ns(op.gate) for op in moment.operations])
+            _apply_amplitude_damp_noise(duration, self._noise_properties.t1_ns, moments, system_qubits)
         return moments
--- cirq-core/cirq/devices/fidelity_test.py
+++ cirq-core/cirq/devices/fidelity_test.py
@@ -1,47 +1,49 @@
 import pytest
 import cirq
 from cirq.testing import assert_equivalent_op_tree
-from cirq.devices.fidelity import Fidelity, NoiseModelFromFidelity
+from cirq.devices.fidelity import NoiseProperties, NoiseModelFromNoiseProperties 
 import numpy as np
 
 
 def test_invalid_arguments():
     with pytest.raises(ValueError, match='At least one metric must be specified'):
-        Fidelity()
+        NoiseProperties()
 
     with pytest.raises(ValueError, match='xeb, pauli error, p00, and p11 must be between 0 and 1'):
-        Fidelity(p00=1.2)
+        NoiseProperties(p00=1.2)
 
     with pytest.raises(ValueError, match='xeb, pauli error, p00, and p11 must be between 0 and 1'):
-        Fidelity(pauli_error=-0.2)
+        NoiseProperties(pauli_error=-0.2)
 
     with pytest.raises(
         ValueError,
         match='Only one of xeb fidelity, pauli error, or decay constant should be defined',
     ):
-        Fidelity(pauli_error=0.2, xeb_fidelity=0.5)
+        NoiseProperties(pauli_error=0.2, xeb_fidelity=0.5)
 
-    with pytest.raises(ValueError, match='A fidelity object must be specified'):
-        NoiseModelFromFidelity(None)
+    with pytest.raises(ValueError, match='A NoiseProperties object must be specified'):
+        NoiseModelFromNoiseProperties(None)
 
 
 def test_constructor():
+    # These and other metrics in the file are purely for testing and
+    # do not necessarily represent actual hardware behavior
     xeb_fidelity = 0.95
     p00 = 0.1
-    t1 = 200.0
+    t1_ns = 200.0
 
     # Create fidelity object with a defined XEB fidelity
-    f_from_xeb = Fidelity(xeb_fidelity=xeb_fidelity, p00=p00, t1=t1)
+    f_from_xeb = NoiseProperties(xeb_fidelity=xeb_fidelity, p00=p00, t1_ns=t1_ns)
 
     assert f_from_xeb.p00 == p00
     assert f_from_xeb.p11 is None
-    assert f_from_xeb.t1 == t1
+    assert f_from_xeb.t1_ns == t1_ns
     assert f_from_xeb.xeb == xeb_fidelity
 
     # Create another fidelity object with the decay constant from the first one
     decay_constant_from_xeb = f_from_xeb.decay_constant
 
-    f_from_decay = Fidelity(decay_constant=decay_constant_from_xeb)
+    f_from_decay = NoiseProperties(decay_constant=decay_constant_from_xeb)
 
     # Check that their depolarization metrics match
     assert np.isclose(xeb_fidelity, f_from_decay.xeb)
@@ -66,9 +68,9 @@ def test_readout_error():
         cirq.Moment([cirq.measure(qubits[0], key='q0'), cirq.measure(qubits[1], key='q1')]),
     )
 
-    # Create noise model from Fidelity object with specified noise
-    f = Fidelity(p00=p00, p11=p11)
-    noise_model = NoiseModelFromFidelity(f)
+    # Create noise model from NoiseProperties object with specified noise
+    f = NoiseProperties(p00=p00, p11=p11)
+    noise_model = NoiseModelFromNoiseProperties(f)
 
     noisy_circuit = cirq.Circuit(noise_model.noisy_moments(circuit, qubits))
 
@@ -85,7 +87,9 @@ def test_readout_error():
 
 
 def test_depolarization_error():
-    pauli_error = 0.1
+    # Account for floating point errors
+    # Needs Cirq issue 3965 to be resolved
+    pauli_error = 0.09999999999999998
 
     # Create qubits and circuit
     qubits = [cirq.LineQubit(0), cirq.LineQubit(1)]
@@ -96,9 +100,9 @@ def test_depolarization_error():
         cirq.Moment([cirq.measure(qubits[0], key='q0'), cirq.measure(qubits[1], key='q1')]),
     )
 
-    # Create noise model from Fidelity object with specified noise
-    f = Fidelity(pauli_error=pauli_error)
-    noise_model = NoiseModelFromFidelity(f)
+    # Create noise model from NoiseProperties object with specified noise
+    f = NoiseProperties(pauli_error=pauli_error)
+    noise_model = NoiseModelFromNoiseProperties(f)
 
     noisy_circuit = cirq.Circuit(noise_model.noisy_moments(circuit, qubits))
 
@@ -111,13 +115,13 @@ def test_depolarization_error():
         cirq.Moment([cirq.H(qubits[1])]),
         cirq.Moment([cirq.depolarize(pauli_error / 3).on_each(qubits)]),
         cirq.Moment([cirq.measure(qubits[0], key='q0'), cirq.measure(qubits[1], key='q1')]),
-    )
-
+        cirq.Moment([cirq.depolarize(pauli_error / 3).on_each(qubits)]),
+   )
     assert_equivalent_op_tree(expected_circuit, noisy_circuit)
 
 
 def test_ampl_damping_error():
-    t1 = 200.0
+    t1_ns = 200.0
 
     # Create qubits and circuit
     qubits = [cirq.LineQubit(0), cirq.LineQubit(1)]
@@ -128,83 +132,108 @@ def test_ampl_damping_error():
         cirq.Moment([cirq.measure(qubits[0], key='q0'), cirq.measure(qubits[1], key='q1')]),
     )
 
-    # Create noise model from Fidelity object with specified noise
-    f = Fidelity(t1=t1)
-    noise_model = NoiseModelFromFidelity(f)
+    # Create noise model from NoiseProperties object with specified noise
+    f = NoiseProperties(t1_ns=t1_ns)
+    noise_model = NoiseModelFromNoiseProperties(f)
 
     noisy_circuit = cirq.Circuit(noise_model.noisy_moments(circuit, qubits))
 
     # Insert expected channels to circuit
     expected_circuit = cirq.Circuit(
         cirq.Moment([cirq.X(qubits[0])]),
-        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-25.0 / t1)).on_each(qubits)]),
+        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-25.0 / t1_ns)).on_each(qubits)]),
         cirq.Moment([cirq.CNOT(qubits[0], qubits[1])]),
-        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-25.0 / t1)).on_each(qubits)]),
+        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-25.0 / t1_ns)).on_each(qubits)]),
         cirq.Moment([cirq.FSimGate(np.pi / 2, np.pi).on_each(qubits)]),
-        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-12.0 / t1)).on_each(qubits)]),
+        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-12.0 / t1_ns)).on_each(qubits)]),
         cirq.Moment([cirq.measure(qubits[0], key='q0'), cirq.measure(qubits[1], key='q1')]),
+       cirq.Moment([cirq.amplitude_damp(1 - np.exp(-4000.0 / t1_ns)).on_each(qubits)])
     )
-
     assert_equivalent_op_tree(expected_circuit, noisy_circuit)
 
 
 def test_combined_error():
     # Helper function to calculate pauli error from depolarization
-    def pauli_error_from_depolarization(pauli_error, t1, duration):
-        t2 = 2 * t1
-        pauli_error_from_t1 = (1 - np.exp(-duration / t2)) / 2 + (1 - np.exp(-duration / t1)) / 4
-        return pauli_error - pauli_error_from_t1
+    def pauli_error_from_depolarization(pauli_error, t1_ns, duration):
+        t2 = 2 * t1_ns
+        pauli_error_from_t1 = (1 - np.exp(-duration / t2)) / 2 + (1 - np.exp(-duration / t1_ns)) / 4
+        if pauli_error >= pauli_error_from_t1:
+          return pauli_error - pauli_error_from_t1
+        return pauli_error
 
-    t1 = 2000.0
+    t1_ns = 2000.0
     p11 = 0.01
-    pauli_error = 0.02
-
+    pauli_error = 0.019999999999999962
     # Create qubits and circuit
     qubits = [cirq.LineQubit(0), cirq.LineQubit(1)]
     circuit = cirq.Circuit(
         cirq.Moment([cirq.X(qubits[0])]),
         cirq.Moment([cirq.CNOT(qubits[0], qubits[1])]),
+        cirq.Moment([cirq.measure(qubits[0], key='q0')]),
         cirq.Moment([cirq.ISwapPowGate().on_each(qubits)]),
         cirq.Moment([cirq.measure(qubits[0], key='q0'), cirq.measure(qubits[1], key='q1')]),
     )
 
-    # Create noise model from Fidelity object with specified noise
-    f = Fidelity(t1=t1, p11=p11, pauli_error=pauli_error)
-    noise_model = NoiseModelFromFidelity(f)
+
+    print("pauli error", pauli_error_from_depolarization(pauli_error, t1_ns, 4000.0))
+    # Create noise model from NoiseProperties object with specified noise
+    f = NoiseProperties(t1_ns=t1_ns, p11=p11, pauli_error=pauli_error)
+    noise_model = NoiseModelFromNoiseProperties(f)
 
     noisy_circuit = cirq.Circuit(noise_model.noisy_moments(circuit, qubits))
+    print("Actual: ")
+    print(noisy_circuit)
 
     # Insert expected channels to circuit
     expected_circuit = cirq.Circuit(
         cirq.Moment([cirq.X(qubits[0])]),
         cirq.Moment(
             [
-                cirq.depolarize(pauli_error_from_depolarization(pauli_error, t1, 25.0) / 3).on_each(
+                cirq.depolarize(pauli_error_from_depolarization(pauli_error, t1_ns, 25.0) / 3).on_each(
                     qubits
                 )
             ]
         ),
-        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-25.0 / t1)).on_each(qubits)]),
+        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-25.0 / t1_ns)).on_each(qubits)]),
         cirq.Moment([cirq.CNOT(qubits[0], qubits[1])]),
         cirq.Moment(
             [
-                cirq.depolarize(pauli_error_from_depolarization(pauli_error, t1, 25.0) / 3).on_each(
+                cirq.depolarize(pauli_error_from_depolarization(pauli_error, t1_ns, 25.0) / 3).on_each(
                     qubits
                 )
             ]
         ),
-        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-25.0 / t1)).on_each(qubits)]),
+        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-25.0 / t1_ns)).on_each(qubits)]),
+        cirq.Moment([cirq.GeneralizedAmplitudeDampingChannel(p=1.0, gamma=p11).on(qubits[0])]),
+        cirq.Moment([cirq.measure(qubits[0], key='q0')]),
+        cirq.Moment(
+            [
+                cirq.depolarize(pauli_error_from_depolarization(pauli_error, t1_ns, 4000.0) / 3).on_each(
+                    qubits
+                )
+            ]
+        ),
+        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-4000.0 / t1_ns)).on_each(qubits)]),
         cirq.Moment([cirq.ISwapPowGate().on_each(qubits)]),
         cirq.Moment(
             [
-                cirq.depolarize(pauli_error_from_depolarization(pauli_error, t1, 32.0) / 3).on_each(
+                cirq.depolarize(pauli_error_from_depolarization(pauli_error, t1_ns, 32.0) / 3).on_each(
                     qubits
                 )
             ]
         ),
-        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-32.0 / t1)).on_each(qubits)]),
+        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-32.0 / t1_ns)).on_each(qubits)]),
         cirq.Moment([cirq.GeneralizedAmplitudeDampingChannel(p=1.0, gamma=p11).on_each(qubits)]),
         cirq.Moment([cirq.measure(qubits[0], key='q0'), cirq.measure(qubits[1], key='q1')]),
-    )
-
+    cirq.Moment(
+            [
+                cirq.depolarize(pauli_error_from_depolarization(pauli_error, t1_ns, 4000.0) / 3).on_each(
+                    qubits
+                )
+            ]
+        ),
+        cirq.Moment([cirq.amplitude_damp(1 - np.exp(-4000.0 / t1_ns)).on_each(qubits)])
+      )
+    print("Expected:")
+    print(expected_circuit)
     assert_equivalent_op_tree(expected_circuit, noisy_circuit)
