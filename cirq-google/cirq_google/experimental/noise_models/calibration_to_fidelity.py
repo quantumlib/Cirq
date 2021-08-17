2bc3a920d7472a7309b2c9fa88df1d44ece0a8f4
Ana Sofia Uzsoy <asuzsoy@google.com>
2021-08-17 18:16:48 +0000

    addigng validate_calibration method

--- cirq-google/cirq_google/experimental/noise_models/calibration_to_fidelity.py
+++ cirq-google/cirq_google/experimental/noise_models/calibration_to_fidelity.py
@@ -3,31 +3,28 @@ import cirq_google
 import numpy as np
 from cirq.devices.fidelity import Fidelity
 
+
 def fidelity_from_calibration(calibration: cirq_google.Calibration):
+    def unpack_from_calibration(metric_name):
+        if metric_name in calibration.keys():
+            return np.mean([value for qubit, value in calibration[metric_name].items()])
+        else:
+            return None
 
-  def unpack_from_calibration(metric_name):
-    if metric_name in calibration.keys():
-      return np.mean([value for qubit, value in calibration[metric_name].items()])
-    else:
-      return None
+    def rb_error_to_decay_constant(rb_pauli_error, N: int = 2):
+        if rb_pauli_error is not None:
+            return 1 - rb_pauli_error / (1 - 1 / N ** 2)
+        else:
+            return None
 
-  def rb_error_to_decay_constant(rb_pauli_error, N: int = 2):
-    if rb_pauli_error is not None:
-      return 1 - rb_pauli_error / (1 - 1 / N**2)
-    else:
-      return None
+    t1_micros = unpack_from_calibration('single_qubit_idle_t1_micros')
+    t1_nanos = t1_micros * 1000 if t1_micros is not None else None
+    xeb_fidelity = unpack_from_calibration('xeb')
+    rb_pauli_error = unpack_from_calibration('single_qubit_rb_pauli_error_per_gate')
+    p00 = unpack_from_calibration('single_qubit_p00_error')
+    p11 = unpack_from_calibration('single_qubit_p11_error')
+    decay_constant = rb_error_to_decay_constant(rb_pauli_error)
 
-  t1_micros = unpack_from_calibration('single_qubit_idle_t1_micros')
-  t1_nanos = t1_micros * 1000 if t1_micros is not None else None
-  xeb_fidelity = unpack_from_calibration('xeb')
-  rb_pauli_error = unpack_from_calibration('single_qubit_rb_pauli_error_per_gate')
-  p00 = unpack_from_calibration('single_qubit_p00_error')
-  p11 = unpack_from_calibration('single_qubit_p11_error')
-  decay_constant = rb_error_to_decay_constant(rb_pauli_error)
-
-  if decay_constant is not None: # can't define both decay constant and xeb
-    return Fidelity(t1 = t1_nanos, decay_constant = decay_constant, p00 = p00, p11 = p11)
-  return Fidelity(t1 = t1_nanos, xeb_fidelity = xeb_fidelity, p00 = p00, p11 = p11)
-
-
-
+    if decay_constant is not None:  # can't define both decay constant and xeb
+        return Fidelity(t1=t1_nanos, decay_constant=decay_constant, p00=p00, p11=p11)
+    return Fidelity(t1=t1_nanos, xeb_fidelity=xeb_fidelity, p00=p00, p11=p11)
--- cirq-google/cirq_google/experimental/noise_models/calibration_to_fidelity_test.py
+++ cirq-google/cirq_google/experimental/noise_models/calibration_to_fidelity_test.py
@@ -7,50 +7,61 @@ from cirq_google.experimental.noise_models.calibration_to_fidelity import fideli
 from google.protobuf.text_format import Merge
 import numpy as np
 
+
 def test_fidelity_from_calibration():
-  xeb_1 = 0.999
-  xeb_2 = 0.996
+    xeb_1 = 0.999
+    xeb_2 = 0.996
 
-  p00_1 = 0.001
-  p00_2 = 0.002
-  p00_3 = 0.003
+    p00_1 = 0.001
+    p00_2 = 0.002
+    p00_3 = 0.003
 
-  t1_1 = 0.005
-  t1_2 = 0.007
-  t1_3 = 0.003
+    t1_1 = 0.005
+    t1_2 = 0.007
+    t1_3 = 0.003
 
-  _CALIBRATION_DATA = Merge(
-      """
+    _CALIBRATION_DATA = Merge(
+        """
     timestamp_ms: 1579214873,
     metrics: [{
         name: 'xeb',
         targets: ['0_0', '0_1'],
         values: [{
-            double_val: """ + str(xeb_1) + """
+            double_val: """
+        + str(xeb_1)
+        + """
         }]
     }, {
         name: 'xeb',
         targets: ['0_0', '1_0'],
         values: [{
-            double_val: """ + str(xeb_2) + """
+            double_val: """
+        + str(xeb_2)
+        + """
         }]
     }, {
         name: 'single_qubit_p00_error',
         targets: ['0_0'],
         values: [{
-            double_val: """ + str(p00_1) + """
+            double_val: """
+        + str(p00_1)
+        + """
         }]
     }, {
         name: 'single_qubit_p00_error',
         targets: ['0_1'],
         values: [{
-            double_val: """ + str(p00_2) + """
+            double_val: """
+        + str(p00_2)
+        + """
         }]
     }, {
         name: 'single_qubit_p00_error',
         targets: ['1_0'],
         values: [{
-            double_val: """ + str(p00_3) + """
+            double_val: """
+        + str(p00_3)
+        + """
         }]
     }, {
         name: 'single_qubit_readout_separation_error',
@@ -74,73 +85,85 @@ def test_fidelity_from_calibration():
         name: 'single_qubit_idle_t1_micros',
         targets: ['0_0'],
         values: [{
-            double_val: """ + str(t1_1) + """
+            double_val: """
+        + str(t1_1)
+        + """
         }]
     }, {
         name: 'single_qubit_idle_t1_micros',
         targets: ['0_1'],
         values: [{
-            double_val: """ + str(t1_2) + """
+            double_val: """
+        + str(t1_2)
+        + """
         }]
     }, {
         name: 'single_qubit_idle_t1_micros',
         targets: ['1_0'],
         values: [{
-            double_val: """ + str(t1_3) + """
+            double_val: """
+        + str(t1_3)
+        + """
         }]
     }]
 """,
-    v2.metrics_pb2.MetricsSnapshot(),
-)
+        v2.metrics_pb2.MetricsSnapshot(),
+    )
 
-  # Create Fidelity objcet from Calibration
-  calibration = cirq_google.Calibration(_CALIBRATION_DATA)
-  f = fidelity_from_calibration(calibration)
+    # Create Fidelity objcet from Calibration
+    calibration = cirq_google.Calibration(_CALIBRATION_DATA)
+    f = fidelity_from_calibration(calibration)
 
-  expected_t1_nanos = np.mean([t1_1, t1_2, t1_3]) * 1000
-  expected_xeb_fidelity = np.mean([xeb_1, xeb_2])
-  expected_p00 = np.mean([p00_1, p00_2, p00_3])
+    expected_t1_nanos = np.mean([t1_1, t1_2, t1_3]) * 1000
+    expected_xeb_fidelity = np.mean([xeb_1, xeb_2])
+    expected_p00 = np.mean([p00_1, p00_2, p00_3])
 
-  assert np.isclose(f.t1,expected_t1_nanos)
-  assert np.isclose(f.xeb,expected_xeb_fidelity)
-  assert np.isclose(f.p00, expected_p00)
+    assert np.isclose(f.t1, expected_t1_nanos)
+    assert np.isclose(f.xeb, expected_xeb_fidelity)
+    assert np.isclose(f.p00, expected_p00)
+
 
 def test_from_calibration_rb():
-  rb_pauli_1 = 0.001
-  rb_pauli_2 = 0.002
-  rb_pauli_3 = 0.003
+    rb_pauli_1 = 0.001
+    rb_pauli_2 = 0.002
+    rb_pauli_3 = 0.003
 
-  _CALIBRATION_DATA_RB = Merge(
-    """
+    _CALIBRATION_DATA_RB = Merge(
+        """
     timestamp_ms: 1579214873,
     metrics: [{
 
         name: 'single_qubit_rb_pauli_error_per_gate',
         targets: ['0_0'],
         values: [{
-            double_val: """ + str(rb_pauli_1) + """
+            double_val: """
+        + str(rb_pauli_1)
+        + """
         }]
     }, {
         name: 'single_qubit_rb_pauli_error_per_gate',
         targets: ['0_1'],
         values: [{
-            double_val: """ + str(rb_pauli_2) + """
+            double_val: """
+        + str(rb_pauli_2)
+        + """
         }]
     }, {
         name: 'single_qubit_rb_pauli_error_per_gate',
         targets: ['1_0'],
         values: [{
-            double_val: """ + str(rb_pauli_3) + """
+            double_val: """
+        + str(rb_pauli_3)
+        + """
         }]
      }]
     """,
-    v2.metrics_pb2.MetricsSnapshot(),
-  )
+        v2.metrics_pb2.MetricsSnapshot(),
+    )
 
- # Create Fidelity object from Calibration
-  rb_calibration = cirq_google.Calibration(_CALIBRATION_DATA_RB)
-  rb_fidelity = fidelity_from_calibration(rb_calibration)
+    # Create Fidelity object from Calibration
+    rb_calibration = cirq_google.Calibration(_CALIBRATION_DATA_RB)
+    rb_fidelity = fidelity_from_calibration(rb_calibration)
 
-  average_pauli_rb = np.mean([rb_pauli_1, rb_pauli_2, rb_pauli_3])
-  assert np.isclose(average_pauli_rb, rb_fidelity.rb_pauli_error())
-
+    average_pauli_rb = np.mean([rb_pauli_1, rb_pauli_2, rb_pauli_3])
+    assert np.isclose(average_pauli_rb, rb_fidelity.rb_pauli_error())
