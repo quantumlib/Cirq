426703ce744f21c7b08d3b54e3c4099299e05e8f
Doug Strain <dstrain@google.com>
2021-08-20 14:19:58 -0700

    Change to isinstance

--- cirq-core/cirq/value/measurement_key_test.py
+++ cirq-core/cirq/value/measurement_key_test.py
@@ -20,8 +20,10 @@ import cirq
 def test_empty_init():
     with pytest.raises(TypeError, match='required positional argument'):
         _ = cirq.MeasurementKey()
-    with pytest.raises(ValueError, match='cannot be empty'):
+    with pytest.raises(ValueError, match='valid string'):
         _ = cirq.MeasurementKey(None)
+    with pytest.raises(ValueError, match='valid string'):
+        _ = cirq.MeasurementKey(4.2)
     # Initialization of empty string should be allowed
     _ = cirq.MeasurementKey('')
 
