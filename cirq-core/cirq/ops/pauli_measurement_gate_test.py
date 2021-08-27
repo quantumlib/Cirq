8a991f3c26a5e26c2cf9d2dbea18db9608239a13
Tanuj Khattar <tanujkhattar@google.com>
2021-08-27 10:45:41 -0700

    fix lint

--- cirq-core/cirq/ops/pauli_measurement_gate_test.py
+++ cirq-core/cirq/ops/pauli_measurement_gate_test.py
@@ -156,10 +156,10 @@ def test_bad_observable_raises():
     with pytest.raises(ValueError, match='Pauli observable .* is empty'):
         _ = cirq.PauliMeasurementGate([])
 
-    with pytest.raises(ValueError, match='Pauli observable .* must be Iterable\[`cirq.Pauli`\]'):
+    with pytest.raises(ValueError, match=r'Pauli observable .* must be Iterable\[`cirq.Pauli`\]'):
         _ = cirq.PauliMeasurementGate([cirq.I, cirq.X, cirq.Y])
 
-    with pytest.raises(ValueError, match='Pauli observable .* must be Iterable\[`cirq.Pauli`\]'):
+    with pytest.raises(ValueError, match=r'Pauli observable .* must be Iterable\[`cirq.Pauli`\]'):
         _ = cirq.PauliMeasurementGate(cirq.DensePauliString('XYZI'))
 
 
