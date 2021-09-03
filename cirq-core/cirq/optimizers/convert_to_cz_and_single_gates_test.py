731a1de2fb9e1a6c0b0b29a277da83de6eea029c
Dax Fohl <dax.fohl@gmail.com>
2021-09-03 06:45:35 -0700

    Fix tests

--- cirq-core/cirq/optimizers/convert_to_cz_and_single_gates_test.py
+++ cirq-core/cirq/optimizers/convert_to_cz_and_single_gates_test.py
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import traceback
+import pytest
 
 import numpy as np
-import pytest
 
 import cirq
 
