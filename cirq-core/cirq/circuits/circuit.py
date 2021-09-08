07af7fe26f51185782a84d6b2f981cc906b26409
Dax Fohl <dax.fohl@gmail.com>
2021-09-08 10:44:17 -0700

    remove merge error

--- cirq-core/cirq/circuits/circuit.py
+++ cirq-core/cirq/circuits/circuit.py
@@ -914,9 +914,6 @@ class AbstractCircuit(abc.ABC):
     def _measurement_key_names_(self) -> AbstractSet[str]:
         return self.all_measurement_key_names()
 
-    def _measurement_key_names_(self) -> AbstractSet[str]:
-        return {key for op in self.all_operations() for key in protocols.measurement_key_names(op)}
-
     def _with_measurement_key_mapping_(self, key_map: Dict[str, str]):
         return self._with_sliced_moments(
             [protocols.with_measurement_key_mapping(moment, key_map) for moment in self.moments]
