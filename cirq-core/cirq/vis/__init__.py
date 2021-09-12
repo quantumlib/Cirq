fd31efae4e38f76c35f8c0960d0604ea49e542fc
Animesh Sinha <AnimeshSinha1309@gmail.com>
2021-09-12 12:46:09 +0530

    Added the density plotter to __init__
    Allow for imports of the new module.

--- cirq-core/cirq/vis/__init__.py
+++ cirq-core/cirq/vis/__init__.py
@@ -20,5 +20,6 @@ from cirq.vis.heatmap import TwoQubitInteractionHeatmap
 from cirq.vis.histogram import integrated_histogram
 
 from cirq.vis.state_histogram import get_state_histogram, plot_state_histogram
+from cirq.vis.density_matrix import plot_density_matrix
 
 from cirq.vis.vis_utils import relative_luminance
