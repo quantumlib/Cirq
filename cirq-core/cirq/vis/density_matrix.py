a1a6e2b909256431c341ad3b9d9b24796d84baa5
Animesh Sinha <AnimeshSinha1309@gmail.com>
2021-09-12 12:41:13 +0530

    Made the Density Matrix Plotter
    Essential plotting features for desnity matrix has been done, tests are complete.

---
+++ cirq-core/cirq/vis/density_matrix.py
@@ -0,0 +1,70 @@
+# Copyright 2021 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import lines, patches
+
+
+def plot_density_matrix(matrix: np.ndarray, ax=None):
+    """Generates a quirk like plot for a given density matrix
+
+    Args:
+        matrix: The density matrix we want to visualize
+        ax: The axes to plot on
+    """
+    plt.style.use('ggplot')
+
+    matrix = matrix.astype(np.complex128)
+    assert len(matrix.shape) == 2, "Density matrix should be a 2-D numpy array"
+    assert matrix.shape[0] == matrix.shape[1], "The density matrix should be square"
+    assert (
+        2 ** int(np.log2(matrix.shape[0])) == matrix.shape[0]
+    ), "The size of the matrix should be a power of 2"
+    num_qubits = int(np.log2(matrix.shape[0]))
+
+    if ax is None:
+        _, ax = plt.subplots(figsize=(10, 10))
+    ax.set_xlim(0 - 0.001, 2 ** num_qubits + 0.001)
+    ax.set_ylim(0 - 0.001, 2 ** num_qubits + 0.001)
+
+    def plot_element_of_density_matrix(x, y, r, phase):
+        circle_out = plt.Circle((x + 0.5, y + 0.5), radius=1 / 2.2, fill=False, color='#333333')
+        circle_in = plt.Circle(
+            (x + 0.5, y + 0.5), radius=r / 2.2, fill=True, color='IndianRed', alpha=0.8
+        )
+        line = lines.Line2D(
+            (x + 0.5, x + 0.5 + np.cos(phase) / 2.2),
+            (y + 0.5, y + 0.5 + np.sin(phase) / 2.2),
+            color='#333333',
+        )
+        rect = patches.Rectangle((x + 0.01, y + 0.01), 1.0 - 0.02, r * (1 - 0.02), alpha=0.25)
+        ax.add_artist(rect)
+        ax.add_artist(circle_in)
+        ax.add_artist(circle_out)
+        ax.add_artist(line)
+
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            plot_element_of_density_matrix(i, j, np.abs(matrix[i][j]), np.angle(matrix[i][j]))
+
+    ticks, labels = np.arange(0.5, matrix.shape[0]), [
+        f"{'0'*(num_qubits - len(f'{i:b}'))}{i:b}" for i in range(matrix.shape[0])
+    ]
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(labels)
+    ax.set_yticks(ticks)
+    ax.set_yticklabels(labels)
+    ax.set_facecolor('#eeeeee')
+    return ax
---
+++ cirq-core/cirq/vis/density_matrix_test.py
@@ -0,0 +1,49 @@
+# Copyright 2021 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for Histogram."""
+
+import numpy as np
+import pytest
+
+from cirq.vis.density_matrix import plot_density_matrix
+
+
+@pytest.mark.parametrize('size', [2, 4, 8, 16])
+def test_density_matrix_plotter(size):
+    r = np.random.random((size, size))
+    theta = np.random.random((size, size)) * 2 * np.pi
+    matrix = r * np.exp(theta * 1j)
+    plot_density_matrix(matrix)
+
+
+def test_density_matrix_type_error():
+    with pytest.raises(AssertionError, match="Density matrix should be a 2-D numpy array"):
+        matrix = np.random.random(size=(4, 4, 4))
+        plot_density_matrix(matrix)
+
+
+def test_density_matrix_size_error():
+    with pytest.raises(AssertionError, match="The size of the matrix should be a power of 2"):
+        r = np.random.random((3, 3))
+        theta = np.random.random((3, 3)) * 2 * np.pi
+        matrix = r * np.exp(theta * 1j)
+        plot_density_matrix(matrix)
+
+
+def test_density_matrix_not_square():
+    with pytest.raises(AssertionError, match="The density matrix should be square"):
+        r = np.random.random((4, 8))
+        theta = np.random.random((4, 8)) * 2 * np.pi
+        matrix = r * np.exp(theta * 1j)
+        plot_density_matrix(matrix)
