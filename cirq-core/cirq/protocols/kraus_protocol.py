e91faacb009150062f602488205acd77b1b5152c
Zshan0 <zeeshan060801@gmail.com>
2021-09-14 15:02:16 +0530

    Fixed typecasting issue

--- cirq-core/cirq/protocols/kraus_protocol.py
+++ cirq-core/cirq/protocols/kraus_protocol.py
@@ -14,7 +14,7 @@
 
 """Protocol and methods for quantum channels."""
 
-from typing import Any, Sequence, Tuple, TypeVar, Union
+from typing import Any, Sequence, Tuple, TypeVar, Union, List
 import warnings
 
 import numpy as np
@@ -29,6 +29,7 @@ from cirq.protocols.decompose_protocol import _try_decompose_into_operations_and
 from cirq.protocols.mixture_protocol import has_mixture
 
 from cirq.ops import Gate
+from cirq.ops.raw_types import Qid
 from cirq.devices import LineQid
 
 from cirq.type_workarounds import NotImplementedType
@@ -212,35 +213,28 @@ def kraus(
 
         return val
 
-    qubits = None
     if isinstance(val, Gate):
         operation = val.on(*LineQid.for_gate(val))
-        qubits = LineQid.for_gate(val)
     else:
         operation = val
 
     decomposed = decompose(operation)
 
     if decomposed != [operation]:
-        if qubits is None:
-            qubits = []
-            for x in decomposed:
-                qubits += list(x.qubits)
-            qubits = sorted(list(set(qubits)))
+        qubits: List[Qid] = []
+        for x in decomposed:
+            qubits.extend(x.qubits)
 
+        qubits = sorted(list(set(qubits)))
         kraus_list = list(map(lambda x: kraus_tensor(x, qubits, default), decomposed))
 
-        if not any([checkEquality(x, default) for x in kraus_list]):
+        if not any([checkEquality(x, default) for x in kraus_list]) or len(kraus_list) == 0:
             kraus_result = kraus_list[0]
 
             for i in range(1, len(kraus_list)):
-                try:
-                    kraus_result = [
-                        op_2.dot(op_1) for op_1 in kraus_result for op_2 in kraus_list[i]
-                    ]
-                except:
-                    print("---->", operation)
-                    exit()
+                kraus_result = [
+                    op_2.dot(op_1) for op_1 in kraus_result for op_2 in kraus_list[i]
+                ]
 
             return tuple(kraus_result)
 
