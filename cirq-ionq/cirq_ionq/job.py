5ae9fd185661917646a93b7c0e8765fb5c8bde7f
Dave Bacon <dabacon@gmail.com>
2021-09-20 22:02:53 -0700

    Explicitly throw TimeoutError when job times out

--- cirq-ionq/cirq_ionq/job.py
+++ cirq-ionq/cirq_ionq/job.py
@@ -199,6 +199,8 @@ class Job:
             if 'failure' in self._job and 'error' in self._job['failure']:
                 error = self._job['failure']['error']
                 raise RuntimeError(f'Job failed. Error message: {error}')
+            if time_waited_seconds >= timeout_seconds:
+                raise TimeoutError(f'Job timed out after waiting {time_waited_seconds} seconds.')
             raise RuntimeError(
                 f'Job was not completed successful. Instead had status: {self.status()}'
             )
