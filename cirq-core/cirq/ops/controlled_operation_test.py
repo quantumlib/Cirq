7c83f33ab9d02e149058026b6042c025a7731410
Tanuj Khattar <tanujkhattar@google.com>
2021-09-24 17:37:12 -0700

    Fix lint

--- cirq-core/cirq/ops/controlled_operation_test.py
+++ cirq-core/cirq/ops/controlled_operation_test.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import Union, Tuple, cast
+import itertools
 
 import numpy as np
 import pytest
 import sympy
-import itertools
 
 import cirq
 from cirq import protocols
