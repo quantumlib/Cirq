75575d07f1bdf81056bc6d6f7a5aa99e8b831eef
Animesh Sinha <AnimeshSinha1309@gmail.com>
2021-09-28 09:09:44 +0530

    Adding tests to ignore name
    Two gates with different names are still equal, added tests for that.

--- cirq-core/cirq/ops/state_preparation_channel_test.py
+++ cirq-core/cirq/ops/state_preparation_channel_test.py
@@ -124,6 +124,11 @@ def test_equality_of_gates():
     gate_2 = cirq.StatePreparationChannel(state)
     assert gate_1 == gate_2, "Equal state not leading to same gate"
     assert not gate_1 == state, "Incompatible objects shouldn't be equal"
+    state = np.array([0, 1, 0, 0], dtype=np.complex64)
+    gate_3 = cirq.StatePreparationChannel(state, name='gate_a')
+    gate_4 = cirq.StatePreparationChannel(state, name='gate_b')
+    assert gate_3 == gate_4, "Equal state with different names not leading to same gate"
+    assert gate_1 != gate_3, "Different states shouldn't lead to same gate"
 
 
 def test_approx_equality_of_gates():
