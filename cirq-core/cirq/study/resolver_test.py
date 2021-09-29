727b32e678eba984aa13f46c82bd56120fe1bb8b
Pieter Eendebak <pieter.eendebak@gmail.com>
2021-09-29 10:40:12 +0200

    fix formatting

--- cirq-core/cirq/study/resolver_test.py
+++ cirq-core/cirq/study/resolver_test.py
@@ -133,11 +133,13 @@ def test_resolve_integer_division():
     resolved = r.value_of(sympy.Symbol('a') / sympy.Symbol('b'))
     assert resolved == 0.5
 
+
 def test_resolve_symbol_division():
     B = sympy.Symbol('B')
     r = cirq.ParamResolver({'a': 1, 'b': B})
     resolved = r.value_of(sympy.Symbol('a') / sympy.Symbol('b'))
     assert resolved == sympy.core.power.Pow(B, -1)
+
 
 def test_param_dict():
     r = cirq.ParamResolver({'a': 0.5, 'b': 0.1})
