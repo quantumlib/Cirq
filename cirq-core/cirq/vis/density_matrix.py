e175cb5d8938f587a9c33ec9ba1afbc753042964
Animesh Sinha <AnimeshSinha1309@gmail.com>
2021-10-03 17:03:49 +0530

    Added optional labels to the plot
    You can add text to see the values being plotted too.

--- cirq-core/cirq/vis/density_matrix.py
+++ cirq-core/cirq/vis/density_matrix.py
@@ -19,7 +19,7 @@ from matplotlib import lines, patches
 from cirq.qis.states import validate_density_matrix
 
 
-def plot_density_matrix(matrix: np.ndarray, ax: plt.Axes = None):
+def plot_density_matrix(matrix: np.ndarray, show_text=False, ax: plt.Axes = None):
     """Generates a plot for a given density matrix shows the magnitude of
     each term of the whole matrix using a proportionally sized circle and
     the phase angle using a line at that angle.
@@ -28,6 +28,7 @@ def plot_density_matrix(matrix: np.ndarray, ax: plt.Axes = None):
 
     Args:
         matrix: The density matrix we want to visualize
+        show_text: Boolean on if to show text labels or not
         ax: The axes to plot on
     """
     plt.style.use('ggplot')
@@ -42,20 +43,30 @@ def plot_density_matrix(matrix: np.ndarray, ax: plt.Axes = None):
     ax.set_ylim(0 - 0.001, 2 ** num_qubits + 0.001)
 
     def plot_element_of_density_matrix(x, y, r, phase):
+        image_opacity = 0.8 if not show_text else 0.4
         circle_out = plt.Circle((x + 0.5, y + 0.5), radius=1 / 2.2, fill=False, color='#333333')
         circle_in = plt.Circle(
-            (x + 0.5, y + 0.5), radius=r / 2.2, fill=True, color='IndianRed', alpha=0.8
+            (x + 0.5, y + 0.5), radius=r / 2.2, fill=True, color='IndianRed', alpha=image_opacity
         )
         line = lines.Line2D(
             (x + 0.5, x + 0.5 + np.cos(phase) / 2.2),
             (y + 0.5, y + 0.5 + np.sin(phase) / 2.2),
             color='#333333',
+            alpha=image_opacity,
         )
         rect = patches.Rectangle((x + 0.01, y + 0.01), 1.0 - 0.02, r * (1 - 0.02), alpha=0.25)
         ax.add_artist(rect)
         ax.add_artist(circle_in)
         ax.add_artist(circle_out)
         ax.add_artist(line)
+        if show_text:
+            plt.text(
+                x + 0.5,
+                y + 0.5,
+                f"{np.round(r, decimals=2)}\n{np.round(phase * 180 / np.pi, decimals=2)} deg",
+                horizontalalignment='center',
+                verticalalignment='center',
+            )
 
     for i in range(matrix.shape[0]):
         for j in range(matrix.shape[1]):
