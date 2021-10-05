072efd699bb1ced0d76c74b25ce323b7a8058d0a
Matthew Harrigan <mpharrigan@google.com>
2021-10-04 17:42:46 -0700

    [cirqflow] QuantumExecutableGroup

--- cirq-google/cirq_google/workflow/quantum_executable.py
+++ cirq-google/cirq_google/workflow/quantum_executable.py
@@ -17,7 +17,7 @@
 import abc
 import dataclasses
 from dataclasses import dataclass
-from typing import Union, Tuple, Optional, Sequence, cast
+from typing import Union, Tuple, Optional, Sequence, cast, Iterable
 
 from cirq import _compat, study
 import cirq
@@ -154,6 +154,56 @@ class QuantumExecutable:
 
     def __repr__(self):
         return _compat.dataclass_repr(self, namespace='cirq_google')
+
+    def _json_dict_(self):
+        return cirq.dataclass_json_dict(self, namespace='cirq.google')
+
+
+@dataclass(frozen=True)
+class QuantumExecutableGroup:
+    """A collection of `QuantumExecutable`s.
+
+    Attributes:
+        executables: A tuple of `cg.QuantumExecutable`.
+    """
+
+    executables: Tuple[QuantumExecutable, ...]
+
+    def __init__(
+        self,
+        executables: Sequence[QuantumExecutable],
+    ):
+        """Initialize and normalize the quantum executable group.
+
+        Args:
+             executables: A sequence of `cg.QuantumExecutable` which will be frozen into a
+                tuple.
+        """
+
+        if not isinstance(executables, tuple):
+            executables = tuple(executables)
+        object.__setattr__(self, 'executables', executables)
+
+        object.__setattr__(self, '_hash', hash(dataclasses.astuple(self)))
+
+    def __len__(self):
+        return len(self.executables)
+
+    def __iter__(self) -> Iterable[QuantumExecutable]:
+        yield from self.executables
+
+    def __str__(self):
+        exe_str = ', '.join(str(exe) for exe in self.executables[:2])
+        if len(self.executables) > 2:
+            exe_str += ', ...'
+
+        return f'QuantumExecutable(executables=[{exe_str}])'
+
+    def __repr__(self):
+        return _compat.dataclass_repr(self, namespace='cirq_google')
+
+    def __hash__(self):
+        return self._hash
 
     def _json_dict_(self):
         return cirq.dataclass_json_dict(self, namespace='cirq.google')
