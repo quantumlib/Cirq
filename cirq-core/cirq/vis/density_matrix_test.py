3415b91c5b2f1e7ab981cf76be2533b49f0c7479
Animesh Sinha <AnimeshSinha1309@gmail.com>
2021-10-08 21:31:58 +0530

    Added some tests and fixed code quality
    Improved tests by testing for elements on the plotted axes, parametrized to remove redundancy, extracted the subfunction and added tests for it, etc.

--- cirq-core/cirq/vis/density_matrix_test.py
+++ cirq-core/cirq/vis/density_matrix_test.py
@@ -15,35 +15,65 @@
 
 import numpy as np
 import pytest
+import matplotlib.pyplot as plt
+from matplotlib import lines, patches, text, spines, axis
 
 import cirq.testing
 from cirq.vis.density_matrix import plot_density_matrix
+from cirq.vis.density_matrix import _plot_element_of_density_matrix
 
 
 @pytest.mark.parametrize('show_text', [True, False])
 @pytest.mark.parametrize('size', [2, 4, 8, 16])
 def test_density_matrix_plotter(size, show_text):
     matrix = cirq.testing.random_density_matrix(size)
-    plot_density_matrix(matrix, show_text=show_text)
+    ax = plot_density_matrix(matrix, show_text=show_text, title='Test Density Matrix Plot')
+    assert ax.get_title() == 'Test Density Matrix Plot'
+    for obj in ax.get_children():
+        assert isinstance(
+            obj,
+            (
+                patches.Circle,
+                spines.Spine,
+                axis.XAxis,
+                axis.YAxis,
+                lines.Line2D,
+                patches.Rectangle,
+                text.Text,
+            ),
+        )
 
 
-def test_density_matrix_type_error():
+@pytest.mark.parametrize(
+    'matrix',
+    [
+        np.random.random(size=(4, 4, 4)),
+        np.random.random((3, 3)) * np.exp(np.random.random((3, 3)) * 2 * np.pi * 1j),
+        np.random.random((4, 8)) * np.exp(np.random.random((4, 8)) * 2 * np.pi * 1j),
+    ],
+)
+def test_density_matrix_type_error(matrix):
     with pytest.raises(ValueError, match="Incorrect shape for density matrix:*"):
-        matrix = np.random.random(size=(4, 4, 4))
         plot_density_matrix(matrix)
 
 
-def test_density_matrix_size_error():
-    with pytest.raises(ValueError, match="Incorrect shape for density matrix:*"):
-        r = np.random.random((3, 3))
-        theta = np.random.random((3, 3)) * 2 * np.pi
-        matrix = r * np.exp(theta * 1j)
-        plot_density_matrix(matrix)
-
-
-def test_density_matrix_not_square():
-    with pytest.raises(ValueError, match="Incorrect shape for density matrix:*"):
-        r = np.random.random((4, 8))
-        theta = np.random.random((4, 8)) * 2 * np.pi
-        matrix = r * np.exp(theta * 1j)
-        plot_density_matrix(matrix)
+@pytest.mark.parametrize('show_text', [True, False])
+@pytest.mark.parametrize('show_rect', [True, False])
+def test_plot_element_of_density_matrix(show_rect, show_text):
+    _, ax = plt.subplots(figsize=(5, 5))
+    ax.set_xlim(0 - 0.001, 2 ** 2 + 0.001)
+    ax.set_ylim(0 - 0.001, 2 ** 2 + 0.001)
+    _plot_element_of_density_matrix(ax, 2, 2, 0.5, 0.1, show_rect=show_rect, show_text=show_text)
+    for obj in ax.get_children():
+        assert isinstance(
+            obj,
+            (
+                patches.Circle,
+                spines.Spine,
+                axis.XAxis,
+                axis.YAxis,
+                lines.Line2D,
+                patches.Rectangle,
+                text.Text,
+            ),
+        )
