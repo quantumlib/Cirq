1b1406e6e063ddf56a951c0d10d8b0f7b22bf72e
vtomole <vtomole2@gmail.com>
2021-10-19 11:46:21 -0500

    Delete some stuff

--- cirq-superstaq/dev_tools/update-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$(git rev-parse --show-toplevel)"
-
-python setup.py bdist_wheel
-twine upload dist/* -u __token__ -p "$PYPI_API_KEY"
