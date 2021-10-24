d64b7c68925813a31554e79160a506218aa2fa97
Rishabh <54768821+anonymousr007@users.noreply.github.com>
2021-10-24 23:24:38 +0000

    Bump dev version from v0.13.0.dev to v0.14.0.dev

--- cirq-google/cirq_google/_version_test.py
+++ cirq-google/cirq_google/_version_test.py
@@ -2,4 +2,4 @@ import cirq_google
 
 
 def test_version():
-    assert cirq_google.__version__ == "0.13.0.dev"
+    assert cirq_google.__version__ == "0.14.0.dev"
--- cirq-ionq/cirq_ionq/_version_test.py
+++ cirq-ionq/cirq_ionq/_version_test.py
@@ -2,4 +2,4 @@ import cirq_ionq
 
 
 def test_version():
-    assert cirq_ionq.__version__ == "0.13.0.dev"
+    assert cirq_ionq.__version__ == "0.14.0.dev"
