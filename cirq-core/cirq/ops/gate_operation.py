8b6d53c9cef2502273dfb5aaf5239d6339ab229a
Michael Broughton <mbbrough@google.com>
2021-10-25 18:53:31 -0700

    include gate_operation

--- cirq-core/cirq/ops/gate_operation.py
+++ cirq-core/cirq/ops/gate_operation.py
@@ -222,27 +222,11 @@ class GateOperation(raw_types.Operation):
         getter = getattr(self.gate, '_measurement_key_name_', None)
         if getter is not None:
             return getter()
-        getter = getattr(self.gate, '_measurement_key_', None)
-        if getter is not None:
-            _warn_or_error(
-                f'_measurement_key_ was used but is deprecated.\n'
-                f'It will be removed in cirq v0.13.\n'
-                f'Use _measurement_key_name_ instead.\n'
-            )
-            return getter()
         return NotImplemented
 
     def _measurement_key_names_(self) -> Optional[AbstractSet[str]]:
         getter = getattr(self.gate, '_measurement_key_names_', None)
         if getter is not None:
-            return getter()
-        getter = getattr(self.gate, '_measurement_keys_', None)
-        if getter is not None:
-            _warn_or_error(
-                f'_measurement_keys_ was used but is deprecated.\n'
-                f'It will be removed in cirq v0.13.\n'
-                f'Use _measurement_key_names_ instead.\n'
-            )
             return getter()
         return NotImplemented
 
