237d4e9af5907f30e5b757d5e36f4bacd8427467
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2021-11-11 08:35:21 -0800

    Add copyright notices.

--- cirq-core/cirq/devices/insertion_noise_model_test.py
+++ cirq-core/cirq/devices/insertion_noise_model_test.py
@@ -1,3 +1,17 @@
+# Copyright 2021 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import cirq
 from cirq.devices.insertion_noise_model import InsertionNoiseModel
 from cirq.devices.noise_utils import (
@@ -13,7 +27,7 @@ def test_insertion_noise():
     model = InsertionNoiseModel(
         {op_id0: cirq.T(q0), op_id1: cirq.H(q1)}, require_physical_tag=False
     )
-    assert model.prepend == False
+    assert not model.prepend
 
     moment_0 = cirq.Moment(cirq.X(q0), cirq.X(q1))
     assert model.noisy_moment(moment_0, system_qubits=[q0, q1]) == [
@@ -57,7 +71,7 @@ def test_require_physical_tag():
     op_id0 = OpIdentifier(cirq.XPowGate, q0)
     op_id1 = OpIdentifier(cirq.ZPowGate, q1)
     model = InsertionNoiseModel({op_id0: cirq.T(q0), op_id1: cirq.H(q1)})
-    assert model.require_physical_tag == True
+    assert model.require_physical_tag
 
     moment_0 = cirq.Moment(cirq.X(q0).with_tags(PHYSICAL_GATE_TAG), cirq.Z(q1))
     assert model.noisy_moment(moment_0, system_qubits=[q0, q1]) == [
--- cirq-google/cirq_google/devices/google_noise_properties_test.py
+++ cirq-google/cirq_google/devices/google_noise_properties_test.py
@@ -1,3 +1,17 @@
+# Copyright 2021 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Dict, List, Tuple
 import numpy as np
 import pytest
@@ -211,13 +225,9 @@ def test_two_qubit_gates(op):
     assert len(noisy_circuit.moments[2].operations) == 1
     fsim_op = noisy_circuit.moments[2].operations[0]
     assert isinstance(fsim_op.gate, cirq.PhasedFSimGate)
-    assert fsim_op == cirq.PhasedFSimGate(
-        theta=0.01,
-        zeta=0.03,
-        chi=0.04,
-        gamma=0.05,
-        phi=0.02
-    ).on(q0, q1)
+    assert fsim_op == cirq.PhasedFSimGate(theta=0.01, zeta=0.03, chi=0.04, gamma=0.05, phi=0.02).on(
+        q0, q1
+    )
 
     # Thermal noise
     assert len(noisy_circuit.moments[3].operations) == 2
