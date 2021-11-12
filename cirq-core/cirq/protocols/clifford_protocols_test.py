2ad141fe1028299fdabc05f906e88bf9416affcd
Dax Fohl <dax.fohl@gmail.com>
2021-11-12 11:00:18 -0800

    tests

---
+++ cirq-core/cirq/protocols/clifford_protocols_test.py
@@ -0,0 +1,81 @@
+# Copyright 2020 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Any, Tuple, Union, Sequence
+
+import numpy as np
+import pytest
+
+import cirq
+from cirq.ops.raw_types import TSelf
+
+
+class CountingGate(cirq.SingleQubitGate):
+    def __init__(self, implemented: bool = True):
+        self._implemented = implemented
+
+    def _apply_to_tableau_(self, tableau: cirq.CliffordTableau, axes, prng):
+        if self._implemented:
+            tableau.n += sum(axes)
+            return True
+        return NotImplemented
+
+    def _apply_to_ch_form_(self, state: cirq.StabilizerStateChForm, axes, prng):
+        if self._implemented:
+            state.n += sum(axes)
+            return True
+        return NotImplemented
+
+
+def test_apply_to_tableau_succeeds():
+    gate = CountingGate()
+    tableau = cirq.CliffordTableau(1)
+    result = cirq.apply_to_tableau(gate, tableau, [2, 3], np.random.RandomState())
+    assert result is True
+    assert tableau.n == 6
+
+
+def test_apply_to_ch_form_succeeds():
+    gate = CountingGate()
+    state = cirq.StabilizerStateChForm(1)
+    result = cirq.apply_to_ch_form(gate, state, [2, 3], np.random.RandomState())
+    assert result is True
+    assert state.n == 6
+
+
+def test_apply_to_tableau_not_implemented_explicitly():
+    gate = CountingGate(implemented=False)
+    tableau = cirq.CliffordTableau(1)
+    result = cirq.apply_to_tableau(gate, tableau, [2, 3], np.random.RandomState())
+    assert result is NotImplemented
+
+
+def test_apply_to_ch_form_not_implemented_explicitly():
+    gate = CountingGate(implemented=False)
+    state = cirq.StabilizerStateChForm(1)
+    result = cirq.apply_to_ch_form(gate, state, [2, 3], np.random.RandomState())
+    assert result is NotImplemented
+
+
+def test_apply_to_tableau_not_implemented_implicitly():
+    gate = cirq.SingleQubitGate()
+    tableau = cirq.CliffordTableau(1)
+    result = cirq.apply_to_tableau(gate, tableau, [2, 3], np.random.RandomState())
+    assert result is NotImplemented
+
+
+def test_apply_to_ch_form_not_implemented_implicitly():
+    gate = cirq.SingleQubitGate()
+    state = cirq.StabilizerStateChForm(1)
+    result = cirq.apply_to_ch_form(gate, state, [2, 3], np.random.RandomState())
+    assert result is NotImplemented
