dfba48d8af6e49b5d21d1d478d4f49de5e3821e0
Dave Bacon <dabacon@gmail.com>
2021-11-18 19:00:03 +0000

    Clarify runtime of gateset contains

--- cirq-core/cirq/ops/gateset.py
+++ cirq-core/cirq/ops/gateset.py
@@ -289,12 +289,12 @@ class Gateset:
                     and tagged variants (i.e. instances of `cirq.TaggedOperation`,
                     `cirq.ControlledOperation` where `op.gate` is None) etc.
 
-        The complexity of the method is:
+        The complexity of the method in terms of the number of `gates`, n, is
             a) O(1) when any default `cirq.GateFamily` instance accepts the given item, except
                 for an Instance GateFamily trying to match an item with a different global phase.
             b) O(n) for all other cases: matching against custom gate families, matching across
-                global phase for the default Instance GateFamily, no match against any underlying
-                gate family.
+                global phase for the default Instance GateFamily, and no match against any
+                underlying gate family.
 
         Args:
             item: The `cirq.Gate` or `cirq.Operation` instance to check containment for.
