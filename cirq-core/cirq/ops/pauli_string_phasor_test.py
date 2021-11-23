90273150ccfd723cea36bf700c742c317cec54f9
daxfohl <dax.fohl@gmail.com>
2021-11-22 19:02:04 -0800

    format

--- cirq-core/cirq/ops/pauli_string_phasor_test.py
+++ cirq-core/cirq/ops/pauli_string_phasor_test.py
@@ -572,7 +572,10 @@ def test_gate_str():
     )
     assert str(gate) == 'exp(iπ0.5*+ZYX)'
 
-    gate = cirq.PauliStringPhasorGate(
-        cirq.DensePauliString('ZYX'), exponent_pos=0.5, exponent_neg=-0.5
-    ) ** -0.5
+    gate = (
+        cirq.PauliStringPhasorGate(
+            cirq.DensePauliString('ZYX'), exponent_pos=0.5, exponent_neg=-0.5
+        )
+        ** -0.5
+    )
     assert str(gate) == 'exp(-iπ0.25*+ZYX)'
