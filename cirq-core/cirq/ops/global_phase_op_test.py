274671c9ace71613c3e1351020f6387deb1eeabf
daxfohl <dax.fohl@gmail.com>
2021-11-23 11:24:38 -0800

    format

--- cirq-core/cirq/ops/global_phase_op_test.py
+++ cirq-core/cirq/ops/global_phase_op_test.py
@@ -290,9 +290,7 @@ def test_gate_protocols():
     for p in [1, 1j, -1]:
         cirq.testing.assert_implements_consistent_protocols(cirq.GlobalPhaseGate(p))
 
-    np.testing.assert_allclose(
-        cirq.unitary(cirq.GlobalPhaseGate(1j)), np.array([[1j]]), atol=1e-8
-    )
+    np.testing.assert_allclose(cirq.unitary(cirq.GlobalPhaseGate(1j)), np.array([[1j]]), atol=1e-8)
 
 
 @pytest.mark.parametrize('phase', [1, 1j, -1])
