ea0e322af897859d291e7741e19908882b0873b3
Tanuj Khattar <tanujkhattar@google.com>
2021-11-30 12:17:51 +0400

    Add complexity tests for merge_operations

--- cirq-core/cirq/optimizers/transformer_primitives_test.py
+++ cirq-core/cirq/optimizers/transformer_primitives_test.py
@@ -323,3 +323,24 @@ def test_merge_operations_merges_connected_component():
           │                           │
 2: ───H───X───────────────────────────X───────────────''',
     )
+
+
+@pytest.mark.parametrize("op_density", [0.1, 0.5, 0.9])
+def test_merge_operations_complexity(op_density):
+    prng = cirq.value.parse_random_state(11011)
+    circuit = cirq.testing.random_circuit(20, 500, op_density, random_state=prng)
+    for merge_func in [
+        lambda _, __: None,
+        lambda op1, _: op1,
+        lambda _, op2: op2,
+        lambda op1, op2: prng.choice([op1, op2, None]),
+    ]:
+
+        def wrapped_merge_func(op1, op2):
+            wrapped_merge_func.num_function_calls += 1
+            return merge_func(op1, op2)
+
+        wrapped_merge_func.num_function_calls = 0
+        _ = cirq.merge_operations(circuit, wrapped_merge_func)
+        total_operations = len([*circuit.all_operations()])
+        assert wrapped_merge_func.num_function_calls <= 2 * total_operations
