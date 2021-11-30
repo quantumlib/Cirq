5fd978b25edc60d455888b9b88435ae7abe59830
daxfohl <dax.fohl@gmail.com>
2021-11-30 13:12:42 -0800

    coverage

--- cirq-core/cirq/ops/pauli_string_phasor_test.py
+++ cirq-core/cirq/ops/pauli_string_phasor_test.py
@@ -423,6 +423,32 @@ def test_gate_init():
     assert v2.exponent_pos == -0.125
 
 
+def test_gate_on():
+    q = cirq.LineQubit(0)
+    g1 = cirq.PauliStringPhasorGate(
+        cirq.DensePauliString('X', coefficient=-1), exponent_neg=0.25, exponent_pos=-0.5
+    )
+
+    op1 = g1.on(q)
+    assert isinstance(op1, cirq.PauliStringPhasor)
+    assert op1.qubits == (q,)
+    assert op1.gate == g1
+    assert op1.pauli_string == cirq.DensePauliString('X').on(q)
+    assert op1.exponent_neg == -0.5
+    assert op1.exponent_pos == 0.25
+
+    g2 = cirq.PauliStringPhasorGate(
+        cirq.DensePauliString('X'), exponent_neg=0.75, exponent_pos=-0.125
+    )
+    op2 = g2.on(q)
+    assert isinstance(op2, cirq.PauliStringPhasor)
+    assert op2.qubits == (q,)
+    assert op2.gate == g2
+    assert op2.pauli_string == cirq.DensePauliString('X').on(q)
+    assert op2.exponent_neg == 0.75
+    assert op2.exponent_pos == -0.125
+
+
 def test_gate_eq_ne_hash():
     q0, q1, q2 = _make_qubits(3)
     eq = cirq.testing.EqualsTester()
