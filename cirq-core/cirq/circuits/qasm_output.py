c33bd9bd6d08650f41b0db5cf69abb3daed72a8f
Jintao YU <yjt98765@gmail.com>
2021-12-16 23:12:38 +0800

    Add JSON serialization support for QasmUGate (#4750)
    This PR provides the serialization support for the `QasmUGate` class following [serialization.md](https://github.com/quantumlib/Cirq/blob/master/docs/dev/serialization.md). Without this support, `QasmUGate` cannot be properly serialized and deserialized, which causes issue #4728.
    
    The test of serialization ability is done through the .repr and .json files under json_test_data folder. These files are also added for `QasmUGate`.
    
    close #4728

--- cirq-core/cirq/circuits/qasm_output.py
+++ cirq-core/cirq/circuits/qasm_output.py
@@ -83,6 +83,17 @@ class QasmUGate(ops.SingleQubitGate):
     def _value_equality_values_(self):
         return self.lmda, self.theta, self.phi
 
+    def _json_dict_(self) -> Dict[str, float]:
+        return {
+            'theta': self.theta,
+            'phi': self.phi,
+            'lmda': self.lmda,
+        }
+
+    @classmethod
+    def _from_json_dict_(cls, theta: float, phi: float, lmda: float, **kwargs) -> 'QasmUGate':
+        return cls(theta, phi, lmda)
+
 
 @value.value_equality
 class QasmTwoQubitGate(ops.Gate):
