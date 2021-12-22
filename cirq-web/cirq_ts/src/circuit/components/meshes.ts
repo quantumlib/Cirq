4370a48e8231d23fc499e74508e55a96177b74fe
Jintao Yu <yjt98765@gmail.com>
2021-12-22 18:57:19 +0800

    Lint

--- cirq-web/cirq_ts/src/circuit/components/meshes.ts
+++ cirq-web/cirq_ts/src/circuit/components/meshes.ts
@@ -239,7 +239,7 @@ export class BoxGate3DSymbol extends Mesh {
       textWidth = context.measureText(label).width;
     } while (textWidth > canvas.width);
 
-    const hsl = new Color(color).getHSL({h: 0, s: 0, l: 0})
+    const hsl = new Color(color).getHSL({h: 0, s: 0, l: 0});
     context.fillStyle = hsl.l < 0.5 ? 'white' : 'black';
     context.fillText(
       label,
