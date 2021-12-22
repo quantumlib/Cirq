8c04286794cbd125d310faf44f281941bd8db701
daxfohl <dax.fohl@gmail.com>
2021-12-22 12:37:18 -0800

    add a test for sympy scope simulation result

--- cirq-core/cirq/ops/classically_controlled_operation_test.py
+++ cirq-core/cirq/ops/classically_controlled_operation_test.py
@@ -779,3 +779,37 @@ d: ═════════════════════════�
         use_unicode_characters=True,
     )
     # pylint: enable=line-too-long
+
+
+def test_sympy_scope_simulation():
+    q0, q1, q2, q3, q_ignored, q_result = cirq.LineQubit.range(6)
+    condition = sympy_parser.parse_expr('a & b | c & d')
+    # We set up condition (a & b | c & d) plus an ignored measurement key, and run through the
+    # combinations of possible values of those (by doing X(q_i)**bits[i] on each), then verify
+    # that the final measurement into m_result is True iff that condition was met.
+    for i in range(32):
+        bits = cirq.big_endian_int_to_bits(i, bit_count=5)
+        inner = cirq.Circuit(
+            cirq.X(q0) ** bits[0],
+            cirq.measure(q0, key='a'),
+            cirq.X(q_result).with_classical_controls(condition),
+            cirq.measure(q_result, key='m_result'),
+        )
+        middle = cirq.Circuit(
+            cirq.X(q1) ** bits[1],
+            cirq.measure(q1, key='b'),
+            cirq.X(q_ignored) ** bits[4],
+            cirq.measure(q_ignored, key=cirq.MeasurementKey('c', ('0',))),
+            cirq.CircuitOperation(inner.freeze(), repetition_ids=['0']),
+        )
+        circuit = cirq.Circuit(
+            cirq.X(q2) ** bits[2],
+            cirq.measure(q2, key='c'),
+            cirq.X(q3) ** bits[3],
+            cirq.measure(q3, key='d'),
+            cirq.CircuitOperation(middle.freeze(), repetition_ids=['0']),
+        )
+        result = cirq.CliffordSimulator().run(circuit)
+        assert result.measurements['0:0:m_result'][0][0] == (
+            bits[0] and bits[1] or bits[2] and bits[3]  # bits[4] irrelevant
+        )
