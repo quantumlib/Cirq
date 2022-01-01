f72a9d1d6ade3ae1390a0bbe1489be64e8d54b4f
daxfohl <dax.fohl@gmail.com>
2022-01-01 09:24:02 -0800

    lint

--- cirq-core/cirq/ops/dimension_adapter_gate.py
+++ cirq-core/cirq/ops/dimension_adapter_gate.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import AbstractSet, Any, Dict, List, Optional, Sequence, Tuple, TYPE_CHECKING, Union
+from typing import AbstractSet, Any, Dict, Optional, Sequence, Tuple, TYPE_CHECKING, Union
 
 import numpy as np
 
