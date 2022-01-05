943ec6d17cba30601cc7ff6da77642ec8e05b318
Anna Nachesa <ashalynd@google.com>
2022-01-05 18:18:41 +0100

    formatting

--- cirq-google/cirq_google/optimizers/two_qubit_gates/gate_compilation.py
+++ cirq-google/cirq_google/optimizers/two_qubit_gates/gate_compilation.py
@@ -3,44 +3,29 @@
 with a product A k A.
 
 Attention: this module is now deprecated! Use the classes from cirq-core instead."""
-from typing import Tuple, NamedTuple
+from typing import Tuple
 
 from dataclasses import dataclass
 import numpy as np
 
 import cirq
-from cirq import TwoQubitGateTabulation, TwoQubitGateTabulationResult, two_qubit_gate_product_tabulation
+from cirq import (
+    TwoQubitGateTabulation,
+    TwoQubitGateTabulationResult,
+    two_qubit_gate_product_tabulation,
+)
 from cirq._compat import deprecated, deprecated_class
 
 _SingleQubitGatePair = Tuple[np.ndarray, np.ndarray]
 
 
+@deprecated_class(
+    deadline='v0.16',
+    fix='Use cirq.TwoQubitGateTabulationResult instead.',
+    name='TwoQubitGateCompilation',
+)
 class TwoQubitGateCompilation(TwoQubitGateTabulationResult):
-    r"""Represents a compilation of a target 2-qubit with respect to a base
-    gate.
-
-    This object encodes the relationship between 4x4 unitary operators
-
-    U_target ~ k_N · U_base · k_{N-1} · ... · k_1 · U_base · k_0
-
-    where U_target, U_base are 2-local and k_j are 1-local.
-
-    Attributes:
-        base_gate: 4x4 unitary denoting U_base above.
-        target_gate: 4x4 unitary denoting U_target above.
-        local_unitaries: Sequence of 2-tuples
-            $(k_{00}, k_{01}), (k_{10}, k_{11}) \ldots$ where
-            $k_j = k_{j0} \otimes k_{j1}$ in the product above.
-            Each $k_{j0}, k_{j1}$ is a 2x2 unitary.
-        actual_gate: 4x4 unitary denoting the right hand side above, ideally
-            equal to U_target.
-        success: Whether actual_gate is expected to be close to U_target.
-    """
-    base_gate_unitary: np.ndarray
-    target_gate: np.ndarray
-    local_unitaries: Tuple[_SingleQubitGatePair, ...]
-    actual_gate: np.ndarray
-    success: bool
+    pass
 
 
 @dataclass
