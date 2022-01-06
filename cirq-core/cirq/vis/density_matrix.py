Fixes #4804: The plotting position computation error was fixed
The plotting positions were dividing by cell size instead of multiplying, fixed that.

cirq-core/cirq/vis/density_matrix.py
@@ -41,18 +41,21 @@ def _plot_element_of_density_matrix(ax, x, y, r, phase, show_rect=False, show_te
     _image_opacity = 0.8 if not show_text else 0.4
 
     circle_out = plt.Circle(
-        (x + 0.5, y + 0.5), radius=1 / _half_cell_size_after_padding, fill=False, color='#333333'
+        (x + 0.5, y + 0.5),
+        radius=1 * _half_cell_size_after_padding,
+        fill=False,
+        color='#333333'
     )
     circle_in = plt.Circle(
         (x + 0.5, y + 0.5),
-        radius=r / _half_cell_size_after_padding,
+        radius=r * _half_cell_size_after_padding,
         fill=True,
         color='IndianRed',
         alpha=_image_opacity,
     )
     line = lines.Line2D(
-        (x + 0.5, x + 0.5 + np.cos(phase) / _half_cell_size_after_padding),
-        (y + 0.5, y + 0.5 + np.sin(phase) / _half_cell_size_after_padding),
+        (x + 0.5, x + 0.5 + np.cos(phase) * _half_cell_size_after_padding),
+        (y + 0.5, y + 0.5 + np.sin(phase) * _half_cell_size_after_padding),
         color='#333333',
         alpha=_image_opacity,
     )