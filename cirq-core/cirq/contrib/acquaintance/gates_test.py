ccdd9e024ca526ee63100c48854a8f154bce63fe
Michael Broughton <mbbrough@google.com>
2022-01-10 23:52:36 -0800

    numpy.

--- cirq-core/cirq/contrib/acquaintance/gates_test.py
+++ cirq-core/cirq/contrib/acquaintance/gates_test.py
@@ -232,7 +232,6 @@ def test_swap_network_init_error():
         cca.SwapNetworkGate((3,))
 
 
-rng = np.random.default_rng()
 part_lens_and_acquaintance_sizes = [
     [[l + 1 for l in numpy.random.poisson(size=n_parts, lam=lam)], numpy.random.poisson(4)]
     for n_parts, lam in product(range(2, 20, 3), range(1, 4))
