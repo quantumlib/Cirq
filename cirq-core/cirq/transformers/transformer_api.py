f627e6689a927b13c703379cb3218f9a98fc9d61
Tanuj Khattar <tanujkhattar@google.com>
2022-01-13 05:07:16 +0530

    Add implementation of TransformerStatsLogger

--- cirq-core/cirq/transformers/transformer_api.py
+++ cirq-core/cirq/transformers/transformer_api.py
@@ -14,14 +14,14 @@
 
 """Defines the API for circuit transformers in Cirq."""
 
-import abc
+import textwrap
 import functools
 from typing import (
     Callable,
     Tuple,
     TypeVar,
     Hashable,
-    Optional,
+    List,
     cast,
     Type,
     overload,
@@ -40,17 +40,29 @@ class LogLevel(enum.Enum):
     """Different logging resolution options for `TransformerStatsLogger`.
 
     Args:
-        ALL:     All levels.
-        WARNING: Designates unwanted or potentially harmful situations.
-        INFO:    Designates informational messages that highlight the actions of a transformer.
-        VERBOSE: Designates fine-grained informational events that are most useful to debug /
+        ALL:     All levels. Used to filter logs when printing.
+        DEBUG:   Designates fine-grained informational events that are most useful to debug /
                  understand in-depth any unexpected behavior of the transformer.
+        INFO:    Designates informational messages that highlight the actions of a transformer.
+        WARNING: Designates unwanted or potentially harmful situations.
+        NONE:    No levels. Used to filter logs when printing.
     """
 
     ALL = 0
-    VERBOSE = 1
-    INFO = 2
-    WARNING = 3
+    DEBUG = 10
+    INFO = 20
+    WARNING = 30
+    NONE = 40
+
+
+@dataclasses.dataclass
+class LoggerNode:
+    transformer_id: int
+    transformer_name: str
+    initial_circuit: 'cirq.AbstractCircuit'
+    final_circuit: 'cirq.AbstractCircuit'
+    logs: Tuple[Tuple[LogLevel, Tuple[str, ...]], ...] = ()
+    nested_loggers: Tuple[int, ...] = ()
 
 
 class TransformerStatsLogger:
@@ -74,32 +86,98 @@ class TransformerStatsLogger:
     The `LogLevel`s can be used to control the input processing and output resolution of the logs.
     """
 
-    @abc.abstractmethod
-    def register_initial(self, circuit: CIRCUIT_TYPE, transformer_name: str) -> None:
+    def __init__(self):
+        """Initializes TransformerStatsLogger."""
+        self._curr_id: int = 0
+        self._logs: List[LoggerNode] = []
+        self._stack: List[int] = []
+
+    def register_initial(self, circuit: 'cirq.AbstractCircuit', transformer_name: str) -> None:
         """Register the beginning of a new transformer stage.
 
         Args:
             circuit: Input circuit to the new transformer stage.
             transformer_name: Name of the new transformer stage.
         """
+        if self._stack:
+            self._logs[self._stack[-1]].nested_loggers += (self._curr_id,)
+        self._logs.append(LoggerNode(self._curr_id, transformer_name, circuit, circuit))
+        self._stack.append(self._curr_id)
+        self._curr_id += 1
 
-    @abc.abstractmethod
     def log(self, *args: str, level: LogLevel = LogLevel.INFO) -> None:
         """Log additional metadata corresponding to the currently active transformer stage.
 
         Args:
             *args: The additional metadata to log.
             level: Logging level to control the amount of metadata that gets put into the context.
-        """
 
-    @abc.abstractmethod
+        Raises:
+            ValueError: If there's no active transformer on the stack.
+        """
+        if len(self._stack) == 0:
+            raise ValueError('No active transformer found.')
+        self._logs[self._stack[-1]].logs += ((level, args),)
+
     def register_final(self, circuit: CIRCUIT_TYPE, transformer_name: str) -> None:
         """Register the end of the currently active transformer stage.
 
         Args:
             circuit: Final transformed output circuit from the transformer stage.
             transformer_name: Name of the (currently active) transformer stage which ends.
+
+        Raises:
+            ValueError: If `transformer_name` is different from currently active transformer name.
         """
+        tid = self._stack.pop()
+        if self._logs[tid].transformer_name != transformer_name:
+            raise ValueError(
+                f"Expected `register_final` call for currently active transformer "
+                f"{self._logs[tid].transformer_name}."
+            )
+        self._logs[tid].final_circuit = circuit
+
+    def show(self, level: LogLevel = LogLevel.INFO) -> None:
+        """Show the stored logs >= level in the desired format."""
+
+        def print_log(log: LoggerNode, pad=''):
+            print(pad, f"Transformer-{1+log.transformer_id}: {log.transformer_name}", sep='')
+            print(pad, "Initial Circuit:", sep='')
+            print(textwrap.indent(str(log.initial_circuit), pad), "\n", sep='')
+            for log_level, log_text in log.logs:
+                if log_level.value >= level.value:
+                    print(pad, log_level, *log_text)
+            print("\n", pad, "Final Circuit:", sep='')
+            print(textwrap.indent(str(log.final_circuit), pad))
+            print("----------------------------------------")
+
+        done = [0] * self._curr_id
+
+        def dfs(log: LoggerNode, pad=''):
+            print_log(log, pad)
+            done[log.transformer_id] = True
+            for child_id in log.nested_loggers:
+                dfs(self._logs[child_id], pad + ' ' * 4)
+
+        for i in range(self._curr_id):
+            if not done[i]:
+                dfs(self._logs[i])
+
+
+class NoOpTransformerStatsLogger(TransformerStatsLogger):
+    """All calls to this logger are a no-op"""
+
+    def register_initial(self, circuit: 'cirq.AbstractCircuit', transformer_name: str) -> None:
+        pass
+
+    def log(self, *args: str, level: LogLevel = LogLevel.INFO) -> None:
+        pass
+
+    def register_final(self, circuit: CIRCUIT_TYPE, transformer_name: str) -> None:
+        pass
+
+    def show(self, level: LogLevel = LogLevel.INFO) -> None:
+        pass
 
 
 @dataclasses.dataclass()
@@ -115,7 +193,7 @@ class TransformerContext:
                 `cirq.VirtualTag` etc.) is a valid tag.
     """
 
-    logger: Optional[TransformerStatsLogger] = None
+    logger: TransformerStatsLogger = NoOpTransformerStatsLogger()
     ignore_tags: Tuple[Hashable, ...] = ()
 
 
@@ -140,11 +218,9 @@ def _transform_and_log(
 ) -> CIRCUIT_TYPE:
     """Helper to log initial and final circuits before and after calling the transformer."""
 
-    if context.logger is not None:
-        context.logger.register_initial(circuit, transformer_name)
+    context.logger.register_initial(circuit, transformer_name)
     circuit = func(circuit, context)
-    if context.logger is not None:
-        context.logger.register_final(circuit, transformer_name)
+    context.logger.register_final(circuit, transformer_name)
     return circuit
 
 
--- cirq-core/cirq/transformers/transformer_api_test.py
+++ cirq-core/cirq/transformers/transformer_api_test.py
@@ -15,6 +15,7 @@ from unittest import mock
 
 import cirq
 from cirq.circuits.circuit import CIRCUIT_TYPE
+from cirq.transformers import LogLevel
 
 import pytest
 
@@ -103,9 +104,172 @@ def test_transformer_decorator(context, transformer):
     circuit = cirq.Circuit(cirq.X(cirq.NamedQubit("a")))
     transformer(circuit, context)
     transformer.mock.assert_called_with(circuit, context)
-    if context.logger is not None:
+    if not isinstance(context.logger, cirq.TransformerStatsLogger):
         transformer_name = (
             transformer.__name__ if hasattr(transformer, '__name__') else type(transformer).__name__
         )
         context.logger.register_initial.assert_called_with(circuit, transformer_name)
         context.logger.register_final.assert_called_with(circuit, transformer_name)
+
+
+@cirq.transformer
+class T1:
+    def __call__(self, circuit: cirq.Circuit, context: cirq.TransformerContext) -> cirq.Circuit:
+        context.logger.log("First Verbose Log", "of T1", level=LogLevel.DEBUG)
+        context.logger.log("Second INFO Log", "of T1", level=LogLevel.INFO)
+        context.logger.log("Third WARNING Log", "of T1", level=LogLevel.WARNING)
+        return circuit + circuit[::-1]
+
+
+t1 = T1()
+
+
+@cirq.transformer
+def t2(circuit: cirq.Circuit, context: cirq.TransformerContext) -> cirq.Circuit:
+    context.logger.log("First INFO Log", "of T2 Start")
+    circuit = t1(circuit, context)
+    context.logger.log("Second INFO Log", "of T2 End")
+    return circuit[::2]
+
+
+@cirq.transformer
+def t3(circuit: cirq.Circuit, context: cirq.TransformerContext) -> cirq.Circuit:
+    context.logger.log("First INFO Log", "of T3 Start")
+    circuit = t1(circuit, context)
+    context.logger.log("Second INFO Log", "of T3 Middle")
+    circuit = t2(circuit, context)
+    context.logger.log("Third INFO Log", "of T3 End")
+    return circuit
+
+
+def test_transformer_stats_logger_raises():
+    with pytest.raises(ValueError, match='No active transformer'):
+        logger = cirq.TransformerStatsLogger()
+        logger.log('test log')
+
+    with pytest.raises(ValueError, match='No active transformer'):
+        logger = cirq.TransformerStatsLogger()
+        logger.register_initial(cirq.Circuit(), 'stage-1')
+        logger.register_final(cirq.Circuit(), 'stage-1')
+        logger.log('test log')
+
+    with pytest.raises(ValueError, match='currently active transformer stage-2'):
+        logger = cirq.TransformerStatsLogger()
+        logger.register_initial(cirq.Circuit(), 'stage-2')
+        logger.register_final(cirq.Circuit(), 'stage-3')
+
+
+def test_transformer_stats_logger_show_levels(capfd):
+    q = cirq.LineQubit.range(2)
+    context = cirq.TransformerContext(logger=cirq.TransformerStatsLogger())
+    initial_circuit = cirq.Circuit(cirq.H.on_each(*q), cirq.CNOT(*q))
+    _ = t1(initial_circuit, context)
+    info_line = 'LogLevel.INFO Second INFO Log of T1'
+    debug_line = 'LogLevel.DEBUG First Verbose Log of T1'
+    warning_line = 'LogLevel.WARNING Third WARNING Log of T1'
+
+    for level in [LogLevel.ALL, LogLevel.DEBUG]:
+        context.logger.show(level)
+        out, _ = capfd.readouterr()
+        assert all(line in out for line in [info_line, debug_line, warning_line])
+
+    context.logger.show(LogLevel.INFO)
+    out, _ = capfd.readouterr()
+    assert info_line in out and warning_line in out and debug_line not in out
+
+    context.logger.show(LogLevel.DEBUG)
+    out, _ = capfd.readouterr()
+    assert info_line in out and warning_line in out and debug_line in out
+
+    context.logger.show(LogLevel.NONE)
+    out, _ = capfd.readouterr()
+    assert all(line not in out for line in [info_line, debug_line, warning_line])
+
+
+def test_transformer_stats_logger_linear_and_nested(capfd):
+    q = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit(cirq.H.on_each(*q), cirq.CNOT(*q))
+    context = cirq.TransformerContext(logger=cirq.TransformerStatsLogger())
+    circuit = t1(circuit, context)
+    circuit = t3(circuit, context)
+    context.logger.show(LogLevel.ALL)
+    out, _ = capfd.readouterr()
+    assert (
+        out.strip()
+        == '''
+Transformer-1: T1
+Initial Circuit:
+0: ───H───@───
+          │
+1: ───H───X───
+
+ LogLevel.DEBUG First Verbose Log of T1
+ LogLevel.INFO Second INFO Log of T1
+ LogLevel.WARNING Third WARNING Log of T1
+
+Final Circuit:
+0: ───H───@───@───H───
+          │   │
+1: ───H───X───X───H───
+----------------------------------------
+Transformer-2: t3
+Initial Circuit:
+0: ───H───@───@───H───
+          │   │
+1: ───H───X───X───H───
+
+ LogLevel.INFO First INFO Log of T3 Start
+ LogLevel.INFO Second INFO Log of T3 Middle
+ LogLevel.INFO Third INFO Log of T3 End
+
+Final Circuit:
+0: ───H───@───H───@───H───@───H───@───
+          │       │       │       │
+1: ───H───X───H───X───H───X───H───X───
+----------------------------------------
+    Transformer-3: T1
+    Initial Circuit:
+    0: ───H───@───@───H───
+              │   │
+    1: ───H───X───X───H───
+
+     LogLevel.DEBUG First Verbose Log of T1
+     LogLevel.INFO Second INFO Log of T1
+     LogLevel.WARNING Third WARNING Log of T1
+
+    Final Circuit:
+    0: ───H───@───@───H───H───@───@───H───
+              │   │           │   │
+    1: ───H───X───X───H───H───X───X───H───
+----------------------------------------
+    Transformer-4: t2
+    Initial Circuit:
+    0: ───H───@───@───H───H───@───@───H───
+              │   │           │   │
+    1: ───H───X───X───H───H───X───X───H───
+
+     LogLevel.INFO First INFO Log of T2 Start
+     LogLevel.INFO Second INFO Log of T2 End
+
+    Final Circuit:
+    0: ───H───@───H───@───H───@───H───@───
+              │       │       │       │
+    1: ───H───X───H───X───H───X───H───X───
+----------------------------------------
+        Transformer-5: T1
+        Initial Circuit:
+        0: ───H───@───@───H───H───@───@───H───
+                  │   │           │   │
+        1: ───H───X───X───H───H───X───X───H───
+
+         LogLevel.DEBUG First Verbose Log of T1
+         LogLevel.INFO Second INFO Log of T1
+         LogLevel.WARNING Third WARNING Log of T1
+
+        Final Circuit:
+        0: ───H───@───@───H───H───@───@───H───H───@───@───H───H───@───@───H───
+                  │   │           │   │           │   │           │   │
+        1: ───H───X───X───H───H───X───X───H───H───X───X───H───H───X───X───H───
+----------------------------------------
+'''.strip()
+    )
