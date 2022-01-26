fd04a4a467a4def7189ee2309f3533a5ecef4cc8
Michael Broughton <mbbrough@google.com>
2022-01-26 14:31:00 -0800

    dep in spec.

--- cirq-google/cirq_google/engine/virtual_engine_factory.py
+++ cirq-google/cirq_google/engine/virtual_engine_factory.py
@@ -72,10 +72,10 @@ def _create_perfect_calibration(device: cirq.Device) -> calibration.Calibration:
         all_metrics[name] = {(q,): [0.0] for q in qubits}
     for name in METRICS_2Q:
         metric_dict: calibration.METRIC_DICT = {}
-        pairs = device.qid_pairs()
+        pairs = device.metadata.nx_graph.edges()
         if pairs is not None:
             for pair in pairs:
-                qubits = [cast(cirq.GridQubit, q) for q in pair.qids]
+                qubits = [cast(cirq.GridQubit, q) for q in pair]
                 metric_dict[(qubits[0], qubits[1])] = [0.0]
         all_metrics[name] = metric_dict
     all_metrics[T1_METRIC_NAME] = {(q,): [PERFECT_T1_VALUE] for q in qubits}
