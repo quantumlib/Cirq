16288decd94e83f1a6b6294b022010a04df897bd
daxfohl <dax.fohl@gmail.com>
2022-01-28 12:54:29 -0800

    move align tests

--- cirq-core/cirq/optimizers/align_left_test.py
+++ cirq-core/cirq/optimizers/align_left_test.py
@@ -45,11 +45,3 @@ def test_align_left():
             ]
         ),
     )
-
-
-def test_classical_control():
-    q0, q1 = cirq.LineQubit.range(2)
-    circuit = cirq.Circuit(
-        cirq.H(q0), cirq.measure(q0, key='m'), cirq.X(q1).with_classical_controls('m')
-    )
-    assert_optimizes(before=circuit, after=circuit.copy())
