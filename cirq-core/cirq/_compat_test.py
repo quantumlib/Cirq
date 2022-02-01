f9482e5871ba0d9ce0f35cd1779b697439e3b14b
Dave Bacon <dabacon@gmail.com>
2022-02-01 15:35:35 -0800

    Fix _compat tests

--- cirq-core/cirq/_compat_test.py
+++ cirq-core/cirq/_compat_test.py
@@ -807,15 +807,15 @@ def _test_broken_module_3_inner():
 
 
 def test_deprecated_module_error_handling_1():
-    subprocess_context(_test_broken_module_1_inner())
+    subprocess_context(_test_broken_module_1_inner)
 
 
 def test_deprecated_module_error_handling_2():
-    subprocess_context(_test_broken_module_2_inner())
+    subprocess_context(_test_broken_module_2_inner)
 
 
 def test_deprecated_module_error_handling_3():
-    subprocess_context(_test_broken_module_3_inner())
+    subprocess_context(_test_broken_module_3_inner)
 
 
 def test_new_module_is_top_level():
