2f2c7ca766551e733a4b13483ea299b8abdab78c
Jintao YU <yjt98765@gmail.com>
2022-02-03 00:17:40 +0800

    Add state conversion logic from _create_partial_act_on_args to the corresponding ActOnArgs (#4847)
    * Move _create_partial_act_on_args to ActOnArgs
    
    * Add with_qubit to ActOnArgs
    
    * Fix CI errors
    
    * Merge initial_state into target_tenson
    
    Also change some import directives.
    
    * Change back a variable name
    
    * Fix coverage error
    
    * Move dtype to the last parameter
    
    * Add state_vector_kronecker_product to __init__.py
    
    * Remove logic from density_matrix_simulator._create_partial_act_on
    
    * Remove logic from sparse_simulator._create_partial_act_on_args
    
    * Remove logic from clifford_simulator._create_partial_act_on_args
    
    Also deprecate positional argument in ActOnStabilizerCHFormArgs.__init__.
    
    * Deprecate positional arguments in ActOnArgs subclasses
    
    * Fix format
    
    * Merge state into initial_state and deprecate state
    
    * Fix format
    
    * Add test cases for positional arguments

--- cirq-core/cirq/sim/clifford/act_on_stabilizer_ch_form_args.py
+++ cirq-core/cirq/sim/clifford/act_on_stabilizer_ch_form_args.py
@@ -12,13 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, TYPE_CHECKING, List, Sequence
+from typing import Any, Dict, TYPE_CHECKING, List, Optional, Sequence, Union
 
 import numpy as np
 
-from cirq import value, ops, protocols
+from cirq import _compat, value, ops, protocols
 from cirq.ops import common_gates, pauli_gates
 from cirq.ops import global_phase_op
+from cirq.sim.clifford import clifford_simulator
 from cirq.sim.clifford.act_on_stabilizer_args import ActOnStabilizerArgs
 
 if TYPE_CHECKING:
@@ -28,12 +29,19 @@ if TYPE_CHECKING:
 class ActOnStabilizerCHFormArgs(ActOnStabilizerArgs):
     """Wrapper around a stabilizer state in CH form for the act_on protocol."""
 
+    @_compat.deprecated_parameter(
+        deadline='v0.15',
+        fix='Specify all the arguments with keywords, use initial_state instead of state.',
+        parameter_desc='positional arguments',
+        match=lambda args, kwargs: len(args) != 1 or 'state' in kwargs,
+    )
     def __init__(
         self,
-        state: 'cirq.StabilizerStateChForm',
-        prng: np.random.RandomState,
-        log_of_measurement_results: Dict[str, Any],
-        qubits: Sequence['cirq.Qid'] = None,
+        state: Optional['cirq.StabilizerStateChForm'] = None,
+        prng: Optional[np.random.RandomState] = None,
+        log_of_measurement_results: Optional[Dict[str, Any]] = None,
+        qubits: Optional[Sequence['cirq.Qid']] = None,
+        initial_state: Union[int, 'cirq.StabilizerStateChForm'] = 0,
     ):
         """Initializes with the given state and the axes for the operation.
 
@@ -47,9 +55,18 @@ class ActOnStabilizerCHFormArgs(ActOnStabilizerArgs):
                 effects.
             log_of_measurement_results: A mutable object that measurements are
                 being recorded into.
+            initial_state: The initial state for the simulation. This can be a
+                full CH form passed by reference which will be modified inplace,
+                or a big-endian int in the computational basis.
         """
         super().__init__(prng, qubits, log_of_measurement_results)
-        self.state = state
+        initial_state = state or initial_state
+        if isinstance(initial_state, int):
+            qubit_map = {q: i for i, q in enumerate(self.qubits)}
+            initial_state = clifford_simulator.CliffordState(
+                qubit_map, initial_state=initial_state
+            ).ch_form
+        self.state = initial_state
 
     def _perform_measurement(self, qubits: Sequence['cirq.Qid']) -> List[int]:
         """Returns the measurement from the stabilizer state form."""
@@ -80,7 +97,12 @@ class ActOnStabilizerCHFormArgs(ActOnStabilizerArgs):
         for i in range(repetitions):
             op = ops.measure(*qubits, key=str(i))
             state = self.state.copy()
-            ch_form_args = ActOnStabilizerCHFormArgs(state, prng, measurements, self.qubits)
+            ch_form_args = ActOnStabilizerCHFormArgs(
+                prng=prng,
+                log_of_measurement_results=measurements,
+                qubits=self.qubits,
+                initial_state=state,
+            )
             protocols.act_on(op, ch_form_args)
         return np.array(list(measurements.values()), dtype=bool)
 
