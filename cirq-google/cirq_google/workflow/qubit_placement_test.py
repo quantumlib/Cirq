e31a478ac49d5e00c3440cf1b3aa3853f47bc0de
Michael Broughton <mbbrough@google.com>
2022-02-02 17:11:42 -0800

    mypy type fix.

--- cirq-google/cirq_google/workflow/qubit_placement_test.py
+++ cirq-google/cirq_google/workflow/qubit_placement_test.py
@@ -126,3 +126,22 @@ def test_random_device_placer_small_device():
             shared_rt_info=cg.SharedRuntimeInfo(run_id='1', device=TestDevice()),
             rs=np.random.RandomState(1),
         )
+
+
+def test_device_missing_metadata():
+    class BadDevice(cirq.Device):
+        pass
+
+    topo = cirq.TiltedSquareLattice(3, 3)
+    qubits = sorted(topo.nodes_to_gridqubits().values())
+    circuit = cirq.experiments.random_rotations_between_grid_interaction_layers_circuit(
+        qubits, depth=8, two_qubit_op_factory=lambda a, b, _: cirq.SQRT_ISWAP(a, b)
+    )
+    qp = cg.RandomDevicePlacer()
+    with pytest.raises(ValueError):
+        qp.place_circuit(
+            circuit,
+            problem_topology=topo,
+            shared_rt_info=cg.SharedRuntimeInfo(run_id='1', device=BadDevice()),
+            rs=np.random.RandomState(1),
+        )
