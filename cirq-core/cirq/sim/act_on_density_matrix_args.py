107c8e28c75543d0a617584164def5d46a1bc7c0
daxfohl <dax.fohl@gmail.com>
2022-02-12 23:29:26 -0800

    docs

--- cirq-core/cirq/sim/act_on_density_matrix_args.py
+++ cirq-core/cirq/sim/act_on_density_matrix_args.py
@@ -28,7 +28,21 @@ if TYPE_CHECKING:
 
 
 class _BufferedDensityMatrix:
+    """Contains the density matrix and buffers for efficient state evolution."""
+
     def __init__(self, density_matrix: np.ndarray, buffer: Optional[List[np.ndarray]] = None):
+        """Initializes the object with the inputs.
+
+        This initializer creates the buffer if necessary.
+
+        Args:
+            density_matrix: The density matrix, must be correctly formatted. The data is not
+                checked for validity here due to performance concerns.
+            buffer: Optional, must be length 3 and same shape as the density matrix. If not
+                provided, a buffer will be created automatically.
+        Raises:
+            ValueError: If the array is not the shape of a density matrix.
+        """
         self._density_matrix = density_matrix
         if buffer is None:
             buffer = [np.empty_like(density_matrix) for _ in range(3)]
@@ -49,9 +63,19 @@ class _BufferedDensityMatrix:
         dtype: Optional['DTypeLike'] = None,
         buffer: Optional[List[np.ndarray]] = None,
     ):
+        """Initializes the object with the inputs.
+
+        This initializer creates the buffer if necessary.
+
+        Args:
+            initial_state: The density matrix, must be correctly formatted. The data is not
+                checked for validity here due to performance concerns.
+            qid_shape: The shape of the density matrix, if the initial state is provided as an int.
+            dtype: The dtype of the density matrix, if the initial state is provided as an int.
+            buffer: Optional, must be length 3 and same shape as the density matrix. If not
+                provided, a buffer will be created automatically.
+        """
         if not isinstance(initial_state, np.ndarray):
-            if qid_shape is None:
-                raise ValueError(f'Must define qid_shape if initial_initial state is not ndarray')
             density_matrix = qis.to_valid_density_matrix(
                 initial_state, len(qid_shape), qid_shape=qid_shape, dtype=dtype
             ).reshape(qid_shape * 2)
@@ -65,6 +89,13 @@ class _BufferedDensityMatrix:
         return cls(density_matrix, buffer)
 
     def copy(self, deep_copy_buffers: bool = True) -> '_BufferedDensityMatrix':
+        """Copies the object.
+
+        Args:
+            deep_copy_buffers: True by default, False to reuse the existing buffers.
+        Returns:
+            A copy of the object.
+        """
         density_matrix = self._density_matrix.copy()
         if deep_copy_buffers:
             buffer = [b.copy() for b in self._buffer]
@@ -76,6 +107,13 @@ class _BufferedDensityMatrix:
         )
 
     def kron(self, other: '_BufferedDensityMatrix') -> '_BufferedDensityMatrix':
+        """Creates the Kronecker product with the other density matrix.
+
+        Args:
+            other: The density matrix with which to kron.
+        Returns:
+            The Kronecker product of the two density matrices.
+        """
         density_matrix = transformations.density_matrix_kronecker_product(
             self._density_matrix, other._density_matrix
         )
@@ -84,6 +122,20 @@ class _BufferedDensityMatrix:
     def factor(
         self, axes: Sequence[int], *, validate=True, atol=1e-07
     ) -> Tuple['_BufferedDensityMatrix', '_BufferedDensityMatrix']:
+        """Factors out the desired axes.
+
+        Args:
+            axes: The axes to factor out. Only the left axes should be provided. For example, to
+                extract [C,A] from density matrix of shape [A,B,C,D,A,B,C,D], `axes` should be
+                [2,0], and the return value will be two density matrices ([C,A,C,A], [B,D,B,D]).
+            validate: Perform a validation that the density matrix factors cleanly.
+            atol: The absolute tolerance for the validation.
+            Returns:
+                A tuple with the `(extracted, remainder)` density matrices, where `extracted` means
+                the sub-matrix which corresponds to the axes requested, and with the axes in the
+                requested order, and where `remainder` means the sub-matrix on the remaining axes,
+                in the same order as the original density matrix.
+        """
         extracted_tensor, remainder_tensor = transformations.factor_density_matrix(
             self._density_matrix, axes, validate=validate, atol=atol
         )
@@ -92,13 +144,28 @@ class _BufferedDensityMatrix:
         return extracted, remainder
 
     def reindex(self, axes: Sequence[int]) -> '_BufferedDensityMatrix':
+        """Transposes the axes of a density matrix to a specified order.
+
+        Args:
+            axes: The desired axis order. Only the left axes should be provided. For example, to
+                transpose [A,B,C,A,B,C] to [C,B,A,C,B,A], `axes` should be [2,1,0].
+        Returns:
+            The transposed density matrix.
+        """
         new_tensor = transformations.transpose_density_matrix_to_axis_order(
             self._density_matrix, axes
         )
         return _BufferedDensityMatrix(density_matrix=new_tensor)
 
     def apply_channel(self, action: Any, axes: Sequence[int]) -> bool:
-        """Apply channel to state."""
+        """Apply channel to state.
+
+        Args:
+            action: The value with a channel to apply.
+            axes: The axes on which to apply the channel.
+        Returns:
+            True if the action succeeded.
+        """
         result = protocols.apply_channel(
             action,
             args=protocols.ApplyChannelArgs(
@@ -122,7 +189,14 @@ class _BufferedDensityMatrix:
     def measure(
         self, axes: Sequence[int], seed: 'cirq.RANDOM_STATE_OR_SEED_LIKE' = None
     ) -> List[int]:
-        """Delegates the call to measure the density matrix."""
+        """Measures the density matrix.
+
+        Args:
+            axes: The axes to measure.
+            seed: The random number seed to use.
+        Returns:
+            The measurements in order.
+        """
         bits, _ = sim.measure_density_matrix(
             self._density_matrix,
             axes,
@@ -138,6 +212,15 @@ class _BufferedDensityMatrix:
         repetitions: int = 1,
         seed: 'cirq.RANDOM_STATE_OR_SEED_LIKE' = None,
     ) -> np.ndarray:
+        """Samples the density matrix.
+
+        Args:
+            axes: The axes to sample.
+            repetitions: The number of samples to make.
+            seed: The random number seed to use.
+        Returns:
+            The samples in order.
+        """
         return sim.sample_density_matrix(
             self._density_matrix,
             axes,
