52c7546fc6864da1465d9a3e64d9202542a1ed05
daxfohl <dax.fohl@gmail.com>
2022-02-12 23:32:30 -0800

    test

--- cirq-core/cirq/sim/act_on_density_matrix_args.py
+++ cirq-core/cirq/sim/act_on_density_matrix_args.py
@@ -48,10 +48,7 @@ class _BufferedDensityMatrix:
             buffer = [np.empty_like(density_matrix) for _ in range(3)]
         self._buffer = buffer
         if len(density_matrix.shape) % 2 != 0:
-            raise ValueError(
-                'The dimension of target_tensor is not divisible by 2.'
-                ' Require explicit qid_shape.'
-            )
+            raise ValueError('The dimension of target_tensor is not divisible by 2.')
         self._qid_shape = density_matrix.shape[: len(density_matrix.shape) // 2]
 
     @classmethod
