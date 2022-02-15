ce6cebecb78a21a615fdc37fb80c62da02c82d52
daxfohl <dax.fohl@gmail.com>
2022-02-15 15:03:15 -0800

    format

--- cirq-core/cirq/ops/classically_controlled_operation_test.py
+++ cirq-core/cirq/ops/classically_controlled_operation_test.py
@@ -488,8 +488,12 @@ def test_scope_flat():
         cirq.measure(q, key='a'),
         cirq.X(q).with_classical_controls('a'),
     )
-    middle = cirq.Circuit(cirq.CircuitOperation(inner.freeze(), repetitions=2, flatten_repetitions=True))
-    outer_subcircuit = cirq.CircuitOperation(middle.freeze(), repetitions=2, flatten_repetitions=True)
+    middle = cirq.Circuit(
+        cirq.CircuitOperation(inner.freeze(), repetitions=2, flatten_repetitions=True)
+    )
+    outer_subcircuit = cirq.CircuitOperation(
+        middle.freeze(), repetitions=2, flatten_repetitions=True
+    )
     circuit = outer_subcircuit.mapped_circuit(deep=True)
     internal_control_keys = [
         str(condition) for op in circuit.all_operations() for condition in cirq.control_keys(op)
