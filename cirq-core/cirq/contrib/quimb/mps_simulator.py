d7100e2014e24e14adc945b4c29db7cc15b9ede3
daxfohl <dax.fohl@gmail.com>
2022-02-24 12:18:49 -0800

    mps quantum state

--- cirq-core/cirq/contrib/quimb/mps_simulator.py
+++ cirq-core/cirq/contrib/quimb/mps_simulator.py
@@ -19,7 +19,7 @@ https://arxiv.org/abs/2002.07730
 
 import dataclasses
 import math
-from typing import Any, Dict, List, Optional, Sequence, Set, TYPE_CHECKING, Union
+from typing import Any, Dict, List, Optional, Sequence, Set, Tuple, TYPE_CHECKING, Union
 
 import numpy as np
 import quimb.tensor as qtn
@@ -216,6 +216,309 @@ class MPSSimulatorStepResult(simulator_base.StepResultBase['MPSState', 'MPSState
 
     def _simulator_state(self):
         return self.state
+
+
+@value.value_equality
+class _MPSState:
+    """A state of the MPS simulation."""
+
+    def __init__(
+        self,
+        qid_shape: Tuple[int, ...],
+        grouping: Dict[int, int],
+        initial_state: int = 0,
+        simulation_options: MPSOptions = MPSOptions(),
+    ):
+        """Creates and MPSState
+
+        Args:
+            simulation_options: Numerical options for the simulation.
+            grouping: How to group qubits together, if None all are individual.
+            initial_state: An integer representing the initial state.
+
+        Raises:
+            ValueError: If the grouping does not cover the qubits.
+        """
+        self._qid_shape = qid_shape
+        self.grouping = grouping
+        self.M = []
+        for _ in range(max(self.grouping.values()) + 1):
+            self.M.append(qtn.Tensor())
+
+        # The order of the qubits matters, because the state |01> is different from |10>. Since
+        # Quimb uses strings to name tensor indices, we want to be able to sort them too. If we are
+        # working with, say, 123 qubits then we want qubit 3 to come before qubit 100, but then
+        # we want write the string '003' which comes before '100' in lexicographic order. The code
+        # below is just simple string formatting.
+        max_num_digits = len(f'{max(grouping.values())}')
+        self.format_i = f'i_{{:0{max_num_digits}}}'
+        self.format_mu = 'mu_{}_{}'
+
+        # TODO(tonybruguier): Instead of relying on sortable indices could you keep a parallel
+        # mapping of e.g. qubit to string-index and do all "logic" on the qubits themselves and
+        # only translate to string-indices when calling a quimb API.
+
+        # TODO(tonybruguier): Refactor out so that the code below can also be used by
+        # circuit_to_tensors in cirq.contrib.quimb.state_vector.
+
+        for axis in reversed(range(len(qid_shape))):
+            d = self._qid_shape[axis]
+            x = np.zeros(d)
+            x[initial_state % d] = 1.0
+
+            n = self.grouping[axis]
+            self.M[n] @= qtn.Tensor(x, inds=(self.i_str(axis),))
+            initial_state = initial_state // d
+        self.simulation_options = simulation_options
+        self.estimated_gate_error_list: List[float] = []
+
+    def i_str(self, i: int) -> str:
+        # Returns the index name for the i'th qid.
+        return self.format_i.format(i)
+
+    def mu_str(self, i: int, j: int) -> str:
+        # Returns the index name for the pair of the i'th and j'th qids. Note
+        # that by convention, the lower index is always the first in the output
+        # string.
+        smallest = min(i, j)
+        largest = max(i, j)
+        return self.format_mu.format(smallest, largest)
+
+    def __str__(self) -> str:
+        return str(qtn.TensorNetwork(self.M))
+
+    def _value_equality_values_(self) -> Any:
+        return self._qid_shape, self.M, self.simulation_options, self.grouping
+
+    def _on_copy(self, target: '_MPSState', deep_copy_buffers: bool = True):
+        target.simulation_options = self.simulation_options
+        target.grouping = self.grouping
+        target.M = [x.copy() for x in self.M]
+        target.estimated_gate_error_list = self.estimated_gate_error_list
+        target._qid_shape = self._qid_shape
+
+    def state_vector(self) -> np.ndarray:
+        """Returns the full state vector.
+
+        Returns:
+            A vector that contains the full state.
+        """
+        tensor_network = qtn.TensorNetwork(self.M)
+        state_vector = tensor_network.contract(inplace=False)
+
+        # Here, we rely on the formatting of the indices, and the fact that we have enough
+        # leading zeros so that 003 comes before 100.
+        sorted_ind = tuple(sorted(state_vector.inds))
+        return state_vector.fuse({'i': sorted_ind}).data
+
+    def partial_trace(self, keep_axes: Set[int]) -> np.ndarray:
+        """Traces out all qubits except keep_qubits.
+
+        Args:
+            keep_axes: The set of axes that are left after computing the
+                partial trace. For example, if we have a circuit for 3 qubits
+                and this parameter only has one qubit, the entire density matrix
+                would be 8x8, but this function returns a 2x2 matrix.
+
+        Returns:
+            An array that contains the partial trace.
+        """
+
+        contracted_inds = set(range(len(self._qid_shape))) - keep_axes
+
+        conj_pfx = "conj_"
+
+        tensor_network = qtn.TensorNetwork(self.M)
+
+        # Rename the internal indices to avoid collisions. Also rename the qubit
+        # indices that are kept. We do not rename the qubit indices that are
+        # traced out.
+        conj_tensor_network = tensor_network.conj()
+        reindex_mapping = {}
+        for M in conj_tensor_network.tensors:
+            for ind in M.inds:
+                if ind not in contracted_inds:
+                    reindex_mapping[ind] = conj_pfx + ind
+        conj_tensor_network.reindex(reindex_mapping, inplace=True)
+        partial_trace = conj_tensor_network @ tensor_network
+
+        forward_inds = list(map(self.i_str, keep_axes))
+        backward_inds = [conj_pfx + forward_ind for forward_ind in forward_inds]
+        return partial_trace.to_dense(forward_inds, backward_inds)
+
+    def to_numpy(self) -> np.ndarray:
+        """An alias for the state vector."""
+        return self.state_vector()
+
+    def apply_op(self, op: 'cirq.Gate', axes: Sequence[int], prng: np.random.RandomState):
+        """Applies a unitary operation, mutating the object to represent the new state.
+
+        op:
+            The operation that mutates the object. Note that currently, only 1-
+            and 2- qubit operations are currently supported.
+        """
+
+        old_inds = tuple(map(self.i_str, axes))
+        new_inds = tuple(['new_' + old_ind for old_ind in old_inds])
+
+        if protocols.has_unitary(op):
+            U = protocols.unitary(op)
+        else:
+            mixtures = protocols.mixture(op)
+            mixture_idx = int(prng.choice(len(mixtures), p=[mixture[0] for mixture in mixtures]))
+            U = mixtures[mixture_idx][1]
+        U = qtn.Tensor(
+            U.reshape([self._qid_shape[axis] for axis in axes] * 2), inds=(new_inds + old_inds)
+        )
+
+        # TODO(tonybruguier): Explore using the Quimb's tensor network natively.
+
+        if len(axes) == 1:
+            n = self.grouping[axes[0]]
+
+            self.M[n] = (U @ self.M[n]).reindex({new_inds[0]: old_inds[0]})
+        elif len(axes) == 2:
+            n, p = [self.grouping[axis] for axis in axes]
+
+            if n == p:
+                self.M[n] = (U @ self.M[n]).reindex(
+                    {new_inds[0]: old_inds[0], new_inds[1]: old_inds[1]}
+                )
+            else:
+                # This is the index on which we do the contraction. We need to add it iff it's
+                # the first time that we do the joining for that specific pair.
+                mu_ind = self.mu_str(n, p)
+                if mu_ind not in self.M[n].inds:
+                    self.M[n].new_ind(mu_ind)
+                if mu_ind not in self.M[p].inds:
+                    self.M[p].new_ind(mu_ind)
+
+                T = U @ self.M[n] @ self.M[p]
+
+                left_inds = tuple(set(T.inds) & set(self.M[n].inds)) + (new_inds[0],)
+                X, Y = T.split(
+                    left_inds,
+                    method=self.simulation_options.method,
+                    max_bond=self.simulation_options.max_bond,
+                    cutoff=self.simulation_options.cutoff,
+                    cutoff_mode=self.simulation_options.cutoff_mode,
+                    get='tensors',
+                    absorb='both',
+                    bond_ind=mu_ind,
+                )
+
+                # Equations (13), (14), and (15):
+                # TODO(tonybruguier): When Quimb 2.0.0 is released, the split()
+                # function should have a 'renorm' that, when set to None, will
+                # allow to compute e_n exactly as:
+                # np.sum(abs((X @ Y).data) ** 2).real / np.sum(abs(T) ** 2).real
+                #
+                # The renormalization would then have to be done manually.
+                #
+                # However, for now, e_n are just the estimated value.
+                e_n = self.simulation_options.cutoff
+                self.estimated_gate_error_list.append(e_n)
+
+                self.M[n] = X.reindex({new_inds[0]: old_inds[0]})
+                self.M[p] = Y.reindex({new_inds[1]: old_inds[1]})
+        else:
+            # NOTE(tonybruguier): There could be a way to handle higher orders. I think this could
+            # involve HOSVDs:
+            # https://en.wikipedia.org/wiki/Higher-order_singular_value_decomposition
+            #
+            # TODO(tonybruguier): Evaluate whether it's even useful to implement and learn more
+            # about HOSVDs.
+            raise ValueError('Can only handle 1 and 2 qubit operations')
+        return True
+
+    def estimation_stats(self):
+        """Returns some statistics about the memory usage and quality of the approximation."""
+
+        num_coefs_used = sum([Mi.data.size for Mi in self.M])
+        memory_bytes = sum([Mi.data.nbytes for Mi in self.M])
+
+        # The computation below is done for numerical stability, instead of directly using the
+        # formula:
+        # estimated_fidelity = \prod_i (1 - estimated_gate_error_list_i)
+        estimated_fidelity = 1.0 + np.expm1(
+            sum(np.log1p(-x) for x in self.estimated_gate_error_list)
+        )
+        estimated_fidelity = round(estimated_fidelity, ndigits=3)
+
+        return {
+            "num_coefs_used": num_coefs_used,
+            "memory_bytes": memory_bytes,
+            "estimated_fidelity": estimated_fidelity,
+        }
+
+    def perform_measurement(
+        self, axes: Sequence[int], prng: np.random.RandomState, collapse_state_vector=True
+    ) -> List[int]:
+        """Performs a measurement over one or more qubits.
+
+        Args:
+            axes: The sequence of axes to measure, in that order.
+            prng: A random number generator, used to simulate measurements.
+            collapse_state_vector: A Boolean specifying whether we should mutate
+                the state after the measurement.
+
+        Raises:
+            ValueError: If the probabilities for the measurements differ too much from one for the
+                tolerance specified in simulation options.
+        """
+        results: List[int] = []
+
+        if collapse_state_vector:
+            state = self
+        else:
+            state = self.copy()
+
+        for axis in axes:
+            # Trace out other qubits
+            M = state.partial_trace(keep_axes={axis})
+            probs = np.diag(M).real
+            sum_probs = sum(probs)
+
+            # Because the computation is approximate, the probabilities do not
+            # necessarily add up to 1.0, and thus we re-normalize them.
+            if abs(sum_probs - 1.0) > self.simulation_options.sum_prob_atol:
+                raise ValueError(f'Sum of probabilities exceeds tolerance: {sum_probs}')
+            norm_probs = [x / sum_probs for x in probs]
+
+            d = self._qid_shape[axis]
+            result: int = int(prng.choice(d, p=norm_probs))
+
+            collapser = np.zeros((d, d))
+            collapser[result][result] = 1.0 / math.sqrt(probs[result])
+
+            old_n = state.i_str(axis)
+            new_n = 'new_' + old_n
+
+            collapser = qtn.Tensor(collapser, inds=(new_n, old_n))
+
+            state.M[axis] = (collapser @ state.M[axis]).reindex({new_n: old_n})
+
+            results.append(result)
+
+        return results
+
+    def sample(
+        self,
+        axes: Sequence[int],
+        repetitions: int = 1,
+        seed: 'cirq.RANDOM_STATE_OR_SEED_LIKE' = None,
+    ) -> np.ndarray:
+
+        measurements: List[List[int]] = []
+
+        for _ in range(repetitions):
+            measurements.append(
+                self.perform_measurement(
+                    axes, value.parse_random_state(seed), collapse_state_vector=False
+                )
+            )
+
+        return np.array(measurements, dtype=int)
 
 
 @value.value_equality
