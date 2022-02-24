57177f7019976f0f1f560d1952bd0399ae287cc5
daxfohl <dax.fohl@gmail.com>
2022-02-24 10:44:47 -0800

    replace deleted functions

--- cirq-core/cirq/sim/act_on_state_vector_args.py
+++ cirq-core/cirq/sim/act_on_state_vector_args.py
@@ -401,6 +401,7 @@ class ActOnStateVectorArgs(ActOnArgs):
         self, axes: Sequence[int], little_endian_bits_int: int = 0, *, big_endian_bits_int: int = 0
     ) -> Tuple[Union[slice, int, 'ellipsis'], ...]:
         """An index for the subspace where the target axes equal a value.
+
         Args:
             axes: The qubits that are specified by the index bits.
             little_endian_bits_int: The desired value of the qubits at the
@@ -408,6 +409,7 @@ class ActOnStateVectorArgs(ActOnArgs):
                 bit of the integer is the desired bit for the first axis, and
                 so forth in increasing order. Can't be specified at the same
                 time as `big_endian_bits_int`.
+
                 When operating on qudits instead of qubits, the same basic logic
                 applies but in a different basis. For example, if the target
                 axes have dimension [a:2, b:3, c:2] then the integer 10
@@ -417,21 +419,28 @@ class ActOnStateVectorArgs(ActOnArgs):
                 bit of the integer is the desired bit for the first axis, and
                 so forth in decreasing order. Can't be specified at the same
                 time as `little_endian_bits_int`.
+
                 When operating on qudits instead of qubits, the same basic logic
                 applies but in a different basis. For example, if the target
                 axes have dimension [a:2, b:3, c:2] then the integer 10
                 decomposes into [a=1, b=2, c=0] via 7 = 1*(3*2) +  2*(2) + 0.
+
         Returns:
             A value that can be used to index into `target_tensor` and
             `available_buffer`, and manipulate only the part of Hilbert space
             corresponding to a given bit assignment.
+
         Example:
             If `target_tensor` is a 4 qubit tensor and `axes` is `[1, 3]` and
             then this method will return the following when given
             `little_endian_bits=0b01`:
+
                 `(slice(None), 0, slice(None), 1, Ellipsis)`
+
             Therefore the following two lines would be equivalent:
+
                 args.target_tensor[args.subspace_index(0b01)] += 1
+
                 args.target_tensor[:, 0, :, 1] += 1
         """
         return linalg.slice_for_qubits_equal_to(
