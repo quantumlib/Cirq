848bfde611182be49496a3e56772d18d7ed3299e
Tanuj Khattar <tanujkhattar@google.com>
2022-03-18 06:04:13 +0530

    Add default decomposition for cirq.QubitPermutationGate in terms of adjacent swaps (#5093)
    - Adds decomposition to `cirq.QubitPermutationGate` in terms of minimum number of adjacent swap operations on qubits. 
    - Part of https://github.com/quantumlib/Cirq/issues/4858
    
    Closes https://github.com/quantumlib/Cirq/issues/5090

--- cirq-core/cirq/ops/permutation_gate.py
+++ cirq-core/cirq/ops/permutation_gate.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, Sequence, Tuple, TYPE_CHECKING
+from typing import Any, Dict, Iterable, Sequence, Tuple, TYPE_CHECKING
 
 from cirq import protocols, value
 from cirq._compat import deprecated
-from cirq.ops import raw_types
+from cirq.ops import raw_types, swap_gates
 
 if TYPE_CHECKING:
     import cirq
@@ -73,6 +73,25 @@ class QubitPermutationGate(raw_types.Gate):
 
     def _has_unitary_(self):
         return True
+
+    def _decompose_(self, qubits: Sequence['cirq.Qid']) -> 'cirq.OP_TREE':
+        n = len(qubits)
+        qubit_ids = [*range(n)]
+        is_sorted = False
+
+        def _swap_if_out_of_order(idx: int) -> Iterable['cirq.Operation']:
+            nonlocal is_sorted
+            if self._permutation[qubit_ids[idx]] > self._permutation[qubit_ids[idx + 1]]:
+                yield swap_gates.SWAP(qubits[idx], qubits[idx + 1])
+                qubit_ids[idx + 1], qubit_ids[idx] = qubit_ids[idx], qubit_ids[idx + 1]
+                is_sorted = False
+
+        while not is_sorted:
+            is_sorted = True
+            for i in range(0, n - 1, 2):
+                yield from _swap_if_out_of_order(i)
+            for i in range(1, n - 1, 2):
+                yield from _swap_if_out_of_order(i)
 
     def _apply_unitary_(self, args: 'cirq.ApplyUnitaryArgs'):
         # Compute the permutation index list.
