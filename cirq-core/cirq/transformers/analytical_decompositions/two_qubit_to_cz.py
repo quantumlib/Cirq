279a3dff647ea07814021cd233b710054dc465b7
Tony Bruguier <tony.bruguier@gmail.com>
2022-03-19 04:17:48 +0000

    Fix imports

--- cirq-core/cirq/transformers/analytical_decompositions/two_qubit_to_cz.py
+++ cirq-core/cirq/transformers/analytical_decompositions/two_qubit_to_cz.py
@@ -18,11 +18,11 @@ from typing import Iterable, List, Sequence, Tuple, Optional, cast, TYPE_CHECKIN
 
 import numpy as np
 
-from cirq import _compat
 from cirq.linalg import predicates
 from cirq.linalg.decompositions import num_cnots_required, extract_right_diag
 
 from cirq import ops, linalg, protocols, circuits
+from cirq.transformers.analytical_decompositions import single_qubit_decompositions
 from cirq.transformers.merge_single_qubit_gates import merge_single_qubit_gates_to_phased_x_and_z
 from cirq.transformers.eject_z import eject_z
 from cirq.transformers.eject_phased_paulis import eject_phased_paulis
