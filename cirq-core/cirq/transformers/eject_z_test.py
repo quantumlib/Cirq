da8c65496e7f0628786a4047dc9c8f235ff5b582
Tanuj Khattar <tanujkhattar@google.com>
2022-03-21 12:10:10 -0700

    Fix pylint errors

--- cirq-core/cirq/transformers/eject_z_test.py
+++ cirq-core/cirq/transformers/eject_z_test.py
@@ -11,10 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import dataclasses
+
 import pytest
 import numpy as np
 import sympy
-import dataclasses
 
 import cirq
 from cirq.transformers.eject_z import _is_swaplike
