9e4ecb8611e4b83f0899f7d827a64f9ca964dfa8
daxfohl <dax.fohl@gmail.com>
2022-03-21 10:18:12 -0700

    lint

--- cirq-core/cirq/sim/simulator_base_test.py
+++ cirq-core/cirq/sim/simulator_base_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import math
-from typing import Any, Dict, List, Sequence, Union
+from typing import Any, Dict, List, Sequence
 
 import numpy as np
 import pytest
