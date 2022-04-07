da3a4a929067b005f16dc2959b01023117df7a06
Matthew Neeley <maffoo@google.com>
2022-04-06 20:10:35 -0700

    Deprecate `gate_set` parameter on engine classes. (#5207)
    Fixes #4995
    
    Review: @verult

--- cirq-google/cirq_google/engine/util.py
+++ cirq-google/cirq_google/engine/util.py
@@ -11,10 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import inspect
 from typing import TypeVar
 
 from google.protobuf import any_pb2
 from google.protobuf.message import Message
+
+import cirq
 
 M = TypeVar('M', bound=Message)
 
@@ -32,3 +36,20 @@ def pack_any(message: Message) -> any_pb2.Any:
 def unpack_any(message: any_pb2.Any, out: M) -> M:
     message.Unpack(out)
     return out
+
+
+def deprecated_gate_set_parameter(func):
+    """Decorates a function that takes a deprecated 'gate_set' parameter."""
+    signature = inspect.signature(func)
+    gate_set_param = signature.parameters['gate_set']
+    assert gate_set_param.default is None  # Must be optional and default to None.
+    idx = list(signature.parameters).index('gate_set')
+
+    decorator = cirq._compat.deprecated_parameter(
+        deadline='v0.15',
+        fix='Remove the gate_set parameter.',
+        parameter_desc='gate_set',
+        match=lambda args, kwargs: 'gate_set' in kwargs
+        or (gate_set_param.kind != inspect.Parameter.KEYWORD_ONLY and len(args) > idx),
+    )
+    return decorator(func)
