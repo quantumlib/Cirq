06efe9928b0002901ebfd849c9914ea78a27b619
Matthew Neeley <maffoo@google.com>
2022-04-08 15:42:10 -0700

    remove unused import

--- cirq-core/cirq/ops/qid_util.py
+++ cirq-core/cirq/ops/qid_util.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional, overload, TYPE_CHECKING, Union
+from typing import overload, TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
     import cirq
