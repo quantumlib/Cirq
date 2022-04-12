6854411f54559acd7076fafa1932d62961c23df2
Dave Bacon <dabacon@gmail.com>
2022-04-11 17:30:12 -0700

    line length

--- cirq-core/cirq/ops/common_gates.py
+++ cirq-core/cirq/ops/common_gates.py
@@ -247,7 +247,7 @@ class XPowGate(eigen_gate.EigenGate, gate_features.SingleQubitGate):
 
 
 class Rx(XPowGate):
-    r"""A gate, with matrix $e^{-i X t/2}$ that rotates around the X axis of the Bloch sphere by $t$.
+    r"""A gate with matrix $e^{-i X t/2}$ that rotates around the X axis of the Bloch sphere by $t$.
 
     The unitary matrix of `cirq.Rx(rads=t)` is:
     $$
