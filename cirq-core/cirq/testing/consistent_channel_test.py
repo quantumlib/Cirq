64b2c69b17c35bc1a8427adeca7329f4267b9d40
Dave Bacon <dabacon@gmail.com>
2022-04-11 21:03:29 -0700

    remove warning

--- cirq-core/cirq/testing/consistent_channel_test.py
+++ cirq-core/cirq/testing/consistent_channel_test.py
@@ -38,7 +38,7 @@ def test_assert_consistent_channel_tolerances():
 
 def test_assert_consistent_channel_invalid():
     channel = cirq.KrausChannel(kraus_ops=(np.array([[1, 1], [0, 0]]), np.array([[1, 0], [0, 0]])))
-    with pytest.raises(AssertionError, match=r"cirq.KrausChannel.*Summed to [[2 1]"):
+    with pytest.raises(AssertionError, match=r"cirq.KrausChannel.*2 1"):
         cirq.testing.assert_consistent_channel(channel)
 
 
