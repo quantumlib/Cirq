217f2523f8f2921d086b0ad05d7de44fc292d219
daxfohl <dax.fohl@gmail.com>
2022-04-13 16:25:44 -0700

    tests

--- cirq-core/cirq/sim/density_matrix_simulator_test.py
+++ cirq-core/cirq/sim/density_matrix_simulator_test.py
@@ -756,9 +756,8 @@ def test_simulate_moment_steps_empty_circuit(dtype: Type[np.number], split: bool
     step = None
     for step in simulator.simulate_moment_steps(circuit):
         pass
-    assert step._simulator_state() == cirq.DensityMatrixSimulatorState(
-        density_matrix=np.array([[1]]), qubit_map={}
-    )
+    assert np.allclose(step.density_matrix(), np.array([[1]]))
+    assert not step.qubit_map
 
 
 @pytest.mark.parametrize('dtype', [np.complex64, np.complex128])
