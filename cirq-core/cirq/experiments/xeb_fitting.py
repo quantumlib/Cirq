4bdd2ae4e0743b76bbd10fe2002925294a297fc3
Doug Strain <dstrain@google.com>
2022-04-18 10:04:07 -0700

    Update cirq-core/cirq/experiments/xeb_fitting.py
    Co-authored-by: Matthew Neeley <mneeley@gmail.com>

--- cirq-core/cirq/experiments/xeb_fitting.py
+++ cirq-core/cirq/experiments/xeb_fitting.py
@@ -421,7 +421,7 @@ def characterize_phased_fsim_parameters_with_xeb(
     )
     return XEBCharacterizationResult(
         optimization_results={pair: optimization_result},
-        final_params={pair: final_params},  # type: ignore
+        final_params={pair: final_params},  # type: ignore[dict-item]
         fidelities_df=fidelities_df,
     )
 
