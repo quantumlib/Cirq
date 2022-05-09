bc94a6c67dd242460300e4979f1192a423e5e607
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2022-05-09 09:05:33 -0700

    Median device calibrations.

--- cirq-google/cirq_google/engine/virtual_engine_factory.py
+++ cirq-google/cirq_google/engine/virtual_engine_factory.py
@@ -28,9 +28,16 @@ from cirq_google.engine.simulated_local_engine import SimulatedLocalEngine
 from cirq_google.engine.simulated_local_processor import SimulatedLocalProcessor
 
 MOST_RECENT_TEMPLATES = {
-    'rainbow': 'rainbow_12_10_2021_device_spec.proto.txt',
-    'weber': 'weber_12_10_2021_device_spec.proto.txt',
+    'rainbow': 'rainbow_2021_12_10_device_spec.proto.txt',
+    'weber': 'weber_2021_12_10_device_spec.proto.txt',
 }
+
+MEDIAN_CALIBRATIONS = {
+    'rainbow': 'rainbow_2021_11_16_calibration.json',
+    'weber': 'weber_2021_03_11_calibration.json',
+}
+
+MEDIAN_CALIBRATION_TIMESTAMPS = {'rainbow': 1635845358175, 'weber': 1635923188204}
 
 METRICS_1Q = [
     'single_qubit_p00_error',
@@ -82,6 +89,29 @@ def _create_perfect_calibration(device: cirq.Device) -> calibration.Calibration:
     snapshot = v2.metrics_pb2.MetricsSnapshot()
     snapshot.timestamp_ms = int(time.time() * 1000)
     return calibration.Calibration(calibration=snapshot, metrics=all_metrics)
+
+
+def load_median_device_calibration(processor_id: str) -> calibration.Calibration:
+    """Loads a median Calibration for the given device.
+
+    Real calibration data from Google's 'rainbow' and 'weber' devices has been
+    saved in Cirq. The calibrations selected are roughly representative of the
+    median performance for that chip.
+
+    Args:
+         processor_id: name of the processor to simulate.
+
+    Raises:
+        ValueError: if processor_id is not a supported QCS processor.
+    """
+    cal_name = MEDIAN_CALIBRATIONS.get(processor_id, None)
+    if cal_name is None:
+        raise ValueError(f"Got processor_id={processor_id}, but no such processor is defined.")
+    path = pathlib.Path(__file__).parent.parent.resolve()
+    with path.joinpath('devices', 'calibrations', cal_name).open() as f:
+        cal = cast(calibration.Calibration, cirq.read_json(f))
+    cal.timestamp = MEDIAN_CALIBRATION_TIMESTAMPS[processor_id]
+    return cal
 
 
 def _create_virtual_processor_from_device(
--- cirq-google/cirq_google/engine/virtual_engine_factory_test.py
+++ cirq-google/cirq_google/engine/virtual_engine_factory_test.py
@@ -51,6 +51,23 @@ def test_create_from_device():
     _test_processor(engine.get_processor('sycamore'))
 
 
+def test_median_device():
+    q0, q1 = cirq.GridQubit.rect(1, 2, 5, 3)
+    cal = factory.load_median_device_calibration('rainbow')
+    # Spot-check an arbitrary set of values to confirm we got the right data.
+    assert cal['parallel_p00_error'][(q0,)][0] == 0.0067149999999998045
+    assert cal['single_qubit_readout_separation_error'][(q1,)][0] == 0.00039635847369929797
+    assert (
+        cal['two_qubit_sycamore_gate_xeb_average_error_per_cycle'][(q0, q1)][0]
+        == 0.0034558565201338043
+    )
+
+
+def test_median_device_bad_processor():
+    with pytest.raises(ValueError, match='Got processor_id=bad_processor'):
+        _ = factory.load_median_device_calibration('bad_processor')
+
+
 def test_create_from_proto():
 
     # Create a minimal gate specification that can handle the test.
