86b1c4908815fadd67edaa3be316c0a78dc30e08
Doug Strain <dstrain@google.com>
2022-05-12 06:36:50 -0700

    Update cirq-core/cirq/sim/simulator_test.py
    Co-authored-by: Matthew Neeley <mneeley@gmail.com>

--- cirq-core/cirq/sim/simulator_test.py
+++ cirq-core/cirq/sim/simulator_test.py
@@ -233,7 +233,7 @@ def test_step_sample_measurement_ops_not_measurement():
 def test_step_sample_measurement_ops_repeated_qubit():
     q0, q1, q2 = cirq.LineQubit.range(3)
     step_result = FakeStepResult(ones_qubits=[q0])
-    with pytest.raises(ValueError, match='Measurement key q.0. repeated'):
+    with pytest.raises(ValueError, match=r'Measurement key q\(0\) repeated'):
         step_result.sample_measurement_ops(
             [cirq.measure(q0), cirq.measure(q1, q2), cirq.measure(q0)]
         )
