bf893e7789f01a1fc7b7dc8bec9546b93a04d5c5
Pavol Juhas <juhas@google.com>
2022-05-26 14:58:42 -0700

    Test cirq.message with a sequence argument
    Not yet ready, expecting test failure.

--- cirq-core/cirq/ops/measure_util_test.py
+++ cirq-core/cirq/ops/measure_util_test.py
@@ -18,6 +18,7 @@ import pytest
 import cirq
 
 
+@pytest.mark.xfail(reason='wip - test cirq.measure with iterable argument')
 def test_measure_qubits():
     a = cirq.NamedQubit('a')
     b = cirq.NamedQubit('b')
@@ -26,8 +27,15 @@ def test_measure_qubits():
     with pytest.raises(ValueError, match='empty set of qubits'):
         _ = cirq.measure()
 
+    with pytest.raises(ValueError, match='empty set of qubits'):
+        _ = cirq.measure([])
+
     assert cirq.measure(a) == cirq.MeasurementGate(num_qubits=1, key='a').on(a)
+    assert cirq.measure([a]) == cirq.MeasurementGate(num_qubits=1, key='a').on(a)
     assert cirq.measure(a, b) == cirq.MeasurementGate(num_qubits=2, key='a,b').on(a, b)
+    assert cirq.measure([a, [b]]) == cirq.MeasurementGate(num_qubits=2, key='a,b').on(a, b)
+    qubit_generator = (q for q in (a, b))
+    assert cirq.measure(qubit_generator) == cirq.MeasurementGate(num_qubits=2, key='a,b').on(a, b)
     assert cirq.measure(b, a) == cirq.MeasurementGate(num_qubits=2, key='b,a').on(b, a)
     assert cirq.measure(a, key='b') == cirq.MeasurementGate(num_qubits=1, key='b').on(a)
     assert cirq.measure(a, invert_mask=(True,)) == cirq.MeasurementGate(
@@ -36,12 +44,21 @@ def test_measure_qubits():
     assert cirq.measure(*cirq.LineQid.for_qid_shape((1, 2, 3)), key='a') == cirq.MeasurementGate(
         num_qubits=3, key='a', qid_shape=(1, 2, 3)
     ).on(*cirq.LineQid.for_qid_shape((1, 2, 3)))
+    assert cirq.measure(cirq.LineQid.for_qid_shape((1, 2, 3)), key='a') == cirq.MeasurementGate(
+        num_qubits=3, key='a', qid_shape=(1, 2, 3)
+    ).on(*cirq.LineQid.for_qid_shape((1, 2, 3)))
 
     with pytest.raises(ValueError, match='ndarray'):
         _ = cirq.measure(np.array([1, 0]))
 
+    with pytest.raises(ValueError, match='ndarray'):
+        _ = cirq.measure([a, np.array([1, 0])])
+
     with pytest.raises(ValueError, match='Qid'):
         _ = cirq.measure("bork")
+
+    with pytest.raises(ValueError, match='Qid'):
+        _ = cirq.measure([a, "bork"])
 
 
 def test_measure_each():
