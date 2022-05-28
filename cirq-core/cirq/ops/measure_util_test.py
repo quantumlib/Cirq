f101c4beccadc4417b103e0cab26e89d010686a2
Pavol Juhas <juhas@google.com>
2022-05-27 17:31:06 -0700

    Test cirq.measure_each with a sequence argument
    Not yet ready, expecting test failure.

--- cirq-core/cirq/ops/measure_util_test.py
+++ cirq-core/cirq/ops/measure_util_test.py
@@ -60,13 +60,19 @@ def test_measure_qubits():
         _ = cirq.measure([a], [b])
 
 
+@pytest.mark.xfail(reason='wip - test cirq.measure_each with iterable argument')
 def test_measure_each():
     a = cirq.NamedQubit('a')
     b = cirq.NamedQubit('b')
 
     assert cirq.measure_each() == []
+    assert cirq.measure_each([]) == []
     assert cirq.measure_each(a) == [cirq.measure(a)]
+    assert cirq.measure_each([a]) == [cirq.measure(a)]
     assert cirq.measure_each(a, b) == [cirq.measure(a), cirq.measure(b)]
+    assert cirq.measure_each([a, b]) == [cirq.measure(a), cirq.measure(b)]
+    qubit_generator = (q for q in (a, b))
+    assert cirq.measure_each(qubit_generator) == [cirq.measure(a), cirq.measure(b)]
     assert cirq.measure_each(a.with_dimension(3), b.with_dimension(3)) == [
         cirq.measure(a.with_dimension(3)),
         cirq.measure(b.with_dimension(3)),
