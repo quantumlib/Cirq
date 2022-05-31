d38c6c18219a191c045454ab49de1e10e363d385
Dave Bacon <dabacon@gmail.com>
2022-05-31 18:39:49 +0000

    Remove unused parameter in test

--- cirq-core/cirq/contrib/paulistring/pauli_string_dag_test.py
+++ cirq-core/cirq/contrib/paulistring/pauli_string_dag_test.py
@@ -12,15 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
-
 import cirq
 
 from cirq.contrib.paulistring import convert_and_separate_circuit, pauli_string_dag_from_circuit
 
 
-@pytest.mark.parametrize('repetition', range(6))
-def test_pauli_string_dag_from_circuit(repetition):
+def test_pauli_string_dag_from_circuit():
     q0, q1, q2 = cirq.LineQubit.range(3)
     c_orig = cirq.testing.nonoptimal_toffoli_circuit(q0, q1, q2)
     c_left, _ = convert_and_separate_circuit(c_orig)
@@ -29,5 +26,5 @@ def test_pauli_string_dag_from_circuit(repetition):
     c_left_reordered = c_left_dag.to_circuit()
 
     cirq.testing.assert_allclose_up_to_global_phase(
-        c_left.unitary(), c_left_reordered.unitary(), atol=1e-7
+        c_left.unitary(), c_left_reordered.unitary(), atol=1e-6
     )
