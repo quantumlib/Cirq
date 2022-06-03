16ece358181660057389aa89e6bb9db875135f2e
Auguste Hirth <augusteh@google.com>
2022-06-03 10:05:57 -0700

    Codeowners and isolated notebook tests fix

--- dev_tools/codeowners_test.py
+++ dev_tools/codeowners_test.py
@@ -53,22 +53,22 @@ QCVV_MAINTAINERS = BASE_MAINTAINERS.union(QCVV_TEAM)
         ("docs/_book.yaml", DOCS_MAINTAINERS),
         # qcvv
         ("cirq-core/cirq/experiments/__init__.py", QCVV_MAINTAINERS),
-        ("docs/qcvv/isolated_xeb.ipynb", QCVV_MAINTAINERS.union(DOCS_MAINTAINERS)),
+        ("docs/noise/qcvv/isolated_xeb.ipynb", QCVV_MAINTAINERS.union(DOCS_MAINTAINERS)),
         # aqt
         ("cirq-aqt/cirq_aqt/__init__.py", AQT_MAINTAINERS),
         ("cirq-aqt/setup.py", AQT_MAINTAINERS),
-        ("docs/aqt/access.md", AQT_MAINTAINERS.union(DOCS_MAINTAINERS)),
-        ("docs/tutorials/aqt/getting_started.ipynb", AQT_MAINTAINERS.union(DOCS_MAINTAINERS)),
+        ("docs/hardware/aqt/access.md", AQT_MAINTAINERS.union(DOCS_MAINTAINERS)),
+        ("docs/hardware/aqt/getting_started.ipynb", AQT_MAINTAINERS.union(DOCS_MAINTAINERS)),
         # pasqal
         ("cirq-pasqal/cirq_pasqal/__init__.py", PASQAL_MAINTAINERS),
         ("cirq-pasqal/setup.py", PASQAL_MAINTAINERS),
-        ("docs/pasqal/access.md", PASQAL_MAINTAINERS.union(DOCS_MAINTAINERS)),
-        ("docs/tutorials/pasqal/getting_started.ipynb", PASQAL_MAINTAINERS.union(DOCS_MAINTAINERS)),
+        ("docs/hardware/pasqal/access.md", PASQAL_MAINTAINERS.union(DOCS_MAINTAINERS)),
+        ("docs/hardware/pasqal/getting_started.ipynb", PASQAL_MAINTAINERS.union(DOCS_MAINTAINERS)),
         # ionq
         ("cirq-ionq/cirq_ionq/__init__.py", IONQ_MAINTAINERS),
         ("cirq-ionq/setup.py", IONQ_MAINTAINERS),
-        ("docs/ionq/access.md", IONQ_MAINTAINERS.union(DOCS_MAINTAINERS)),
-        ("docs/tutorials/ionq/getting_started.ipynb", IONQ_MAINTAINERS.union(DOCS_MAINTAINERS)),
+        ("docs/hardware/ionq/access.md", IONQ_MAINTAINERS.union(DOCS_MAINTAINERS)),
+        ("docs/hardware/ionq/getting_started.ipynb", IONQ_MAINTAINERS.union(DOCS_MAINTAINERS)),
         # google
         ("cirq-google/cirq_google/__init__.py", GOOGLE_MAINTAINERS),
         ("cirq-google/setup.py", GOOGLE_MAINTAINERS),
