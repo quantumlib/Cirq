2bff437a87c4e010591aca5ccf35ce66bf02562a
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2022-06-15 13:57:16 -0700

    Measurement confusion maps (#5480)
    * Measurement confusion maps
    
    * format
    
    * mypy+format
    
    * Error on deferred confused measure
    
    * Also change SimulatesSamples behavior.
    
    * Test SimulatesSamples
    
    * docstring zero note

--- cirq-core/cirq/sim/simulator.py
+++ cirq-core/cirq/sim/simulator.py
@@ -919,10 +919,12 @@ class StepResult(Generic[TSimulatorState], metaclass=abc.ABCMeta):
             key = gate.key
             out = np.zeros(shape=(repetitions, len(op.qubits)), dtype=np.int8)
             inv_mask = gate.full_invert_mask()
+            cmap = gate.confusion_map
             for i, q in enumerate(op.qubits):
                 out[:, i] = indexed_sample[:, qubits_to_index[q]]
                 if inv_mask[i]:
                     out[:, i] ^= out[:, i] < 2
+            self._confuse_results(out, op.qubits, cmap, seed)
             if _allow_repeated:
                 if key not in results:
                     results[key] = []
@@ -934,6 +936,28 @@ class StepResult(Generic[TSimulatorState], metaclass=abc.ABCMeta):
             if not _allow_repeated
             else {k: np.array(v).swapaxes(0, 1) for k, v in results.items()}
         )
+
+    def _confuse_results(
+        self,
+        bits: np.ndarray,
+        qubits: Sequence['cirq.Qid'],
+        confusion_map: Dict[Tuple[int, ...], np.ndarray],
+        seed: 'cirq.RANDOM_STATE_OR_SEED_LIKE' = None,
+    ) -> None:
+        """Mutates `bits` using the confusion_map.
+
+        Compare with _confuse_result in cirq-core/cirq/sim/simulation_state.py.
+        """
+        prng = value.parse_random_state(seed)
+        for rep in bits:
+            dims = [q.dimension for q in qubits]
+            for indices, confuser in confusion_map.items():
+                mat_dims = [dims[k] for k in indices]
+                row = value.big_endian_digits_to_int((rep[k] for k in indices), base=mat_dims)
+                new_val = prng.choice(len(confuser), p=confuser[row])
+                new_bits = value.big_endian_int_to_digits(new_val, base=mat_dims)
+                for i, k in enumerate(indices):
+                    rep[k] = new_bits[i]
 
 
 # When removing this, also remove the check in simulate_sweep_iter.
