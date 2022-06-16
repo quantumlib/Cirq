9b8475b73abc66297b9329f9e14bda70eae159de
Sid Madhuk <smadhuk@google.com>
2022-06-16 10:29:33 -0700

    Fix formatting issues

--- dev_tools/import_only_modules.py
+++ dev_tools/import_only_modules.py
@@ -42,10 +42,9 @@ class ImportOnlyModulesChecker(checkers.BaseChecker):
                 imported_module.import_module(name, True)
                 # Good, we could import "name" as a module relative to the "imported_module".
             except astroid.AstroidImportError:
-                self.add_message(
-                    'import-only-modules',
-                    node=node,
-                )
+                self.add_message('import-only-modules',
+                                 node=node,
+                                 args=(name, modname))
             except astroid.AstroidBuildingException:
                 # Some other error.
                 pass
