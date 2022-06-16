quantumlib/Cirq

commit 2bff437a87c4e010591aca5ccf35ce66bf02562a

Measurement confusion maps (#5480)
* Measurement confusion maps

* format

* mypy+format

* Error on deferred confused measure

* Also change SimulatesSamples behavior.

* Test SimulatesSamples

* docstring zero note

cirq-core/cirq/ops/measurement_gate.py
@@ -16,7 +16,7 @@ from typing import Any, Dict, FrozenSet, Iterable, Optional, Tuple, Sequence, TY
 
 import numpy as np
 
-from cirq import protocols, value
+from cirq import _compat, protocols, value
 from cirq.ops import raw_types
 
 if TYPE_CHECKING:
@@ -40,6 +40,7 @@ class MeasurementGate(raw_types.Gate):
         key: Union[str, 'cirq.MeasurementKey'] = '',
         invert_mask: Tuple[bool, ...] = (),
         qid_shape: Tuple[int, ...] = None,
+        confusion_map: Optional[Dict[Tuple[int, ...], np.ndarray]] = None,
     ) -> None:
         """Inits MeasurementGate.
 
@@ -52,10 +53,15 @@ class MeasurementGate(raw_types.Gate):
                 Qubits with indices past the end of the mask are not flipped.
             qid_shape: Specifies the dimension of each qid the measurement
                 applies to.  The default is 2 for every qubit.
+            confusion_map: A map of qubit index sets (using indices in the
+                operation generated from this gate) to the 2D confusion matrix
+                for those qubits. Indices not included use the identity.
+                Applied before invert_mask if both are provided.
 
         Raises:
-            ValueError: If the length of invert_mask is greater than num_qubits.
-                or if the length of qid_shape doesn't equal num_qubits.
+            ValueError: If invert_mask or confusion_map have indices
+                greater than the available qubit indices, or if the length of
+                qid_shape doesn't equal num_qubits.
         """
         if qid_shape is None:
             if num_qubits is None:
@@ -74,6 +80,9 @@ class MeasurementGate(raw_types.Gate):
         self._invert_mask = invert_mask or ()
         if self.invert_mask is not None and len(self.invert_mask) > self.num_qubits():
             raise ValueError('len(invert_mask) > num_qubits')
+        self._confusion_map = confusion_map or {}
+        if any(x >= self.num_qubits() for idx in self._confusion_map for x in idx):
+            raise ValueError('Confusion matrices have index out of bounds.')
 
     @property
     def key(self) -> str:
@@ -87,6 +96,10 @@ class MeasurementGate(raw_types.Gate):
     def invert_mask(self) -> Tuple[bool, ...]:
         return self._invert_mask
 
+    @property
+    def confusion_map(self) -> Dict[Tuple[int, ...], np.ndarray]:
+        return self._confusion_map
+
     def _qid_shape_(self) -> Tuple[int, ...]:
         return self._qid_shape
 
@@ -98,7 +111,11 @@ class MeasurementGate(raw_types.Gate):
         if key == self.key:
             return self
         return MeasurementGate(
-            self.num_qubits(), key=key, invert_mask=self.invert_mask, qid_shape=self._qid_shape
+            self.num_qubits(),
+            key=key,
+            invert_mask=self.invert_mask,
+            qid_shape=self._qid_shape,
+            confusion_map=self.confusion_map,
         )
 
     def _with_key_path_(self, path: Tuple[str, ...]):
@@ -116,14 +133,22 @@ class MeasurementGate(raw_types.Gate):
         return self.with_key(protocols.with_measurement_key_mapping(self.mkey, key_map))
 
     def with_bits_flipped(self, *bit_positions: int) -> 'MeasurementGate':
-        """Toggles whether or not the measurement inverts various outputs."""
+        """Toggles whether or not the measurement inverts various outputs.
+
+        This only affects the invert_mask, which is applied after confusion
+        matrices if any are defined.
+        """
         old_mask = self.invert_mask or ()
         n = max(len(old_mask) - 1, *bit_positions) + 1
         new_mask = [k < len(old_mask) and old_mask[k] for k in range(n)]
         for b in bit_positions:
             new_mask[b] = not new_mask[b]
         return MeasurementGate(
-            self.num_qubits(), key=self.key, invert_mask=tuple(new_mask), qid_shape=self._qid_shape
+            self.num_qubits(),
+            key=self.key,
+            invert_mask=tuple(new_mask),
+            qid_shape=self._qid_shape,
+            confusion_map=self.confusion_map,
         )
 
     def full_invert_mask(self) -> Tuple[bool, ...]:
@@ -166,12 +191,17 @@ class MeasurementGate(raw_types.Gate):
         self, args: 'cirq.CircuitDiagramInfoArgs'
     ) -> 'cirq.CircuitDiagramInfo':
         symbols = ['M'] * self.num_qubits()
+        flipped_indices = {i for i, x in enumerate(self.full_invert_mask()) if x}
+        confused_indices = {x for idxs in self.confusion_map for x in idxs}
 
-        # Show which output bits are negated.
-        if self.invert_mask:
-            for i, b in enumerate(self.invert_mask):
-                if b:
-                    symbols[i] = '!M'
+        # Show which output bits are negated and/or confused.
+        for i in range(self.num_qubits()):
+            prefix = ''
+            if i in flipped_indices:
+                prefix += '!'
+            if i in confused_indices:
+                prefix += '?'
+            symbols[i] = prefix + symbols[i]
 
         # Mention the measurement key.
         label_map = args.label_map or {}
@@ -184,7 +214,7 @@ class MeasurementGate(raw_types.Gate):
         return protocols.CircuitDiagramInfo(symbols)
 
     def _qasm_(self, args: 'cirq.QasmArgs', qubits: Tuple['cirq.Qid', ...]) -> Optional[str]:
-        if not all(d == 2 for d in self._qid_shape):
+        if self.confusion_map or not all(d == 2 for d in self._qid_shape):
             return NotImplemented
         args.validate_version('2.0')
         invert_mask = self.invert_mask
@@ -202,7 +232,7 @@ class MeasurementGate(raw_types.Gate):
     def _quil_(
         self, qubits: Tuple['cirq.Qid', ...], formatter: 'cirq.QuilFormatter'
     ) -> Optional[str]:
-        if not all(d == 2 for d in self._qid_shape):
+        if self.confusion_map or not all(d == 2 for d in self._qid_shape):
             return NotImplemented
         invert_mask = self.invert_mask
         if len(invert_mask) < len(qubits):
@@ -222,28 +252,39 @@ class MeasurementGate(raw_types.Gate):
             args.append(f'key={self.mkey!r}')
         if self.invert_mask:
             args.append(f'invert_mask={self.invert_mask!r}')
+        if self.confusion_map:
+            proper_map_str = ', '.join(
+                f"{k!r}: {_compat.proper_repr(v)}" for k, v in self.confusion_map.items()
+            )
+            args.append(f'confusion_map={{{proper_map_str}}}')
         arg_list = ', '.join(args)
         return f'cirq.measure({arg_list})'
 
     def __repr__(self):
-        qid_shape_arg = ''
+        args = [f'{self.num_qubits()!r}', f'{self.mkey!r}', f'{self.invert_mask}']
         if any(d != 2 for d in self._qid_shape):
-            qid_shape_arg = f', {self._qid_shape!r}'
-        return (
-            f'cirq.MeasurementGate('
-            f'{self.num_qubits()!r}, '
-            f'{self.mkey!r}, '
-            f'{self.invert_mask}'
-            f'{qid_shape_arg})'
-        )
+            args.append(f'qid_shape={self._qid_shape!r}')
+        if self.confusion_map:
+            proper_map_str = ', '.join(
+                f"{k!r}: {_compat.proper_repr(v)}" for k, v in self.confusion_map.items()
+            )
+            args.append(f'confusion_map={{{proper_map_str}}}')
+        return f'cirq.MeasurementGate({", ".join(args)})'
 
     def _value_equality_values_(self) -> Any:
-        return self.key, self.invert_mask, self._qid_shape
+        hashable_cmap = frozenset(
+            (idxs, tuple(v for _, v in np.ndenumerate(cmap)))
+            for idxs, cmap in self._confusion_map.items()
+        )
+        return self.key, self.invert_mask, self._qid_shape, hashable_cmap
 
     def _json_dict_(self) -> Dict[str, Any]:
-        other = {}
+        other: Dict[str, Any] = {}
         if not all(d == 2 for d in self._qid_shape):
             other['qid_shape'] = self._qid_shape
+        if self.confusion_map:
+            json_cmap = [(k, v.tolist()) for k, v in self.confusion_map.items()]
+            other['confusion_map'] = json_cmap
         return {
             'num_qubits': len(self._qid_shape),
             'key': self.key,
@@ -252,12 +293,15 @@ class MeasurementGate(raw_types.Gate):
         }
 
     @classmethod
-    def _from_json_dict_(cls, num_qubits, key, invert_mask, qid_shape=None, **kwargs):
+    def _from_json_dict_(
+        cls, num_qubits, key, invert_mask, qid_shape=None, confusion_map=None, **kwargs
+    ):
         return cls(
             num_qubits=num_qubits,
             key=value.MeasurementKey.parse_serialized(key),
             invert_mask=tuple(invert_mask),
             qid_shape=None if qid_shape is None else tuple(qid_shape),
+            confusion_map={tuple(k): np.array(v) for k, v in confusion_map or []},
         )
 
     def _has_stabilizer_effect_(self) -> Optional[bool]:
@@ -268,7 +312,7 @@ class MeasurementGate(raw_types.Gate):
 
         if not isinstance(sim_state, SimulationState):
             return NotImplemented
-        sim_state.measure(qubits, self.key, self.full_invert_mask())
+        sim_state.measure(qubits, self.key, self.full_invert_mask(), self.confusion_map)
         return True
 
 

cirq-core/cirq/sim/simulation_state.py
@@ -100,7 +100,13 @@ class SimulationState(SimulationStateBase, Generic[TState], metaclass=abc.ABCMet
     def prng(self) -> np.random.RandomState:
         return self._prng
 
-    def measure(self, qubits: Sequence['cirq.Qid'], key: str, invert_mask: Sequence[bool]):
+    def measure(
+        self,
+        qubits: Sequence['cirq.Qid'],
+        key: str,
+        invert_mask: Sequence[bool],
+        confusion_map: Dict[Tuple[int, ...], np.ndarray],
+    ):
         """Measures the qubits and records to `log_of_measurement_results`.
 
         Any bitmasks will be applied to the measurement record.
@@ -111,12 +117,14 @@ class SimulationState(SimulationStateBase, Generic[TState], metaclass=abc.ABCMet
                 that operations should only store results under keys they have
                 declared in a `_measurement_key_names_` method.
             invert_mask: The invert mask for the measurement.
+            confusion_map: The confusion matrices for the measurement.
 
         Raises:
             ValueError: If a measurement key has already been logged to a key.
         """
         bits = self._perform_measurement(qubits)
-        corrected = [bit ^ (bit < 2 and mask) for bit, mask in zip(bits, invert_mask)]
+        confused = self._confuse_result(bits, qubits, confusion_map)
+        corrected = [bit ^ (bit < 2 and mask) for bit, mask in zip(confused, invert_mask)]
         self._classical_data.record_measurement(
             value.MeasurementKey.parse_serialized(key), corrected, qubits
         )
@@ -129,6 +137,27 @@ class SimulationState(SimulationStateBase, Generic[TState], metaclass=abc.ABCMet
         if self._state is not None:
             return self._state.measure(self.get_axes(qubits), self.prng)
         raise NotImplementedError()
+
+    def _confuse_result(
+        self,
+        bits: List[int],
+        qubits: Sequence['cirq.Qid'],
+        confusion_map: Dict[Tuple[int, ...], np.ndarray],
+    ):
+        """Applies confusion matrices to measured results.
+
+        Compare with _confuse_results in cirq-core/cirq/sim/simulator.py.
+        """
+        confused = list(bits)
+        dims = [q.dimension for q in qubits]
+        for indices, confuser in confusion_map.items():
+            mat_dims = [dims[k] for k in indices]
+            row = value.big_endian_digits_to_int((bits[k] for k in indices), base=mat_dims)
+            new_val = self.prng.choice(len(confuser), p=confuser[row])
+            new_bits = value.big_endian_int_to_digits(new_val, base=mat_dims)
+            for i, k in enumerate(indices):
+                confused[k] = new_bits[i]
+        return confused
 
     def sample(
         self,

cirq-core/cirq/ops/measurement_gate_test.py
@@ -40,6 +40,11 @@ def test_measure_init(num_qubits):
     )
     assert cirq.MeasurementGate(num_qubits, 'a', invert_mask=(True,)).invert_mask == (True,)
     assert cirq.qid_shape(cirq.MeasurementGate(num_qubits, 'a')) == (2,) * num_qubits
+    cmap = {(0,): np.array([[0, 1], [1, 0]])}
+    assert cirq.MeasurementGate(num_qubits, 'a', confusion_map=cmap).confusion_map == cmap
+
+
+def test_measure_init_num_qubit_agnostic():
     assert cirq.qid_shape(cirq.MeasurementGate(3, 'a', qid_shape=(1, 2, 3))) == (1, 2, 3)
     assert cirq.qid_shape(cirq.MeasurementGate(key='a', qid_shape=(1, 2, 3))) == (1, 2, 3)
     with pytest.raises(ValueError, match='len.* >'):
@@ -48,6 +53,8 @@ def test_measure_init(num_qubits):
         cirq.MeasurementGate(5, 'a', qid_shape=(1, 2))
     with pytest.raises(ValueError, match='valid string'):
         cirq.MeasurementGate(2, qid_shape=(1, 2), key=None)
+    with pytest.raises(ValueError, match='Confusion matrices have index out of bounds'):
+        cirq.MeasurementGate(1, 'a', confusion_map={(1,): np.array([[0, 1], [1, 0]])})
     with pytest.raises(ValueError, match='Specify either'):
         cirq.MeasurementGate()
 
@@ -68,9 +75,13 @@ def test_measurement_eq():
         lambda: cirq.MeasurementGate(1, 'a'),
         lambda: cirq.MeasurementGate(1, 'a', invert_mask=()),
         lambda: cirq.MeasurementGate(1, 'a', qid_shape=(2,)),
+        lambda: cirq.MeasurementGate(1, 'a', confusion_map={}),
     )
     eq.add_equality_group(cirq.MeasurementGate(1, 'a', invert_mask=(True,)))
     eq.add_equality_group(cirq.MeasurementGate(1, 'a', invert_mask=(False,)))
+    eq.add_equality_group(
+        cirq.MeasurementGate(1, 'a', confusion_map={(0,): np.array([[0, 1], [1, 0]])})
+    )
     eq.add_equality_group(cirq.MeasurementGate(1, 'b'))
     eq.add_equality_group(cirq.MeasurementGate(2, 'a'))
     eq.add_equality_group(
@@ -160,6 +171,31 @@ def test_qudit_measure_quil():
         )
 
 
+def test_confused_measure_qasm():
+    q0 = cirq.LineQubit(0)
+    assert (
+        cirq.qasm(
+            cirq.measure(q0, key='a', confusion_map={(0,): np.array([[0, 1], [1, 0]])}),
+            args=cirq.QasmArgs(),
+            default='not implemented',
+        )
+        == 'not implemented'
+    )
+
+
+def test_confused_measure_quil():
+    q0 = cirq.LineQubit(0)
+    qubit_id_map = {q0: '0'}
+    with cirq.testing.assert_deprecated(deadline='v1.0', count=3):
+        assert (
+            cirq.quil(
+                cirq.measure(q0, key='a', confusion_map={(0,): np.array([[0, 1], [1, 0]])}),
+                formatter=cirq.QuilFormatter(qubit_id_map=qubit_id_map, measurement_id_map={}),
+            )
+            is None
+        )
+
+
 def test_measurement_gate_diagram():
     # Shows key.
     assert cirq.circuit_diagram_info(
@@ -200,6 +236,26 @@ b: ───M───
 a: ───!M───
       │
 b: ───M────
+""",
+    )
+    cirq.testing.assert_has_diagram(
+        cirq.Circuit(cirq.measure(a, b, confusion_map={(1,): np.array([[0, 1], [1, 0]])})),
+        """
+a: ───M────
+      │
+b: ───?M───
+""",
+    )
+    cirq.testing.assert_has_diagram(
+        cirq.Circuit(
+            cirq.measure(
+                a, b, invert_mask=(False, True), confusion_map={(1,): np.array([[0, 1], [1, 0]])}
+            )
+        ),
+        """
+a: ───M─────
+      │
+b: ───!?M───
 """,
     )
     cirq.testing.assert_has_diagram(
@@ -281,11 +337,37 @@ def test_op_repr():
         "key=cirq.MeasurementKey(name='out'), "
         "invert_mask=(False, True))"
     )
+    assert repr(
+        cirq.measure(
+            a,
+            b,
+            key='out',
+            invert_mask=(False, True),
+            confusion_map={(0,): np.array([[0, 1], [1, 0]], dtype=np.int64)},
+        )
+    ) == (
+        "cirq.measure(cirq.LineQubit(0), cirq.LineQubit(1), "
+        "key=cirq.MeasurementKey(name='out'), "
+        "invert_mask=(False, True), "
+        "confusion_map={(0,): np.array([[0, 1], [1, 0]], dtype=np.int64)})"
+    )
+
+
+def test_repr():
+    gate = cirq.MeasurementGate(
+        3, 'a', (True, False), (1, 2, 3), {(2,): np.array([[0, 1], [1, 0]], dtype=np.int64)}
+    )
+    assert repr(gate) == (
+        "cirq.MeasurementGate(3, cirq.MeasurementKey(name='a'), (True, False), "
+        "qid_shape=(1, 2, 3), confusion_map={(2,): np.array([[0, 1], [1, 0]], dtype=np.int64)})"
+    )
 
 
 def test_act_on_state_vector():
     a, b = [cirq.LineQubit(3), cirq.LineQubit(1)]
-    m = cirq.measure(a, b, key='out', invert_mask=(True,))
+    m = cirq.measure(
+        a, b, key='out', invert_mask=(True,), confusion_map={(1,): np.array([[0, 1], [1, 0]])}
+    )
 
     args = cirq.StateVectorSimulationState(
         available_buffer=np.empty(shape=(2, 2, 2, 2, 2)),
@@ -295,7 +377,7 @@ def test_act_on_state_vector():
         dtype=np.complex64,
     )
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [1, 0]}
+    assert args.log_of_measurement_results == {'out': [1, 1]}
 
     args = cirq.StateVectorSimulationState(
         available_buffer=np.empty(shape=(2, 2, 2, 2, 2)),
@@ -307,7 +389,7 @@ def test_act_on_state_vector():
         dtype=np.complex64,
     )
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [1, 1]}
+    assert args.log_of_measurement_results == {'out': [1, 0]}
 
     args = cirq.StateVectorSimulationState(
         available_buffer=np.empty(shape=(2, 2, 2, 2, 2)),
@@ -321,16 +403,18 @@ def test_act_on_state_vector():
     cirq.act_on(m, args)
     datastore = cast(cirq.ClassicalDataDictionaryStore, args.classical_data)
     out = cirq.MeasurementKey('out')
-    assert args.log_of_measurement_results == {'out': [0, 1]}
-    assert datastore.records[out] == [(0, 1)]
+    assert args.log_of_measurement_results == {'out': [0, 0]}
+    assert datastore.records[out] == [(0, 0)]
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [0, 1]}
-    assert datastore.records[out] == [(0, 1), (0, 1)]
+    assert args.log_of_measurement_results == {'out': [0, 0]}
+    assert datastore.records[out] == [(0, 0), (0, 0)]
 
 
 def test_act_on_clifford_tableau():
     a, b = [cirq.LineQubit(3), cirq.LineQubit(1)]
-    m = cirq.measure(a, b, key='out', invert_mask=(True,))
+    m = cirq.measure(
+        a, b, key='out', invert_mask=(True,), confusion_map={(1,): np.array([[0, 1], [1, 0]])}
+    )
     # The below assertion does not fail since it ignores non-unitary operations
     cirq.testing.assert_all_implemented_act_on_effects_match_unitary(m)
 
@@ -340,7 +424,7 @@ def test_act_on_clifford_tableau():
         prng=np.random.RandomState(),
     )
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [1, 0]}
+    assert args.log_of_measurement_results == {'out': [1, 1]}
 
     args = cirq.CliffordTableauSimulationState(
         tableau=cirq.CliffordTableau(num_qubits=5, initial_state=8),
@@ -349,7 +433,7 @@ def test_act_on_clifford_tableau():
     )
 
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [1, 1]}
+    assert args.log_of_measurement_results == {'out': [1, 0]}
 
     args = cirq.CliffordTableauSimulationState(
         tableau=cirq.CliffordTableau(num_qubits=5, initial_state=10),
@@ -359,16 +443,18 @@ def test_act_on_clifford_tableau():
     cirq.act_on(m, args)
     datastore = cast(cirq.ClassicalDataDictionaryStore, args.classical_data)
     out = cirq.MeasurementKey('out')
-    assert args.log_of_measurement_results == {'out': [0, 1]}
-    assert datastore.records[out] == [(0, 1)]
+    assert args.log_of_measurement_results == {'out': [0, 0]}
+    assert datastore.records[out] == [(0, 0)]
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [0, 1]}
-    assert datastore.records[out] == [(0, 1), (0, 1)]
+    assert args.log_of_measurement_results == {'out': [0, 0]}
+    assert datastore.records[out] == [(0, 0), (0, 0)]
 
 
 def test_act_on_stabilizer_ch_form():
     a, b = [cirq.LineQubit(3), cirq.LineQubit(1)]
-    m = cirq.measure(a, b, key='out', invert_mask=(True,))
+    m = cirq.measure(
+        a, b, key='out', invert_mask=(True,), confusion_map={(1,): np.array([[0, 1], [1, 0]])}
+    )
     # The below assertion does not fail since it ignores non-unitary operations
     cirq.testing.assert_all_implemented_act_on_effects_match_unitary(m)
 
@@ -376,14 +462,14 @@ def test_act_on_stabilizer_ch_form():
         qubits=cirq.LineQubit.range(5), prng=np.random.RandomState(), initial_state=0
     )
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [1, 0]}
+    assert args.log_of_measurement_results == {'out': [1, 1]}
 
     args = cirq.StabilizerChFormSimulationState(
         qubits=cirq.LineQubit.range(5), prng=np.random.RandomState(), initial_state=8
     )
 
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [1, 1]}
+    assert args.log_of_measurement_results == {'out': [1, 0]}
 
     args = cirq.StabilizerChFormSimulationState(
         qubits=cirq.LineQubit.range(5), prng=np.random.RandomState(), initial_state=10
@@ -391,16 +477,22 @@ def test_act_on_stabilizer_ch_form():
     cirq.act_on(m, args)
     datastore = cast(cirq.ClassicalDataDictionaryStore, args.classical_data)
     out = cirq.MeasurementKey('out')
-    assert args.log_of_measurement_results == {'out': [0, 1]}
-    assert datastore.records[out] == [(0, 1)]
+    assert args.log_of_measurement_results == {'out': [0, 0]}
+    assert datastore.records[out] == [(0, 0)]
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [0, 1]}
-    assert datastore.records[out] == [(0, 1), (0, 1)]
+    assert args.log_of_measurement_results == {'out': [0, 0]}
+    assert datastore.records[out] == [(0, 0), (0, 0)]
 
 
 def test_act_on_qutrit():
     a, b = [cirq.LineQid(3, dimension=3), cirq.LineQid(1, dimension=3)]
-    m = cirq.measure(a, b, key='out', invert_mask=(True,))
+    m = cirq.measure(
+        a,
+        b,
+        key='out',
+        invert_mask=(True,),
+        confusion_map={(1,): np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])},
+    )
 
     args = cirq.StateVectorSimulationState(
         available_buffer=np.empty(shape=(3, 3, 3, 3, 3)),
@@ -412,7 +504,7 @@ def test_act_on_qutrit():
         dtype=np.complex64,
     )
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [2, 2]}
+    assert args.log_of_measurement_results == {'out': [2, 0]}
 
     args = cirq.StateVectorSimulationState(
         available_buffer=np.empty(shape=(3, 3, 3, 3, 3)),
@@ -424,7 +516,7 @@ def test_act_on_qutrit():
         dtype=np.complex64,
     )
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [2, 1]}
+    assert args.log_of_measurement_results == {'out': [2, 2]}
 
     args = cirq.StateVectorSimulationState(
         available_buffer=np.empty(shape=(3, 3, 3, 3, 3)),
@@ -436,4 +528,4 @@ def test_act_on_qutrit():
         dtype=np.complex64,
     )
     cirq.act_on(m, args)
-    assert args.log_of_measurement_results == {'out': [0, 2]}
+    assert args.log_of_measurement_results == {'out': [0, 0]}

commit 25388af7d96edb88ef320a08374e8b9b871126b3

Guard confusion_map usage (#5534)
Fixes the breakage created by #5480.

cirq-core/cirq/ops/measurement_gate.py
@@ -312,7 +312,19 @@ class MeasurementGate(raw_types.Gate):
 
         if not isinstance(sim_state, SimulationState):
             return NotImplemented
-        sim_state.measure(qubits, self.key, self.full_invert_mask(), self.confusion_map)
+        try:
+            sim_state.measure(
+                qubits, self.key, self.full_invert_mask(), confusion_map=self.confusion_map
+            )
+        except TypeError as e:
+            # Ensure that the error was due to confusion_map.
+            if not any("unexpected keyword argument 'confusion_map'" in arg for arg in e.args):
+                raise
+            _compat._warn_or_error(
+                "Starting in v0.16, SimulationState subclasses will be required to accept "
+                "a 'confusion_map' argument. See SimulationState.measure for details."
+            )
+            sim_state.measure(qubits, self.key, self.full_invert_mask())
         return True
 
 

cirq-core/cirq/sim/simulation_state.py
@@ -105,7 +105,7 @@ class SimulationState(SimulationStateBase, Generic[TState], metaclass=abc.ABCMet
         qubits: Sequence['cirq.Qid'],
         key: str,
         invert_mask: Sequence[bool],
-        confusion_map: Dict[Tuple[int, ...], np.ndarray],
+        confusion_map: Optional[Dict[Tuple[int, ...], np.ndarray]] = None,
     ):
         """Measures the qubits and records to `log_of_measurement_results`.
 
@@ -123,7 +123,8 @@ class SimulationState(SimulationStateBase, Generic[TState], metaclass=abc.ABCMet
             ValueError: If a measurement key has already been logged to a key.
         """
         bits = self._perform_measurement(qubits)
-        confused = self._confuse_result(bits, qubits, confusion_map)
+        if confusion_map is not None:
+            confused = self._confuse_result(bits, qubits, confusion_map)
         corrected = [bit ^ (bit < 2 and mask) for bit, mask in zip(confused, invert_mask)]
         self._classical_data.record_measurement(
             value.MeasurementKey.parse_serialized(key), corrected, qubits

cirq-core/cirq/ops/measurement_gate_test.py
@@ -12,11 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import cast
+from typing import Any, Dict, Optional, Sequence, Tuple, Union, cast
 import numpy as np
 import pytest
 
 import cirq
+from cirq.type_workarounds import NotImplementedType
 
 
 @pytest.mark.parametrize(
@@ -529,3 +530,57 @@ def test_act_on_qutrit():
     )
     cirq.act_on(m, args)
     assert args.log_of_measurement_results == {'out': [0, 0]}
+
+
+def test_act_on_no_confusion_map_deprecated():
+    class OldSimState(cirq.StateVectorSimulationState):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.measured = False
+
+        def _act_on_fallback_(
+            self, action: Any, qubits: Sequence['cirq.Qid'], allow_decompose: bool = True
+        ) -> Union[bool, NotImplementedType]:
+            return NotImplemented  # coverage: ignore
+
+        def measure(  # type: ignore
+            self, qubits: Sequence['cirq.Qid'], key: str, invert_mask: Sequence[bool]
+        ):
+            self.measured = True
+
+    qubits = cirq.LineQubit.range(2)
+    old_state = OldSimState(qubits=qubits)
+    m = cirq.measure(*qubits, key='test')
+    with cirq.testing.assert_deprecated('confusion_map', deadline='v0.16'):
+        cirq.act_on(m, old_state)
+    assert old_state.measured
+
+
+def test_act_on_no_confusion_map_scope_limited():
+    error_msg = "error from deeper in measure"
+
+    class ErrorProneSimState(cirq.StateVectorSimulationState):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.measured = False
+
+        def _act_on_fallback_(
+            self, action: Any, qubits: Sequence['cirq.Qid'], allow_decompose: bool = True
+        ) -> Union[bool, NotImplementedType]:
+            return NotImplemented  # coverage: ignore
+
+        def measure(
+            self,
+            qubits: Sequence['cirq.Qid'],
+            key: str,
+            invert_mask: Sequence[bool],
+            confusion_map: Optional[Dict[Tuple[int, ...], np.ndarray]] = None,
+        ):
+            raise TypeError(error_msg)
+
+    # Verify that the check doesn't prevent other errors from being raised
+    qubits = cirq.LineQubit.range(2)
+    sv_state = ErrorProneSimState(qubits=qubits)
+    m = cirq.measure(*qubits, key='test')
+    with pytest.raises(TypeError, match=error_msg):
+        cirq.act_on(m, sv_state)