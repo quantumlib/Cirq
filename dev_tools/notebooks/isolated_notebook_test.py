30654272aa2a47289ae0f57ca787af16e7ac9327
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2022-06-23 09:09:50 -0700

    whoops other way

--- dev_tools/notebooks/isolated_notebook_test.py
+++ dev_tools/notebooks/isolated_notebook_test.py
@@ -55,7 +55,7 @@ NOTEBOOKS_DEPENDING_ON_UNRELEASED_FEATURES: List[str] = [
     'docs/noise/qcvv/xeb_calibration_example.ipynb',
     'docs/named_topologies.ipynb',
     'docs/representing_noise.ipynb',
-    'docs/tutorials/educators/intro.ipynb',
+    'docs/start/intro.ipynb',
 ]
 
 # By default all notebooks should be tested, however, this list contains exceptions to the rule
