51f82ed01eb138f3e41e7a8415e827e58ee97919
daxfohl <dax.fohl@gmail.com>
2022-06-23 22:26:02 -0700

    mypy

--- cirq-core/cirq/circuits/circuit_operation.py
+++ cirq-core/cirq/circuits/circuit_operation.py
@@ -313,12 +313,14 @@ class CircuitOperation(ops.Operation):
         if circuit_keys and self.use_repetition_ids:
             self._ensure_deterministic_loop_count()
             if self.repetition_ids is not None:
-                circuit_keys = {
+                circuit_keys = frozenset(
                     key.with_key_path_prefix(repetition_id)
                     for repetition_id in self.repetition_ids
                     for key in circuit_keys
-                }
-        circuit_keys = {key.with_key_path_prefix(*self.parent_path) for key in circuit_keys}
+                )
+        circuit_keys = frozenset(
+            key.with_key_path_prefix(*self.parent_path) for key in circuit_keys
+        )
         return frozenset(
             protocols.with_measurement_key_mapping(key, dict(self.measurement_key_map))
             for key in circuit_keys
