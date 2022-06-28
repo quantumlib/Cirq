81894ae33479fb32300385b81c7fcad214ffe63f
Pavol Juhas <juhas@google.com>
2022-06-28 14:11:20 -0700

    Fix typing of the `default` argument in protocols.commutes
    Require the `default` argument of the `commutes()` function
    to be bool, None, or a local marker-type for raising TypeError.
    Use dedicated local marker-type _TRaiseTypeErrorIfNotProvided
    instead of np.array instance.

--- cirq-core/cirq/contrib/paulistring/pauli_string_dag.py
+++ cirq-core/cirq/contrib/paulistring/pauli_string_dag.py
@@ -21,7 +21,9 @@ from cirq.contrib import circuitdag
 def pauli_string_reorder_pred(op1: ops.Operation, op2: ops.Operation) -> bool:
     ps1 = cast(ops.PauliStringGateOperation, op1).pauli_string
     ps2 = cast(ops.PauliStringGateOperation, op2).pauli_string
-    return protocols.commutes(ps1, ps2)
+    result = protocols.commutes(ps1, ps2)
+    assert result is not None, "commutes should raise exception when indeterminate"
+    return result
 
 
 def pauli_string_dag_from_circuit(circuit: circuits.Circuit) -> circuitdag.CircuitDag:
