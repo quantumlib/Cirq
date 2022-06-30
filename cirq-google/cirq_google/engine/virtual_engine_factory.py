f292e6ca5ec7a0b94ccba2a92a60524140a20187
Auguste Hirth <augusteh@google.com>
2022-06-30 15:07:36 -0700

    SimulatedLocalEngine creation helper function

--- cirq-google/cirq_google/engine/virtual_engine_factory.py
+++ cirq-google/cirq_google/engine/virtual_engine_factory.py
@@ -14,16 +14,19 @@
 
 """Functions to instantiate SimulatedLocalEngines to simulate various Google Devices."""
 import json
-from typing import cast, Iterable, List, Optional, Union
+from typing import cast, Iterable, List, Optional, Union, Type
 import pathlib
 import time
 
 import google.protobuf.text_format as text_format
 import cirq
+from cirq.sim.simulator import SimulatesSamples
 from cirq_google.api import v2
 from cirq_google.engine import calibration, engine_validator, simulated_local_processor, util
 from cirq_google.devices import grid_device
+from cirq_google.devices.google_noise_properties import NoiseModelFromGoogleNoiseProperties
 from cirq_google.serialization import serializable_gate_set
+from cirq_google.engine.calibration_to_noise_properties import noise_properties_from_calibration
 from cirq_google.engine.simulated_local_engine import SimulatedLocalEngine
 from cirq_google.engine.simulated_local_processor import SimulatedLocalProcessor
 
@@ -367,3 +370,35 @@ def create_noiseless_virtual_engine_from_latest_templates() -> SimulatedLocalEng
     processor_ids = list(MOST_RECENT_TEMPLATES.keys())
     template_names = [MOST_RECENT_TEMPLATES[k] for k in processor_ids]
     return create_noiseless_virtual_engine_from_templates(processor_ids, template_names)
+
+def create_noisy_virtual_engine_from_processor_id_and_simulator_class(
+    processor_id: str, simulator_class: Type[SimulatesSamples], **kwargs
+) -> SimulatedLocalEngine:
+    """Creates a virtual engine with a noisy simulator based on a processor id.
+
+    Args:
+        processor_id: The string name of a processor that has available noise data.
+        simulator_class: The class of the type of simulator to be initialized.
+        kwargs: Other arguments which are passed through to the simulator initializer.
+            The 'noise' argument will be overwritten with a new noise model.
+
+    Returns:
+        A SimulatedLocalEngine that uses a simulator of type simulator_class with a
+            noise model based on available noise data for the processor processor_id.
+    """
+
+    calibration = load_median_device_calibration(processor_id)
+    noise_properties = noise_properties_from_calibration(calibration)
+    noise_model = NoiseModelFromGoogleNoiseProperties(noise_properties)
+    kwargs["noise"] = noise_model
+    simulator = simulator_class(**kwargs)
+
+    device = create_device_from_processor_id(processor_id)
+    simulated_processor = SimulatedLocalProcessor(
+        processor_id=processor_id,
+        sampler=simulator,
+        device=device,
+        calibrations={calibration.timestamp // 1000: calibration},
+    )
+
+    return SimulatedLocalEngine([simulated_processor])
