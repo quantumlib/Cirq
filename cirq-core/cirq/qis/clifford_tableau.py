413b304bb98ae5195f893952aa19872402298d43
vtomole <vtomole2@gmail.com>
2022-07-06 18:07:01 -0500

    Remove otpional

--- cirq-core/cirq/qis/clifford_tableau.py
+++ cirq-core/cirq/qis/clifford_tableau.py
@@ -441,13 +441,11 @@ class CliffordTableau(StabilizerState):
         generators above generate the full Pauli group on n qubits."""
         return [self._row_to_dense_pauli(i) for i in range(self.n)]
 
-    def _measure(self, q, prng: Optional[np.random.RandomState]) -> int:
+    def _measure(self, q, prng: np.random.RandomState) -> int:
         """Performs a projective measurement on the q'th qubit.
 
         Returns: the result (0 or 1) of the measurement.
         """
-        if prng is None:
-            prng = np.random
         is_commuting = True
         for i in range(self.n, 2 * self.n):
             if self.xs[i, q]:
@@ -585,6 +583,6 @@ class CliffordTableau(StabilizerState):
         pass
 
     def measure(
-        self, axes: Sequence[int], seed: Optional['cirq.RANDOM_STATE_OR_SEED_LIKE'] = None
+        self, axes: Sequence[int], seed: 'cirq.RANDOM_STATE_OR_SEED_LIKE' = np.random
     ) -> List[int]:
         return [self._measure(axis, seed) for axis in axes]
