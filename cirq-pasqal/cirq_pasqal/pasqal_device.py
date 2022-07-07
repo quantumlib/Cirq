2d8fa079171a4e3534747a9d9f092e973da150c5
Michael Broughton <mbbrough@google.com>
2022-07-07 01:02:11 -0700

    lint.

--- cirq-pasqal/cirq_pasqal/pasqal_device.py
+++ cirq-pasqal/cirq_pasqal/pasqal_device.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Callable, List, Sequence, Any, Union, Dict
+from typing import Sequence, Any, Union, Dict
 import numpy as np
 import networkx as nx
 
