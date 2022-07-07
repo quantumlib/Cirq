dd98f55f448499659bb061f1dd5eb75bf8692b13
vtomole <vtomole2@gmail.com>
2022-07-07 14:37:38 -0500

    Lint

--- cirq-core/cirq/qis/clifford_tableau_test.py
+++ cirq-core/cirq/qis/clifford_tableau_test.py
@@ -59,10 +59,14 @@ def test_tableau_ivalid_initial_state():
     with pytest.raises(ValueError, match="1 row, of even length and of type bool."):
         cirq.CliffordTableau(1, rs=np.zeros(1, dtype=bool))
 
-    with pytest.raises(ValueError, match="2*num_qubits rows, num_qubits columns, and of type bool."):
+    with pytest.raises(
+        ValueError, match="2*num_qubits rows, num_qubits columns, and of type bool."
+    ):
         cirq.CliffordTableau(1, xs=np.zeros(1, dtype=bool))
 
-    with pytest.raises(ValueError, match="2*num_qubits rows, num_qubits columns, and of type bool."):
+    with pytest.raises(
+        ValueError, match="2*num_qubits rows, num_qubits columns, and of type bool."
+    ):
         cirq.CliffordTableau(1, zs=np.zeros(1, dtype=bool))
 
 
