e1f7cb777474fe6a136b93e164fe0e2e93bbf130
Doug Strain <dstrain@google.com>
2022-07-11 11:10:25 -0700

    Remove CrossEntropyResult and CrossEntropyResultDict (#5687)
    - Also remove the whole cross_entropy_benchmarking.py
        since those were the last exposed objects from it.
    - Note that these were JSON serializable objects and the
        replacements are in reCirq.  Thus, we just deserialize to
        a dictionary of values instead.

--- cirq-core/cirq/protocols/json_test_data/spec.py
+++ cirq-core/cirq/protocols/json_test_data/spec.py
@@ -182,7 +182,7 @@ TestSpec = ModuleJsonTestSpec(
         'Unique',
         'DEFAULT_RESOLVERS',
     ],
-    deprecated={'CrossEntropyResult': 'v0.16', 'CrossEntropyResultDict': 'v0.16'},
+    deprecated={},
     tested_elsewhere=[
         # SerializableByKey does not follow common serialization rules.
         # It is tested separately in test_context_serialization.
