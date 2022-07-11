11aaefcf8cb2ba1b496cc7afd788ee3709f118a5
Doug Strain <dstrain@google.com>
2022-07-11 11:38:30 -0700

    Remove deprecated netural_atoms classes (#5695)
    - Removes NeutralAtomDevice and ConvertToNeutralAtomGates

--- cirq-core/cirq/__init__.py
+++ cirq-core/cirq/__init__.py
@@ -654,12 +654,7 @@ from cirq.protocols import (
 )
 
 from cirq.ion import ConvertToIonGates, IonDevice
-from cirq.neutral_atoms import (
-    ConvertToNeutralAtomGates,
-    is_native_neutral_atom_gate,
-    is_native_neutral_atom_op,
-    NeutralAtomDevice,
-)
+from cirq.neutral_atoms import is_native_neutral_atom_gate, is_native_neutral_atom_op
 
 from cirq.vis import (
     Heatmap,
