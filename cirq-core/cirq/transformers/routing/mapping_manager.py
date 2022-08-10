618fb3e4a04a10b16e9c62241fb4ea38203dd9a1
Ammar Eltigani <ammartigani12@gmail.com>
2022-08-10 16:31:36 -0700

    ran continuous integration checks

--- cirq-core/cirq/transformers/routing/mapping_manager.py
+++ cirq-core/cirq/transformers/routing/mapping_manager.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import networkx as nx
 from typing import Dict, TYPE_CHECKING
+import networkx as nx
 
 from cirq import ops, protocols
 
@@ -24,15 +24,12 @@ if TYPE_CHECKING:
 class MappingManager:
     """Class that keeps track of the mapping of logical to physical qubits and provides
     convenience methods for distance queries on the physical qubits.
-    
+
     Qubit variables with the characer 'p' preppended to them are physical and qubits with  the
     character 'l' preppended to them are logical qubits.
     """
-    def __init__(
-        self,
-        device_graph: nx.Graph,
-        initial_mapping: Dict[ops.Qid, ops.Qid]
-        ) -> None:
+
+    def __init__(self, device_graph: nx.Graph, initial_mapping: Dict[ops.Qid, ops.Qid]) -> None:
         """Initializes MappingManager.
 
         Args:
@@ -40,9 +37,9 @@ class MappingManager:
             circuit_graph: connectivity graph of the qubits in the input circuit.
             initial_mapping: the initial mapping of logical (keys) to physical qubits (values).
         """
-        self.device_graph = device_graph 
+        self.device_graph = device_graph
         self._map = initial_mapping.copy()
-        self._inverse_map = {v:k for k,v in self._map.items()}
+        self._inverse_map = {v: k for k, v in self._map.items()}
         self._induced_subgraph = nx.induced_subgraph(self.device_graph, self._map.values())
         self._shortest_paths_matrix = dict(nx.all_pairs_shortest_path(self._induced_subgraph))
 
@@ -64,7 +61,7 @@ class MappingManager:
     def dist_on_device(self, lq1: ops.Qid, lq2: ops.Qid) -> int:
         """Finds shortest path distance path between the corresponding physical qubits for logical
         qubits q1 and q2 on the device.
-        
+
         Args:
             lq1: the first logical qubit.
             lq2: the second logical qubit.
@@ -72,38 +69,41 @@ class MappingManager:
         Returns:
             The shortest path distance.
         """
-        return len(self._shortest_paths_matrix[self._map[lq1]][self._map[lq2]])-1
+        return len(self._shortest_paths_matrix[self._map[lq1]][self._map[lq2]]) - 1
 
     def can_execute(self, op: ops.Operation) -> bool:
         """Finds whether the given operation can be executed on the device.
-        
+
         Args:
             op: an operation on logical qubits.
 
-        Returns: 
+        Returns:
             Whether the given operation is executable on the device.
         """
         return protocols.num_qubits(op) < 2 or self.dist_on_device(*op.qubits) == 1
 
     def apply_swap(self, lq1: ops.Qid, lq2: ops.Qid) -> None:
         """Swaps two logical qubits in the map and in the inverse map.
-        
+
         Args:
             lq1: the first logical qubit.
             lq2: the second logical qubit.
         """
-        self._map[lq1], self._map[lq2] = self._map[lq2], self._map[lq1] 
+        self._map[lq1], self._map[lq2] = self._map[lq2], self._map[lq1]
 
         pq1 = self._map[lq1]
         pq2 = self._map[lq2]
-        self._inverse_map[pq1], self._inverse_map[pq2] = self._inverse_map[pq2], self._inverse_map[pq1]
+        self._inverse_map[pq1], self._inverse_map[pq2] = (
+            self._inverse_map[pq2],
+            self._inverse_map[pq1],
+        )
 
     def mapped_op(self, op: ops.Operation) -> ops.Operation:
         """Transforms the given operation with the qubits in self._map.
-        
+
         Args:
             op: an operation on logical qubits.
-            
+
         Returns:
             The same operation on corresponding physical qubits."""
         return op.transform_qubits(self._map)
--- cirq-core/cirq/transformers/routing/mapping_manager_test.py
+++ cirq-core/cirq/transformers/routing/mapping_manager_test.py
@@ -13,27 +13,43 @@
 # limitations under the License.
 
 import networkx as nx
+from networkx.utils.misc import graphs_equal
 
 import cirq
 
 
 def test_mapping_manager():
-    device_graph = nx.Graph([
-        (cirq.NamedQubit("a"), cirq.NamedQubit("b")),
-        (cirq.NamedQubit("b"), cirq.NamedQubit("c")),
-        (cirq.NamedQubit("c"), cirq.NamedQubit("d")),
-
-        (cirq.NamedQubit("a"), cirq.NamedQubit("e")),
-        (cirq.NamedQubit("e"), cirq.NamedQubit("d")),
-        ])
+    device_graph = nx.Graph(
+        [
+            (cirq.NamedQubit("a"), cirq.NamedQubit("b")),
+            (cirq.NamedQubit("b"), cirq.NamedQubit("c")),
+            (cirq.NamedQubit("c"), cirq.NamedQubit("d")),
+            (cirq.NamedQubit("a"), cirq.NamedQubit("e")),
+            (cirq.NamedQubit("e"), cirq.NamedQubit("d")),
+        ]
+    )
     q = cirq.LineQubit.range(5)
     initial_mapping = {
         q[1]: cirq.NamedQubit("a"),
         q[3]: cirq.NamedQubit("b"),
         q[2]: cirq.NamedQubit("c"),
         q[4]: cirq.NamedQubit("d"),
-        }
+    }
     mm = cirq.transformers.routing.MappingManager(device_graph, initial_mapping)
+
+    # test correct induced subgraph
+    expected_induced_subgraph = nx.Graph(
+        [
+            (cirq.NamedQubit("a"), cirq.NamedQubit("b")),
+            (cirq.NamedQubit("b"), cirq.NamedQubit("c")),
+            (cirq.NamedQubit("c"), cirq.NamedQubit("d")),
+        ]
+    )
+    assert graphs_equal(mm.induced_subgraph, expected_induced_subgraph)
+
+    # test mapped_op
+    mapped_one_three = mm.mapped_op(cirq.CNOT(q[1], q[3]))
+    assert mapped_one_three.qubits == (cirq.NamedQubit("a"), cirq.NamedQubit("b"))
 
     # adjacent qubits have distance 1 and are thus executable
     assert mm.dist_on_device(q[1], q[3]) == 1
@@ -52,25 +68,33 @@ def test_mapping_manager():
     assert mm.can_execute(cirq.CNOT(q[1], q[2]))
     assert mm.dist_on_device(q[1], q[3]) == 2
     assert mm.can_execute(cirq.CNOT(q[1], q[3])) is False
-
     # the swapped qubits are still executable
     assert mm.can_execute(cirq.CNOT(q[2], q[3]))
-
     # distance between other qubits doesn't change
     assert mm.dist_on_device(q[1], q[4]) == 3
-
     # test applying swaps to inverse map is correct
-    mm.inverse_map == {v:k for k,v in mm.map.items()}
+    assert mm.inverse_map == {v: k for k, v in mm.map.items()}
+    # test mapped_op after switching qubits
+    mapped_one_two = mm.mapped_op(cirq.CNOT(q[1], q[2]))
+    assert mapped_one_two.qubits == (cirq.NamedQubit("a"), cirq.NamedQubit("b"))
 
     # apply same swap and test shortest path for a couple pairs
     mm.apply_swap(q[3], q[2])
-    assert mm.shortest_path(q[1], q[2]) == [cirq.NamedQubit("a"), cirq.NamedQubit("b"), cirq.NamedQubit("c")]
+    assert mm.shortest_path(q[1], q[2]) == [
+        cirq.NamedQubit("a"),
+        cirq.NamedQubit("b"),
+        cirq.NamedQubit("c"),
+    ]
     assert mm.shortest_path(q[2], q[3]) == [cirq.NamedQubit("c"), cirq.NamedQubit("b")]
     assert mm.shortest_path(q[1], q[3]) == [cirq.NamedQubit("a"), cirq.NamedQubit("b")]
 
-    shortest_one_to_four = [cirq.NamedQubit("a"), cirq.NamedQubit("b"), cirq.NamedQubit("c"), cirq.NamedQubit("d")]
+    shortest_one_to_four = [
+        cirq.NamedQubit("a"),
+        cirq.NamedQubit("b"),
+        cirq.NamedQubit("c"),
+        cirq.NamedQubit("d"),
+    ]
     assert mm.shortest_path(q[1], q[4]) == shortest_one_to_four
 
     # shortest path on symmetric qubit reverses the list
     assert mm.shortest_path(q[4], q[1]) == shortest_one_to_four[::-1]
-
