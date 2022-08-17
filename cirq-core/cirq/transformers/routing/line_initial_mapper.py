9ecb52a607b4353ae4c7e74e362f52c8502e0566
Ammar Eltigani <ammartigani12@gmail.com>
2022-08-17 14:13:18 -0700

    changed interface for LineInitialMapper and added better tests; test for directed graph value equality remaining

--- cirq-core/cirq/transformers/routing/line_initial_mapper.py
+++ cirq-core/cirq/transformers/routing/line_initial_mapper.py
@@ -14,10 +14,11 @@
 
 """Concrete implementation of AbstractInitialMapper that places lines of qubits onto the device."""
 
+from re import X
 from typing import Dict, Optional, TYPE_CHECKING
 import networkx as nx
 
-from cirq import circuits, value
+from cirq import value, _compat
 from cirq.transformers import routing
 
 if TYPE_CHECKING:
@@ -28,18 +29,15 @@ if TYPE_CHECKING:
 class LineInitialMapper(routing.AbstractInitialMapper):
     """Places logical qubits in the circuit onto physical qubits on the device."""
 
-    def __init__(self, device_graph: nx.Graph, circuit: circuits.AbstractCircuit):
+    def __init__(self, device_graph: nx.Graph) -> None:
         """Initializes a LineInitialMapper.
+
         Args:
             device_graph: device graph
-            circuit_graph: circuit graph
         """
         self.device_graph = device_graph
-        self.circuit = circuit
-        self.circuit_graph = self._make_circuit_graph()
-        self._map: Dict['cirq.Qid', 'cirq.Qid'] = {}
 
-    def _make_circuit_graph(self) -> nx.Graph:
+    def _make_circuit_graph(self, circuit: 'cirq.AbstractCircuit') -> nx.Graph:
         """Creates a (potentially incomplete) qubit connectivity graph of the circuit.
 
         Iterates over the moments circuit from left to right drawing edges between logical qubits
@@ -49,12 +47,15 @@ class LineInitialMapper(routing.AbstractInitialMapper):
         At this point the graph is forest of paths and/or simple cycles. For each simple cycle, make
         it a path by removing the last edge that was added to it.
 
+        Args:
+            circuit: the input circuit with logical qubits
+
         Returns:
-            The qubit connectivity graph of the circuit.
+            The (potentially incomplete) qubit connectivity graph of the circuit.
         """
         circuit_graph = nx.Graph()
         edge_order = 0
-        for op in self.circuit.all_operations():
+        for op in circuit.all_operations():
             circuit_graph.add_nodes_from(op.qubits)
             if len(op.qubits) == 2 and all(
                 circuit_graph.degree[op.qubits[i]] < 2 for i in range(2)
@@ -73,7 +74,7 @@ class LineInitialMapper(routing.AbstractInitialMapper):
                 found = False
         return circuit_graph
 
-    def initial_mapping(self) -> Dict['cirq.Qid', 'cirq.Qid']:
+    def initial_mapping(self, circuit: 'cirq.AbstractCircuit') -> Dict['cirq.Qid', 'cirq.Qid']:
         """Maps disjoint lines of logical qubits onto lines of physical qubits.
 
         Starting from the center physical qubit on the device, attempts to map disjoint lines of
@@ -86,13 +87,30 @@ class LineInitialMapper(routing.AbstractInitialMapper):
             (iii) Map the second line segment
             (iv)  etc.
 
+        Args:
+            circuit: the input circuit with logical qubits
+
         Returns:
             a dictionary that maps logical qubits in the circuit (keys) to physical qubits on the
             device (values).
         """
-        if len(self._map) is not 0:
-            return self._map
+        return self._initial_mapping(circuit.freeze())
 
+    @_compat.cached_method
+    def _initial_mapping(self, circuit: 'cirq.FrozenCircuit') -> Dict['cirq.Qid', 'cirq.Qid']:
+        """Maps disjoint lines of logical qubits onto lines of physical qubits.
+
+        Helper for 'initial_mapping' that takes a (hashable) frozen circuit to cache the result.
+
+        Args:
+            circuit: the input circuit with logical qubits
+
+        Returns:
+            a dictionary that maps logical qubits in the circuit (keys) to physical qubits on the
+            device (values).
+        """
+        qubit_map: Dict['cirq.Qid', 'cirq.Qid'] = {}
+        circuit_graph = self._make_circuit_graph(circuit)
         physical_center = nx.center(self.device_graph)[0]
 
         def next_physical(current_physical: 'cirq.Qid') -> 'cirq.Qid':
@@ -112,55 +130,70 @@ class LineInitialMapper(routing.AbstractInitialMapper):
             return self._closest_unmapped_qubit(physical_center)
 
         def next_logical(current_logical: 'cirq.Qid') -> Optional['cirq.Qid']:
-            for neighbor in self.circuit_graph.neighbors(current_logical):
-                if self.circuit_graph.nodes[neighbor]["mapped"] is False:
+            for neighbor in circuit_graph.neighbors(current_logical):
+                if circuit_graph.nodes[neighbor]["mapped"] is False:
                     return neighbor
             return None
 
         for pq in self.device_graph.nodes:
             self.device_graph.nodes[pq]["mapped"] = False
-        for lq in self.circuit_graph.nodes:
-            self.circuit_graph.nodes[lq]["mapped"] = False
+        for lq in circuit_graph.nodes:
+            circuit_graph.nodes[lq]["mapped"] = False
 
         current_physical = physical_center
-        for logical_cc in nx.connected_components(self.circuit_graph):
+        for logical_cc in nx.connected_components(circuit_graph):
             if len(logical_cc) == 1:
                 continue
+            # logical_cc is a set, make it a sorted list to guarantee deterministic behavior
+            logical_cc = sorted(logical_cc)
 
             current_physical = next_physical(current_physical)
             # start by mapping a logical line from one of its endpoints.
-            current_logical = next(q for q in logical_cc if self.circuit_graph.degree(q) == 1)
+            current_logical = next(q for q in logical_cc if circuit_graph.degree(q) == 1)
 
             while current_logical is not None:
                 self.device_graph.nodes[current_physical]["mapped"] = True
-                self.circuit_graph.nodes[current_logical]["mapped"] = True
-                self._map[current_logical] = current_physical
-                current_physical = next_physical(current_physical)
+                circuit_graph.nodes[current_logical]["mapped"] = True
+                qubit_map[current_logical] = current_physical
                 current_logical = next_logical(current_logical)
+                if current_logical is not None:
+                    current_physical = next_physical(current_physical)
 
-        self._map_remaining_qubits()
-        return self._map
+        self._map_remaining_qubits(circuit, circuit_graph, qubit_map)
+        return qubit_map
 
-    def _map_remaining_qubits(self) -> None:
-        # map logical qubits that interact in 'self.circuit' but have missing edges in the circuit
-        # graph
-        for op in self.circuit.all_operations():
+    def _map_remaining_qubits(
+        self,
+        circuit: 'cirq.AbstractCircuit',
+        circuit_graph: nx.Graph,
+        qubit_map: Dict['cirq.Qid', 'cirq.Qid'],
+    ) -> None:
+        """Maps remaining qubits that are not incident to edges in the circuit_graph.
+
+        First maps logical qubits that interact in circuit but have missing edges in the circuit
+        graph. Then maps logical qubits that don't interact with any other logical qubits in the
+        circuit.
+
+        Args:
+            circuit_graph: the (potentially incomplete) qubit connectivity graph of the circuit.
+            qubit_map: the mapping of logical to physical qubits done so far.
+        """
+        for op in circuit.all_operations():
             if len(op.qubits) == 2:
                 q1, q2 = op.qubits
-                if q1 not in self._map.keys():
-                    physical = self._closest_unmapped_qubit(self._map[q2])
-                    self._map[q1] = physical
+                if q1 not in qubit_map.keys():
+                    physical = self._closest_unmapped_qubit(qubit_map[q2])
+                    qubit_map[q1] = physical
                     self.device_graph.nodes[physical]["mapped"] = True
                 # 'elif' because at least one must be mapped already
-                elif q2 not in self._map.keys():
-                    physical = self._closest_unmapped_qubit(self._map[q1])
-                    self._map[q2] = physical
+                elif q2 not in qubit_map.keys():
+                    physical = self._closest_unmapped_qubit(qubit_map[q1])
+                    qubit_map[q2] = physical
                     self.device_graph.nodes[physical]["mapped"] = True
 
-        # map logical qubits that don't interact with any other logical qubits in the circuit
-        for isolated_qubit in (q for q in self.circuit_graph.nodes if q not in self._map):
-            physical = self._closest_unmapped_qubit(self._map[next(iter(self._map))])
-            self._map[isolated_qubit] = physical
+        for isolated_qubit in (q for q in circuit_graph.nodes if q not in qubit_map):
+            physical = self._closest_unmapped_qubit(qubit_map[next(iter(qubit_map))])
+            qubit_map[isolated_qubit] = physical
             self.device_graph.nodes[physical]["mapped"] = True
 
     def _closest_unmapped_qubit(self, source: 'cirq.Qid') -> 'cirq.Qid':
@@ -181,18 +214,28 @@ class LineInitialMapper(routing.AbstractInitialMapper):
                     return successor
         raise ValueError("No available physical qubits left on the device.")
 
-    def _value_equality_values_(self):
-        graph_equality = (
+    # Tanuj, should this be made a helper method somewhere since it is the same code being used
+    # by MappingManager too (and likely subsequent initial mapping strategies)? Particularly,
+    # since there is no good way of representating a graph using a hashable object, the
+    # 'graph_equality' bit here can become standard or we can file an issue for it.
+    def _value_equality_values_(self) -> None:
+        """Two LineInitialMapper(s) are equal if they execute on the same device graph."""
+        if nx.is_directed(self.device_graph):
+            return (
+                tuple(sorted(self.device_graph.nodes)),
+                tuple(sorted(self.device_graph.edges)),
+                True,
+            )
+        return (
             tuple(sorted(self.device_graph.nodes)),
             tuple(sorted(tuple(sorted(edge)) for edge in self.device_graph.edges)),
-            nx.is_directed(self.device_graph),
+            False,
         )
-        map_equality = tuple(sorted(self.initial_mapping().items()))
-        return (graph_equality, map_equality, self.circuit.freeze())
 
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
         adj_dict = dict(self.device_graph.adjacency())
-        return f'cirq.LineInitialMapper(nx.Graph({adj_dict}), {repr(self.circuit)})'
+        graph_type = 'nx.DiGraph' if nx.is_directed(self.device_graph) else 'nx.Graph'
+        return f'cirq.LineInitialMapper({graph_type}({adj_dict}))'
--- cirq-core/cirq/transformers/routing/line_initial_mapper_test.py
+++ cirq-core/cirq/transformers/routing/line_initial_mapper_test.py
@@ -30,27 +30,62 @@ def construct_small_circuit():
     )
 
 
-def construct_large_circuit(k: int):
+def construct_step_circuit(k: int):
     q = cirq.LineQubit.range(k)
     return cirq.Circuit([cirq.CNOT(q[i], q[i + 1]) for i in range(k - 1)])
 
 
-# TODO: finish this
-def test_large_circuit_grid_device():
-    # also call with grid device too small in size for # of qubits (expect failure)
-    pass
+def test_line_breaking_on_grid_device():
+    # tests
+    #   -if strategy is able to map into several small lines if fails to map onto one long line
+    #   -if # of physical qubits <= # of logical qubits then strategy should succeed
+
+    step_circuit = construct_step_circuit(49)
+    device = cirq.testing.construct_grid_device(7)
+    device_graph = device.metadata.nx_graph
+    mapper = cirq.LineInitialMapper(device_graph)
+    mapping = mapper.initial_mapping(step_circuit)
+    mapped_circuit = step_circuit.transform_qubits(mapping)
+
+    # all qubits in the input circuit are placed on the device
+    assert set(mapping.keys()) == set(step_circuit.all_qubits())
+
+    # the first two moments are executable
+    device.validate_circuit(mapped_circuit[:2])
+
+    # the induced graph of the device on the physical qubits in the map is connected
+    assert nx.is_connected(nx.induced_subgraph(device_graph, mapping.values()))
+
+    step_circuit = construct_step_circuit(50)
+    with pytest.raises(ValueError, match="No available physical qubits left on the device"):
+        mapper.initial_mapping(step_circuit)
 
 
-# TODO: finish this
-def test_small_circuit_grid_device():
-    # call initial_mapping() twice (expect the same thing)
-    pass
+def test_small_circuit_on_grid_device():
+    circuit = construct_small_circuit()
+
+    device_graph = cirq.testing.construct_grid_device(7).metadata.nx_graph
+    mapper = cirq.LineInitialMapper(device_graph)
+    mapping = mapper.initial_mapping(circuit)
+
+    assert nx.center(device_graph)[0] == cirq.GridQubit(3, 3)
+    mapped_circuit = circuit.transform_qubits(mapping)
+    diagram = """(3, 2): ───────────X───
+
+(3, 3): ───@───────────
+           │
+(4, 2): ───┼───────@───
+           │       │
+(4, 3): ───X───X───X───
+               │
+(5, 3): ───────@───────"""
+    cirq.testing.assert_has_diagram(mapped_circuit, diagram)
 
 
 @pytest.mark.parametrize(
     "qubits, n_moments, op_density, random_state",
     [
-        (10 * size, 20 * size, 0.4, seed)
+        (10 * size, 20 * size, density, seed)
         for size in range(1, 3)
         for seed in range(10)
         for density in [0.4, 0.5, 0.6]
@@ -64,48 +99,41 @@ def test_random_circuits_grid_device(
     )
     device = cirq.testing.construct_grid_device(7)
     device_graph = device.metadata.nx_graph
-    mapper = cirq.LineInitialMapper(circuit=c_orig, device_graph=device_graph)
-    mapping = mapper.initial_mapping()
+    mapper = cirq.LineInitialMapper(device_graph)
+    mapping = mapper.initial_mapping(c_orig)
     c_mapped = c_orig.transform_qubits(mapping)
 
-    # all qubits in the input circuit are placed on the device
     assert set(mapping.keys()) == set(c_orig.all_qubits())
 
-    # the first two moments are executable
     device.validate_circuit(c_mapped[:2])
 
-    # the induced graph of the device on the physical qubits in the map is connected
     assert nx.is_connected(nx.induced_subgraph(device_graph, mapping.values()))
 
 
 def test_value_equality():
-    # equality across device_graphs already tested in mapping_manager_test. Check for equality
-    # across circuits
-    device_graph = cirq.testing.construct_grid_device(7).metadata.nx_graph
     equals_tester = cirq.testing.EqualsTester()
-    small_one = construct_small_circuit()
-    small_two = construct_small_circuit()
-    equals_tester.add_equality_group(
-        cirq.LineInitialMapper(device_graph, small_one),
-        cirq.LineInitialMapper(device_graph, small_two),
-    )
-    equals_tester.add_equality_group(
-        cirq.LineInitialMapper(device_graph, construct_large_circuit(30))
-    )
+
+    small_circuit = construct_small_circuit()
+    step_circuit = construct_step_circuit(5)
+
+    # undirected case
+    mapper_one = cirq.LineInitialMapper(cirq.testing.construct_grid_device(7).metadata.nx_graph)
+    mapper_one.initial_mapping(small_circuit)
+    mapper_two = cirq.LineInitialMapper(cirq.testing.construct_grid_device(7).metadata.nx_graph)
+    mapper_one.initial_mapping(step_circuit)
+    equals_tester.add_equality_group(mapper_one, mapper_two)
+
+    mapper_three = cirq.LineInitialMapper(cirq.testing.construct_grid_device(6).metadata.nx_graph)
+    equals_tester.add_equality_group(mapper_three)
 
 
 def test_str():
-    circuit = construct_small_circuit()
     device_graph = cirq.testing.construct_grid_device(7).metadata.nx_graph
-    mapper = cirq.LineInitialMapper(device_graph, circuit)
-    assert (
-        str(mapper)
-        == f'cirq.LineInitialMapper(nx.Graph({dict(device_graph.adjacency())}), {repr(circuit)})'
-    )
+    mapper = cirq.LineInitialMapper(device_graph)
+    assert str(mapper) == f'cirq.LineInitialMapper(nx.Graph({dict(device_graph.adjacency())}))'
 
 
 def test_repr():
-    circuit = construct_small_circuit()
     device_graph = cirq.testing.construct_grid_device(7).metadata.nx_graph
-    mapper = cirq.LineInitialMapper(device_graph, circuit)
+    mapper = cirq.LineInitialMapper(device_graph)
     cirq.testing.assert_equivalent_repr(mapper, setup_code='import cirq\nimport networkx as nx')
