7819263f51f9bdb39502efd8dd3373c6c2c7aacb
Ammar Eltigani <ammartigani12@gmail.com>
2022-08-16 17:11:37 -0700

    added grid testing device

---
+++ cirq-core/cirq/testing/routing_devices.py
@@ -0,0 +1,65 @@
+# Copyright 2021 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Provides test devices that can validate circuits during a routing procedure."""
+
+from typing import Optional, TYPE_CHECKING
+import networkx as nx
+
+from cirq import devices
+
+if TYPE_CHECKING:
+    import cirq
+
+
+class RoutingTestingDevice(devices.Device):
+    """Testing device to be used only for testing qubit connectivity in routing procedures."""
+
+    def __init__(self, metadata: devices.DeviceMetadata) -> None:
+        self._metadata = metadata
+
+    @property
+    def metadata(self) -> Optional[devices.DeviceMetadata]:
+        return self._metadata
+
+    def validate_operation(self, operation: 'cirq.Operation') -> None:
+        for q in operation.qubits:
+            if q not in self._metadata.qubit_set:
+                raise ValueError(f'Qubit not on device: {q!r}.')
+
+        if (
+            len(operation.qubits) == 2
+            and operation.qubits[0] not in self._metadata.nx_graph[operation.qubits[1]]
+        ):
+            raise ValueError(f'Qubit pair is not valid on device: {operation.qubits!r}.')
+
+
+def construct_grid_device(d: int) -> RoutingTestingDevice:
+    qubits = devices.GridQubit.square(d)
+
+    nx_graph = nx.Graph()
+    row_edges = [
+        (devices.GridQubit(i, j), devices.GridQubit(i, j + 1))
+        for i in range(d)
+        for j in range(d - 1)
+    ]
+    col_edges = [
+        (devices.GridQubit(i, j), devices.GridQubit(i + 1, j))
+        for j in range(d)
+        for i in range(d - 1)
+    ]
+    nx_graph.add_edges_from(row_edges)
+    nx_graph.add_edges_from(col_edges)
+
+    metadata = devices.DeviceMetadata(qubits, nx_graph)
+    return RoutingTestingDevice(metadata)
---
+++ cirq-core/cirq/testing/routing_devices_test.py
@@ -0,0 +1,46 @@
+
+# Copyright 2021 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pytest
+import cirq
+import networkx as nx
+
+from cirq.testing.routing_devices import construct_grid_device
+
+
+def test_grid_device():
+    device = construct_grid_device(5)
+    device_graph = device.metadata.nx_graph
+
+    assert all(q in device_graph.nodes for q in cirq.GridQubit.square(5))
+    assert nx.is_isomorphic(nx.grid_2d_graph(5, 5), device_graph)
+
+
+def test_op_validation():
+    device = construct_grid_device(5)
+
+    with pytest.raises(ValueError, match="Qubit not on device"):
+        device.validate_operation(cirq.X(cirq.NamedQubit("a")))
+    with pytest.raises(ValueError, match="Qubit not on device"):
+        device.validate_operation(cirq.CNOT(cirq.NamedQubit("a"), cirq.GridQubit(0, 0)))
+    with pytest.raises(ValueError, match="Qubit not on device"):
+        device.validate_operation(cirq.CNOT(cirq.GridQubit(5, 4), cirq.GridQubit(4, 4)))
+
+    with pytest.raises(ValueError, match="Qubit pair is not valid on device"):
+        device.validate_operation(cirq.CNOT(cirq.GridQubit(0, 0), cirq.GridQubit(0, 2)))
+    with pytest.raises(ValueError, match="Qubit pair is not valid on device"):
+        device.validate_operation(cirq.CNOT(cirq.GridQubit(2, 0), cirq.GridQubit(0, 0)))
+
+    device.validate_operation(cirq.CNOT(cirq.GridQubit(0, 0), cirq.GridQubit(0, 1)))
+    device.validate_operation(cirq.CNOT(cirq.GridQubit(1, 0), cirq.GridQubit(0, 0)))
