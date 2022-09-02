b12a13d4dcc8a3682828d3e04dfe1ed71205dc64
Tanuj Khattar <tanujkhattar@google.com>
2022-09-02 16:09:34 -0700

    Address comments

--- benchmarks/circuit_construction.py
+++ benchmarks/circuit_construction.py
@@ -11,35 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Set
+import itertools
+from typing import Tuple, Set, Sequence
 
 import cirq
 
 
-def get_qubits(distance: int):
-    data_qubits: Set[cirq.Qid] = set()
-    z_measure_qubits: Set[cirq.Qid] = set()
-    x_measure_qubits: Set[cirq.Qid] = set()
-    for x in range(distance + 1):
-        for y in range(distance + 1):
-            if x < distance and y < distance:
-                data_qubits.add(cirq.GridQubit(2 * x + 1, 2 * y + 1))
-            on_boundary_x = (x == 0) or (x == distance)
-            on_boundary_y = (y == 0) or (y == distance)
-            parity = x % 2 != y % 2
-            if (on_boundary_x and parity) or (on_boundary_y and not parity):
-                continue
-            if parity:
-                z_measure_qubits.add(cirq.GridQubit(2 * x, 2 * y))
-            else:
-                x_measure_qubits.add(cirq.GridQubit(2 * x, 2 * y))
-    return data_qubits, z_measure_qubits, x_measure_qubits
-
-
 def rotated_surface_code_memory_z_cycle(
-    data_qubits, z_measure_qubits, x_measure_qubits, z_order, x_order
-) -> cirq.OP_TREE:
-    yield cirq.Moment([cirq.H(q) for q in x_measure_qubits])
+    data_qubits: Set[cirq.Qid],
+    z_measure_qubits: Set[cirq.Qid],
+    x_measure_qubits: Set[cirq.Qid],
+    z_order: Sequence[Tuple[int, int]],
+    x_order: Sequence[Tuple[int, int]],
+) -> cirq.Circuit:
+    """Constructs a circuit for a single round of rotated memory Z surface code.
+
+    Args:
+        data_qubits: data qubits for the surface code patch.
+        z_measure_qubits: measure qubits to measure Z stabilizers for surface code patch.
+        x_measure_qubits: measure qubits to measure X stabilizers for surface code patch.
+        z_order: Specifies the order in which the 2/4 data qubit neighbours of a Z measure qubit
+            should be processed.
+        x_order: Specifies the order in which the 2/4 data qubit neighbours of a X measure qubit
+            should be processed.
+
+    Returns:
+        A `cirq.Circuit` for a single round of rotated memory Z surface code cycle.
+    """
+
+    circuit = cirq.Circuit()
+    circuit += cirq.Moment([cirq.H(q) for q in x_measure_qubits])
     for k in range(4):
         op_list = []
         for measure_qubits, add, is_x in [
@@ -50,21 +51,46 @@ def rotated_surface_code_memory_z_cycle(
                 q_data = q_meas + add
                 if q_data in data_qubits:
                     op_list.append(cirq.CNOT(q_meas, q_data) if is_x else cirq.CNOT(q_data, q_meas))
-        yield cirq.Moment(op_list)
-    yield cirq.Moment([cirq.H(q) for q in x_measure_qubits])
-    yield cirq.Moment(cirq.measure_each(*x_measure_qubits, *z_measure_qubits))
+        circuit += cirq.Moment(op_list)
+    circuit += cirq.Moment([cirq.H(q) for q in x_measure_qubits])
+    circuit += cirq.Moment(cirq.measure_each(*x_measure_qubits, *z_measure_qubits))
+    return circuit
 
 
 def surface_code_circuit(
     distance: int, num_rounds: int, moment_by_moment: bool = True
 ) -> cirq.Circuit:
-    data_qubits, z_measure_qubits, x_measure_qubits = get_qubits(distance)
+    """Constructs a rotated memory Z surface code circuit with `distance` and `num_rounds`.
+
+    The circuit has `dxd` data qubits and `d ** 2 - 1` measure qubits, where `d` is the distance
+    of surface code. For more details on rotated surface codes and qubit indexing, see figure 13
+    https://arxiv.org/abs/1111.4022.
+
+    Args:
+        distance: Distance of the surface code.
+        num_rounds: Number of error correction rounds for memory Z experiment.
+        moment_by_moment: If True, the circuit is constructed moment-by-moment instead of
+            operation-by-operation. This is useful to benchmark different circuit construction
+            patterns for the same circuit.
+
+    Returns:
+        A `cirq.Circuit` for surface code memory Z experiment for `distance` and `num_rounds`.
+    """
+
+    def ndrange(*ranges: Tuple[int, ...]):
+        return itertools.product(*[range(*r) for r in ranges])
+
+    data_qubits = {cirq.q(2 * x + 1, 2 * y + 1) for x, y in ndrange((distance,), (distance,))}
+    z_measure_qubits = {
+        cirq.q(2 * x, 2 * y) for x, y in ndrange((1, distance), (distance + 1,)) if x % 2 != y % 2
+    }
+    x_measure_qubits = {
+        cirq.q(2 * x, 2 * y) for x, y in ndrange((distance + 1,), (1, distance)) if x % 2 == y % 2
+    }
     x_order = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
     z_order = [(1, 1), (-1, 1), (1, -1), (-1, -1)]
-    surface_code_cycle = cirq.Circuit(
-        rotated_surface_code_memory_z_cycle(
-            data_qubits, x_measure_qubits, z_measure_qubits, x_order, z_order
-        )
+    surface_code_cycle = rotated_surface_code_memory_z_cycle(
+        data_qubits, x_measure_qubits, z_measure_qubits, x_order, z_order
     )
     if moment_by_moment:
         return cirq.Circuit(
@@ -76,7 +102,7 @@ def surface_code_circuit(
         )
 
 
-class SurfaceCode_Rotated_Memory_Z:
+class SurfaceCodeRotatedMemoryZ:
     pretty_name = "Surface Code Rotated Memory-Z Benchmarks."
     params = [*range(3, 26, 2)]
     param_names = ["distance"]
@@ -100,7 +126,7 @@ class SurfaceCode_Rotated_Memory_Z:
         return len(circuit)
 
 
-class NDCircuit:
+class XOnAllQubitsCircuit:
     pretty_name = "N * D times X gate on all qubits."
     params = [[1, 10, 100, 1000], [1, 10, 100, 1000]]
     param_names = ["Number of Qubits(N)", "Depth(D)"]
