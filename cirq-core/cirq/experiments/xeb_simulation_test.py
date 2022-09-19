b8462e1bbf8c1e11a4aacab214cd466ea268b0a5
vtomole <vtomole2@gmail.com>
2022-09-19 15:35:14 -0500

    Removed time import

--- cirq-core/cirq/experiments/xeb_simulation_test.py
+++ cirq-core/cirq/experiments/xeb_simulation_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import multiprocessing
-import time
 from typing import Dict, Any, Optional
 from typing import Sequence
 
