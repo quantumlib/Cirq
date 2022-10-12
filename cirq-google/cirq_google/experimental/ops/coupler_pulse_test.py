96be00432a9ca8777e69bff5f2858d3d38e259a6
eliottrosenberg <61400172+eliottrosenberg@users.noreply.github.com>
2022-10-12 15:12:01 -0700

    Update coupler_pulse_test.py

--- cirq-google/cirq_google/experimental/ops/coupler_pulse_test.py
+++ cirq-google/cirq_google/experimental/ops/coupler_pulse_test.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pytest
 
 import cirq
 import cirq_google.experimental.ops.coupler_pulse as coupler_pulse
