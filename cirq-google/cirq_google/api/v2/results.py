04bd0419936fb37594323f665db2e25587c7e5db
Matthew Neeley <maffoo@google.com>
2022-10-21 14:59:30 -0700

    Remove unused import

--- cirq-google/cirq_google/api/v2/results.py
+++ cirq-google/cirq_google/api/v2/results.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import cast, Dict, Hashable, Iterable, List, Optional, Sequence
-from collections import Counter, OrderedDict
+from collections import OrderedDict
 import dataclasses
 import numpy as np
 
