7626d95a1cedeb378f373e52f805a021a9c31d59
Matthew Neeley <maffoo@google.com>
2022-12-12 17:24:23 -0800

    Add single-qubit detuning parameters to CouplerPulse

--- cirq-google/cirq_google/experimental/ops/coupler_pulse_test.py
+++ cirq-google/cirq_google/experimental/ops/coupler_pulse_test.py
@@ -86,17 +86,17 @@ def test_coupler_pulse_str_repr():
         hold_time=cirq.Duration(nanos=10), coupling_mhz=25.0, rise_time=cirq.Duration(nanos=18)
     )
     assert (
-        str(gate)
-        == 'CouplerPulse(hold_time=10 ns, coupling_mhz=25.0, '
-        + 'rise_time=18 ns, padding_time=2500.0 ps)'
+        str(gate) == 'CouplerPulse(hold_time=10 ns, coupling_mhz=25.0, '
+        'rise_time=18 ns, padding_time=2500.0 ps, q0_detune_mhz=0.0, q1_detune_mhz=0.0)'
     )
     assert (
-        repr(gate)
-        == 'cirq_google.experimental.ops.coupler_pulse.CouplerPulse('
-        + 'hold_time=cirq.Duration(nanos=10), '
-        + 'coupling_mhz=25.0, '
-        + 'rise_time=cirq.Duration(nanos=18), '
-        + 'padding_time=cirq.Duration(picos=2500.0))'
+        repr(gate) == 'cirq_google.experimental.ops.coupler_pulse.CouplerPulse('
+        'hold_time=cirq.Duration(nanos=10), '
+        'coupling_mhz=25.0, '
+        'rise_time=cirq.Duration(nanos=18), '
+        'padding_time=cirq.Duration(picos=2500.0), '
+        'q0_detune_mhz=0.0, '
+        'q1_detune_mhz=0.0)'
     )
 
 
