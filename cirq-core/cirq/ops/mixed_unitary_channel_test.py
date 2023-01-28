adf31e6296d49e2d8084138e9061130fe2b80e41
Ben Greiner <code@bnavigator.de>
2023-01-28 22:30:13 +0100

    Change numpy dtype repr

--- cirq-core/cirq/ops/mixed_unitary_channel_test.py
+++ cirq-core/cirq/ops/mixed_unitary_channel_test.py
@@ -95,16 +95,16 @@ def test_matrix_mixture_str():
 
 def test_matrix_mixture_repr():
     mix = [
-        (0.5, np.array([[1, 0], [0, 1]], dtype=np.complex64)),
-        (0.5, np.array([[0, 1], [1, 0]], dtype=np.complex64)),
+        (0.5, np.array([[1, 0], [0, 1]], dtype=np.dtype('complex64'))),
+        (0.5, np.array([[0, 1], [1, 0]], dtype=np.dtype('complex64'))),
     ]
     half_flip = cirq.MixedUnitaryChannel(mix, key='flip')
     assert (
         repr(half_flip)
         == """\
 cirq.MixedUnitaryChannel(mixture=[\
-(0.5, np.array([[(1+0j), 0j], [0j, (1+0j)]], dtype=np.complex64)), \
-(0.5, np.array([[0j, (1+0j)], [(1+0j), 0j]], dtype=np.complex64))], \
+(0.5, np.array([[(1+0j), 0j], [0j, (1+0j)]], dtype=np.dtype('complex64'))), \
+(0.5, np.array([[0j, (1+0j)], [(1+0j), 0j]], dtype=np.dtype('complex64')))], \
 key='flip')"""
     )
 
