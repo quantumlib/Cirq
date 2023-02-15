26326247764ef0b2505427d590fe72b8f59c1a03
jon donovan <donovan@ionq.co>
2023-02-15 12:53:10 -0800

    Mocks

--- cirq-ionq/cirq_ionq/ionq_client_test.py
+++ cirq-ionq/cirq_ionq/ionq_client_test.py
@@ -253,6 +253,9 @@ def test_ionq_client_get_job_retry_409(mock_get):
     mock_get.side_effect = [response1, response2]
     response1.ok = False
     response1.status_code = requests.codes.conflict
+    response1.request.method = "GET"
+    response2.ok = True
+    response2.json.return_value = {'foo': 'bar'}
 
     client = ionq.ionq_client._IonQClient(remote_host='http://example.com', api_key='to_my_heart')
     response = client.get_job(job_id='job_id')
