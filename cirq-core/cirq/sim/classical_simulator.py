28a1049234128bf313a0a765105424455f4f33cf
naerabati <naerabati@imsa.edu>
2023-04-03 22:12:30 -0500

    edited the qubit order and tests

---
+++ cirq-core/cirq/sim/classical_simulator.py
@@ -0,0 +1,71 @@
+from typing import Dict
+from collections import defaultdict
+
+import cirq
+import numpy as np 
+
+
+class ClassicalSimulator():
+
+
+
+##very basic simulator that only accepts Xgates and CNot gates and return a 3d Numpy array
+    """
+    Run a simulation, mimicking quantum hardware.
+
+        Args:
+            circuit: The circuit to simulate.
+            param_resolver: Parameters to run with the program.
+            repetitions: Number of times to repeat the run. It is expected that
+                this is validated greater than zero before calling this method.
+
+        Returns:
+            A dictionary from measurement gate key to measurement
+            results. Measurement results are stored in a 3-dimensional
+            numpy array, the first dimension corresponding to the repetition.
+            the second to the instance of that key in the circuit, and the
+            third to the actual boolean measurement results (ordered by the
+            qubits being measured.)
+ """
+    def run( circuit: 'cirq.AbstactCircuit', param_resolver: 'cirq.ParamResolver',
+        repetitions: int) -> Dict[str, np.ndarray]:
+       
+     results_dict = {}
+     values_dict = defaultdict(int)
+       
+     for moment in circuit:
+        for op in moment: 
+           gate = op.gate
+           if(isinstance(gate, cirq.XPowGate) and gate.exponent == 1):
+            values_dict[op.qubits[0]] = 1 - values_dict[op.qubits[0]]
+      
+           if(isinstance(gate, cirq.CNotPowGate) and gate.exponent == 1):
+                if values_dict[op.qubits[0]] == 1:
+                     values_dict[op.qubits[1]] = 1 - values_dict[op.qubits[1]]
+
+           if(isinstance(gate, cirq.MeasurementGate)):
+                qubits_in_order = op.qubits
+            ##add the new instance of a key to the numpy array in results dictionary
+                if(gate.key) in results_dict.keys():
+                    shape = len(qubits_in_order)
+                    current_array = results_dict[gate.key]
+                    new_instance = np.zeros(shape, dtype=np.uint8)
+                    for bits in range(0, len(qubits_in_order)):
+                        new_instance[bits] = values_dict[qubits_in_order[bits]]
+                        results_dict[gate.key] = np.insert(current_array, 1, new_instance, axis=1)
+                else:
+                    ##create the array for the results dictionary
+                     shape = (repetitions, 1, len(qubits_in_order))
+                     new_array = np.zeros(shape, dtype=np.uint8)
+                     for reps in range(0, repetitions):
+                        for instances in range(0, 1):
+                            for bits in range(0, len(qubits_in_order)):
+                                new_array[reps][instances][bits] = values_dict[qubits_in_order[bits]]
+                     results_dict[gate.key] = new_array     
+
+            
+           return results_dict 
+
+
+
+
---
+++ cirq-core/cirq/sim/classical_simulator_test.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pytest
+import cirq
+import classical_simulator 
+
+class TestSimulator():
+
+  def test_x_gate():
+    q0 = cirq.LineQubit.range(1)
+    circuit =  cirq.Circuit(cirq.X(q0), cirq.measure(cirq.LineQubit(q0), key='k'))
+    expected_results ={'key': np.array[[[1]]]}
+    simulator = ClassicalSimulator()
+    assert expect_result == classical_simulator.run(circuit)
+    
+        
+  def test_x_gate():
+    q0, q1 = cirq.LineQubit.range(2)
+    circuit  = cirq.Circuit()
+    circuit =  cirq.Circuit(cirq.X(q0), cirq.measure(cirq.LineQubit(q0), key='k'))
+    expected_results = {'key': np.array[[[0]]]}
+    results = run(circuit = circuit, param_resolver=None, repetitions = 1)
+    assert results == expected_results
+
+
+def test_CNOT():
+  q0, q1 = cirq.LineQubit.range(2)
+  circuit  = cirq.Circuit()
+  circuit =  cirq.Circuit(cirq.X(q0), cirq.CNOT(q0, q1), cirq.measure(cirq.LineQubit(q1), key='k'))
+  expected_results = {'key': np.array[[[1]]]}
+  results = run(circuit = circuit, param_resolver=None, repetitions = 1 )
+  assert results == expected_results
+
+
+def test_measurment_gate():
+  q0, q1 = cirq.LineQubit.range(2)
+  circuit  = cirq.Circuit()
+  circuit =  cirq.Circuit(cirq.measure(cirq.LineQubit(q0, q1), key='key'))
+  expected_results = {'key': np.array[[[0, 0]]]}
+  results = run(circuit = circuit, param_resolver=None, repetitions = 1 )
+  assert results == expected_results
+
+
+def test_qubit_order():
+  q0, q1 = cirq.LineQubit.range(2)
+  circuit  = cirq.Circuit()
+  circuit =  cirq.Circuit(cirq.X(q0), cirq.CNOT(q0, q1), cirq.X(q0), cirq.measure(cirq.LineQubit(q1, q0), key='key'))
+  expected_results = {'key': np.array[[[1,0]]]}
+  results = run(circuit = circuit, param_resolver=None, repetitions = 1 )
+  assert results == expected_results
+
+
+def test_same_key_instances():
+  q0, q1 = cirq.LineQubit.range(2)
+  circuit  = cirq.Circuit()
+  circuit =  cirq.Circuit(cirq.X(q0), cirq.measure(cirq.LineQubit(q0, q1), key='key'), cirq.X(q0), cirq.measure(cirq.LineQubit(q0, q1), key='key'))
+  expected_results = {'key': np.array[[[1,0],[0,0]]]}
+  results = run(circuit = circuit, param_resolver=None, repetitions = 1 )
+  assert results == expected_results
+
+def test_same_key_instances_order():
+  q0, q1 = cirq.LineQubit.range(2)
+  circuit  = cirq.Circuit()
+  circuit =  cirq.Circuit(cirq.X(q0), cirq.measure(cirq.LineQubit(q0, q1), key='key'),  cirq.measure(cirq.LineQubit(q1, q0), key='key'))
+  expected_results = {'key': np.array[[[1,0],[0,1]]]}
+  results = run(circuit = circuit, param_resolver=None, repetitions = 1 )
+  assert results == expected_results
+
+
+        
+    
