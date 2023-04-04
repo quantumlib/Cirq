fa1102c685454e94a3510eeae4201e561b53aa49
naerabati <naerabati@imsa.edu>
2023-04-04 00:12:26 -0500

    fixed some error in the test file

--- cirq-core/cirq/sim/classical_simulator_test.py
+++ cirq-core/cirq/sim/classical_simulator_test.py
@@ -7,29 +7,22 @@ class TestSimulator():
 
   def test_x_gate():
     q0 = cirq.LineQubit.range(1)
-    circuit =  cirq.Circuit(cirq.X(q0), cirq.measure(cirq.LineQubit(q0), key='k'))
-    expected_results ={'key': np.array[[[1]]]}
-    simulator = ClassicalSimulator()
-    assert expect_result == classical_simulator.run(circuit)
+    circuit  = cirq.Circuit()
+    circuit =  cirq.Circuit(cirq.X(q0), cirq.measure(cirq.LineQubit(q0), key='key'))
+    expected_results = {'key': np.array([[[0]]], dtype=np.uint8)}
+    sim = cirq.ClassicalSimulator()
+    results = sim.run(circuit = circuit, param_resolver=None, repetitions = 1 )
+    assert results == expected_results
     
-        
-  def test_x_gate():
+  def test_CNOT():
     q0, q1 = cirq.LineQubit.range(2)
     circuit  = cirq.Circuit()
-    circuit =  cirq.Circuit(cirq.X(q0), cirq.measure(cirq.LineQubit(q0), key='k'))
-    expected_results = {'key': np.array[[[0]]]}
-    results = run(circuit = circuit, param_resolver=None, repetitions = 1)
+    circuit.append(cirq.X(q0))
+    circuit.append(cirq.CNOT(q0, q1))
+    circuit.append(cirq.measure(q1, key='key'))
+    expected_results = {'key': np.array([[[1]]], dtype=np.uint8)}
+    results = run(circuit = circuit, param_resolver=None, repetitions = 1 )
     assert results == expected_results
-
-
-def test_CNOT():
-  q0, q1 = cirq.LineQubit.range(2)
-  circuit  = cirq.Circuit()
-  circuit =  cirq.Circuit(cirq.X(q0), cirq.CNOT(q0, q1), cirq.measure(cirq.LineQubit(q1), key='k'))
-  expected_results = {'key': np.array[[[1]]]}
-  results = run(circuit = circuit, param_resolver=None, repetitions = 1 )
-  assert results == expected_results
-
 
 def test_measurment_gate():
   q0, q1 = cirq.LineQubit.range(2)
