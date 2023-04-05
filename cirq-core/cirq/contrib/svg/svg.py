cde65be8b05ea1f8fbbf5765637a2c951ec04ea5
Tanuj Khattar <tanujkhattar@google.com>
2023-04-05 14:46:07 -0700

    Update cirq.contrib.svg to escape < and > characters (#6055)
    * Update cirq.contrib.svg to escape < and > characters
    
    * Add type: ignore for mypy

--- cirq-core/cirq/contrib/svg/svg.py
+++ cirq-core/cirq/contrib/svg/svg.py
@@ -23,6 +23,10 @@ def fixup_text(text: str):
     if '[cirq.VirtualTag()]' in text:
         # https://github.com/quantumlib/Cirq/issues/2905
         return text.replace('[cirq.VirtualTag()]', '')
+    if '<' in text:
+        return text.replace('<', '&lt;')
+    if '>' in text:
+        return text.replace('>', '&gt;')
     return text
 
 
