c492a3fa9a0400e58aba029eb59e3e314e0bae78
Spencer Churchill <churchill@ionq.co>
2023-04-24 09:39:27 -0700

    add _IonQClient.get_results test

--- cirq-ionq/cirq_ionq/ionq_client_test.py
+++ cirq-ionq/cirq_ionq/ionq_client_test.py
@@ -347,6 +347,26 @@ def test_ionq_client_get_job_retry(mock_get):
 
 
 @mock.patch('requests.get')
+def test_ionq_client_get_results(mock_get):
+    mock_get.return_value.ok = True
+    mock_get.return_value.json.return_value = {'foo': 'bar'}
+    client = ionq.ionq_client._IonQClient(remote_host='http://example.com', api_key='to_my_heart')
+    response = client.get_results(job_id='job_id', aggregation='average')
+    assert response == {'foo': 'bar'}
+
+    expected_headers = {
+        'Authorization': 'apiKey to_my_heart',
+        'Content-Type': 'application/json',
+        'User-Agent': client._user_agent(),
+    }
+    mock_get.assert_called_with(
+        'http://example.com/v0.3/jobs/job_id/results',
+        headers=expected_headers,
+        params={'aggregation': 'average'},
+    )
+
+
+@mock.patch('requests.get')
 def test_ionq_client_list_jobs(mock_get):
     mock_get.return_value.ok = True
     mock_get.return_value.json.return_value = {'jobs': [{'id': '1'}, {'id': '2'}]}
