2fff50581296ef2f6349224bfc48c2ea6b156992
Noureldin <noureldinyosri@gmail.com>
2023-05-12 14:34:50 +0100

    fix lint

--- cirq-core/cirq/sim/simulation_util.py
+++ cirq-core/cirq/sim/simulation_util.py
@@ -1,3 +1,16 @@
+# Copyright 2023 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from typing import Sequence, Tuple
 
 import numpy as np
@@ -5,7 +18,9 @@ import numpy as np
 from cirq import linalg
 
 
-def state_probabilities(state_vector: np.ndarray, indices: Sequence[int], qid_shape: Tuple[int, ...]) -> np.ndarray:
+def state_probabilities(
+    state_vector: np.ndarray, indices: Sequence[int], qid_shape: Tuple[int, ...]
+) -> np.ndarray:
     """Returns the probabilities for a state/measurement on the given indices.
 
     Args:
