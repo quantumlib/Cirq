fe22867ea5aa6c32ba4a99f97d08cf3de7f3f0c1
Doug Strain <dstrain@google.com>
2023-05-22 09:39:15 -0700

    Fix python 3.7 import.

--- cirq-google/cirq_google/study/parameter.py
+++ cirq-google/cirq_google/study/parameter.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, Optional, Protocol, Sequence
+from typing import Any, Dict, Optional, Sequence
+from typing_extensions import Protocol
 import dataclasses
 import cirq
 
