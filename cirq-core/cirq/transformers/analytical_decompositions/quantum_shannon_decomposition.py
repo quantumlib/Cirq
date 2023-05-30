e194580e643cb75271cac768ea397f7e8c2a1476
Uzair Faruqui <94143161+uzzzzzzz@users.noreply.github.com>
2023-05-30 13:11:34 -0400

    Update quantum_shannon_decomposition.py

--- cirq-core/cirq/transformers/analytical_decompositions/quantum_shannon_decomposition.py
+++ cirq-core/cirq/transformers/analytical_decompositions/quantum_shannon_decomposition.py
@@ -42,9 +42,23 @@ def quantum_shannon_decomposition(qubits: 'List[cirq.Qid]', u: np.ndarray) -> 'c
         qubits: List of qubits in order of significance
         u: Numpy array for unitary matrix representing gate to be decomposed
 
-    Returns:
-        List of 2-qubit and 1-qubit operations from the set
+    Calls:
+        (Base Case)
+        1._single_qubit_decomposition
+            OR
+        (Recursive Case)
+        1. _msb_demuxer
+        2. _multiplexed_cossin
+        3. _msb_demuxer
+        
+    Yields:
+        A single 2-qubit or 1-qubit operations from OP TREE
+        composed from the set
            { CNOT, rz, ry, ZPowGate }
+           
+    Raises:
+        ValueError: If the u matrix is not of shape (2^n,2^n)
+        ValueError: If the u matrix is non-unitary
     """
     if not predicates.is_unitary(u):  # Check that u is unitary
         raise ValueError(
