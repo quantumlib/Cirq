43d0c0539a9dc1204fe3504363df7724e5690b4f
Uzair Faruqui <94143161+uzzzzzzz@users.noreply.github.com>
2023-06-05 14:41:39 -0700

    Update quantum_shannon_decomposition_test.py
    Remove circuit_1 and circuit_2 from test_qsd_n_qubit

--- cirq-core/cirq/transformers/analytical_decompositions/quantum_shannon_decomposition_test.py
+++ cirq-core/cirq/transformers/analytical_decompositions/quantum_shannon_decomposition_test.py
@@ -42,9 +42,9 @@ def test_random_qsd_n_qubit(n_qubits):
 def test_qsd_n_qubit_errors():
     qubits = [cirq.NamedQubit(f'q{i}') for i in range(3)]
     with pytest.raises(ValueError, match="shaped numpy array"):
-        circuit_1 = cirq.Circuit(quantum_shannon_decomposition(qubits, np.eye(9)))
+        cirq.Circuit(quantum_shannon_decomposition(qubits, np.eye(9)))
     with pytest.raises(ValueError, match="is_unitary"):
-        circuit_2 = cirq.Circuit(quantum_shannon_decomposition(qubits, np.ones((8, 8))))
+        cirq.Circuit(quantum_shannon_decomposition(qubits, np.ones((8, 8))))
 
 
 def test_random_single_qubit_decomposition():
