7617bbd84884ed3a512b237840f8c197092aeaca
Noureldin <noureldinyosri@gmail.com>
2023-06-05 10:58:10 +0100

    fix coverage

--- cirq-core/cirq/protocols/apply_unitary_protocol_test.py
+++ cirq-core/cirq/protocols/apply_unitary_protocol_test.py
@@ -757,3 +757,13 @@ def test_strat_apply_unitary_from_decompose():
             [cirq.LineQubit(0)],
             args,
         )
+
+
+def test_unitary_construction():
+    with pytest.raises(TypeError):
+        _ = cirq.ApplyUnitaryArgs.for_unitary()
+
+    np.testing.assert_allclose(
+        cirq.ApplyUnitaryArgs.for_unitary(num_qubits=3).target_tensor,
+        cirq.eye_tensor((2,) * 3, dtype=np.complex128),
+    )
