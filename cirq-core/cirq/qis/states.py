291b5fb8f29b37fdf6ab6548dd27f5a405485961
S Jeeva <43230676+jeeva2812@users.noreply.github.com>
2023-07-04 15:59:21 +0000

    Handle shape mismatch for dirac_notation

--- cirq-core/cirq/qis/states.py
+++ cirq-core/cirq/qis/states.py
@@ -718,6 +718,13 @@ def dirac_notation(
     if qid_shape is None:
         qid_shape = (2,) * (len(state_vector).bit_length() - 1)
 
+    if len(state_vector) != np.prod(qid_shape, dtype=np.int64):
+        raise ValueError(
+            'state_vector has incorrect size. Expected {} but was {}.'.format(
+                np.prod(qid_shape, dtype=np.int64), len(state_vector)
+            )
+        )
+
     digit_separator = '' if max(qid_shape, default=0) < 10 else ','
     perm_list = [
         digit_separator.join(seq)
