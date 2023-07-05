a3b94ba3d8305c7ea47a070322eeb17c513a53a9
S Jeeva <43230676+jeeva2812@users.noreply.github.com>
2023-07-05 04:12:09 +0000

    Fix lint issues

--- cirq-core/cirq/qis/states_test.py
+++ cirq-core/cirq/qis/states_test.py
@@ -476,9 +476,9 @@ def test_dirac_notation_precision():
 
 
 def test_dirac_notation_invalid():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='state_vector has incorrect size'):
         _ = cirq.dirac_notation([0.0, 0.0, 1.0])
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='state_vector has incorrect size'):
         _ = cirq.dirac_notation([1.0, 1.0], qid_shape=(3,))
 
 
