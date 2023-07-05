b2f783530a4e04255c2043b1e94bd035be1183de
naerabati <naerabati@imsa.edu>
2023-07-05 14:45:10 -0500

    reverted files

--- cirq-core/cirq/sim/classical_simulator_test.py
+++ cirq-core/cirq/sim/classical_simulator_test.py
@@ -51,7 +51,9 @@ class TestSimulator:
         circuit.append(cirq.X(q0))
         circuit.append(cirq.CCNOT(q0, q1, q2))
         circuit.append(cirq.measure((q0, q1, q2), key='key'))
-        expected_results = {'key': np.array([[[0, 0, 0], [1, 0, 0], [0, 1, 0] [1, 1, 1]]], dtype=np.uint8)}
+        expected_results = {
+            'key': np.array([[[0, 0, 0], [1, 0, 0], [0, 1, 0][1, 1, 1]]], dtype=np.uint8)
+        }
         sim = cirq.ClassicalSimulator()
         results = sim._run(circuit=circuit, param_resolver=None, repetitions=1)
         np.testing.assert_equal(results, expected_results)
