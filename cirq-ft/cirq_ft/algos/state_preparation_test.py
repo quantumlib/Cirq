4ae87c7202c0a4553c9b7609c62b6ce74f4e58ce
Tanuj Khattar <tanujkhattar@google.com>
2023-07-17 14:54:33 -0700

    Simplify StatePreparationAliasSampling tests by relying on Cirq simulators capability to simulate operations allocating ancilla qubits

--- cirq-ft/cirq_ft/algos/state_preparation_test.py
+++ cirq-ft/cirq_ft/algos/state_preparation_test.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import itertools
-
 import cirq
 import cirq_ft
 import numpy as np
@@ -22,46 +20,21 @@ from cirq_ft.algos.generic_select_test import get_1d_Ising_lcu_coeffs
 from cirq_ft.infra.jupyter_tools import execute_notebook
 
 
-def construct_gate_helper_and_qubit_order(data, eps):
-    gate = cirq_ft.StatePreparationAliasSampling.from_lcu_probs(
-        lcu_probabilities=data, probability_epsilon=eps
-    )
-    g = cirq_ft.testing.GateHelper(gate)
-    context = cirq.DecompositionContext(cirq.ops.SimpleQubitManager())
-
-    def map_func(op: cirq.Operation, _):
-        gateset = cirq.Gateset(cirq_ft.And, cirq_ft.LessThanEqualGate, cirq_ft.LessThanGate)
-        return cirq.Circuit(
-            cirq.decompose(op, on_stuck_raise=None, keep=gateset.validate, context=context)
-        )
-
-    # TODO: Do not decompose {cq.And, cq.LessThanEqualGate, cq.LessThanGate} because the
-    # `cq.map_clean_and_borrowable_qubits` currently gets confused and is not able to re-map qubits
-    # optimally; which results in a higher number of ancillas thus the tests fails due to OOO.
-    decomposed_circuit = cirq.map_operations_and_unroll(
-        g.circuit, map_func, raise_if_add_qubits=False
-    )
-    greedy_mm = cirq_ft.GreedyQubitManager(prefix="_a", size=25, maximize_reuse=True)
-    decomposed_circuit = cirq_ft.map_clean_and_borrowable_qubits(decomposed_circuit, qm=greedy_mm)
-    # We are fine decomposing the `cq.And` gates once the qubit re-mapping is complete. Ideally,
-    # we shouldn't require this two step process.
-    arithmetic_gateset = cirq.Gateset(cirq_ft.LessThanEqualGate, cirq_ft.LessThanGate)
-    decomposed_circuit = cirq.Circuit(
-        cirq.decompose(decomposed_circuit, keep=arithmetic_gateset.validate, on_stuck_raise=None)
-    )
-    ordered_input = list(itertools.chain(*g.quregs.values()))
-    qubit_order = cirq.QubitOrder.explicit(ordered_input, fallback=cirq.QubitOrder.DEFAULT)
-    return g, qubit_order, decomposed_circuit
-
-
 @pytest.mark.parametrize("num_sites, epsilon", [[2, 3e-3], [3, 3.0e-3], [4, 5.0e-3], [7, 8.0e-3]])
 def test_state_preparation_via_coherent_alias_sampling(num_sites, epsilon):
     lcu_coefficients = get_1d_Ising_lcu_coeffs(num_sites)
-    g, qubit_order, decomposed_circuit = construct_gate_helper_and_qubit_order(
-        lcu_coefficients, epsilon
+    gate = cirq_ft.StatePreparationAliasSampling.from_lcu_probs(
+        lcu_probabilities=lcu_coefficients.tolist(), probability_epsilon=epsilon
     )
-    # assertion to ensure that simulating the `decomposed_circuit` doesn't run out of memory.
-    assert len(decomposed_circuit.all_qubits()) < 25
+    g = cirq_ft.testing.GateHelper(gate)
+    qubit_order = g.operation.qubits
+    # TODO(#6197): This is currently necessary because `SimulationProductState` is used only
+    # at the top-level and ends up being more efficient than directly using
+    # `StateVectorSimulationState`. Ideally, we should be able to use `g.circuit` directly.
+    decomposed_circuit = cirq.Circuit(cirq.decompose_once(g.operation))
+
+    # Assertion to ensure that simulating the `decomposed_circuit` doesn't run out of memory.
+    assert len(decomposed_circuit.all_qubits()) == len(qubit_order) < 20
     result = cirq.Simulator(dtype=np.complex128).simulate(
         decomposed_circuit, qubit_order=qubit_order
     )
@@ -82,7 +55,12 @@ def test_state_preparation_via_coherent_alias_sampling(num_sites, epsilon):
 
 def test_state_preparation_via_coherent_alias_sampling_diagram():
     data = np.asarray(range(1, 5)) / np.sum(range(1, 5))
-    g, qubit_order, _ = construct_gate_helper_and_qubit_order(data, 0.05)
+    gate = cirq_ft.StatePreparationAliasSampling.from_lcu_probs(
+        lcu_probabilities=data.tolist(), probability_epsilon=0.05
+    )
+    g = cirq_ft.testing.GateHelper(gate)
+    qubit_order = g.operation.qubits
+
     circuit = cirq.Circuit(cirq.decompose_once(g.operation))
     cirq.testing.assert_has_diagram(
         circuit,
