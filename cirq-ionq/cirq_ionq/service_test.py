f7907fedfc92b620eb1290dceb6f9a73cb7bd728
Spencer Churchill <churchill@ionq.co>
2023-08-02 10:59:00 -0700

    fix api_versioning failure

--- cirq-ionq/cirq_ionq/service_test.py
+++ cirq-ionq/cirq_ionq/service_test.py
@@ -226,8 +226,8 @@ def test_service_remote_host_from_env_var_ionq():
 
 @mock.patch.dict(os.environ, {}, clear=True)
 def test_service_remote_host_default():
-    service = ionq.Service(api_key='tomyheart', api_version='v0.1')
-    assert service.remote_host == 'https://api.ionq.co/v0.1'
+    service = ionq.Service(api_key='tomyheart', api_version='v0.3')
+    assert service.remote_host == 'https://api.ionq.co/v0.3'
 
 
 @mock.patch.dict(
