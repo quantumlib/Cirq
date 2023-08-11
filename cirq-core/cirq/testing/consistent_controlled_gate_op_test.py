c193c482dfbacf2f6e6924dbd6821ee917591006
Tanuj Khattar <tanujkhattar@google.com>
2023-08-11 10:47:42 -0700

    Fix bug in controlled() method of CCX and CCZ gates (#6237)
    * Fix bug in controlled() method of CCX and CCZ gates
    
    * Fix typing

--- cirq-core/cirq/testing/consistent_controlled_gate_op_test.py
+++ cirq-core/cirq/testing/consistent_controlled_gate_op_test.py
@@ -76,3 +76,14 @@ def test_assert_controlled_and_controlled_by_identical():
         cirq.testing.assert_controlled_and_controlled_by_identical(
             GoodGate(), num_controls=[1, 2], control_values=[(1,), (1, 1, 1)]
         )
+
+
+def test_assert_controlled_unitary_consistent():
+    cirq.testing.assert_controlled_and_controlled_by_identical(
+        GoodGate(exponent=0.5, global_shift=1 / 3)
+    )
+
+    with pytest.raises(AssertionError):
+        cirq.testing.assert_controlled_and_controlled_by_identical(
+            BadGate(exponent=0.5, global_shift=1 / 3)
+        )
