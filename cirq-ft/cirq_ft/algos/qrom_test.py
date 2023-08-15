66886ef55653c5a8c28a3a148cd7c99f6e659a9a
Pavol Juhas <juhas@google.com>
2023-08-15 16:06:49 -0700

    Clean up temporary comment

--- cirq-ft/cirq_ft/algos/qrom_test.py
+++ cirq-ft/cirq_ft/algos/qrom_test.py
@@ -22,7 +22,6 @@ from cirq_ft.infra.bit_tools import iter_bits
 from cirq_ft.infra.jupyter_tools import execute_notebook
 
 
-# TODO(juhas) - delete this comment - trigger for Changed files test
 @pytest.mark.parametrize(
     "data", [[[1, 2, 3, 4, 5]], [[1, 2, 3], [4, 5, 10]], [[1], [2], [3], [4], [5], [6]]]
 )
