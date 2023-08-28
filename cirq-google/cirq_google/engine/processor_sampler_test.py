1f60c96e008b7eba2b92f6efdedbd7dd5b17ede5
Jose Urruticoechea <urruti@google.com>
2023-08-28 23:28:03 +0000

    Add device configuration  parameters to processor sampler class

--- cirq-google/cirq_google/engine/processor_sampler_test.py
+++ cirq-google/cirq_google/engine/processor_sampler_test.py
@@ -24,15 +24,29 @@ from cirq_google.engine.abstract_processor import AbstractProcessor
 @pytest.mark.parametrize('circuit', [cirq.Circuit(), cirq.FrozenCircuit()])
 def test_run_circuit(circuit):
     processor = mock.create_autospec(AbstractProcessor)
-    sampler = cg.ProcessorSampler(processor=processor)
+    run_name = "RUN_NAME"
+    device_config_name = "DEVICE_CONFIG_NAME"
+    sampler = cg.ProcessorSampler(
+        processor=processor, run_name=run_name, device_config_name=device_config_name
+    )
     params = [cirq.ParamResolver({'a': 1})]
     sampler.run_sweep(circuit, params, 5)
-    processor.run_sweep_async.assert_called_with(params=params, program=circuit, repetitions=5)
+    processor.run_sweep_async.assert_called_with(
+        params=params,
+        program=circuit,
+        repetitions=5,
+        run_name=run_name,
+        device_config_name=device_config_name,
+    )
 
 
 def test_run_batch():
     processor = mock.create_autospec(AbstractProcessor)
-    sampler = cg.ProcessorSampler(processor=processor)
+    run_name = "RUN_NAME"
+    device_config_name = "DEVICE_CONFIG_NAME"
+    sampler = cg.ProcessorSampler(
+        processor=processor, run_name=run_name, device_config_name=device_config_name
+    )
     a = cirq.LineQubit(0)
     circuit1 = cirq.Circuit(cirq.X(a))
     circuit2 = cirq.Circuit(cirq.Y(a))
@@ -42,13 +56,21 @@ def test_run_batch():
     params_list = [params1, params2]
     sampler.run_batch(circuits, params_list, 5)
     processor.run_batch_async.assert_called_with(
-        params_list=params_list, programs=circuits, repetitions=5
+        params_list=params_list,
+        programs=circuits,
+        repetitions=5,
+        run_name=run_name,
+        device_config_name=device_config_name,
     )
 
 
 def test_run_batch_identical_repetitions():
     processor = mock.create_autospec(AbstractProcessor)
-    sampler = cg.ProcessorSampler(processor=processor)
+    run_name = "RUN_NAME"
+    device_config_name = "DEVICE_CONFIG_NAME"
+    sampler = cg.ProcessorSampler(
+        processor=processor, run_name=run_name, device_config_name=device_config_name
+    )
     a = cirq.LineQubit(0)
     circuit1 = cirq.Circuit(cirq.X(a))
     circuit2 = cirq.Circuit(cirq.Y(a))
@@ -58,7 +80,11 @@ def test_run_batch_identical_repetitions():
     params_list = [params1, params2]
     sampler.run_batch(circuits, params_list, [5, 5])
     processor.run_batch_async.assert_called_with(
-        params_list=params_list, programs=circuits, repetitions=5
+        params_list=params_list,
+        programs=circuits,
+        repetitions=5,
+        run_name=run_name,
+        device_config_name=device_config_name,
     )
 
 
@@ -78,10 +104,14 @@ def test_run_batch_bad_number_of_repetitions():
 
 def test_run_batch_differing_repetitions():
     processor = mock.create_autospec(AbstractProcessor)
+    run_name = "RUN_NAME"
+    device_config_name = "DEVICE_CONFIG_NAME"
+    sampler = cg.ProcessorSampler(
+        processor=processor, run_name=run_name, device_config_name=device_config_name
+    )
     job = mock.Mock()
     job.results.return_value = []
     processor.run_sweep.return_value = job
-    sampler = cg.ProcessorSampler(processor=processor)
     a = cirq.LineQubit(0)
     circuit1 = cirq.Circuit(cirq.X(a))
     circuit2 = cirq.Circuit(cirq.Y(a))
@@ -91,7 +121,13 @@ def test_run_batch_differing_repetitions():
     params_list = [params1, params2]
     repetitions = [1, 2]
     sampler.run_batch(circuits, params_list, repetitions)
-    processor.run_sweep_async.assert_called_with(params=params2, program=circuit2, repetitions=2)
+    processor.run_sweep_async.assert_called_with(
+        params=params2,
+        program=circuit2,
+        repetitions=2,
+        run_name=run_name,
+        device_config_name=device_config_name,
+    )
     processor.run_batch_async.assert_not_called()
 
 
