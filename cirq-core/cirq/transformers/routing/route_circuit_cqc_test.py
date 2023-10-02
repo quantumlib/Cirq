df79ec87b61ff799ce6cc586b0da65b508368687
Shef Ashiru <sefunmi@vt.edu>
2023-10-02 11:27:37 -0400

    router mishandling mid-circuit measurements Fixes #6293

--- cirq-core/cirq/transformers/routing/route_circuit_cqc_test.py
+++ cirq-core/cirq/transformers/routing/route_circuit_cqc_test.py
@@ -106,18 +106,6 @@ def test_circuit_with_measurement_gates():
     cirq.testing.assert_same_circuits(routed_circuit, circuit)
 
 
-def test_circuit_with_mid_circuit_measurement_gates_sample_issue6293():
-    device = cirq.testing.construct_ring_device(3)
-    device_graph = device.metadata.nx_graph
-    q = cirq.LineQubit.range(3)
-    circuit = cirq.Circuit(cirq.CZ(q[0], q[1]), cirq.CZ(q[1], q[2]), cirq.CZ(q[0], q[2]),cirq.MeasurementGate(3).on(*q),cirq.H.on_each(*q), cirq.measure_each(*q))
-    hard_coded_mapper = cirq.HardCodedInitialMapper({q[i]: q[i] for i in range(3)})
-    router = cirq.RouteCQC(device_graph)
-    routed_circuit = router(circuit, initial_mapper=hard_coded_mapper)
-    print(circuit)
-    print(routed_circuit)
-
-
 def test_circuit_with_non_unitary_and_global_phase():
     device = cirq.testing.construct_ring_device(4)
     device_graph = device.metadata.nx_graph
