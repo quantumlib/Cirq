c02e4762d98a7a066320ef3afcaefe628b22d9fe
dripto <dripto@google.com>
2023-10-12 11:59:54 -0700

    adds test

--- cirq-core/cirq/transformers/analytical_decompositions/two_qubit_state_preparation_test.py
+++ cirq-core/cirq/transformers/analytical_decompositions/two_qubit_state_preparation_test.py
@@ -72,6 +72,20 @@ def test_prepare_two_qubit_state_using_cz(state):
 
 
 @pytest.mark.parametrize("state", STATES_TO_PREPARE)
+def test_prepare_two_qubit_state_using_iswap(state):
+    state = cirq.to_valid_state_vector(state, num_qubits=2)
+    q = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit(cirq.prepare_two_qubit_state_using_iswap(*q, state))
+    ops_cz = [*circuit.findall_operations(lambda op: op.gate == cirq.CZ)]
+    ops_2q = [*circuit.findall_operations(lambda op: cirq.num_qubits(op) > 1)]
+    assert ops_cz == ops_2q
+    assert len(ops_cz) <= 1
+    assert cirq.allclose_up_to_global_phase(
+        circuit.final_state_vector(ignore_terminal_measurements=False, dtype=np.complex64), state
+    )
+
+
+@pytest.mark.parametrize("state", STATES_TO_PREPARE)
 @pytest.mark.parametrize("use_sqrt_iswap_inv", [True, False])
 def test_prepare_two_qubit_state_using_sqrt_iswap(state, use_sqrt_iswap_inv):
     state = cirq.to_valid_state_vector(state, num_qubits=2)
