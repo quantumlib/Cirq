75adbfa38a8813e1197ffc57e3df0685899c5c77
Nour Yosri <noureldinyosri@gmail.com>
2024-01-10 08:14:14 -0800

    add comment and fix string substitution

--- dev_tools/packaging/create_pypirc.sh
+++ dev_tools/packaging/create_pypirc.sh
@@ -1,9 +1,12 @@
+#!bin/bash
+
+# Create default pypi config file with token authentication.
 echo """
 [pypi]
 username = __token__
-password = ${{ secrets.CIRQ_PYPI_TOKEN }}
+password = ${CIRQ_PYPI_TOKEN}
 
 [testpypi]
 username = __token__
-password = ${{ secrets.CIRQ_TEST_PYPI_TOKEN }}
+password = ${CIRQ_TEST_PYPI_TOKEN}
 """ >> $HOME/.pypirc
