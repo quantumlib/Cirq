85261cd2204eac1f117765f18b9dd7731f147f35
Pavol Juhas <juhas@google.com>
2024-01-10 10:08:45 -0800

    Make the create_pypirc.sh script safer
    * avoid accidental modification of user's pypirc, instead
      have the script output config without changing any file
    
    * use absolute path in the shebang
    
    * drop unnecessary triple quotes.
      In bash they just concatenate an empty string.
    
    * fail script early if required environment variables do not exist

--- dev_tools/packaging/create_pypirc.sh
+++ dev_tools/packaging/create_pypirc.sh
@@ -1,13 +1,14 @@
-#!bin/bash
+#!/bin/bash
 
-# Create default pypi config file with token authentication.
+# Output default pypi config file with token authentication.
 # refrence: https://packaging.python.org/en/latest/specifications/pypirc/#common-configurations
-echo """
+
+echo "\
 [pypi]
 username = __token__
-password = ${CIRQ_PYPI_TOKEN}
+password = ${CIRQ_PYPI_TOKEN:?}
 
 [testpypi]
 username = __token__
-password = ${CIRQ_TEST_PYPI_TOKEN}
-""" >> $HOME/.pypirc
+password = ${CIRQ_TEST_PYPI_TOKEN:?}
+"
