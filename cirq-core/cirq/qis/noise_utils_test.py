b91f1fddac10dcf73128036679a53af8fcf82d18
Nour Yosri <noureldinyosri@gmail.com>
2024-02-08 14:26:28 -0800

    redo

--- cirq-core/cirq/qis/noise_utils_test.py
+++ cirq-core/cirq/qis/noise_utils_test.py
@@ -15,9 +15,7 @@
 import numpy as np
 import pytest
 
-import cirq
-from cirq.devices.noise_utils import (
-    OpIdentifier,
+from cirq.qis.noise_utils import (
     decay_constant_to_xeb_fidelity,
     decay_constant_to_pauli_error,
     pauli_error_to_decay_constant,
@@ -26,47 +24,6 @@ from cirq.devices.noise_utils import (
     average_error,
     decoherence_pauli_error,
 )
-
-
-def test_op_identifier():
-    op_id = OpIdentifier(cirq.XPowGate)
-    assert cirq.X(cirq.LineQubit(1)) in op_id
-    assert cirq.Rx(rads=1) in op_id
-
-
-def test_op_identifier_subtypes():
-    gate_id = OpIdentifier(cirq.Gate)
-    xpow_id = OpIdentifier(cirq.XPowGate)
-    x_on_q0_id = OpIdentifier(cirq.XPowGate, cirq.LineQubit(0))
-    assert xpow_id.is_proper_subtype_of(gate_id)
-    assert x_on_q0_id.is_proper_subtype_of(xpow_id)
-    assert x_on_q0_id.is_proper_subtype_of(gate_id)
-    assert not xpow_id.is_proper_subtype_of(xpow_id)
-
-
-def test_op_id_str():
-    op_id = OpIdentifier(cirq.XPowGate, cirq.LineQubit(0))
-    assert str(op_id) == "<class 'cirq.ops.common_gates.XPowGate'>(cirq.LineQubit(0),)"
-    assert repr(op_id) == (
-        "cirq.devices.noise_utils.OpIdentifier(cirq.ops.common_gates.XPowGate, cirq.LineQubit(0))"
-    )
-
-
-def test_op_id_swap():
-    q0, q1 = cirq.LineQubit.range(2)
-    base_id = OpIdentifier(cirq.CZPowGate, q0, q1)
-    swap_id = OpIdentifier(base_id.gate_type, *base_id.qubits[::-1])
-    assert cirq.CZ(q0, q1) in base_id
-    assert cirq.CZ(q0, q1) not in swap_id
-    assert cirq.CZ(q1, q0) not in base_id
-    assert cirq.CZ(q1, q0) in swap_id
-
-
-def test_op_id_instance():
-    q0 = cirq.LineQubit.range(1)[0]
-    gate = cirq.SingleQubitCliffordGate.from_xz_map((cirq.X, False), (cirq.Z, False))
-    op_id = OpIdentifier(gate, q0)
-    cirq.testing.assert_equivalent_repr(op_id)
 
 
 @pytest.mark.parametrize(
