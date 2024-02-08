9b632102609fe65c4c3d0d78d02c7565dad62075
Shef Ashiru <sefunmi@vt.edu>
2024-02-08 16:18:36 -0500

    format and lint fixes

--- cirq-core/cirq/sim/classical_simulator.py
+++ cirq-core/cirq/sim/classical_simulator.py
@@ -14,11 +14,11 @@
 
 
 from typing import Dict, Generic, Any, Sequence, TYPE_CHECKING, List
-from cirq import ops, qis, value
+from cirq import ops, qis
 from cirq.value import big_endian_int_to_bits
 from cirq.ops.raw_types import Qid
 from cirq import sim
-from cirq.sim.simulation_state import TSimulationState, SimulationState, SimulationStateBase
+from cirq.sim.simulation_state import TSimulationState, SimulationState
 import numpy as np
 
 if TYPE_CHECKING:
@@ -73,7 +73,9 @@ class ClassicalBasisSimState(SimulationState[ClassicalBasisState]):
         return False
 
 
-class ClassicalStateStepResult(sim.StepResultBase['ClassicalBasisSimState'], Generic[TSimulationState]):
+class ClassicalStateStepResult(
+    sim.StepResultBase['ClassicalBasisSimState'], Generic[TSimulationState]
+):
     """The step result provided by `ClassicalStateSimulator.simulate_moment_steps`."""
 
 
