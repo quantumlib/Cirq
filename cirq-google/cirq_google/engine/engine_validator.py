21ebf3538a03e7301f1cc01a7b235e73cccbb6ff
Seneca Meeks <senecacmeeks@gmail.com>
2024-02-16 23:18:48 +0000

    add todo

--- cirq-google/cirq_google/engine/engine_validator.py
+++ cirq-google/cirq_google/engine/engine_validator.py
@@ -65,6 +65,8 @@ def _verify_measurements(circuits):
             raise RuntimeError('Code must measure at least one qubit.')
 
 
+# TODO: update validate methods to accept a `cirq.AbstractCircuit``
+# instead of a `Sequence` since batching is no longer supported.
 def validate_program(
     circuits: Sequence[cirq.AbstractCircuit],
     sweeps: Sequence[cirq.Sweepable],
