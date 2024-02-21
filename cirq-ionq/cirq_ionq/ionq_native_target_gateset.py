f2ab95395656b1ac78e930a97fb1e7312983ec5b
Radu Marginean <radu.marg@gmail.com>
2024-02-21 17:15:04 +0200

    Remove old code.

--- cirq-ionq/cirq_ionq/ionq_native_target_gateset.py
+++ cirq-ionq/cirq_ionq/ionq_native_target_gateset.py
@@ -17,13 +17,11 @@ import cirq
 import numpy as np
 
 from cirq import linalg
-from cirq.linalg.tolerance import near_zero_mod
 from cirq import ops
 
 from typing import Any
 from typing import Dict
 from typing import List
-from typing import Tuple
 
 from .ionq_native_gates import GPIGate, GPI2Gate, MSGate, ZZGate, VirtualZGate
 
@@ -154,79 +152,3 @@ class ForteNativeGateset(IonqNativeGatesetBase):
 
     def __repr__(self) -> str:
         return f'cirq_ionq.ForteNativeGateset(atol={self.atol})'
-    
-
-# def _single_qubit_matrix_to_gates(mat: np.ndarray, tolerance: float) -> List[cirq.Gate]:
-#     """Implements a single-qubit operation with few gates.
-#        Code is based on existing implementation from Cirq core.
-
-#     Args:
-#         mat: The 2x2 unitary matrix of the operation to implement.
-#         tolerance: A limit on the amount of error introduced by the
-#             construction.
-
-#     Returns:
-#         A list of gates that, when applied in order, perform the desired
-#             operation.
-#     """
-#     rotations = _single_qubit_matrix_to_pauli_rotations(mat, tolerance)
-#     return [pauli**ht for pauli, ht in rotations]
-
-
-# def _single_qubit_matrix_to_pauli_rotations(mat: np.ndarray, atol: float) -> List[Tuple[cirq.Gate, float]]:
-#     """Implements a single-qubit operation with few rotations.
-#        Code is based on existing implementation from Cirq core.
-
-#     Args:
-#         mat: The 2x2 unitary matrix of the operation to implement.
-#         atol: A limit on the amount of absolute error introduced by the
-#             construction.
-
-#     Returns:
-#         A list of (Pauli, half_turns) tuples that, when applied in order,
-#         perform the desired operation.
-#     """
-
-#     def is_clifford_rotation(half_turns):
-#         return near_zero_mod(half_turns, 0.5, atol=atol)
-
-#     def to_quarter_turns(half_turns):
-#         return round(2 * half_turns) % 4
-
-#     def is_quarter_turn(half_turns):
-#         return is_clifford_rotation(half_turns) and to_quarter_turns(half_turns) % 2 == 1
-
-#     def is_half_turn(half_turns):
-#         return is_clifford_rotation(half_turns) and to_quarter_turns(half_turns) == 2
-
-#     def is_no_turn(half_turns):
-#         return is_clifford_rotation(half_turns) and to_quarter_turns(half_turns) == 0
-
-#     # Decompose matrix
-#     z_rad_before, y_rad, z_rad_after = linalg.deconstruct_single_qubit_matrix_into_angles(mat)
-#     z_ht_before = z_rad_before / np.pi - 0.5
-#     m_ht = y_rad / np.pi
-#     m_pauli = GPIGate(phi=0)
-#     z_ht_after = z_rad_after / np.pi + 0.5
-    
-#     # Clean up angles
-#     if is_clifford_rotation(z_ht_before):
-#         if (is_quarter_turn(z_ht_before) or is_quarter_turn(z_ht_after)) ^ (
-#             is_half_turn(m_ht) and is_no_turn(z_ht_before - z_ht_after)
-#         ):
-#             z_ht_before += 0.5
-#             z_ht_after -= 0.5
-#             m_pauli = GPIGate(phi=np.pi/2)!!!!
-#         if is_half_turn(z_ht_before) or is_half_turn(z_ht_after):
-#             z_ht_before -= 1
-#             z_ht_after += 1
-#             m_ht = -m_ht
-#     if is_no_turn(m_ht):
-#         z_ht_before += z_ht_after
-#         z_ht_after = 0
-#     elif is_half_turn(m_ht):
-#         z_ht_after -= z_ht_before
-#         z_ht_before = 0
-
-#     rotation_list = [(VirtualZGate(theta=np.pi), z_ht_before), (m_pauli, m_ht), (VirtualZGate(theta=-np.pi), z_ht_after)]
-#     return [(pauli, ht) for pauli, ht in rotation_list if not is_no_turn(ht)]
