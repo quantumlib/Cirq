ea8da1cc380546002730e6bd65a019d00a60d697
Pavol Juhas <juhas@google.com>
2024-04-09 15:11:55 -0700

    Improve repr of `_BaseAncillaQid` classes with prefix (#6555)
    * Add failing repr tests for CleanQubit and BorrowableQubit with prefix
    
    * Quote prefix string in repr of `_BaseAncillaQid` classes

--- cirq-core/cirq/ops/qubit_manager_test.py
+++ cirq-core/cirq/ops/qubit_manager_test.py
@@ -31,6 +31,10 @@ def test_clean_qubits():
     assert str(q) == '_c(2) (d=3)'
     assert repr(q) == 'cirq.ops.CleanQubit(2, dim=3)'
 
+    q = cqi.CleanQubit(3, dim=4, prefix="a")
+    assert str(q) == 'a_c(3) (d=4)'
+    assert repr(q) == "cirq.ops.CleanQubit(3, dim=4, prefix='a')"
+
     assert cqi.CleanQubit(1) < cqi.CleanQubit(2)
 
 
@@ -57,6 +61,10 @@ def test_borrow_qubits():
     assert q.with_dimension(10) == cqi.BorrowableQubit(20, dim=10)
     assert str(q) == '_b(20) (d=4)'
     assert repr(q) == 'cirq.ops.BorrowableQubit(20, dim=4)'
+
+    q = cqi.BorrowableQubit(30, dim=4, prefix="a")
+    assert str(q) == 'a_b(30) (d=4)'
+    assert repr(q) == "cirq.ops.BorrowableQubit(30, dim=4, prefix='a')"
 
     assert cqi.BorrowableQubit(1) < cqi.BorrowableQubit(2)
 
