72b109b5c36c1dd29a3f7b615e4f63c3759459f6
Pragya <163991062+prag16@users.noreply.github.com>
2024-05-11 06:09:17 +0530

    Update uniform_superposition_gate_test.py
    test_uniform_superposition_gate() removed. Correct parameterization is done as per the review comments.

--- cirq-core/cirq/ops/uniform_superposition_gate_test.py
+++ cirq-core/cirq/ops/uniform_superposition_gate_test.py
@@ -16,8 +16,9 @@ import numpy as np
 import pytest
 import cirq
 
-
-def _assert_generated_unitary_is_uniform(m_value: int, n: int) -> None:
+@pytest.mark.parameterize(['m', 'n'],[[m, n] for n in range(3, 7)
+                                      for m in np.random.randint(1, 1 << n, size=3)])
+def test_generated_unitary_is_uniform(m: int, n: int) -> None:
     r"""
     The code checks that the unitary matrix corresponds to the generated uniform superposition
     states (see uniform_superposition_gate.py). It is enough to check that the
@@ -26,7 +27,7 @@ def _assert_generated_unitary_is_uniform(m_value: int, n: int) -> None:
     entries are all "1"s (excluding the normalization factor of $\frac{1}{\sqrt{M}}$ and the
     remaining $2^n-M$ entries are all "0"s.
     """
-    gate = cirq.UniformSuperpositionGate(m_value, n)
+    gate = cirq.UniformSuperpositionGate(int(m), int(n))
     qregx = cirq.LineQubit.range(n)
     qcircuit = cirq.Circuit(gate.on(*qregx))
 
@@ -34,14 +35,6 @@ def _assert_generated_unitary_is_uniform(m_value: int, n: int) -> None:
 
     np.testing.assert_allclose(
         unitary_matrix1[:, 0],
-        (1 / np.sqrt(m_value)) * np.array([1] * m_value + [0] * (2**n - m_value)),
+        (1 / np.sqrt(m)) * np.array([1] * m + [0] * (2**n - m)),
         atol=1e-8,
     )
-
-@pytest.mark.parametrize('low_value, max_value', [(3,17),(17,29),(30,89),(89,127),(128,257)])
-def test_uniform_superposition_gate(low_value,max_value):
-    r"""The code tests the creation of M uniform superposition states,
-    where M ranges from low_value to max_value."""
-    for mm in range(low_value, max_value):
-        n = int(np.ceil(np.log2(mm)))
-        _assert_generated_unitary_is_uniform(mm, n)
