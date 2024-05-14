9472a77ed0c6dfd43bda9ed25589f3865bbb791d
Pragya <163991062+prag16@users.noreply.github.com>
2024-05-15 04:11:52 +0530

    Update uniform_superposition_gate_test.py
    Added one more test case.

--- cirq-core/cirq/ops/uniform_superposition_gate_test.py
+++ cirq-core/cirq/ops/uniform_superposition_gate_test.py
@@ -20,7 +20,7 @@ import cirq
 @pytest.mark.parametrize(
     ['m', 'n'],
     [[int(m), n] for n in range(3, 7) for m in np.random.randint(1, 1 << n, size=3)]
-    + [(1, 1), (-2, 1), (-3.1, 2), (6, -4)],
+    + [(1, 1), (-2, 1), (-3.1, 2), (6, -4), (5, 6.1)],
 )
 def test_generated_unitary_is_uniform(m: int, n: int) -> None:
     r"""The code checks that the unitary matrix corresponds to the generated uniform superposition
