a00e5d7fcdc7148da551926841a5120f14cd3c9b
Pragya <163991062+prag16@users.noreply.github.com>
2024-05-18 00:42:41 +0530

    Update uniform_superposition_gate.py
    Implemented the suggested change.

--- cirq-core/cirq/ops/uniform_superposition_gate.py
+++ cirq-core/cirq/ops/uniform_superposition_gate.py
@@ -27,7 +27,7 @@ if TYPE_CHECKING:
 class UniformSuperpositionGate(raw_types.Gate):
     r"""Creates a uniform superposition state on the states $[0, M)$
     The gate creates the state $\frac{1}{\sqrt{M}}\sum_{j=0}^{M-1}\ket{j}$
-    (where 1<= M <= 2^n), using n qubits, according to the Shukla-Vedula algorithm [SV24].
+    (where $1\leq M \leq 2^n$), using n qubits, according to the Shukla-Vedula algorithm [SV24].
     References:
         [SV24]
         [An efficient quantum algorithm for preparation of uniform quantum superposition
@@ -63,9 +63,7 @@ class UniformSuperpositionGate(raw_types.Gate):
 
         if self._m_value == 1:  #  if m_value is 1, do nothing
             return
-        if (
-            self._m_value & (self._m_value - 1)
-        ) == 0:  # if m_value is an integer power of 2
+        if (self._m_value & (self._m_value - 1)) == 0:  # if m_value is an integer power of 2
             m = self._m_value.bit_length() - 1
             yield H.on_each(qreg[:m])
             return
@@ -87,16 +85,12 @@ class UniformSuperpositionGate(raw_types.Gate):
             yield H(qreg[i]).controlled_by(qreg[l_value[1]], control_values=[False])
 
         for m in range(1, len(l_value) - 1):
-            theta = -2 * np.arccos(
-                np.sqrt(2 ** l_value[m] / (self._m_value - m_current))
-            )
+            theta = -2 * np.arccos(np.sqrt(2 ** l_value[m] / (self._m_value - m_current)))
             yield ry(theta).on(qreg[l_value[m + 1]]).controlled_by(
                 qreg[l_value[m]], control_values=[0]
             )
             for i in range(l_value[m], l_value[m + 1]):
-                yield H.on(qreg[i]).controlled_by(
-                    qreg[l_value[m + 1]], control_values=[0]
-                )
+                yield H.on(qreg[i]).controlled_by(qreg[l_value[m + 1]], control_values=[0])
 
             m_current = m_current + 2 ** (l_value[m])
 
