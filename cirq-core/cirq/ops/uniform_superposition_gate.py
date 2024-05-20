41803bfd2b9f9bd0d3fd1c321c66f369a7fd2e34
Pragya <163991062+prag16@users.noreply.github.com>
2024-05-21 02:09:16 +0530

    Update uniform_superposition_gate.py

--- cirq-core/cirq/ops/uniform_superposition_gate.py
+++ cirq-core/cirq/ops/uniform_superposition_gate.py
@@ -110,9 +110,6 @@ class UniformSuperpositionGate(raw_types.Gate):
             return (self._m_value == other._m_value) and (self._num_qubits == other._num_qubits)
         return False
 
-    def _value_equality_values_(self):
-        return tuple(self._m_value, self._num_qubits)
-
     def __repr__(self) -> str:
         return f'UniformSuperpositionGate(m_value={self._m_value}, num_qubits={self._num_qubits})'
 
