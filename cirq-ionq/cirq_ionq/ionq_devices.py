fbd3c01faa331ea2c8162f3998c02975286d0bcc
Pavol Juhas <juhas@google.com>
2024-05-29 14:17:28 -0700

    Remove deprecated function cirq_ionq.ionq_devices.decompose_to_device
    Long past the "v0.16" deadline.

--- cirq-ionq/cirq_ionq/ionq_devices.py
+++ cirq-ionq/cirq_ionq/ionq_devices.py
@@ -72,34 +72,3 @@ class IonQAPIDevice(cirq.Device):
 
     def is_api_gate(self, operation: cirq.Operation) -> bool:
         return operation in self.gateset
-
-
-@cirq._compat.deprecated(
-    deadline='v0.16',
-    fix='Use cirq.optimize_for_target_gateset(circuit, '
-    'gateset=cirq_ionq.IonQTargetGateset(atol)) '
-    'instead.',
-)
-def decompose_to_device(operation: cirq.Operation, atol: float = 1e-8) -> cirq.OP_TREE:
-    """Decompose operation to ionq native operations.
-
-
-    Merges single qubit operations and decomposes two qubit operations
-    into CZ gates.
-
-    Args:
-        operation: `cirq.Operation` to decompose.
-        atol: absolute error tolerance to use when declaring two unitary
-            operations equal.
-
-    Returns:
-        cirq.OP_TREE containing decomposed operations.
-
-    Raises:
-        ValueError: If supplied operation cannot be decomposed
-            for the ionq device.
-
-    """
-    return cirq.optimize_for_target_gateset(
-        cirq.Circuit(operation), gateset=ionq_gateset.IonQTargetGateset(), ignore_failures=False
-    ).all_operations()
