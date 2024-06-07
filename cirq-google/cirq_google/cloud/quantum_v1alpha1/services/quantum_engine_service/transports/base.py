db0b9fa69f46734c6d61b1c308e6c494ec341989
Seneca Meeks <senecacmeeks@gmail.com>
2024-06-07 00:42:50 -0700

    mypy

--- cirq-google/cirq_google/cloud/quantum_v1alpha1/services/quantum_engine_service/transports/base.py
+++ cirq-google/cirq_google/cloud/quantum_v1alpha1/services/quantum_engine_service/transports/base.py
@@ -23,7 +23,7 @@ from google.api_core import exceptions as core_exceptions
 from google.api_core import gapic_v1
 from google.api_core import retry as retries
 from google.auth import credentials as ga_credentials
-from google.oauth2 import service_account  # type: ignore
+from google.oauth2 import service_account
 
 from cirq_google.cloud.quantum_v1alpha1.types import engine
 from cirq_google.cloud.quantum_v1alpha1.types import quantum
--- cirq-google/cirq_google/cloud/quantum_v1alpha1/services/quantum_engine_service/transports/grpc_asyncio.py
+++ cirq-google/cirq_google/cloud/quantum_v1alpha1/services/quantum_engine_service/transports/grpc_asyncio.py
@@ -166,7 +166,7 @@ class QuantumEngineServiceGrpcAsyncIOTransport(QuantumEngineServiceTransport):
 
         if channel:
             # Ignore credentials if a channel was passed.
-            credentials = False
+            credentials = None
             # If a channel was explicitly provided, set it.
             self._grpc_channel = channel
             self._ssl_channel_credentials = None
