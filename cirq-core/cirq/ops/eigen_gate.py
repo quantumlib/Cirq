quantumlib/Cirq

commit 8d09a3220962c4e5eeed30ea33b9d120619b585f

Fix equality for cphase gate initialized with sympy constant (#6669)
Problem: In sympy-1.13.0 the expressions `sympy.S.One == 1.0` and
`sympy.S.Half == 0.5` evaluate to False which causes
inequality between `cirq.cphase(sympy.pi)` and `cirq.CZ`.

Solution: Check equality with parameterized exponents converted to float.

Fixes: #6663

cirq-core/cirq/ops/eigen_gate.py
@@ -309,8 +309,12 @@ class EigenGate(raw_types.Gate):
     def _canonical_exponent(self):
         if self._canonical_exponent_cached is None:
             period = self._period()
-            if not period or protocols.is_parameterized(self._exponent):
+            if not period:
                 self._canonical_exponent_cached = self._exponent
+            elif protocols.is_parameterized(self._exponent):
+                self._canonical_exponent_cached = self._exponent
+                if isinstance(self._exponent, sympy.Expr) and self._exponent.is_constant():
+                    self._canonical_exponent_cached = float(self._exponent)
             else:
                 self._canonical_exponent_cached = self._exponent % period
         return self._canonical_exponent_cached

cirq-core/cirq/ops/common_gates_test.py
@@ -899,8 +899,6 @@ def test_cphase_unitary(angle_rads, expected_unitary):
     np.testing.assert_allclose(cirq.unitary(cirq.cphase(angle_rads)), expected_unitary)
 
 
-# TODO(#6663): fix this use case.
-@pytest.mark.xfail
 def test_parameterized_cphase():
     assert cirq.cphase(sympy.pi) == cirq.CZ
     assert cirq.cphase(sympy.pi / 2) == cirq.CZ**0.5

commit 1529ba00bad2b9f0baa342ba3055add1a4ba46e7

Eliminate awful slowdown of `EigenGate._value_equality_values_`
Problem: The `sympy.Expr.is_constant()` introduced in #6669 is very
slow causing glacial-speed equality checks of parameterized gates.

Solution: Check if expression is sympy.Number instead which still
satisfies the equality fix added in #6669.

Timing of
  check/pytest --numprocesses=0 --randomly-seed=0 \
    cirq-core/cirq/transformers/analytical_decompositions/two_qubit_to_sqrt_iswap_test.py
Before: 945 s   After: 18 s,  Before #6669: 18 s.

cirq-core/cirq/ops/eigen_gate.py
@@ -313,7 +313,7 @@ class EigenGate(raw_types.Gate):
                 self._canonical_exponent_cached = self._exponent
             elif protocols.is_parameterized(self._exponent):
                 self._canonical_exponent_cached = self._exponent
-                if isinstance(self._exponent, sympy.Expr) and self._exponent.is_constant():
+                if isinstance(self._exponent, sympy.Number):
                     self._canonical_exponent_cached = float(self._exponent)
             else:
                 self._canonical_exponent_cached = self._exponent % period