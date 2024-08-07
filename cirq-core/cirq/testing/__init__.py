1cf646543d95ef38f88c7fc0a6a18e799d2376ed
Pavol Juhas <juhas@google.com>
2024-08-07 16:16:55 -0700

    Keep imported modules sorted

--- cirq-core/cirq/testing/__init__.py
+++ cirq-core/cirq/testing/__init__.py
@@ -96,6 +96,8 @@ from cirq.testing.op_tree import assert_equivalent_op_tree
 
 from cirq.testing.order_tester import OrderTester
 
+from cirq.testing.pytest_randomly_utils import retry_once_with_later_random_values
+
 from cirq.testing.random_circuit import (
     DEFAULT_GATE_DOMAIN,
     random_circuit,
@@ -107,8 +109,6 @@ from cirq.testing.repr_pretty_tester import (
     assert_repr_pretty_contains,
     FakePrinter,
 )
-
-from cirq.testing.pytest_randomly_utils import retry_once_with_later_random_values
 
 from cirq.testing.routing_devices import (
     construct_grid_device,
