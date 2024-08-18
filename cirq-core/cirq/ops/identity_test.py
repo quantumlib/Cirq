066d7282526fa5379c42ed130c670970a980a688
Michael A. Perlin <mika.perlin@gmail.com>
2024-08-18 11:10:15 -0500

    Update cirq-core/cirq/ops/identity_test.py
    Co-authored-by: Pavol Juhas <pavol.juhas@gmail.com>

--- cirq-core/cirq/ops/identity_test.py
+++ cirq-core/cirq/ops/identity_test.py
@@ -211,5 +211,6 @@ def test_identity_short_circuits_act_on():
 
 
 def test_identity_commutes():
-    assert cirq.I._commutes_(cirq.X)
-    assert cirq.I._commutes_("placeholder") is NotImplemented
+    assert cirq.commutes(cirq.I, cirq.X)
+    with pytest.raises(TypeError):
+        cirq.commutes(cirq.I, "Gate")
