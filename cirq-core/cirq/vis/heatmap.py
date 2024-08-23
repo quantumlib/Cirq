c4966f999efb226140935eb1c78d4b66f50407e2
Pavol Juhas <juhas@google.com>
2024-08-22 23:22:18 -0700

    Future bright future will you satisfy mypy?

--- cirq-core/cirq/vis/heatmap.py
+++ cirq-core/cirq/vis/heatmap.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import copy
 from dataclasses import astuple, dataclass
 from typing import (
