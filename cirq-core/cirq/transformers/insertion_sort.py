f11a0e4c903cb6ee79a271ae1569ff7425492703
Nour Yosri <noureldinyosri@gmail.com>
2024-10-16 16:55:24 -0700

    add missing files

---
+++ cirq-core/cirq/transformers/insertion_sort.py
@@ -0,0 +1,47 @@
+# Copyright 2024 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Transformer that sorts the operations that commute in increasing order using their qubits tuple as comparison key."""
+
+from typing import Optional, TYPE_CHECKING, List
+
+from cirq import protocols, circuits
+from cirq.transformers import transformer_api
+
+if TYPE_CHECKING:
+    import cirq
+
+
+@transformer_api.transformer(add_deep_support=True)
+def insertion_sort_transformer(
+    circuit: 'cirq.AbstractCircuit',
+    *,
+    context: Optional['cirq.TransformerContext'] = None,
+) -> 'cirq.Circuit':
+    """Sorts the operations using their `.qubits` property as comparison key.
+
+    Operations are swapped only if they commute.
+
+    Args:
+        circuit: input circuit.
+        context: optional TransformerContext (not used),    
+    """
+    final_operations: List['cirq.Operation'] = []
+    for op in circuit.all_operations():
+        st = []
+        while len(final_operations) and op.qubits < final_operations[-1].qubits and protocols.commutes(final_operations[-1], op, default=None):
+            st.append(final_operations.pop())
+        final_operations.append(op)
+        final_operations.extend(st[::-1])
+    return circuits.Circuit(final_operations)
