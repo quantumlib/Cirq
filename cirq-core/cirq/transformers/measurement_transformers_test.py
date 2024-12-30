df439c192949273441630bc9f22ca080340ee165
daxfo <dax.fohl@gmail.com>
2024-12-29 17:04:53 -0800

    test simulation equivalence

--- cirq-core/cirq/transformers/measurement_transformers_test.py
+++ cirq-core/cirq/transformers/measurement_transformers_test.py
@@ -762,9 +762,7 @@ def test_drop_terminal():
 def test_drop_terminal_qudit():
     q0, q1 = cirq.LineQid.range(2, dimension=3)
     circuit = cirq.Circuit(
-        cirq.CircuitOperation(
-            cirq.FrozenCircuit(cirq.measure(q0, q1, key='a~b', invert_mask=[0, 1]))
-        )
+        cirq.CircuitOperation(cirq.FrozenCircuit(cirq.measure(q0, q1, key='m', invert_mask=[0, 1])))
     )
     dropped = cirq.drop_terminal_measurements(circuit)
     cirq.testing.assert_same_circuits(
@@ -780,6 +778,21 @@ def test_drop_terminal_qudit():
             )
         ),
     )
+    # Verify behavior equivalent to simulator (invert_mask swaps 0,1 but leaves 2 alone)
+    dropped.append(cirq.measure(q0, q1, key='m'))
+    sim = cirq.Simulator()
+    c0 = sim.simulate(circuit, initial_state=[0, 0])
+    d0 = sim.simulate(dropped, initial_state=[0, 0])
+    assert np.all(c0.measurements['m'] == [0, 1])
+    assert np.all(d0.measurements['m'] == [0, 1])
+    c1 = sim.simulate(circuit, initial_state=[1, 1])
+    d1 = sim.simulate(dropped, initial_state=[1, 1])
+    assert np.all(c1.measurements['m'] == [1, 0])
+    assert np.all(d1.measurements['m'] == [1, 0])
+    c2 = sim.simulate(circuit, initial_state=[2, 2])
+    d2 = sim.simulate(dropped, initial_state=[2, 2])
+    assert np.all(c2.measurements['m'] == [2, 2])
+    assert np.all(d2.measurements['m'] == [2, 2])
 
 
 def test_drop_terminal_nonterminal_error():
