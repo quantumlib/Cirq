0fd6387b28cec59f8913bf6e46444198bcc640d3
Michael Hucka <mhucka@caltech.edu>
2025-01-09 13:17:32 -0800

    Add PyPI Trove classifiers to setup.py files (#6892)
    * Add PyPI Trove classifiers
    
    This adds classifier terms to improve software discoverability and
    findability on PyPI. Unfortunately, there is not (yet) a classifier
    term for quantum computing, and thus the terms used here are fairly
    general, although they are still useful to add.
    
    * Add new Trove term for quantum computing
    
    * Add new Trove term for quantum computing

--- cirq-google/setup.py
+++ cirq-google/setup.py
@@ -78,4 +78,18 @@ setup(
         'cirq_google.devices.specifications': ['*'],
         'cirq_google.json_test_data': ['*'],
     },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Quantum Computing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed",
+    ],
 )
