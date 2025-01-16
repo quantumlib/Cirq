6969d59e2d11c10a71cf6499e503a54c041e0aa0
Doug Strain <dstrain@google.com>
2025-01-16 07:16:48 -0800

    Update classically_controlled_operation.py
    I think the backslashes are causing issues with the doc parser.

--- cirq-core/cirq/ops/classically_controlled_operation.py
+++ cirq-core/cirq/ops/classically_controlled_operation.py
@@ -50,8 +50,8 @@ class ClassicallyControlledOperation(raw_types.Operation):
 
     >>> import cirq
     >>> a, b, c = cirq.LineQubit.range(3)
-    >>> circuit1 = cirq.Circuit( \
-    ...     cirq.measure(a, key='control_key'), \
+    >>> circuit1 = cirq.Circuit(
+    ...     cirq.measure(a, key='control_key'),
     ...     cirq.X(b).with_classical_controls('control_key'))
     >>> print(circuit1)
     0: ─────────────M───────
@@ -59,9 +59,9 @@ class ClassicallyControlledOperation(raw_types.Operation):
     1: ─────────────╫───X───
                     ║   ║
     control_key: ═══@═══^═══
-    >>> circuit2 = cirq.Circuit([ \
-    ...     cirq.measure(a, key='control_key1'), \
-    ...     cirq.measure(b, key='control_key2'), \
+    >>> circuit2 = cirq.Circuit([
+    ...     cirq.measure(a, key='control_key1'),
+    ...     cirq.measure(b, key='control_key2'),
     ...     cirq.X(c).with_classical_controls('control_key1', 'control_key2')])
     >>> print(circuit2)
                      ┌──┐
