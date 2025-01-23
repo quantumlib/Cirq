fdcd7692d38d7e56af8db03dfa1ed74bb9240c9b
daxfo <dax.fohl@gmail.com>
2025-01-22 16:42:20 -0800

    lint

--- cirq-core/cirq/transformers/stratify.py
+++ cirq-core/cirq/transformers/stratify.py
@@ -15,7 +15,7 @@
 """Transformer pass to repack circuits avoiding simultaneous operations with different classes."""
 
 import itertools
-from typing import TYPE_CHECKING, Type, Callable, Dict, Optional, Union, Iterable, Sequence, List
+from typing import TYPE_CHECKING, Type, Callable, Optional, Union, Iterable, Sequence, List
 
 from cirq import ops, circuits, protocols, _import
 from cirq.transformers import transformer_api
