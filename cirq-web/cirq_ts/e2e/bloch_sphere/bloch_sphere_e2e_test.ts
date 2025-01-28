0866981991104c50074b279ed7441e88de0627aa
Pavol Juhas <juhas@google.com>
2025-01-28 11:31:27 -0800

    Remove temporary PNG-saving code from cirq-web tests

--- cirq-web/cirq_ts/e2e/bloch_sphere/bloch_sphere_e2e_test.ts
+++ cirq-web/cirq_ts/e2e/bloch_sphere/bloch_sphere_e2e_test.ts
@@ -14,7 +14,6 @@
 
 import puppeteer from 'puppeteer';
 import {expect} from 'chai';
-import {copyFileSync} from 'fs';
 import {readFileSync} from 'fs';
 import pixelmatch from 'pixelmatch';
 import * as PNG from 'pngjs';
@@ -89,9 +88,6 @@ describe('Bloch sphere', () => {
         {threshold: 0.1}
       );
 
-      // save the actual png in a permanent file
-      copyFileSync(outputPath, 'bloch_sphere_actual.png');
-
       expect(pixels).to.equal(0);
     });
 
@@ -124,9 +120,6 @@ describe('Bloch sphere', () => {
         height,
         {threshold: 0.1}
       );
-
-      // save the actual png in a permanent file
-      copyFileSync(newVectorOutputPath, 'bloch_sphere_actual_custom.png');
 
       expect(pixels).to.equal(0);
     });
