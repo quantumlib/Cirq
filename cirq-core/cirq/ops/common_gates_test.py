329c670edb343808056931ac5f1d2e1c3cdf162c
daxfo <dax.fohl@gmail.com>
2025-01-29 17:15:03 -0800

    Y, Z gates, parameterize test

--- cirq-core/cirq/ops/common_gates_test.py
+++ cirq-core/cirq/ops/common_gates_test.py
@@ -1302,9 +1302,11 @@ def test_wrong_dims():
         _ = cirq.Z.on(cirq.LineQid(0, dimension=3))
 
 
-def test_parameterized_pauli():
-    gate = cirq.XPowGate(exponent=sympy.Symbol('exp'))
+@pytest.mark.parametrize('gate_type', [cirq.XPowGate, cirq.YPowGate, cirq.ZPowGate])
+@pytest.mark.parametrize('exponent', [sympy.Symbol('s'), sympy.Symbol('s') * 2])
+def test_parameterized_pauli_expansion(gate_type, exponent):
+    gate = gate_type(exponent=exponent)
     pauli = cirq.pauli_expansion(gate)
-    gate_resolved = cirq.resolve_parameters(gate, {'exp': 0.5})
-    pauli_resolved = cirq.resolve_parameters(pauli, {'exp': 0.5})
-    assert pauli_resolved == cirq.pauli_expansion(gate_resolved)
+    gate_resolved = cirq.resolve_parameters(gate, {'s': 0.5})
+    pauli_resolved = cirq.resolve_parameters(pauli, {'s': 0.5})
+    assert cirq.approx_eq(pauli_resolved, cirq.pauli_expansion(gate_resolved))
