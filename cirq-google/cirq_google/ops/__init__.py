a735849a0137723cc1209776b8bfadc01b818088
Doug Strain <dstrain@google.com>
2025-02-06 08:54:08 -0800

    Forgot to add json files.

--- cirq-google/cirq_google/ops/__init__.py
+++ cirq-google/cirq_google/ops/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Qubit Gates, Operations, and Tags useful for Google devices. """
+"""Qubit Gates, Operations, and Tags useful for Google devices."""
 
 from cirq_google.ops.calibration_tag import CalibrationTag as CalibrationTag
 
