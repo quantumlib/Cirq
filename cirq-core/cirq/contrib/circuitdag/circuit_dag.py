a1670e67c7fe449502e83677378b6f7cf2609321
daxfo <dax.fohl@gmail.com>
2025-02-12 13:30:38 -0800

    readd a couple cirq imports in contrib

--- cirq-core/cirq/contrib/circuitdag/circuit_dag.py
+++ cirq-core/cirq/contrib/circuitdag/circuit_dag.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Callable, Dict, Generic, Iterator, TypeVar, cast, TYPE_CHECKING
+from typing import Any, Callable, Dict, Generic, Iterator, TypeVar, cast
 
 import functools
 import networkx
