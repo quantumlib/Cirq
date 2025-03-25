806fdc6009212e3b69fb76c10b054b3cc6acad09
Doug Strain <dstrain@google.com>
2025-03-25 06:39:41 -0700

    Put back test.

--- cirq-google/cirq_google/engine/engine_program_test.py
+++ cirq-google/cirq_google/engine/engine_program_test.py
@@ -317,6 +317,17 @@ def mock_grpc_client():
         yield _fixture
 
 
+@mock.patch('cirq_google.engine.engine_client.EngineClient.get_program_async')
+def test_get_circuit_unsupported_program_type(get_program_async):
+    program = cg.EngineProgram('a', 'b', EngineContext())
+    get_program_async.return_value = quantum.QuantumProgram(
+        code=any_pb2.Any(type_url='type.googleapis.com/unknown.proto')
+    )
+
+    with pytest.raises(ValueError, match='unknown.proto'):
+        program.get_circuit()
+
+
 @mock.patch('cirq_google.engine.engine_client.EngineClient.delete_program_async')
 def test_delete(delete_program_async):
     program = cg.EngineProgram('a', 'b', EngineContext())
