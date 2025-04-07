3f4cc1a542f4c88b684bb579f86073a06a3b02c5
Pavol Juhas <juhas@google.com>
2025-04-07 16:12:59 -0700

    Address pandas warning about deprecated DataFrameGroupBy.apply default (#7235)
    * Address pandas warning about deprecated DataFrameGroupBy.apply default
    
    The current default is to pass grouping columns to the apply function,
    which in our case does not use them at all.  Here we exclude them with
    `include_groups=False` adopting the new behavior.
    
    Adding temporary check that the new output is the same.
    
    * Clean up temporary assertion (all tests passed)

--- cirq-core/cirq/experiments/xeb_fitting.py
+++ cirq-core/cirq/experiments/xeb_fitting.py
@@ -138,7 +138,7 @@ def benchmark_2q_xeb_fidelities(
     else:
         groupby_names = ['cycle_depth']
 
-    return df.groupby(groupby_names).apply(per_cycle_depth).reset_index()
+    return df.groupby(groupby_names).apply(per_cycle_depth, include_groups=False).reset_index()
 
 
 class XEBCharacterizationOptions(ABC):
@@ -702,7 +702,7 @@ def fit_exponential_decays(fidelities_df: pd.DataFrame) -> pd.DataFrame:
         groupby = ['layer_i', 'pair_i', 'pair']
     else:
         groupby = ['pair']
-    return fidelities_df.groupby(groupby).apply(_per_pair)
+    return fidelities_df.groupby(groupby).apply(_per_pair, include_groups=False)
 
 
 def before_and_after_characterization(
