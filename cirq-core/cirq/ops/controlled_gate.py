416bafd6704241662d9e670881ef4d9305fb4e08
Codrut <codrut@google.com>
2025-04-13 21:08:29 +0200

    Be precise which 2-cycle needs to be avoided in the decomposition.

--- cirq-core/cirq/ops/controlled_gate.py
+++ cirq-core/cirq/ops/controlled_gate.py
@@ -163,8 +163,12 @@ class ControlledGate(raw_types.Gate):
             self.num_controls(), self.control_values, self.control_qid_shape
         )
         # Prefer the subgate controlled version if available
-        if controlled_sub_gate.__class__ != self.__class__:
-            return controlled_sub_gate.on(*qubits)
+        if self != controlled_sub_gate:
+            # Prevent 2-cycle from appearing in recursive decomposition
+            if not isinstance(controlled_sub_gate, ControlledGate) or not isinstance(
+                controlled_sub_gate.sub_gate, common_gates.CZPowGate
+            ):
+                return controlled_sub_gate.on(*qubits)
         if (
             protocols.has_unitary(self.sub_gate)
             and protocols.num_qubits(self.sub_gate) == 1
