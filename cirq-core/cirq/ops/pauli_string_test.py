e5e39ba7ff2bfd14bc37d5a9ecfc9c4c56b0239d
Revanth Gundala <revanth.gundala@gmail.com>
2025-04-17 13:31:07 -0700

    Updated lint + added issue number

--- cirq-core/cirq/ops/pauli_string_test.py
+++ cirq-core/cirq/ops/pauli_string_test.py
@@ -2144,7 +2144,12 @@ def test_pauli_ops_identity_gate_operation():
     paulis = (cirq.I(q), cirq.X(q), cirq.Y(q), cirq.Z(q))
     for p1 in paulis:
         for p2 in paulis:
-            # TODO: Support addition and subtraction of identity gate operations
-            if p1 == cirq.I(q) and p2 == cirq.I(q): continue
-            assert isinstance(p1 + p2, (cirq.PauliSum, cirq.PauliString)), f"Addition failed for {p1} + {p2}"
-            assert isinstance(p1 - p2, (cirq.PauliSum, cirq.PauliString)), f"Subtraction failed for {p1} - {p2}"
+            # TODO: Issue #7280 - Support addition and subtraction of identity gate operations.
+            if p1 == cirq.I(q) and p2 == cirq.I(q):
+                continue
+            assert isinstance(
+                p1 + p2, (cirq.PauliSum, cirq.PauliString)
+            ), f"Addition failed for {p1} + {p2}"
+            assert isinstance(
+                p1 - p2, (cirq.PauliSum, cirq.PauliString)
+            ), f"Subtraction failed for {p1} - {p2}"
