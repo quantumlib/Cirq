efb20da0327c878fc2daecb2a7eb3407adbeeaf7
Revanth Gundala <revanth.gundala@gmail.com>
2025-04-22 14:57:39 -0700

    3 params

--- cirq-core/cirq/contrib/qasm_import/_parser_test.py
+++ cirq-core/cirq/contrib/qasm_import/_parser_test.py
@@ -1636,10 +1636,8 @@ def test_iswap_gate():
         for name, stmt in QasmParser.qelib_gates.items()
     ],
 )
-@pytest.mark.parametrize("theta,theta_str", [(np.pi / 4, "pi/4"), (np.pi / 2, "pi/2")])
-def test_all_qelib_gates_unitary_equivalence(
-    qasm_gate, cirq_gate, num_params, num_args, theta, theta_str
-):
+@pytest.mark.parametrize("theta,theta_str", [(np.pi/4, "pi/4"), (np.pi/2, "pi/2"), (np.pi, "pi")])
+def test_all_qelib_gates_unitary_equivalence(qasm_gate, cirq_gate, num_params, num_args, theta, theta_str):
     thetas = [theta] * num_params
     params_str = f"({','.join(theta_str for _ in range(num_params))})" if num_params else ""
     qubit_names, qubits = [], []
