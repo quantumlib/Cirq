d2fd3cc99855150b43a78d33c6e5bb24fdf4ecaf
Akshita <36688304+Akshita07@users.noreply.github.com>
2025-05-20 01:36:52 +0530

    Update Type Hints (#7348)
    * update type hints for files in cirq-aqt
    
    * fix formatting

--- cirq-aqt/cirq_aqt/aqt_target_gateset.py
+++ cirq-aqt/cirq_aqt/aqt_target_gateset.py
@@ -16,8 +16,6 @@
 
 from __future__ import annotations
 
-from typing import List
-
 import numpy as np
 
 import cirq
@@ -67,6 +65,6 @@ class AQTTargetGateset(cirq.TwoQubitCompilationTargetGateset):
         return NotImplemented
 
     @property
-    def postprocess_transformers(self) -> List[cirq.TRANSFORMER]:
+    def postprocess_transformers(self) -> list[cirq.TRANSFORMER]:
         """List of transformers which should be run after decomposing individual operations."""
         return [cirq.drop_negligible_operations, cirq.drop_empty_moments]
