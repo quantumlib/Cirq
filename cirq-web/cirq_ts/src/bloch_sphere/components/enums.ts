de2705489b3cd58d72027bf88ad647b7daf24c80
Akshita07 <aksingh0728@gmail.com>
2025-05-20 21:36:42 +0530

    remove cirq-ts subdirectory

--- cirq-web/cirq_ts/src/bloch_sphere/components/enums.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Enum to used for Meridians to determine the
- * direction they should face and what type
- * of meridian they are
- */
-export enum Orientation {
-  HORIZONTAL,
-  VERTICAL,
-}
