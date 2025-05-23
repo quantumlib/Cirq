1c0ba5b068eff07394d05d6c1a9ce93dd8b177ce
Dax Fohl <dax.fohl@gmail.com>
2025-05-23 08:42:38 -0700

    Flatten controlled-CZ and controlled-CX more consistently (#7365)
    Fixes #7241 by absorbing the control layer of controlled-CZ into the
    control itself, leaving a controlled-Z, and does the equivalent for CX /
    X. As documented in the issue, this approach allows for more consistency
    in how controlled gates are represented.
    
    As stated in the linked issue, almost all existing controlled-CZ cases
    already work this way; the only outlier is when control_values==[0].
    Eliminating that outlier allows most of the special handling to be
    consolidated in the base gates (Z and X), so it's already possible to
    see some benefit from the added consistency.

--- cirq-core/cirq/ops/controlled_gate.py
+++ cirq-core/cirq/ops/controlled_gate.py
@@ -151,12 +151,7 @@ class ControlledGate(raw_types.Gate):
         )
         # Prefer the subgate controlled version if available
         if self != controlled_sub_gate:
-            # Prevent 2-cycle from appearing in the recursive decomposition
-            # TODO: Remove after #7241 is resolved
-            if not isinstance(controlled_sub_gate, ControlledGate) or not isinstance(
-                controlled_sub_gate.sub_gate, common_gates.CZPowGate
-            ):
-                return controlled_sub_gate.on(*qubits)
+            return controlled_sub_gate.on(*qubits)
         if (
             protocols.has_unitary(self.sub_gate)
             and protocols.num_qubits(self.sub_gate) == 1
