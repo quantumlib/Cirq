f2dac86b643bf14de84ba5d68b0eeaf51d88e396
Pavol Juhas <juhas@google.com>
2025-05-29 17:03:19 -0700

    Add test failing on bug in GoogleNoiseProperties serialization
    GoogleNoiseProperties from `calibration_to_noise_properties()`
    cannot be serialized.  Here is a test for that failure.

--- cirq-google/cirq_google/engine/calibration_to_noise_properties_test.py
+++ cirq-google/cirq_google/engine/calibration_to_noise_properties_test.py
@@ -329,6 +329,56 @@ def test_zphase_data():
             assert prop.fsim_errors[OpIdentifier(gate, *qs[::-1])] == values[i]
 
 
+def test_json_serde_works():
+    qubits = [cirq.GridQubit(0, 0), cirq.GridQubit(0, 1), cirq.GridQubit(1, 0)]
+    pauli_error = [0.001, 0.002, 0.003]
+    incoherent_error = [0.0001, 0.0002, 0.0003]
+    p00_error = [0.004, 0.005, 0.006]
+    p11_error = [0.007, 0.008, 0.009]
+    t1_micros = [10, 20, 30]
+    syc_pauli = [0.01, 0.02]
+    iswap_pauli = [0.03, 0.04]
+    syc_angles = [
+        cirq.PhasedFSimGate(theta=0.011, phi=-0.021, zeta=-0.031, gamma=0.043),
+        cirq.PhasedFSimGate(theta=-0.012, phi=0.022, zeta=0.032, gamma=-0.044),
+    ]
+    iswap_angles = [
+        cirq.PhasedFSimGate(theta=-0.013, phi=0.023, zeta=0.031, gamma=-0.043),
+        cirq.PhasedFSimGate(theta=0.014, phi=-0.024, zeta=-0.032, gamma=0.044),
+    ]
+
+    # Create NoiseProperties object from Calibration
+    calibration = get_mock_calibration(
+        pauli_error,
+        incoherent_error,
+        p00_error,
+        p11_error,
+        t1_micros,
+        syc_pauli,
+        iswap_pauli,
+        syc_angles,
+        iswap_angles,
+    )
+
+    qubit_pairs = [(qubits[0], qubits[1]), (qubits[0], qubits[2])]
+    zphase_data = {
+        "syc": {
+            "zeta": {qubit_pairs[0]: syc_angles[0].zeta, qubit_pairs[1]: syc_angles[1].zeta},
+            "gamma": {qubit_pairs[0]: syc_angles[0].gamma, qubit_pairs[1]: syc_angles[1].gamma},
+        },
+        "sqrt_iswap": {
+            "zeta": {qubit_pairs[0]: iswap_angles[0].zeta, qubit_pairs[1]: iswap_angles[1].zeta},
+            "gamma": {qubit_pairs[0]: iswap_angles[0].gamma, qubit_pairs[1]: iswap_angles[1].gamma},
+        },
+    }
+
+    prop = cirq_google.noise_properties_from_calibration(calibration, zphase_data)
+
+    json_text = cirq.to_json(prop)
+    prop1 = cirq.read_json(json_text=json_text)
+    assert prop1 == prop
+
+
 def test_incomplete_calibration():
     pauli_error = [0.001, 0.002, 0.003]
     p00_error = [0.004, 0.005, 0.006]
