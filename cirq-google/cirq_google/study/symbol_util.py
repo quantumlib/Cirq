5c96d022d538e6c1dfdde60825a90e068bcbccad
Bicheng Ying <bichengying@gmail.com>
2025-06-26 11:55:08 -0700

    Add AnalogTrajectory and FreqMap (#7437)
    Add FreqMap and AnalogTrajectory into cirq_google.experiments.
    
    In this PR, two major differences compared with Trond did before.
    
    1. `AnalogTrajectory` initialized with classmethod so that the class
    member is always full_trajectory
    2. Add the resolving ability in the `FreqMap` so that we can provide the
    symbol into it and plot it with resolver like `pulse_plot`
    
    New usage example
    ```py
    traj1 = (20 * tu.ns, {"q0_1": 5 * tu.GHz}, {})
    traj2 = (sympy.Symbol("t1"), {"q0_2": 8 * tu.GHz}, {})
    traj3 = (
        40 * tu.ns,
        {"q0_0": 8 * tu.GHz, "q0_1": None, "q0_2": None},
        {("q0_0", "q0_1"): 5 * tu.MHz, ("q0_1", "q0_2"): 8 * tu.MHz},
    )
    trajs = [traj1, traj2, traj3]
    
    analog_traj = AnalogTrajectory.from_sparse_trajecotry(trajs)
    analog_traj.plot(resolver={"t1": 50*tu.ns})
    ```

---
+++ cirq-google/cirq_google/study/symbol_util.py
@@ -0,0 +1,53 @@
+# Copyright 2025 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import AbstractSet, Any, TypeAlias
+
+import sympy
+import tunits as tu
+
+import cirq
+
+# The gate is intended for the google internal use, hence the typing style
+# follows more on the t-unit + symbol instead of float + symbol style.
+ValueOrSymbol: TypeAlias = tu.Value | sympy.Basic
+
+# A sentile for not finding the key in resolver.
+NOT_FOUND = "__NOT_FOUND__"
+
+
+def direct_symbol_replacement(x, resolver: cirq.ParamResolver):
+    """A shortcut for value resolution to avoid tu.unit compare with float issue."""
+    if isinstance(x, sympy.Symbol):
+        value = resolver.param_dict.get(x.name, NOT_FOUND)
+        if value == NOT_FOUND:
+            value = resolver.param_dict.get(x, NOT_FOUND)
+        if value != NOT_FOUND:
+            return value
+        return x  # pragma: no cover
+    return x
+
+
+def dict_param_name(dict_with_value: dict[Any, ValueOrSymbol] | None) -> AbstractSet[str]:
+    """Find the names of all parameterized value in a dictionary."""
+    if dict_with_value is None:
+        return set()
+    return {v.name for v in dict_with_value.values() if cirq.is_parameterized(v)}
+
+
+def is_parameterized_dict(dict_with_value: dict[Any, ValueOrSymbol] | None) -> bool:
+    """Check if any values in the dictionary is parameterized."""
+    if dict_with_value is None:
+        return False  # pragma: no cover
+    return any(cirq.is_parameterized(v) for v in dict_with_value.values())
