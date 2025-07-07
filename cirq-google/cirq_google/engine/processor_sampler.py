b3fe8749054648b247dc0cdafe37a7c84209a6f0
Devon Yates <devyates@google.com>
2025-07-07 15:46:45 -0700

    Add `max_concurrent_jobs` to `Engine.get_sampler()`. (#7442)
    Adds `max_concurrent_jobs` to the utility function
    [Engine.get_sampler](https://github.com/dyates/Cirq/blob/b8ca4844ef80102aa83ace1093552f25c02b101a/cirq-google/cirq_google/engine/engine.py#L580)
    to allow setting the [same field in the
    `ProcessSampler`]((https://github.com/quantumlib/Cirq/blob/4c7e393eb9fcefcba0c3c509e32cc354197cf014/cirq-google/cirq_google/engine/processor_sampler.py#L37)).
    This field throttles the number concurrent jobs that are sent to the
    backend (via `run_batch`).
    
    ---------
    
    Co-authored-by: Pavol Juhas <juhas@google.com>

--- cirq-google/cirq_google/engine/processor_sampler.py
+++ cirq-google/cirq_google/engine/processor_sampler.py
@@ -113,3 +113,8 @@ class ProcessorSampler(cirq.Sampler):
     @property
     def device_config_name(self) -> str:
         return self._device_config_name
+
+    @property
+    def max_concurrent_jobs(self) -> int:
+        assert self._concurrent_job_limiter.capacity is not None
+        return self._concurrent_job_limiter.capacity
