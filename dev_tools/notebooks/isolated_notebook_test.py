081684d94c23b9e9f646bc96c906451942ae0106
Pavol Juhas <juhas@google.com>
2025-07-10 22:18:46 -0700

    Make `list_all_notebooks` work at any CWD
    Let it return a list of absolute notebook paths.
    Adjust `filter_notebooks` to accept either absolute or relative paths.
    Also make `_list_changed_notebooks` return absolute paths and
    work at any cwd.

--- dev_tools/notebooks/isolated_notebook_test.py
+++ dev_tools/notebooks/isolated_notebook_test.py
@@ -35,7 +35,7 @@ import warnings
 import pytest
 
 from dev_tools import shell_tools
-from dev_tools.notebooks import filter_notebooks, list_all_notebooks, rewrite_notebook
+from dev_tools.notebooks import filter_notebooks, list_all_notebooks, REPO_ROOT, rewrite_notebook
 
 # these notebooks rely on features that are not released yet
 # after every release we should raise a PR and empty out this list
@@ -105,19 +105,20 @@ def _find_base_revision():
     raise ValueError("Can't find a base revision to compare the files with.")
 
 
-def _list_changed_notebooks() -> set[str]:
+def _list_changed_notebooks() -> list[str]:
     try:
         rev = _find_base_revision()
-        output = subprocess.check_output(f'git diff --diff-filter=d --name-only {rev}'.split())
-        lines = output.decode('utf-8').splitlines()
+        output = subprocess.check_output(
+            f'git diff --diff-filter=d --name-only {rev}'.split(), cwd=REPO_ROOT, text=True
+        )
+        changed_files = [str(REPO_ROOT.joinpath(f)) for f in output.splitlines()]
         # run all tests if this file or any of the dev tool dependencies change
         if any(
-            l
-            for l in lines
-            if l.endswith("isolated_notebook_test.py") or l.startswith("dev_tools/requirements")
+            f.endswith("isolated_notebook_test.py") or "/dev_tools/requirements/" in f
+            for f in changed_files
         ):
             return list_all_notebooks()
-        return set(l for l in lines if l.endswith(".ipynb"))
+        return [f for f in changed_files if f.endswith(".ipynb")]
     except ValueError as e:
         # It would be nicer if we could somehow automatically skip the execution of this completely,
         # however, in order to be able to rely on parallel pytest (xdist) we need parametrization to
@@ -129,7 +130,7 @@ def _list_changed_notebooks() -> set[str]:
             f"No changed notebooks are tested "
             f"(this is expected in non-notebook tests in CI): {e}"
         )
-        return set()
+        return []
 
 
 def _partitioned_test_cases(notebooks):
