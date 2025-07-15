bb2e86bc691b6165c3de74b28c67933c588385ab
Pavol Juhas <juhas@google.com>
2025-07-15 11:56:19 -0700

    Fix resolution of notebook paths when pytest is not at the repo root (#7487)
    Create the `out` directory for notebook outputs at the repository
    root regardless of the pytest execution directory.
    
    Follow-up to #7481

--- dev_tools/notebooks/notebook_test.py
+++ dev_tools/notebooks/notebook_test.py
@@ -107,18 +107,19 @@ def test_notebooks_against_cirq_head(
     Lines in this file that do not have `->` are ignored.
     """
     notebook_file = os.path.basename(notebook_path)
-    notebook_rel_dir = os.path.dirname(os.path.relpath(notebook_path, "."))
+    notebook_rel_dir = os.path.dirname(os.path.relpath(notebook_path, REPO_ROOT))
     out_path = f"out/{notebook_rel_dir}/{notebook_file[:-6]}.out.ipynb"
     rewritten_notebook_path = rewrite_notebook(notebook_path)
     papermill_flags = "--no-request-save-on-cell-execute --autosave-cell-every 0"
-    cmd = f"""mkdir -p out/{notebook_rel_dir}
-papermill {rewritten_notebook_path} {out_path} {papermill_flags}"""
+    cmd = f"papermill {rewritten_notebook_path} {REPO_ROOT/out_path} {papermill_flags}"
 
+    REPO_ROOT.joinpath("out", notebook_rel_dir).mkdir(parents=True, exist_ok=True)
     result = shell_tools.run(
         cmd,
         log_run_to_stderr=False,
         shell=True,
         check=False,
+        cwd=REPO_ROOT,
         capture_output=True,
         env=env_with_temporary_pip_target,
     )
