05d977925ac71e06c391b0998f8d90ed06922b75
wang-anthony03 <anthonywang03@icloud.com>
2025-07-19 11:41:41 -0400

    Added circuit.reverse() method and tests covering functionality

--- cirq-core/cirq/circuits/circuit.py
+++ cirq-core/cirq/circuits/circuit.py
@@ -1714,6 +1714,7 @@ class Circuit(AbstractCircuit):
     *   batch_remove
     *   batch_insert_into
     *   insert_at_frontier
+    *   reverse
 
     Circuits can also be iterated over,
 
@@ -2462,6 +2463,16 @@ class Circuit(AbstractCircuit):
                 self._moments[k] = self._moments[k].without_operations_touching(qubits)
         self._mutated()
 
+    def reverse(self) -> None:
+        """Reverses the moments in the circuit, and the operations in the moments."""
+        # Work on a copy in case validation fails halfway through.
+        copy = self.copy()
+        backwards = []
+        for moment in copy[::-1]:
+            backwards.append(Moment(reversed(moment.operations)))
+        self._moments = backwards
+        self._mutated()
+
     @property
     def moments(self) -> Sequence[cirq.Moment]:
         return self._moments
@@ -2485,6 +2496,8 @@ class Circuit(AbstractCircuit):
             # Keep moments aligned
             c_noisy += Circuit(op_tree)
         return c_noisy
+    
+    
 
 
 def _pick_inserted_ops_moment_indices(
