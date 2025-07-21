c9f7cdac02af25ab018181906a0ac925fad23862
Pavol Juhas <juhas@google.com>
2025-07-20 19:40:48 -0700

    Clean up cirq.contrib.json_test_data.spec
    Sync resolver cache with the contrib package `_class_resolver_dictionary`
    and Move previously serializable classes from `should_not_be_serialized`
    to `tested_elsewhere`.
    
    Also add copyright notice.

--- cirq-core/cirq/contrib/json_test_data/spec.py
+++ cirq-core/cirq/contrib/json_test_data/spec.py
@@ -1,5 +1,17 @@
-# pylint: disable=wrong-or-nonexistent-copyright-notice
-from __future__ import annotations
+# Copyright 2025 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 import pathlib
 
@@ -12,24 +24,9 @@ TestSpec = ModuleJsonTestSpec(
     packages=[cirq.contrib],
     test_data_path=pathlib.Path(__file__).parent,
     not_yet_serializable=[],
-    should_not_be_serialized=[
-        "QuantumVolumeResult",
-        "SwapPermutationGate",
-        "BayesianNetworkGate",
-        "Unique",
-        "CircuitDag",
-    ],
-    resolver_cache={
-        k: v
-        for k, v in _class_resolver_dictionary().items()
-        if k
-        not in {
-            "QuantumVolumeResult",
-            "SwapPermutationGate",
-            "BayesianNetworkGate",
-            "Unique",
-            "CircuitDag",
-        }
-    },
+    should_not_be_serialized=["Unique", "CircuitDag"],
+    resolver_cache=_class_resolver_dictionary(),
     deprecated={},
+    # TODO: #7520 - create .json and .repr for these so they can be tested here
+    tested_elsewhere=["QuantumVolumeResult", "SwapPermutationGate", "BayesianNetworkGate"],
 )
