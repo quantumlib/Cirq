e1ae09cb930a35da5ee2db94a42e462a77ec8ce3
Bicheng Ying <bichengying@gmail.com>
2025-08-01 18:22:02 +0000

    Fix test

--- cirq-google/cirq_google/api/v2/sweeps_test.py
+++ cirq-google/cirq_google/api/v2/sweeps_test.py
@@ -370,25 +370,24 @@ def test_sweep_from_proto_with_func_on_resursive_sweep_succeeds(expected_sweep):
     assert round_trip_sweep == expected_sweep
 
 
-def test_sweep_with_list_sweep():
+@pytest.mark.parametrize("use_float64", [True, False])
+def test_sweep_with_list_sweep(use_float64):
     ls = cirq.study.to_sweep([{'a': 1, 'b': 2}, {'a': 3, 'b': 4}])
-    proto = v2.sweep_to_proto(ls)
+    proto = v2.sweep_to_proto(ls, use_float64=use_float64)
     expected = v2.run_context_pb2.Sweep()
     expected.sweep_function.function_type = v2.run_context_pb2.SweepFunction.ZIP
     p1 = expected.sweep_function.sweeps.add()
     p1.single_sweep.parameter_key = 'a'
-    p1.single_sweep.points.points.extend([1, 3])
+
     p2 = expected.sweep_function.sweeps.add()
     p2.single_sweep.parameter_key = 'b'
-    p2.single_sweep.points.points.extend([2, 4])
-    assert proto == expected
 
-    # Encode into float64 isntead
-    proto = v2.sweep_to_proto(ls, use_float64=True)
-    p1.single_sweep.points.ClearField('points')
-    p1.single_sweep.points.points_double.extend([1.0, 3.0])
-    p2.single_sweep.points.ClearField('points')
-    p2.single_sweep.points.points_double.extend([2, 4])
+    if use_float64:
+        p1.single_sweep.points.points_double.extend([1, 3])
+        p2.single_sweep.points.points_double.extend([2, 4])
+    else:
+        p1.single_sweep.points.points.extend([1, 3])
+        p2.single_sweep.points.points.extend([2, 4])
     assert proto == expected
 
 
