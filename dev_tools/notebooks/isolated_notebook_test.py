b5ca9707c0f205c8c2678fa2dc8f6857a1cd1953
Michael Hucka <mhucka@caltech.edu>
2025-08-11 18:59:00 -0700

    Test all notebooks vs stable Cirq after the release (#7561)
    Make all notebook tests use stable cirq after the 1.6.0 release.
    
    Ref:
    https://github.com/quantumlib/Cirq/blob/main/docs/dev/notebooks.md#lifecycle
    
    Fixes #7556
    
    ---------
    
    Co-authored-by: Pavol Juhas <juhas@google.com>

--- dev_tools/notebooks/isolated_notebook_test.py
+++ dev_tools/notebooks/isolated_notebook_test.py
@@ -42,16 +42,7 @@ from dev_tools.notebooks import filter_notebooks, list_all_notebooks, REPO_ROOT,
 # note that these notebooks are still tested in dev_tools/notebook_test.py
 # Please, always indicate in comments the feature used for easier bookkeeping.
 
-NOTEBOOKS_DEPENDING_ON_UNRELEASED_FEATURES: list[str] = [
-    # Requires `load_device_noise_properties` from #7369
-    'docs/hardware/qubit_picking.ipynb',
-    'docs/simulate/noisy_simulation.ipynb',
-    'docs/simulate/quantum_virtual_machine.ipynb',
-    'docs/simulate/qvm_basic_example.ipynb',
-    # Remove once the renaming of `whitelisted_users` -> `allowlisted_users`
-    # throughout cirq_google is released.
-    'docs/simulate/virtual_engine_interface.ipynb',
-]
+NOTEBOOKS_DEPENDING_ON_UNRELEASED_FEATURES: list[str] = []
 
 # By default all notebooks should be tested, however, this list contains exceptions to the rule
 # please always add a reason for skipping.
