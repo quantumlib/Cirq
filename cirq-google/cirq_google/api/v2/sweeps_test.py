39b3e6b325a2b7fa94fdd871c0fe9ef33afbda7a
Doug Strain <dstrain@google.com>
2025-08-12 11:40:43 -0700

    Improve serialization of Sweepable objects (#7563)
    - Sweepable objects (such as lists of dicts or ParamResolvers) get
    serialized into cirq_google run_contexts by converting them to sweeps
    first then converts the sweeps. This is hugely inefficient as compared
    to directly serializing the dictionary directly.
    - This changes run_context_to_sweep to directly serialize Sweepable
    objects.
    - It adds a check to verify the serialization is equivalent to the old
    way of doing things.
    - This PR also reorders some isinstance checks to make it more
    efficient.
    
    For the following:
    ```
        sweepable = [{f'x_{i}':i+t for i in range(500)} for t in range(500)]
        _ = v2.run_context_to_proto(sweepable, 1000, compress_proto=False)
    ```
    
    This change speeds the code from 2.4 seconds to 0.5 seconds.

--- cirq-google/cirq_google/api/v2/sweeps_test.py
+++ cirq-google/cirq_google/api/v2/sweeps_test.py
@@ -473,3 +473,41 @@ def test_tunits_round_trip(sweep, use_float64):
 def test_const_sweep_with_numpy_types_roundtrip(value):
     sweep = cirq.Points('const', [value])
     assert v2.sweep_from_proto(v2.sweep_to_proto(sweep)) == sweep
+
+
+@pytest.mark.parametrize(
+    'sweepable', [{'a': 4}, {'a': 2, 'b': 8}, [{'a': 1, 'b': 2}, {'a': 3, 'b': 4}]]
+)
+@pytest.mark.parametrize('use_resolver', [True, False])
+def test_run_context_to_proto_sweepable(sweepable: cirq.Sweepable, use_resolver: bool) -> None:
+    """Sweepable objects are accepted by run_context_to_proto.
+
+    These, like lists of dictionaries and ParamResolvers should be
+    converted to sweeps by the serializer.
+
+    This test ensures that the sweepable is serialized the same
+    as the corresponding sweep.
+    """
+    if use_resolver:
+        if isinstance(sweepable, list):
+            sweepable = [cirq.ParamResolver(element) for element in sweepable]
+        else:
+            assert isinstance(sweepable, dict)
+            sweepable = cirq.ParamResolver(sweepable)
+
+    # Previous behavior: convert sweepable to sweep
+    sweeps = cirq.to_sweeps(sweepable)
+    expected_sweep = v2.run_context_pb2.RunContext()
+    for sweep in sweeps:
+        sweep_proto = expected_sweep.parameter_sweeps.add()
+        sweep_proto.repetitions = 1000
+        v2.sweep_to_proto(sweep, out=sweep_proto.sweep)
+
+    # New behavior: directly convert sweepable to proto
+    actual_sweep = v2.run_context_to_proto(sweepable, 1000, compress_proto=False)
+    assert expected_sweep == actual_sweep
+
+
+def test_invalid_sweepable_raises() -> None:
+    with pytest.raises(TypeError):
+        _ = v2.run_context_to_proto(cirq.X, 1000, compress_proto=False)  # type: ignore[arg-type]
