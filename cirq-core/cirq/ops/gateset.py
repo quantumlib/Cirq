1f001334b2770dcb8b0a0641137679b06f156713
Doug Strain <dstrain@google.com>
2025-08-14 14:23:47 -0700

    Change order of checking containment within a gateset (#7578)
    Gate Families have two main modes of operations: 'type' gate families
    that check containment versus a type of gate and 'instance' gate
    families which check whether the operation matches a specific
    instantiation of the gate. 'instance' checking is much slower.
    
    - This PR switches the order of checking gate families. It now checks
    "type" gate families before "instance" gate families.
    - Type gate family checks are simple `isinstance` checks and should be
    fast.
    - Instance gate families check instance equality, which is a lot slower,
    since it has to compare value equality for every operation for
    containment.
    
    This speeds up the validation of a sample circuit with 400 moments of 18
    single-qubit operations from 1.5 seconds to 0.11 seconds.

--- cirq-core/cirq/ops/gateset.py
+++ cirq-core/cirq/ops/gateset.py
@@ -416,13 +416,6 @@ class Gateset:
         g = item if isinstance(item, raw_types.Gate) else item.gate
         assert g is not None, f'`item`: {item} must be a gate or have a valid `item.gate`'
 
-        if g in self._instance_gate_families:
-            assert item in self._instance_gate_families[g], (
-                f"{item} instance matches {self._instance_gate_families[g]} but "
-                f"is not accepted by it."
-            )
-            return True
-
         for gate_mro_type in type(g).mro():
             if gate_mro_type in self._type_gate_families:
                 assert item in self._type_gate_families[gate_mro_type], (
@@ -430,6 +423,13 @@ class Gateset:
                     f"{self._type_gate_families[gate_mro_type]} but is not accepted by it."
                 )
                 return True
+
+        if g in self._instance_gate_families:
+            assert item in self._instance_gate_families[g], (
+                f"{item} instance matches {self._instance_gate_families[g]} but "
+                f"is not accepted by it."
+            )
+            return True
 
         return any(item in gate_family for gate_family in self._gates)
 
