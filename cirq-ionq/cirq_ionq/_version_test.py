1a4ac41204e44ddf0e322a769cd492df070b6586
Pavol Juhas <juhas@google.com>
2025-08-13 17:37:52 -0700

    Bump cirq version to 1.6.1

--- cirq-ionq/cirq_ionq/_version_test.py
+++ cirq-ionq/cirq_ionq/_version_test.py
@@ -3,4 +3,4 @@ import cirq_ionq
 
 
 def test_version():
-    assert cirq_ionq.__version__ == "1.6.0"
+    assert cirq_ionq.__version__ == "1.6.1"
--- cirq-pasqal/cirq_pasqal/_version_test.py
+++ cirq-pasqal/cirq_pasqal/_version_test.py
@@ -3,4 +3,4 @@ import cirq_pasqal
 
 
 def test_version():
-    assert cirq_pasqal.__version__ == "1.6.0"
+    assert cirq_pasqal.__version__ == "1.6.1"
