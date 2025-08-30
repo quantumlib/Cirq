bbda5fc37049626a3957d0e91628a8dc7a9d96a8
Pavol Juhas <juhas@google.com>
2025-08-29 19:18:26 -0700

    os.walk accepts a path argument

--- dev_tools/modules.py
+++ dev_tools/modules.py
@@ -181,7 +181,7 @@ def _rewrite_version(version_file: Path, old: str, new: str) -> None:
 
 
 def _find_version_file(top: Path) -> Path:
-    for root, dirnames, files in os.walk(str(top)):
+    for root, dirnames, files in os.walk(top):
         if "build" in dirnames:
             dirnames.remove("build")
         if "_version.py" in files:
