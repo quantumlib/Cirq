f06362ae58af072de00e19c9572ec2a881d23ef2
Pavol Juhas <juhas@google.com>
2025-09-01 22:47:02 -0700

    Improve wheel-building process (#7613)
    - Use SPDX license expression instead of deprecated classifier
    - Use `pip wheel` to create reproducible distribution wheels
    - Specify build-backed for Cirq and its sub-packages
    - Make sure `setuptools.build_meta` can import our `dev_tools`
    
    This makes the wheels created by `produce-package.sh` reproducible
    and resolves deprecation warnings on execution of setup.py
    and on deprecated license classifier.
    
    Fixes #6502 and #6623

--- setup.py
+++ setup.py
@@ -12,9 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pathlib
 import runpy
+import sys
 
 from setuptools import setup
+
+# Ensure we can import our own dev_tools
+sys.path.insert(0, str(pathlib.Path(__file__).parent.absolute()))
 
 from dev_tools import modules
 from dev_tools.requirements import explode
@@ -52,7 +57,7 @@ setup(
     python_requires='>=3.11.0',
     install_requires=requirements,
     extras_require={'dev_env': dev_requirements},
-    license='Apache 2',
+    license='Apache-2.0',
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -62,7 +67,6 @@ setup(
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX :: Linux",
