bcd80cea262aef10e4e935f910deb2801f2f0d43
Pavol Juhas <juhas@google.com>
2025-10-24 17:22:34 -0700

    beitd - handle scalar binary base first than fall back to general case

--- cirq-core/cirq/value/digits.py
+++ cirq-core/cirq/value/digits.py
@@ -168,14 +168,14 @@ def big_endian_int_to_digits(
         >>> cirq.big_endian_int_to_digits(11, base=[2, 3, 4])
         [0, 2, 3]
     """
+    if digit_count and base == 2:
+        binary_chars = bin(val).removeprefix('0b')
+        zeros_count = digit_count - len(binary_chars)
+        if zeros_count >= 0:
+            return [0] * zeros_count + [1 if c == "1" else 0 for c in binary_chars]
     if isinstance(base, int):
         if digit_count is None:
             raise ValueError('No digit count. Provide `digit_count` when base is an int.')
-        if base == 2:
-            binary_chars = bin(val).removeprefix('0b')
-            zeros_count = digit_count - len(binary_chars)
-            if zeros_count >= 0:
-                return [0] * zeros_count + [1 if c == "1" else 0 for c in binary_chars]
         base = (base,) * digit_count
     else:
         base = tuple(base)
