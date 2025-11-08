580b7981abf17dc8d5f8e1d0588e85e89769e8ac
Rahil Jain <51580546+RahilJain1366@users.noreply.github.com>
2025-11-07 18:17:24 -0600

    review fixes #7520

--- cirq-core/cirq/contrib/json.py
+++ cirq-core/cirq/contrib/json.py
@@ -2,7 +2,6 @@
 """Functions for JSON serialization and de-serialization for classes in Contrib."""
 
 from __future__ import annotations
-import warnings
 import functools
 
 from cirq.protocols.json_serialization import _register_resolver, DEFAULT_RESOLVERS, ObjectFactory
@@ -38,34 +37,20 @@ def _class_resolver_dictionary() -> dict[str, ObjectFactory]:
     ]
     return {cls.__name__: cls for cls in classes}
 
-class _DeprecatedDefaultContribResolvers(list):
-    """Wrapper to emit deprecation warning when DEFAULT_CONTRIB_RESOLVERS is accessed."""
-
-    def __init__(self):
-        super().__init__([contrib_class_resolver] + DEFAULT_RESOLVERS)
-        self._warning_shown = False
-
-    def __iter__(self):
-        self._show_warning()
-        return super().__iter__()
-
-    def __getitem__(self, item):
-        self._show_warning()
-        return super().__getitem__(item)
-
-    def _show_warning(self):
-        if not self._warning_shown:
-            warnings.warn(
-                "DEFAULT_CONTRIB_RESOLVERS is deprecated. "
-                "Contrib classes are now automatically resolved through the standard JSON resolver. "
-                "You can remove the 'resolvers' parameter from assert_json_roundtrip_works calls.",
-                DeprecationWarning,
-                stacklevel=4
-            )
-            self._warning_shown = True
-
-
-DEFAULT_CONTRIB_RESOLVERS = _DeprecatedDefaultContribResolvers()
-
-
 _register_resolver(_class_resolver_dictionary)
+
+DEFAULT_CONTRIB_RESOLVERS = [contrib_class_resolver, *DEFAULT_RESOLVERS]
+
+_DEFAULT_CONTRIB_RESOLVERS_DEPRECATION_MESSAGE = (
+    'DEFAULT_CONTRIB_RESOLVERS will no longer be supported.'
+    'Contrib classes are now automatically resolved through the standard JSON resolver.'
+    'You can remove the "resolvers" parameter from assert_json_roundtrip_works calls.'
+)
+from cirq import _compat
+
+_compat.deprecate_attributes(
+    __name__,
+    {
+        'DEFAULT_CONTRIB_RESOLVERS': ('v1.8', _DEFAULT_CONTRIB_RESOLVERS_DEPRECATION_MESSAGE),
+    },
+)
