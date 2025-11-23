635c3645948c84a25e52c8ea6b6d9457a698825e
mhucka <mhucka@google.com>
2025-11-23 04:33:44 +0000

    Try to fix type issues

--- cirq-ionq/cirq_ionq/sampler.py
+++ cirq-ionq/cirq_ionq/sampler.py
@@ -107,7 +107,12 @@ class Sampler(cirq.Sampler):
             job_results = [job.results(timeout_seconds=self._timeout_seconds) for job in jobs]
         else:
             job_results = [job.results() for job in jobs]
-        flattened_job_results = list(itertools.chain.from_iterable(job_results))
+        flattened_job_results: list[results.QPUResult | results.SimulatorResult] = []
+        for res in job_results:
+            if isinstance(res, list):
+                flattened_job_results.extend(res)
+            else:
+                flattened_job_results.append(res)
         cirq_results = []
         for result, params in zip(flattened_job_results, resolvers):
             if isinstance(result, results.QPUResult):
