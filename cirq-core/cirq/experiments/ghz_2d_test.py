69dca5df1fcbf616e385aac5f1f79ee23addf598
Pavol Juhas <juhas@google.com>
2025-12-09 17:46:22 -0800

    Capitalize module constants and add type annotations
    Also clean some unnecessary comments.

--- cirq-core/cirq/experiments/ghz_2d_test.py
+++ cirq-core/cirq/experiments/ghz_2d_test.py
@@ -24,7 +24,7 @@ import cirq
 import cirq.experiments.ghz_2d as ghz_2d
 
 
-def _create_mock_graph():
+def _create_mock_graph() -> tuple[nx.Graph, cirq.GridQubit]:
     qubits = cirq.GridQubit.rect(6, 6)
     g = nx.Graph()
     for q in qubits:
@@ -36,17 +36,17 @@ def _create_mock_graph():
     return g, cirq.GridQubit(3, 3)
 
 
-graph, center_qubit = _create_mock_graph()
+GRAPH, CENTER_QUBIT = _create_mock_graph()
 
 
-@pytest.mark.parametrize("num_qubits", list(range(1, len(graph.nodes) + 1)))
+@pytest.mark.parametrize("num_qubits", list(range(1, len(GRAPH.nodes) + 1)))
 @pytest.mark.parametrize("randomized", [True, False])
 @pytest.mark.parametrize("add_dd_and_align_right", [True, False])
 def test_ghz_circuits_size(num_qubits: int, randomized: bool, add_dd_and_align_right: bool) -> None:
     """Tests the size of the GHZ circuits."""
     circuit = ghz_2d.generate_2d_ghz_circuit(
-        center_qubit,
-        graph,
+        CENTER_QUBIT,
+        GRAPH,
         num_qubits=num_qubits,
         randomized=randomized,
         add_dd_and_align_right=add_dd_and_align_right,
@@ -56,15 +56,15 @@ def test_ghz_circuits_size(num_qubits: int, randomized: bool, add_dd_and_align_r
 
 @pytest.mark.parametrize("num_qubits", [2, 3, 4, 5, 6, 8, 10])
 @pytest.mark.parametrize("randomized", [True, False])
-@pytest.mark.parametrize("add_dd_and_align_right", [True, False])  # , True
+@pytest.mark.parametrize("add_dd_and_align_right", [True, False])
 def test_ghz_circuits_state(
     num_qubits: int, randomized: bool, add_dd_and_align_right: bool
 ) -> None:
     """Tests the state vector form of the GHZ circuits."""
 
     circuit = ghz_2d.generate_2d_ghz_circuit(
-        center_qubit,
-        graph,
+        CENTER_QUBIT,
+        GRAPH,
         num_qubits=num_qubits,
         randomized=randomized,
         add_dd_and_align_right=add_dd_and_align_right,
@@ -84,7 +84,7 @@ def test_ghz_circuits_state(
 def test_transform_circuit_properties() -> None:
     """Tests that _transform_circuit preserves circuit properties."""
     circuit = ghz_2d.generate_2d_ghz_circuit(
-        center_qubit, graph, num_qubits=9, randomized=False, add_dd_and_align_right=False
+        CENTER_QUBIT, GRAPH, num_qubits=9, randomized=False, add_dd_and_align_right=False
     )
     transformed_circuit = ghz_2d._transform_circuit(circuit)
 
@@ -108,8 +108,8 @@ def test_ghz_circuits_bfs_order(num_qubits: int) -> None:
     """Verifies that the circuit construction maintains BFS order"""
 
     circuit = ghz_2d.generate_2d_ghz_circuit(
-        center_qubit,
-        graph,
+        CENTER_QUBIT,
+        GRAPH,
         num_qubits=num_qubits,
         randomized=False,  # Test must run on the deterministic BFS order
         add_dd_and_align_right=False,  # Test must run on the raw circuit
@@ -122,8 +122,8 @@ def test_ghz_circuits_bfs_order(num_qubits: int) -> None:
             if isinstance(op.gate, cirq.CZPowGate):
                 qubits = op.qubits
 
-                dist_q0 = manhattan_distance(center_qubit, cast(cirq.GridQubit, qubits[0]))
-                dist_q1 = manhattan_distance(center_qubit, cast(cirq.GridQubit, qubits[1]))
+                dist_q0 = manhattan_distance(CENTER_QUBIT, cast(cirq.GridQubit, qubits[0]))
+                dist_q1 = manhattan_distance(CENTER_QUBIT, cast(cirq.GridQubit, qubits[1]))
 
                 child_qubit_distance = max(dist_q0, dist_q1)
 
@@ -136,23 +136,21 @@ def test_ghz_circuits_bfs_order(num_qubits: int) -> None:
     included_qubits = circuit.all_qubits()
     if included_qubits:
         max_dist_required = max(
-            manhattan_distance(center_qubit, cast(cirq.GridQubit, q)) for q in included_qubits
+            manhattan_distance(CENTER_QUBIT, cast(cirq.GridQubit, q)) for q in included_qubits
         )
         assert max_dist_seen == max_dist_required
 
 
-def test_ghz_invalid_inputs():
+def test_ghz_invalid_inputs() -> None:
     """Tests that the function raises errors for invalid inputs."""
 
     with pytest.raises(ValueError, match="num_qubits must be a positive integer."):
-        ghz_2d.generate_2d_ghz_circuit(center_qubit, graph, num_qubits=0)  # invalid
+        ghz_2d.generate_2d_ghz_circuit(CENTER_QUBIT, GRAPH, num_qubits=0)
 
     with pytest.raises(
         ValueError, match="num_qubits cannot exceed the total number of qubits on the processor."
     ):
-        ghz_2d.generate_2d_ghz_circuit(
-            center_qubit, graph, num_qubits=len(graph.nodes) + 1  # invalid
-        )
+        ghz_2d.generate_2d_ghz_circuit(CENTER_QUBIT, GRAPH, num_qubits=len(GRAPH.nodes) + 1)
 
 
 def test_dynamical_decoupling_is_applied() -> None:
