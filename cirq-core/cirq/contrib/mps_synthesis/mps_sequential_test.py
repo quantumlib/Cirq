c6f6fa5eb149c28a007be70bffdeeba4b49063f4
Pavol Juhas <juhas@google.com>
2026-01-21 17:37:10 -0800

    Small tweaks for test readability
    No change in the effective code.

--- cirq-core/cirq/contrib/mps_synthesis/mps_sequential_test.py
+++ cirq-core/cirq/contrib/mps_synthesis/mps_sequential_test.py
@@ -28,11 +28,11 @@ def test_compile_single_qubit_state() -> None:
     np.testing.assert_allclose(cirq.final_state_vector(circuit), state, atol=1e-6)
 
 
-@pytest.mark.parametrize("N", [5, 8, 10, 11])
-def test_compile_area_law_states(N: int) -> None:
+@pytest.mark.parametrize("num_qubits", [5, 8, 10, 11])
+def test_compile_area_law_states(num_qubits: int) -> None:
     # Given `random_superposition` can generate volume-law entangled states,
     # we manually construct an exclusively area-law entangled state here
-    state = np.random.rand(2**N) + 1j * np.random.rand(2**N)
+    state = np.random.rand(2**num_qubits) + 1j * np.random.rand(2**num_qubits)
     state /= np.linalg.norm(state)
 
     circuit: cirq.Circuit = mps_circuit_from_statevector(state, max_num_layers=6)
@@ -50,7 +50,7 @@ def test_compile_trivial_state_with_mps_pass() -> None:
     trivial_circuit.append(cirq.ry(np.pi / 2).on(qubits[9]))
     trivial_circuit.append(cirq.rx(np.pi).on(qubits[9]))
     trivial_circuit.append(cirq.rz(np.pi / 4).on(qubits[9]))
-    for i in range(8, -1, -1):
+    for i in reversed(range(8)):
         trivial_circuit.append(cirq.CNOT(qubits[i + 1], qubits[i]))
         trivial_circuit.append(cirq.rz(-np.pi / (2 ** (9 - i))).on(qubits[i]))
         trivial_circuit.append(cirq.CNOT(qubits[i + 1], qubits[i]))
@@ -58,11 +58,11 @@ def test_compile_trivial_state_with_mps_pass() -> None:
         trivial_circuit.append(cirq.ry(np.pi / 2).on(qubits[i]))
         trivial_circuit.append(cirq.rx(np.pi).on(qubits[i]))
         trivial_circuit.append(cirq.rz(np.pi / 4).on(qubits[i]))
-    for i in range(8, -1, -1):
+    for i in reversed(range(8)):
         trivial_circuit.append(cirq.rz(np.pi / (2 ** (9 - i))).on(qubits[i]))
-    for i in range(8, -1, -1):
+    for i in reversed(range(8)):
         trivial_circuit.append(cirq.CNOT(qubits[i + 1], qubits[i]))
-    for i in range(8, -1, -1):
+    for i in reversed(range(8)):
         trivial_circuit.append(cirq.CNOT(qubits[i], qubits[i + 1]))
 
     state = cirq.final_state_vector(trivial_circuit)
