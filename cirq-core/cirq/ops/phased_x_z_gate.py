257c9dcc858ede2d75fede8ab173b0a5ecd12298
Pavol Juhas <juhas@google.com>
2026-01-23 12:14:56 -0800

    Fix computation of normalized PhasedXZGate parameters
    Add helper with optimized expression from `PhasedXZGate._canonical`.

--- cirq-core/cirq/ops/phased_x_z_gate.py
+++ cirq-core/cirq/ops/phased_x_z_gate.py
@@ -336,9 +336,29 @@ class PhasedXZGate(raw_types.Gate):
             abs((x := round(self._x_exponent, 2)) - self._x_exponent) <= tol
             and abs((z := round(self._z_exponent, 2)) - self._z_exponent) <= tol
             and abs((a := round(self._axis_phase_exponent, 2)) - self._axis_phase_exponent) <= tol
-            and (x % 2, z % 2, a % 2) in _clifford_as_phasedzx_params()
+            and _canonical_xza_mod_2(x, z, a) in _clifford_as_phasedzx_params()
         )
         return result
+
+
+def _canonical_xza_mod_2(
+    x_exponent: float, z_exponent: float, axis_phase_exponent: float
+) -> tuple[float, float, float]:
+    """Return canonical values of PhasedXZGate parameters modulo 2.
+
+    Optimized helper for `PhasedXZGate._has_stabilizer_effect_`.
+    """
+    # The result must be consistent with PhasedXZGate._canonical
+    x = x_exponent % 2
+    a = 0.0 if x == 0 else axis_phase_exponent % 2
+    z = z_exponent % 2
+    if x == 1 and z != 0:
+        a = (a + z / 2) % 2
+        z = 0
+    if 0.5 < a <= 1.5:
+        a = (a - 1) % 2
+        x = 2 - x if x else x
+    return (x, z, a)
 
 
 @functools.cache
