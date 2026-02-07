1ca3e19081a46f59f1f7784e30774459dd8e529a
Joe Schulte <joesho112358@gmail.com>
2026-02-06 21:36:38 -0500

    fixing grid qubit issues

--- cirq-core/cirq/devices/grid_qubit_test.py
+++ cirq-core/cirq/devices/grid_qubit_test.py
@@ -239,14 +239,14 @@ BA"""
 
 def test_addition_subtraction() -> None:
     # GridQubits
-    assert (*cirq.GridQubit(1, 2), 2, 5) == cirq.GridQubit(3, 7)
-    assert (*cirq.GridQubit(1, 2), 0, 0) == cirq.GridQubit(1, 2)
-    assert (*cirq.GridQubit(1, 2), -1, 0) == cirq.GridQubit(0, 2)
+    assert cirq.GridQubit(1, 2) + (2, 5) == cirq.GridQubit(3, 7)  # noqa
+    assert cirq.GridQubit(1, 2) + (0, 0) == cirq.GridQubit(1, 2)  # noqa
+    assert cirq.GridQubit(1, 2) + (-1, 0) == cirq.GridQubit(0, 2)  # noqa
     assert cirq.GridQubit(1, 2) - (2, 5) == cirq.GridQubit(-1, -3)
     assert cirq.GridQubit(1, 2) - (0, 0) == cirq.GridQubit(1, 2)
     assert cirq.GridQubit(1, 2) - (-1, 0) == cirq.GridQubit(2, 2)
 
-    assert (2, 5, *cirq.GridQubit(1, 2)) == cirq.GridQubit(3, 7)
+    assert (2, 5) + cirq.GridQubit(1, 2) == cirq.GridQubit(3, 7)  # noqa
     assert (2, 5) - cirq.GridQubit(1, 2) == cirq.GridQubit(1, 3)
 
     assert cirq.GridQubit(1, 2) + cirq.GridQubit(3, 5) == cirq.GridQubit(4, 7)
@@ -254,17 +254,17 @@ def test_addition_subtraction() -> None:
     assert cirq.GridQubit(1, -2) + cirq.GridQubit(3, 5) == cirq.GridQubit(4, 3)
 
     # GridQids
-    assert (*cirq.GridQid(1, 2, dimension=3), 2, 5) == cirq.GridQid(3, 7, dimension=3)
-    assert (*cirq.GridQid(1, 2, dimension=3), 0, 0) == cirq.GridQid(1, 2, dimension=3)
-    assert (*cirq.GridQid(1, 2, dimension=3), -1, 0) == cirq.GridQid(0, 2, dimension=3)
+    assert cirq.GridQid(1, 2, dimension=3) + (2, 5) == cirq.GridQid(3, 7, dimension=3)  # noqa
+    assert cirq.GridQid(1, 2, dimension=3) + (0, 0) == cirq.GridQid(1, 2, dimension=3)  # noqa
+    assert cirq.GridQid(1, 2, dimension=3) + (-1, 0) == cirq.GridQid(0, 2, dimension=3)  # noqa
     assert cirq.GridQid(1, 2, dimension=3) - (2, 5) == cirq.GridQid(-1, -3, dimension=3)
     assert cirq.GridQid(1, 2, dimension=3) - (0, 0) == cirq.GridQid(1, 2, dimension=3)
     assert cirq.GridQid(1, 2, dimension=3) - (-1, 0) == cirq.GridQid(2, 2, dimension=3)
 
-    assert (2, 5, *cirq.GridQid(1, 2, dimension=3)) == cirq.GridQid(3, 7, dimension=3)
+    assert (2, 5) + cirq.GridQid(1, 2, dimension=3) == cirq.GridQid(3, 7, dimension=3)  # noqa
     assert (2, 5) - cirq.GridQid(1, 2, dimension=3) == cirq.GridQid(1, 3, dimension=3)
 
-    assert (*cirq.GridQid(1, 2, dimension=3), *cirq.GridQid(3, 5, dimension=3)) == cirq.GridQid(
+    assert cirq.GridQid(1, 2, dimension=3) + cirq.GridQid(3, 5, dimension=3) == cirq.GridQid(
         4, 7, dimension=3
     )
     assert cirq.GridQid(3, 5, dimension=3) - cirq.GridQid(2, 1, dimension=3) == cirq.GridQid(
@@ -310,11 +310,11 @@ def test_unsupported_add() -> None:
     with pytest.raises(TypeError, match='1'):
         _ = cirq.GridQubit(1, 1) + 1  # type: ignore[operator]
     with pytest.raises(TypeError, match='(1,)'):
-        _ = (*cirq.GridQubit(1, 1), 1)
+        _ = cirq.GridQubit(1, 1) + (1,)  # noqa
     with pytest.raises(TypeError, match='(1, 2, 3)'):
-        _ = (*cirq.GridQubit(1, 1), 1, 2, 3)
+        _ = cirq.GridQubit(1, 1) + (1, 2, 3)  # noqa
     with pytest.raises(TypeError, match='(1, 2.0)'):
-        _ = (*cirq.GridQubit(1, 1), 1, 2.0)
+        _ = cirq.GridQubit(1, 1) + (1, 2.0)  # noqa
 
     with pytest.raises(TypeError, match='1'):
         _ = cirq.GridQubit(1, 1) - 1  # type: ignore[operator]
