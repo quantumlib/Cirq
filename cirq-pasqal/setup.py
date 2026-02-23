912a76e6a6eada2361f898b523947b42577cfb1f
Joe Schulte <joesho112358@gmail.com>
2026-02-23 02:58:42 -0500

    Ruff furb implementation (#7907)
    Adding checks for #7505 :
    
    [FURB101 -
    read-whole-file](https://docs.astral.sh/ruff/rules/read-whole-file/)
    [FURB103 -
    write-whole-file](https://docs.astral.sh/ruff/rules/write-whole-file/)
    [FURB105 -
    print-empty-string](https://docs.astral.sh/ruff/rules/print-empty-string/)
    [FURB116 -
    f-string-number-format](https://docs.astral.sh/ruff/rules/f-string-number-format/)
    [FURB132 -
    check-and-remove-from-set](https://docs.astral.sh/ruff/rules/check-and-remove-from-set/)
    [FURB142 -
    for-loop-set-mutations](https://docs.astral.sh/ruff/rules/for-loop-set-mutations/)
    [FURB171 -
    single-item-membership-test](https://docs.astral.sh/ruff/rules/single-item-membership-test/)
    
    ---------
    
    Co-authored-by: Pavol Juhas <juhas@google.com>

--- cirq-pasqal/setup.py
+++ cirq-pasqal/setup.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pathlib
 import runpy
 
 from setuptools import find_packages, setup
@@ -25,8 +26,7 @@ name = 'cirq-pasqal'
 description = 'A Cirq package to simulate and connect to Pasqal quantum computers'
 
 # README file as long_description.
-with open('README.md', encoding='utf-8') as file:
-    long_description = file.read()
+long_description = pathlib.Path('README.md').read_text(encoding='utf-8')
 
 # Read in requirements
 with open('requirements.txt', encoding='utf-8') as file:
