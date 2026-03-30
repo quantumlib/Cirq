9e21fdb1f03dfab8574c5ceaad4aac512134be6d
Michael Hucka <mhucka@google.com>
2026-03-29 20:53:50 -0700

    Undo this change

--- cirq-core/cirq/contrib/quimb/state_vector.py
+++ cirq-core/cirq/contrib/quimb/state_vector.py
@@ -23,7 +23,7 @@ def _get_quimb_version():
     version = quimb.__version__
     try:
         return tuple(int(x) for x in version.split('.')), version
-    except Exception:
+    except:  # pragma: no cover
         return (0, 0), version
 
 
