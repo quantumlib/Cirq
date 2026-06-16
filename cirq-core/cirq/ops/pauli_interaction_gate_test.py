952c425de7c9fd6a2676a249ea445bea6c636987
Pavol Juhas <juhas@google.com>
2026-06-16 14:07:31 -0700

    pytest - parametrize tests using collection containers only
    Replace deprecated non-collection iterables in `@pytest.mark.parametrize`.
    Address deprecation warning `PytestRemovedIn10Warning`.
    
    Ref: https://docs.pytest.org/en/stable/deprecations.html#parametrize-iterators

--- cirq-core/cirq/ops/pauli_interaction_gate_test.py
+++ cirq-core/cirq/ops/pauli_interaction_gate_test.py
@@ -26,11 +26,13 @@ _bools = (False, True)
 _paulis = (cirq.X, cirq.Y, cirq.Z)
 
 
-def _all_interaction_gates(exponents=(1,)):
-    for pauli0, invert0, pauli1, invert1, e in itertools.product(
-        _paulis, _bools, _paulis, _bools, exponents
-    ):
-        yield cirq.PauliInteractionGate(pauli0, invert0, pauli1, invert1, exponent=e)
+def _all_interaction_gates(exponents=(1,)) -> list[cirq.PauliInteractionGate]:
+    return [
+        cirq.PauliInteractionGate(pauli0, invert0, pauli1, invert1, exponent=e)
+        for pauli0, invert0, pauli1, invert1, e in itertools.product(
+            _paulis, _bools, _paulis, _bools, exponents
+        )
+    ]
 
 
 @pytest.mark.parametrize('gate', _all_interaction_gates())
