4d2189e50c5c94781b93f88f7127e73026c15b86
arettig <arettig@google.com>
2026-07-29 00:36:47 +0000

    Move merge_gate to new benchmark
    Removed the merge_single_qubit_gates_to_phxz benchmarks from the
    dynamic decoupling benchmark and put them in a new file.

--- benchmarks/merge_single_qubit_gates_perf.py
+++ benchmarks/merge_single_qubit_gates_perf.py
@@ -1,4 +1,4 @@
-# Copyright 2025 The Cirq Developers
+# Copyright 2026 The Cirq Developers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -50,40 +50,21 @@ def make_fake_trotter_circuit(num_cycles: int):
     for layer, qubits in zip(LAYERS_GRID_QUBIT_PAIRS, QUBITS_PER_LAYER):
         moments.append(cirq.Moment((cirq.Y**0.3)(qubit) for qubit in qubits))
         moments.append(cirq.Moment(cirq.CZ(*pair) for pair in layer))
-    return cirq.Circuit.from_moments(*moments) * num_cycles + cirq.Moment(cirq.M(*all_qubits))
+    trotter_circuit = cirq.Circuit.from_moments(*moments) * num_cycles + cirq.Moment(
+        cirq.M(*all_qubits)
+    )
+    return cirq.transformers.gauge_compiling.cz_gauge.CZGaugeTransformer(trotter_circuit)
 
 
-@pytest.mark.parametrize(
-    ["num_cycles", "num_circuits"], [(5, 10), pytest.param(50, 100, marks=pytest.mark.slow)]
-)
-@pytest.mark.benchmark(group="dynamical_decoupling", max_time=10)
-def test_dynamical_decoupling_sweep(benchmark, num_cycles: int, num_circuits: int) -> None:
-    def _f(num_cycles: int, num_circuits: int) -> cirq.Circuit:
-        circuit = make_fake_trotter_circuit(num_cycles)
-        circuit1, sweep1 = cirq.transformers.gauge_compiling.CZGaugeTransformer.as_sweep(
-            circuit, N=num_circuits
-        )
-        circuit2, sweep2 = cirq.merge_single_qubit_gates_to_phxz_symbolized(circuit1, sweep=sweep1)
-        assert len(sweep2)
-        circuit3 = cirq.add_dynamical_decoupling(circuit2)
-        return circuit3
-
-    _ = benchmark(_f, num_cycles=num_cycles, num_circuits=num_circuits)
+@pytest.mark.parametrize(["num_cycles"], [(500,)])
+@pytest.mark.benchmark(group="merge_single_qubit_gates", max_time=10)
+def test_merge_single_qubit_gates_to_phxz(benchmark, num_cycles: int) -> None:
+    circuit = make_fake_trotter_circuit(num_cycles)
+    benchmark(cirq.merge_single_qubit_gates_to_phxz, circuit)
 
 
-@pytest.mark.parametrize(
-    ["num_cycles", "num_circuits"], [(5, 10), pytest.param(50, 100, marks=pytest.mark.slow)]
-)
-@pytest.mark.benchmark(group="dynamical_decoupling", max_time=10)
-def test_dynamical_decoupling_batch(benchmark, num_cycles: int, num_circuits: int) -> None:
-    def _f(num_cycles: int, num_circuits: int) -> list[cirq.Circuit]:
-        circuit = make_fake_trotter_circuit(num_cycles)
-        circuits_batch1 = [
-            cirq.transformers.gauge_compiling.cz_gauge.CZGaugeTransformer(circuit)
-            for _ in range(num_circuits)
-        ]
-        circuits_batch2 = [cirq.merge_single_qubit_moments_to_phxz(c) for c in circuits_batch1]
-        circuits_batch3 = [cirq.add_dynamical_decoupling(c) for c in circuits_batch2]
-        return circuits_batch3
-
-    _ = benchmark(_f, num_cycles=num_cycles, num_circuits=num_circuits)
+@pytest.mark.parametrize(["num_cycles"], [(1000,)])
+@pytest.mark.benchmark(group="merge_single_qubit_gates", max_time=10)
+def test_merge_single_qubit_moments_to_phxz_batch(benchmark, num_cycles: int) -> None:
+    circuit = make_fake_trotter_circuit(num_cycles)
+    benchmark(cirq.merge_single_qubit_moments_to_phxz, circuit)
