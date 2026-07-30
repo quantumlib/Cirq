94c6414e8f0df1106a193e8ec9fa0ad2d342b0b3
Doug Strain <dstrain@google.com>
2026-07-30 11:57:00 -0700

    Allow Unrecognized gates to pass through creation of a GridDevice (#8223)
    - Previously, the creation of a GridDevice would serialize to a proto
    and back (what?) in order to add missing gate representations to the
    gateset (like if you had PhasedXPowGate but not XPowGate).
    - This changes the code to directly use GateRepresentations to add these
    missing values.
    - This allows unrecognized gates that don't exist in the list of public
    gate representations to be ignored and kept in the Gateset.

--- cirq-google/cirq_google/devices/grid_device_test.py
+++ cirq-google/cirq_google/devices/grid_device_test.py
@@ -706,12 +706,6 @@ def test_device_from_device_information_equals_device_from_proto():
             None,
         ),
         (
-            'Unrecognized gate',
-            [(cirq.GridQubit(0, 0), cirq.GridQubit(0, 1))],
-            cirq.Gateset(cirq.H),
-            None,
-        ),
-        (
             'Some gate_durations keys are not found in gateset',
             [(cirq.GridQubit(0, 0), cirq.GridQubit(0, 1))],
             cirq.Gateset(cirq.CZ),
@@ -733,6 +727,18 @@ def test_from_device_information_invalid_input(error_match, qubit_pairs, gateset
         grid_device.GridDevice._from_device_information(
             qubit_pairs=qubit_pairs, gateset=gateset, gate_durations=gate_durations
         )
+
+
+def test_from_device_information_unrecognized_gate():
+    gateset = cirq.Gateset(cirq.H)
+    gate_durations = {cirq.GateFamily(cirq.H): cirq.Duration(picos=1_000)}
+    device = grid_device.GridDevice._from_device_information(
+        qubit_pairs=[(cirq.GridQubit(0, 0), cirq.GridQubit(0, 1))],
+        gateset=gateset,
+        gate_durations=gate_durations,
+    )
+    assert cirq.GateFamily(cirq.H) in device.metadata.gateset.gates
+    assert device.metadata.gate_durations == {cirq.GateFamily(cirq.H): cirq.Duration(picos=1_000)}
 
 
 def test_from_device_information_fsim_gate_family():
@@ -860,3 +866,17 @@ def test_qubit_attributes_unlisted_qubit():
 
     with pytest.raises(ValueError, match="is not in valid_qubits"):
         _ = cirq_google.GridDevice.from_proto(spec)
+
+
+def test_to_proto_invalid_gate_durations():
+    metadata = cirq.GridDeviceMetadata(
+        qubit_pairs=[(cirq.GridQubit(0, 0), cirq.GridQubit(0, 1))],
+        gateset=cirq.Gateset(cirq.PhasedXZGate, cirq.XPowGate),
+        gate_durations={
+            cirq.GateFamily(cirq.PhasedXZGate): cirq.Duration(picos=1_000),
+            cirq.GateFamily(cirq.XPowGate): cirq.Duration(picos=2_000),
+        },
+    )
+    device = cirq_google.GridDevice(metadata)
+    with pytest.raises(ValueError, match="Multiple gate families"):
+        device.to_proto()
