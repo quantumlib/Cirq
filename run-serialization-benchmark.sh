dc06e734ce6941cd456f5e62351f3dc6fd3903e0
Pavol Juhas <juhas@google.com>
2026-08-06 15:04:03 -0700

    Clean up temporary benchmark scripts and data

--- run-serialization-benchmark.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pytest -p no:randomly \
-    --benchmark-enable \
-    --benchmark-min-rounds=9 \
-    --benchmark-autosave \
-    "benchmarks/serialization_perf.py::test_json_serialization[100-1000]"
