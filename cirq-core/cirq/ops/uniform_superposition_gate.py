1e58ddf7400269652883f4f5bb0d73993b06fc2a
Tina Kim <tinakim522@outlook.com>
2026-08-06 06:46:54 +0200

    Put display math on separate lines in docstring.

--- cirq-core/cirq/ops/uniform_superposition_gate.py
+++ cirq-core/cirq/ops/uniform_superposition_gate.py
@@ -33,7 +33,9 @@ class UniformSuperpositionGate(raw_types.Gate):
     r"""Creates a uniform superposition state on the states $[0, M)$.
 
     The gate creates the state
-    $$\frac{1}{\sqrt{M}}\sum_{j=0}^{M-1}|j\rangle$$
+    $$
+    \frac{1}{\sqrt{M}}\sum_{j=0}^{M-1}|j\rangle
+    $$
     (where $1 \leq M \leq 2^n$), using n qubits, according to the
     Shukla-Vedula algorithm [SV24].
 
