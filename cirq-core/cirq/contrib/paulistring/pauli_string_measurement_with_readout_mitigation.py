4f1354e0a60297da5dd01d7a44e26fc4652fd260
ddddddanni <danniwang@google.com>
2026-08-20 18:51:31 -0700

    Fix lint

--- cirq-core/cirq/contrib/paulistring/pauli_string_measurement_with_readout_mitigation.py
+++ cirq-core/cirq/contrib/paulistring/pauli_string_measurement_with_readout_mitigation.py
@@ -1207,7 +1207,7 @@ def generate_trex_and_readout_circuits(
         readout_circuits = [
             circuits.Circuit.from_moments(
                 # Note: An empty moment may be inserted here if readout_choices_i contains
-                # all false values. This is intentional to ensure the depth and measurement 
+                # all false values. This is intentional to ensure the depth and measurement
                 # timing of all readout circuits remain consistent and uniform.
                 circuits.Moment(
                     ops.X.on_each(
