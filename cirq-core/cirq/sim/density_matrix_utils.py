ce46b5d5921b4b7b5d1eccff70a8c4248664e576
mhucka <mhucka@google.com>
2026-09-08 00:47:49 +0000

    Run formatter

--- cirq-core/cirq/sim/density_matrix_utils.py
+++ cirq-core/cirq/sim/density_matrix_utils.py
@@ -16,8 +16,8 @@
 
 from __future__ import annotations
 
-from collections.abc import Sequence
 import math
+from collections.abc import Sequence
 from typing import TYPE_CHECKING
 
 import numpy as np
