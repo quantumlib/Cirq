e82c40b93ce253ac348f80265556f4280e9c641d
Eliott Rosenberg <eliottrosenbrg@google.com>
2026-09-16 20:05:47 +0000

    fix one more default run name

--- cirq-google/cirq_google/engine/engine_processor_test.py
+++ cirq-google/cirq_google/engine/engine_processor_test.py
@@ -1148,7 +1148,7 @@ def test_get_config_from_snapshot(client):
 def test_get_config_not_found(client):
     project_id = "test_project_id"
     processor_id = "test_proc_id"
-    default_run = Run(id="current")
+    default_run = Run(id="default")
     config_name = "default"
 
     client().get_quantum_processor_config_async.return_value = None
